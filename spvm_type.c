3450776c31507bbf060c55112c1ffaca4b624486
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-06-19 17:58:37 +0900

    copy resolved type function to type

--- spvm_type.c
+++ spvm_type.c
@@ -109,3 +109,93 @@ void SPVM_TYPE_build_parts(SPVM_COMPILER* compiler, SPVM_TYPE* type, SPVM_ARRAY*
   }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include "spvm_compiler_allocator.h"
+#include "spvm_package.h"
+#include "spvm_compiler.h"
+
+const char* const SPVM_TYPE_C_CORE_NAMES[] = {
+  "byte",
+  "short",
+  "int",
+  "long",
+  "float",
+  "double",
+  "byte[]",
+  "short[]",
+  "int[]",
+  "long[]",
+  "float[]",
+  "double[]",
+};
+
+_Bool SPVM_TYPE_is_array(SPVM_COMPILER* compiler, SPVM_TYPE* resolved_type) {
+  (void)compiler;
+  
+  int32_t length = (int32_t)strlen(resolved_type->name);
+  
+  if (strlen(resolved_type->name) >= 2) {
+    char char1 = resolved_type->name[length - 2];
+    char char2 = resolved_type->name[length - 1];
+    
+    if (char1 == '[' && char2 == ']') {
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  }
+  else {
+    return 0;
+  }
+}
+
+_Bool SPVM_TYPE_is_array_numeric(SPVM_COMPILER* compiler, SPVM_TYPE* resolved_type) {
+  (void)compiler;
+  
+  const char* name = resolved_type->name;
+  
+  if (strcmp(name, "char[]") == 0 || strcmp(name, "byte[]") == 0 || strcmp(name, "short[]") == 0
+    || strcmp(name, "int[]") == 0 || strcmp(name, "long[]") == 0 || strcmp(name, "float[]") == 0 || strcmp(name, "double[]") == 0)
+  {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+_Bool SPVM_TYPE_is_integral(SPVM_COMPILER* compiler, SPVM_TYPE* resolved_type) {
+  (void)compiler;
+  
+  if (resolved_type->id <= SPVM_TYPE_C_ID_LONG) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+_Bool SPVM_TYPE_is_numeric(SPVM_COMPILER* compiler, SPVM_TYPE* resolved_type) {
+  (void)compiler;
+  
+  if (resolved_type && resolved_type->id <= SPVM_TYPE_C_ID_DOUBLE) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
--- spvm_type.h
+++ spvm_type.h
@@ -29,4 +29,50 @@ SPVM_TYPE* SPVM_TYPE_new(SPVM_COMPILER* compiler);
 void SPVM_TYPE_build_parts(SPVM_COMPILER* compiler, SPVM_TYPE* type, SPVM_ARRAY* parts);
 _Bool SPVM_TYPE_resolve_type(SPVM_COMPILER* compiler, SPVM_OP* op_type, int32_t name_length);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+enum {
+  SPVM_TYPE_C_CORE_LENGTH = 12
+};
+
+enum {
+  SPVM_TYPE_C_ID_BYTE,
+  SPVM_TYPE_C_ID_SHORT,
+  SPVM_TYPE_C_ID_INT,
+  SPVM_TYPE_C_ID_LONG,
+  SPVM_TYPE_C_ID_FLOAT,
+  SPVM_TYPE_C_ID_DOUBLE,
+  SPVM_TYPE_C_ID_ARRAY_BYTE,
+  SPVM_TYPE_C_ID_ARRAY_SHORT,
+  SPVM_TYPE_C_ID_ARRAY_INT,
+  SPVM_TYPE_C_ID_ARRAY_LONG,
+  SPVM_TYPE_C_ID_ARRAY_FLOAT,
+  SPVM_TYPE_C_ID_ARRAY_DOUBLE,
+};
+
+extern const char* const SPVM_TYPE_C_CORE_NAMES[];
+
+_Bool SPVM_TYPE_is_integral(SPVM_COMPILER* compiler, SPVM_TYPE* resolved_type);
+_Bool SPVM_TYPE_is_numeric(SPVM_COMPILER* compiler, SPVM_TYPE* resolved_type);
+_Bool SPVM_TYPE_is_array(SPVM_COMPILER* compiler, SPVM_TYPE* resolved_type);
+_Bool SPVM_TYPE_is_array_numeric(SPVM_COMPILER* compiler, SPVM_TYPE* resolved_type);
+
+
 #endif
