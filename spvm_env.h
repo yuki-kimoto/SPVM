facb759c0e1f630f79e7de83f2b84848b17d3a45
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-06-27 20:47:15 +0900

    cleanup

--- spvm_env.h
+++ spvm_env.h
@@ -1,6 +1,10 @@
 #ifndef SPVM_ENV_H
 #define SPVM_ENV_H
 
+#include "spvm_base.h"
+
+SPVM_DATA** SPVM_API_get_array_values_ref(SPVM_ENV* env, SPVM_DATA_ARRAY* data_array);
+
 // SPVM_ENV
 struct SPVM_env {
   int32_t* constant_pool;
