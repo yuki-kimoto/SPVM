c08ee7f120c083d7e78956dd10a1b4aaccef5a12
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-07-07 16:33:18 +0900

    add object_args_length, object_arg_indexes_constant_pool_index, object_my_vars_length, object_my_var_indexes_constant_pool_index

--- spvm_constant_pool_sub.h
+++ spvm_constant_pool_sub.h
@@ -11,7 +11,13 @@ struct SPVM_constant_pool_sub {
   int32_t operand_stack_max;
   int32_t my_vars_length;
   int32_t args_length;
+  int32_t object_args_length;
+  int32_t object_arg_indexes_constant_pool_index;
+  int32_t object_my_vars_length;
+  int32_t object_my_var_indexes_constant_pool_index;
   int32_t return_type_id;
+  _Bool has_object_arg;
+  _Bool has_object_my_var;
   _Bool is_native;
 };
 
