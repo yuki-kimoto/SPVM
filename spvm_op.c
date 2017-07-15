e0e0bbc9e43b61db18737f8d74105a113ec3695e
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-07-15 12:05:38 +0900

    cleanup if

--- spvm_op.c
+++ spvm_op.c
@@ -348,6 +348,7 @@ SPVM_OP* SPVM_OP_build_if_statement(SPVM_COMPILER* compiler, SPVM_OP* op_if, SPV
     SPVM_OP_insert_child(compiler, op_list, op_list->last, op_not_block);
     SPVM_OP_insert_child(compiler, op_block_false, op_block_false->last, op_list);
   }
+  op_block_false->flag |= SPVM_OP_C_FLAG_BLOCK_IF_FALSE;
   
   // Create true block if needed
   if (op_block_true->code != SPVM_OP_C_CODE_BLOCK) {
@@ -359,17 +360,12 @@ SPVM_OP* SPVM_OP_build_if_statement(SPVM_COMPILER* compiler, SPVM_OP* op_if, SPV
     SPVM_OP_insert_child(compiler, op_list, op_list->last, op_not_block);
     SPVM_OP_insert_child(compiler, op_block_true, op_block_true->last, op_list);
   }
-  
   op_block_true->flag |= SPVM_OP_C_FLAG_BLOCK_IF_TURE;
-  op_block_false->flag |= SPVM_OP_C_FLAG_BLOCK_IF_FALSE;
+  op_block_true->flag |= SPVM_OP_C_FLAG_BLOCK_HAS_ELSE;
   
   SPVM_OP_insert_child(compiler, op_if, op_if->last, op_condition);
   SPVM_OP_insert_child(compiler, op_if, op_if->last, op_block_true);
   SPVM_OP_insert_child(compiler, op_if, op_if->last, op_block_false);
-  
-  if (op_block_false->code == SPVM_OP_C_CODE_BLOCK) {
-    op_block_true->flag |= SPVM_OP_C_FLAG_BLOCK_HAS_ELSE;
-  }
   
   return op_if;
 }
