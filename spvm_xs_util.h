1b9c1fa291cd8ffa4a8188a2c4ed9e15fa3d48f9
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-07-17 13:14:16 +0900

    add SPVM_XS_UTIL_get_field_id

--- spvm_xs_util.h
+++ spvm_xs_util.h
@@ -40,11 +40,21 @@ const char* SPVM_XS_UTIL_get_field_type(const char* package_name, const char* fi
     SV* sv_package_info = *sv_package_info_ptr;
     HV* hv_package_info = (HV*)SvRV(sv_package_info);
     
-    SV** sv_field_name_ptr = hv_fetch(hv_package_info, field_name, strlen(field_name), 0);
-    if (sv_field_name_ptr) {
-      SV* sv_field_name = *sv_field_name_ptr;
+    SV** sv_field_info_ptr = hv_fetch(hv_package_info, field_name, strlen(field_name), 0);
+    if (sv_field_info_ptr) {
+      SV* sv_field_info = *sv_field_info_ptr;
+      HV* hv_field_info = (HV*)SvRV(sv_field_info);
       
-      return SvPV_nolen(sv_field_name);
+      SV** sv_field_type_ptr = hv_fetch(hv_package_info, "type", strlen("type"), 0);
+      if (sv_field_type_ptr) {
+        SV* sv_field_type = *sv_field_type_ptr;
+        const char* field_type = SvPV_nolen(sv_field_type);
+        
+        return field_type;
+      }
+      else {
+        return NULL;
+      }
     }
     else {
       return NULL;
@@ -53,7 +63,40 @@ const char* SPVM_XS_UTIL_get_field_type(const char* package_name, const char* fi
   else {
     return NULL;
   }
+}
 
+const char* SPVM_XS_UTIL_get_field_id(const char* package_name, const char* field_name) {
+  // Field symbol table
+  HV* hv_field_symtable = get_hv("SPVM::FIELD_SYMTABLE", 0);
+  
+  SV** sv_package_info_ptr = hv_fetch(hv_field_symtable, package_name, strlen(package_name), 0);
+  if (sv_package_info_ptr) {
+    SV* sv_package_info = *sv_package_info_ptr;
+    HV* hv_package_info = (HV*)SvRV(sv_package_info);
+    
+    SV** sv_field_info_ptr = hv_fetch(hv_package_info, field_name, strlen(field_name), 0);
+    if (sv_field_info_ptr) {
+      SV* sv_field_info = *sv_field_info_ptr;
+      HV* hv_field_info = (HV*)SvRV(sv_field_info);
+      
+      SV** sv_field_id_ptr = hv_fetch(hv_package_info, "id", strlen("id"), 0);
+      if (sv_field_id_ptr) {
+        SV* sv_field_id = *sv_field_id_ptr;
+        const char* field_id = SvPV_nolen(sv_field_id);
+        
+        return field_id;
+      }
+      else {
+        return NULL;
+      }
+    }
+    else {
+      return NULL;
+    }
+  }
+  else {
+    return NULL;
+  }
 }
 
 SV* SPVM_XS_UTIL_new_sv_array(const char* type, SPVM_API_ARRAY* array) {
