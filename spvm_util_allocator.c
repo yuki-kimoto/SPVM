e8d67f59403694d1a3d1016bdc0006467b736538
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-07-18 13:08:58 +0900

    cleanup

--- spvm_util_allocator.c
+++ spvm_util_allocator.c
@@ -30,20 +30,3 @@ void* SPVM_UTIL_ALLOCATOR_safe_malloc_i32_zero(int32_t count, int32_t size) {
   
   return block;
 }
-
-void* SPVM_UTIL_ALLOCATOR_safe_realloc_i32(void* ptr, int32_t count, int32_t size) {
-
-  assert(count >= 0);
-  assert(size >= 0);
-  assert(count <= INT32_MAX / size);
-
-  int32_t block_size = count * size;
-  void* block = realloc(ptr, block_size);
-  
-  if (!block) {
-    fprintf(stderr, "Failed to reallocate memory. Sperl will abort.\n");
-    abort();
-  }
-
-  return block;
-}
--- spvm_util_allocator.h
+++ spvm_util_allocator.h
@@ -3,7 +3,5 @@
 
 void* SPVM_UTIL_ALLOCATOR_safe_malloc_i32(int32_t count, int32_t size);
 void* SPVM_UTIL_ALLOCATOR_safe_malloc_i32_zero(int32_t count, int32_t size);
-void* SPVM_UTIL_ALLOCATOR_safe_realloc_i32(void* ptr, int32_t count, int32_t size);
-void* SPVM_UTIL_ALLOCATOR_safe_realloc_i32_zero(void* ptr, int32_t count, int32_t size);
 
 #endif
