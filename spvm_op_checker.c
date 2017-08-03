a40c43df6656f4542c55267b374760af96182b14
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-08-03 22:24:19 +0900

    check case count

--- spvm_op_checker.c
+++ spvm_op_checker.c
@@ -488,6 +488,11 @@ void SPVM_OP_CHECKER_check(SPVM_COMPILER* compiler) {
                       if (op_switch_stack->length > 0) {
                         SPVM_OP* op_switch = SPVM_DYNAMIC_ARRAY_fetch(op_switch_stack, op_switch_stack->length - 1);
                         SPVM_SWITCH_INFO* switch_info = op_switch->uv.switch_info;
+                        if (switch_info->op_cases->length == SPVM_LIMIT_C_CASES) {
+                          SPVM_yyerror_format(compiler, "Too many case statements at %s line %d\n", op_cur->file, op_cur->line);
+                          return NULL;
+                        }
+
                         SPVM_DYNAMIC_ARRAY_push(switch_info->op_cases, op_cur);
                       }
                       break;
