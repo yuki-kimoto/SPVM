07176b10da10c7069b98519b12d9725bce2b1d48
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-08-17 22:53:50 +0900

    cleanup extension

--- spvm_extention_bind.c
+++ spvm_extention_bind.c
@@ -8,51 +8,51 @@ void SPVM_EXTENTION_BIND_bind_core_extentions(SPVM_COMPILER* compiler, SPVM_SUB*
   (void)compiler;
   
   if (strcmp(sub_abs_name, "stdout::println_byte") == 0) {
-    sub->native_address = SPVM_EXTENTION_stdout__println_byte;
+    sub->native_address = SPVM__stdout__println_byte;
   }
   else if (strcmp(sub_abs_name, "stdout::println_short") == 0) {
-    sub->native_address = SPVM_EXTENTION_stdout__println_short;
+    sub->native_address = SPVM__stdout__println_short;
   }
   else if (strcmp(sub_abs_name, "stdout::println_int") == 0) {
-    sub->native_address = SPVM_EXTENTION_stdout__println_int;
+    sub->native_address = SPVM__stdout__println_int;
   }
   else if (strcmp(sub_abs_name, "stdout::println_long") == 0) {
-    sub->native_address = SPVM_EXTENTION_stdout__println_long;
+    sub->native_address = SPVM__stdout__println_long;
   }
   else if (strcmp(sub_abs_name, "stdout::println_float") == 0) {
-    sub->native_address = SPVM_EXTENTION_stdout__println_float;
+    sub->native_address = SPVM__stdout__println_float;
   }
   else if (strcmp(sub_abs_name, "stdout::println_double") == 0) {
-    sub->native_address = SPVM_EXTENTION_stdout__println_double;
+    sub->native_address = SPVM__stdout__println_double;
   }
   else if (strcmp(sub_abs_name, "stdout::print_byte") == 0) {
-    sub->native_address = SPVM_EXTENTION_stdout__print_byte;
+    sub->native_address = SPVM__stdout__print_byte;
   }
   else if (strcmp(sub_abs_name, "stdout::print_short") == 0) {
-    sub->native_address = SPVM_EXTENTION_stdout__print_short;
+    sub->native_address = SPVM__stdout__print_short;
   }
   else if (strcmp(sub_abs_name, "stdout::print_int") == 0) {
-    sub->native_address = SPVM_EXTENTION_stdout__print_int;
+    sub->native_address = SPVM__stdout__print_int;
   }
   else if (strcmp(sub_abs_name, "stdout::print_long") == 0) {
-    sub->native_address = SPVM_EXTENTION_stdout__print_long;
+    sub->native_address = SPVM__stdout__print_long;
   }
   else if (strcmp(sub_abs_name, "stdout::print_float") == 0) {
-    sub->native_address = SPVM_EXTENTION_stdout__print_float;
+    sub->native_address = SPVM__stdout__print_float;
   }
   else if (strcmp(sub_abs_name, "stdout::print_double") == 0) {
-    sub->native_address = SPVM_EXTENTION_stdout__print_double;
+    sub->native_address = SPVM__stdout__print_double;
   }
   else if (strcmp(sub_abs_name, "stdout::println") == 0) {
-    sub->native_address = SPVM_EXTENTION_stdout__println;
+    sub->native_address = SPVM__stdout__println;
   }
   else if (strcmp(sub_abs_name, "stdout::sum_int") == 0) {
-    sub->native_address = SPVM_EXTENTION_stdout__sum_int;
+    sub->native_address = SPVM__stdout__sum_int;
   }
   else if (strcmp(sub_abs_name, "stdout::test1") == 0) {
-    sub->native_address = SPVM_EXTENTION_stdout__test1;
+    sub->native_address = SPVM__stdout__test1;
   }
   else if (strcmp(sub_abs_name, "stdout::test2") == 0) {
-    sub->native_address = SPVM_EXTENTION_stdout__test2;
+    sub->native_address = SPVM__stdout__test2;
   }
 }
