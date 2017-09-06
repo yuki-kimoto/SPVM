367e12f6cd03044d87cfcc945150d345ba1b9e25
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-09-06 22:23:20 +0900

    fix create_lib_dll.pl

--- create_lib_dll.pl
+++ create_lib_dll.pl
@@ -2,13 +2,13 @@ use strict;
 use warnings;
 use ExtUtils::CBuilder;
 
-use File::Copy 'copy';
+use File::Copy 'move';
 use File::Path 'mkpath';
 
 my $cbuilder = ExtUtils::CBuilder->new(config => {optimize => '-O3'});
 
 my $obj_file = $cbuilder->compile(
-  source => 'spvm_std.c',
+  source => 'spvm_lib_std.c',
   include_dirs => ['lib/SPVM']
 );
 my $lib_file = $cbuilder->link(objects => $obj_file);
@@ -16,5 +16,5 @@ my $lib_file = $cbuilder->link(objects => $obj_file);
 mkpath 'blib/arch/auto/SPVM/std';
 
 my $lib_file_blib = 'blib/arch/auto/SPVM/std/std.so';
-copy $lib_file, $lib_file_blib
+move($lib_file, $lib_file_blib)
   or die "Can't move $lib_file to $lib_file_blib";
