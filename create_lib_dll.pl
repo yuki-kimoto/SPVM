a0d18aeca4f2a90b83c8da539d6db27be33cf9d1
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-09-06 22:07:27 +0900

    add library dll create logic

---
+++ create_lib_dll.pl
@@ -0,0 +1,20 @@
+use strict;
+use warnings;
+use ExtUtils::CBuilder;
+
+use File::Copy 'copy';
+use File::Path 'mkpath';
+
+my $cbuilder = ExtUtils::CBuilder->new(config => {optimize => '-O3'});
+
+my $obj_file = $cbuilder->compile(
+  source => 'spvm_std.c',
+  include_dirs => ['lib/SPVM']
+);
+my $lib_file = $cbuilder->link(objects => $obj_file);
+
+mkpath 'blib/arch/auto/SPVM/std';
+
+my $lib_file_blib = 'blib/arch/auto/SPVM/std/std.so';
+copy $lib_file, $lib_file_blib
+  or die "Can't move $lib_file to $lib_file_blib";
