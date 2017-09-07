095a8795d7801418a6ac40233e48075ed66024e4
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-09-07 23:29:04 +0900

    fix some windows dll bug

--- create_lib_dll.pl
+++ create_lib_dll.pl
@@ -12,12 +12,42 @@ my @libs = ('std', 'Math');
 
 my $dlext = $Config{dlext};
 
+my $func_list = {
+  std => [qw(
+    println_byte
+    println_short
+    println_int
+    println_long
+    println_float
+    println_double
+    println
+
+    print_byte
+    print_short
+    print_int
+    print_long
+    print_float
+    print_double
+    
+    sum_int
+    test1
+    test2
+  )],
+  Math => [
+    'sin'
+  ]
+};
+
 for my $lib (@libs) {
   my $obj_file = $cbuilder->compile(
     source => "spvm_lib/spvm_lib_${lib}.c",
     include_dirs => ['lib/SPVM']
   );
-  my $lib_file = $cbuilder->link(objects => $obj_file, module_name => "SPVM::$lib");
+  my $lib_file = $cbuilder->link(
+    objects => $obj_file,
+    module_name => "SPVM::$lib",
+    dl_func_list => $func_list->{$lib}
+  );
 
   mkpath "blib/arch/auto/SPVM/${lib}";
 
