3304813ad4825be9d4f6c5ae9b375dd9ed518159
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-09-12 17:43:49 +0900

    fix Inline2 test bug

--- t/lib/SPVM/TestCase/Inline2.native/Inline2.c
+++ t/lib/SPVM/TestCase/Inline2.native/Inline2.c
@@ -2,9 +2,9 @@
 
 #include <Inline2_util.h>
 
-int32_t SPVM__TestCase__Inline2__sum(SPVM_API* api, SPVM_API_VALUE* args) {
+int32_t SPVM__TestCase__Inline2__mul(SPVM_API* api, SPVM_API_VALUE* args) {
 
-  int32_t total = args[0].int_value + args[1].int_value;
+  int32_t total = args[0].int_value * args[1].int_value;
   
   return total;
 }
