b4e202e5c1301c0d02beeeb3b5d487613dcd874b
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-09-25 21:14:39 +0900

    add SPVM::Integer

---
+++ lib/SPVM/Integer.pm
@@ -0,0 +1,36 @@
+package SPVM::Integer;
+
+1;
+
+=head1 NAME
+
+SPVM::Integer - Integer Utility
+
+=head1 CONSTANT FUNCTIONS
+
+=head2 MIN_VALUE
+
+B<int MIN_VALUE()>
+
+A constant holding the minimum value a int can have, C<-2147483648>.
+
+=head2 MAX_VALUE
+
+B<int MAX_VALUE()>
+
+A constant holding the maximum value a int can have, C<2147483647>.
+
+=head2 SIZE
+
+B<int SIZE>
+
+The number of bits used to represent a int value in two's complement binary form.
+
+=head2 BYTES
+
+B<int BYTES()>
+
+The number of ints used to represent a int value in two's complement binary form.
+
+=head1 FUNCTIONS
+
