fc23f77c857dae680e853299160f275aa32e6860
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-09-25 21:35:25 +0900

    add SPVM::Float, SPVM::Double parts

---
+++ lib/SPVM/Double.pm
@@ -0,0 +1,36 @@
+package SPVM::Double;
+
+1;
+
+=head1 NAME
+
+SPVM::Double - Double Utility
+
+=head1 CONSTANT FUNCTIONS
+
+=head2 MIN_VALUE
+
+B<double MIN_VALUE()>
+
+A constant holding the minimum value a double can have, C<-128>.
+
+=head2 MAX_VALUE
+
+B<double MAX_VALUE()>
+
+A constant holding the maximum value a double can have, C<127>.
+
+=head2 SIZE
+
+B<int SIZE>
+
+The number of bits used to represent a double value in two's complement binary form.
+
+=head2 BYTES
+
+B<int BYTES()>
+
+The number of doubles used to represent a double value in two's complement binary form.
+
+=head1 FUNCTIONS
+
---
+++ lib/SPVM/Float.pm
@@ -0,0 +1,36 @@
+package SPVM::Float;
+
+1;
+
+=head1 NAME
+
+SPVM::Float - Float Utility
+
+=head1 CONSTANT FUNCTIONS
+
+=head2 MIN_VALUE
+
+B<float MIN_VALUE()>
+
+A constant holding the minimum value a float can have, C<-128>.
+
+=head2 MAX_VALUE
+
+B<float MAX_VALUE()>
+
+A constant holding the maximum value a float can have, C<127>.
+
+=head2 SIZE
+
+B<int SIZE>
+
+The number of bits used to represent a float value in two's complement binary form.
+
+=head2 BYTES
+
+B<int BYTES()>
+
+The number of floats used to represent a float value in two's complement binary form.
+
+=head1 FUNCTIONS
+
