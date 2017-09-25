da52c1c337ef087012616230386d4fc9c7124f25
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-09-25 20:45:46 +0900

    add SPVM::Short

---
+++ lib/SPVM/Short.pm
@@ -0,0 +1,36 @@
+package SPVM::Short;
+
+1;
+
+=head1 NAME
+
+SPVM::Short - Short Utility
+
+=head1 CONSTANT FUNCTIONS
+
+=head2 MIN_VALUE
+
+B<short MIN_VALUE()>
+
+A constant holding the minimum value a short can have, C<-32768>.
+
+=head2 MAX_VALUE
+
+B<short MAX_VALUE()>
+
+A constant holding the maximum value a short can have, C<32767>.
+
+=head2 SIZE
+
+B<int SIZE>
+
+The number of bits used to represent a short value in two's complement binary form.
+
+=head2 BYTES
+
+B<int BYTES()>
+
+The number of shorts used to represent a short value in two's complement binary form.
+
+=head1 FUNCTIONS
+
