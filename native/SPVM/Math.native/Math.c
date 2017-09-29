488fea4426edc989d74eed875acd9080e69e295a
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-09-29 20:17:34 +0900

    add cbrt

--- native/SPVM/Math.native/Math.c
+++ native/SPVM/Math.native/Math.c
@@ -101,3 +101,11 @@ double SPVM__Math__sqrt(SPVM_API* api, SPVM_API_VALUE* args) {
   
   return value;
 }
+
+double SPVM__Math__cbrt(SPVM_API* api, SPVM_API_VALUE* args) {
+  (void)api;
+  
+  double value = cbrt(args[0].double_value);
+  
+  return value;
+}
