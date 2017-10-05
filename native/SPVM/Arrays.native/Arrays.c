997430ff932b6cb96b6ee2fee1379f4d50a5a0ee
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-10-05 21:35:54 +0900

    add Arrays::copy_of_xxx

--- native/SPVM/Arrays.native/Arrays.c
+++ native/SPVM/Arrays.native/Arrays.c
@@ -87,3 +87,171 @@ SPVM_API_OBJECT* SPVM__Arrays__copy_of_short(SPVM_API* api, SPVM_API_VALUE* args
   
   return copy;
 }
+
+SPVM_API_OBJECT* SPVM__Arrays__copy_of_int(SPVM_API* api, SPVM_API_VALUE* args) {
+  (void)api;
+  
+  SPVM_API_OBJECT* original = args[0].object_value;
+  
+  if (!original) {
+    SPVM_API_OBJECT* exception = api->new_byte_array_string(api, "First argument must be not undef(SPVM::Arrays::copy_of_int())");
+    api->set_exception(api, exception);
+    return NULL;
+  }
+  
+  int32_t new_length = args[1].int_value;
+  
+  if (new_length < 0) {
+    SPVM_API_OBJECT* exception = api->new_byte_array_string(api, "Second argument must be not negative value(SPVM::Arrays::copy_of_int())");
+    api->set_exception(api, exception);
+    return NULL;
+  }
+  
+  int32_t length = api->get_array_length(api, original);
+  
+  SPVM_API_OBJECT* copy = api->new_int_array(api, new_length);
+  
+  int32_t copy_length;
+  if (new_length > length) {
+    copy_length = length;
+  }
+  else {
+    copy_length = new_length;
+  }
+  
+  int32_t* original_elements = api->get_int_array_elements(api, original);
+  
+  int32_t* copy_elements = api->get_int_array_elements(api, copy);
+  
+  if (copy_length > 0) {
+    memcpy(copy_elements, original_elements, copy_length * sizeof(int16_t));
+  }
+  
+  return copy;
+}
+
+SPVM_API_OBJECT* SPVM__Arrays__copy_of_long(SPVM_API* api, SPVM_API_VALUE* args) {
+  (void)api;
+  
+  SPVM_API_OBJECT* original = args[0].object_value;
+  
+  if (!original) {
+    SPVM_API_OBJECT* exception = api->new_byte_array_string(api, "First argument must be not undef(SPVM::Arrays::copy_of_long())");
+    api->set_exception(api, exception);
+    return NULL;
+  }
+  
+  int32_t new_length = args[1].int_value;
+  
+  if (new_length < 0) {
+    SPVM_API_OBJECT* exception = api->new_byte_array_string(api, "Second argument must be not negative value(SPVM::Arrays::copy_of_long())");
+    api->set_exception(api, exception);
+    return NULL;
+  }
+  
+  int32_t length = api->get_array_length(api, original);
+  
+  SPVM_API_OBJECT* copy = api->new_long_array(api, new_length);
+  
+  int32_t copy_length;
+  if (new_length > length) {
+    copy_length = length;
+  }
+  else {
+    copy_length = new_length;
+  }
+  
+  int64_t* original_elements = api->get_long_array_elements(api, original);
+  
+  int64_t* copy_elements = api->get_long_array_elements(api, copy);
+  
+  if (copy_length > 0) {
+    memcpy(copy_elements, original_elements, copy_length * sizeof(int16_t));
+  }
+  
+  return copy;
+}
+
+SPVM_API_OBJECT* SPVM__Arrays__copy_of_float(SPVM_API* api, SPVM_API_VALUE* args) {
+  (void)api;
+  
+  SPVM_API_OBJECT* original = args[0].object_value;
+  
+  if (!original) {
+    SPVM_API_OBJECT* exception = api->new_byte_array_string(api, "First argument must be not undef(SPVM::Arrays::copy_of_float())");
+    api->set_exception(api, exception);
+    return NULL;
+  }
+  
+  int32_t new_length = args[1].int_value;
+  
+  if (new_length < 0) {
+    SPVM_API_OBJECT* exception = api->new_byte_array_string(api, "Second argument must be not negative value(SPVM::Arrays::copy_of_float())");
+    api->set_exception(api, exception);
+    return NULL;
+  }
+  
+  int32_t length = api->get_array_length(api, original);
+  
+  SPVM_API_OBJECT* copy = api->new_float_array(api, new_length);
+  
+  int32_t copy_length;
+  if (new_length > length) {
+    copy_length = length;
+  }
+  else {
+    copy_length = new_length;
+  }
+  
+  float* original_elements = api->get_float_array_elements(api, original);
+  
+  float* copy_elements = api->get_float_array_elements(api, copy);
+  
+  if (copy_length > 0) {
+    memcpy(copy_elements, original_elements, copy_length * sizeof(int16_t));
+  }
+  
+  return copy;
+}
+
+SPVM_API_OBJECT* SPVM__Arrays__copy_of_double(SPVM_API* api, SPVM_API_VALUE* args) {
+  (void)api;
+  
+  SPVM_API_OBJECT* original = args[0].object_value;
+  
+  if (!original) {
+    SPVM_API_OBJECT* exception = api->new_byte_array_string(api, "First argument must be not undef(SPVM::Arrays::copy_of_double())");
+    api->set_exception(api, exception);
+    return NULL;
+  }
+  
+  int32_t new_length = args[1].int_value;
+  
+  if (new_length < 0) {
+    SPVM_API_OBJECT* exception = api->new_byte_array_string(api, "Second argument must be not negative value(SPVM::Arrays::copy_of_double())");
+    api->set_exception(api, exception);
+    return NULL;
+  }
+  
+  int32_t length = api->get_array_length(api, original);
+  
+  SPVM_API_OBJECT* copy = api->new_double_array(api, new_length);
+  
+  int32_t copy_length;
+  if (new_length > length) {
+    copy_length = length;
+  }
+  else {
+    copy_length = new_length;
+  }
+  
+  double* original_elements = api->get_double_array_elements(api, original);
+  
+  double* copy_elements = api->get_double_array_elements(api, copy);
+  
+  if (copy_length > 0) {
+    memcpy(copy_elements, original_elements, copy_length * sizeof(int16_t));
+  }
+  
+  return copy;
+}
--- t/lib/SPVM/TestCase/Extension.native/Extension.c
+++ t/lib/SPVM/TestCase/Extension.native/Extension.c
@@ -3,7 +3,7 @@
 #include <spvm_api.h>
 
 int32_t SPVM__TestCase__Extension__sum(SPVM_API* api, SPVM_API_VALUE* args) {
-
+  
   int32_t total = args[0].int_value + args[1].int_value;
   
   return total;
