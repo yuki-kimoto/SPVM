bfe499e8f63b0d3befb7dcf1569c20edab9603d6
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-10-05 21:11:25 +0900

    add Arrays::copy_of_short

--- native/SPVM/Arrays.native/Arrays.c
+++ native/SPVM/Arrays.native/Arrays.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "spvm_api.h"
 
@@ -39,7 +40,49 @@ SPVM_API_OBJECT* SPVM__Arrays__copy_of_byte(SPVM_API* api, SPVM_API_VALUE* args)
   int8_t* copy_elements = api->get_byte_array_elements(api, copy);
   
   if (copy_length > 0) {
-    memcpy(copy_elements, original_elements, copy_length);
+    memcpy(copy_elements, original_elements, copy_length * sizeof(int8_t));
+  }
+  
+  return copy;
+}
+
+SPVM_API_OBJECT* SPVM__Arrays__copy_of_short(SPVM_API* api, SPVM_API_VALUE* args) {
+  (void)api;
+  
+  SPVM_API_OBJECT* original = args[0].object_value;
+  
+  if (!original) {
+    SPVM_API_OBJECT* exception = api->new_byte_array_string(api, "First argument must be not undef(SPVM::Arrays::copy_of_short())");
+    api->set_exception(api, exception);
+    return NULL;
+  }
+  
+  int32_t new_length = args[1].int_value;
+  
+  if (new_length < 0) {
+    SPVM_API_OBJECT* exception = api->new_byte_array_string(api, "Second argument must be not negative value(SPVM::Arrays::copy_of_short())");
+    api->set_exception(api, exception);
+    return NULL;
+  }
+  
+  int32_t length = api->get_array_length(api, original);
+  
+  SPVM_API_OBJECT* copy = api->new_short_array(api, new_length);
+  
+  int32_t copy_length;
+  if (new_length > length) {
+    copy_length = length;
+  }
+  else {
+    copy_length = new_length;
+  }
+  
+  int16_t* original_elements = api->get_short_array_elements(api, original);
+  
+  int16_t* copy_elements = api->get_short_array_elements(api, copy);
+  
+  if (copy_length > 0) {
+    memcpy(copy_elements, original_elements, copy_length * sizeof(int16_t));
   }
   
   return copy;
