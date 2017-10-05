368dfe3c3c88f2e69f0f7a32b63771bcd26f8bd4
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-10-05 21:49:22 +0900

    add doc

--- lib/SPVM/Arrays.pm
+++ lib/SPVM/Arrays.pm
@@ -4,9 +4,21 @@ package SPVM::Arrays;
 
 =head1 NAME
 
-SPVM::Double - Array Utility
+SPVM::Arrays - Array Utility
 
 =head1 CONSTANT FUNCTIONS
 
 =head1 FUNCTIONS
+
+=head2 copy_of_byte
+
+=head2 copy_of_short
+
+=head2 copy_of_int
+
+=head2 copy_of_long
+
+=head2 copy_of_float
+
+=head2 copy_of_double
 
