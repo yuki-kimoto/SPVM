fc1304ed4fdc82ddeeae0e3a317c2397d1b5b701
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-10-05 15:20:13 +0900

    implement SPVM::Arrays::copy_of_byte

--- native/SPVM/Arrays.native/Arrays.c
+++ native/SPVM/Arrays.native/Arrays.c
@@ -1,9 +1,38 @@
 #include <assert.h>
+#include <stdlib.h>
 
 #include "spvm_api.h"
 
 SPVM_API_OBJECT* SPVM__Arrays__copy_of_byte(SPVM_API* api, SPVM_API_VALUE* args) {
   (void)api;
   
-  return 0;
+  SPVM_API_OBJECT* original = args[0].object_value;
+  
+  if (!original) {
+    SPVM_API_OBJECT* exception = api->new_byte_array_string(api, "First argument must be not undef(SPVM::Arrays::copy_of_byte())");
+    api->set_exception(api, exception);
+    return NULL;
+  }
+  
+  int32_t new_length = args[1].int_value;
+  
+  int32_t length = api->get_array_length(api, original);
+  
+  SPVM_API_OBJECT* copy = api->new_byte_array(api, new_length);
+  
+  int32_t copy_length;
+  if (new_length > length) {
+    copy_length = length;
+  }
+  else {
+    copy_length = new_length;
+  }
+  
+  int8_t* original_elements = api->get_byte_array_elements(api, original);
+  
+  int8_t* copy_elements = api->get_byte_array_elements(api, copy);
+  
+  memcpy(copy_elements, original_elements, copy_length);
+  
+  return copy;
 }
