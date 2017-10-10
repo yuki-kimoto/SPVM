7a3eb2e9ab1149e453e169e8f55295b3f5e1dd92
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-10-10 21:24:32 +0900

    improve pod

--- lib/SPVM/Arrays.pm
+++ lib/SPVM/Arrays.pm
@@ -12,13 +12,24 @@ SPVM::Arrays - Array Utility
 
 =head2 copy_of_byte
 
+  sub copy_of_byte($original : byte[], $new_length : int) : native byte[];
+
 =head2 copy_of_short
+
+  sub copy_of_short($original : short[], $new_length : int) : native short[];
 
 =head2 copy_of_int
 
+  sub copy_of_int($original : int[], $new_length : int) : native int[];
+
 =head2 copy_of_long
+
+  sub copy_of_long($original : long[], $new_length : int) : native long[];
 
 =head2 copy_of_float
 
+  sub copy_of_float($original : float[], $new_length : int) : native float[];
+
 =head2 copy_of_double
 
+  sub copy_of_double($original : double[], $new_length : int) : native double[];
