2d669248d453144829da71a1818a87b61e81e577
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-10-31 21:10:39 +0900

    remove pop optimization

--- spvm_op_checker.c
+++ spvm_op_checker.c
@@ -329,17 +329,6 @@ void SPVM_OP_CHECKER_check(SPVM_COMPILER* compiler) {
                       op_cur->code = SPVM_OP_C_CODE_NULL;
                     }
                   }
-                  else if (op_cur->first->code == SPVM_OP_C_CODE_ASSIGN) {
-                    op_cur->code = SPVM_OP_C_CODE_NULL;
-                  }
-                  else if (op_cur->first->code == SPVM_OP_C_CODE_VAR) {
-                    op_cur->code = SPVM_OP_C_CODE_NULL;
-                    op_cur->first->code = SPVM_OP_C_CODE_NULL;
-                  }
-                  else if (op_cur->first->code == SPVM_OP_C_CODE_ASSIGN_PROCESS && op_cur->first->first->code  == SPVM_OP_C_CODE_VAR) {
-                    op_cur->code = SPVM_OP_C_CODE_NULL;
-                    op_cur->first->first->code = SPVM_OP_C_CODE_NULL;
-                  }
                   break;
                 }
                 case SPVM_OP_C_CODE_SWITCH: {
