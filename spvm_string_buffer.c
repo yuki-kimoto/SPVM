0a167375de176c7d87a3251e247bde095b846e4e
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-10-31 11:39:10 +0900

    implement string_buffer

--- spvm_string_buffer.c
+++ spvm_string_buffer.c
@@ -18,50 +18,30 @@ SPVM_STRING_BUFFER* SPVM_STRING_BUFFER_new(int32_t page_byte_size) {
   return string_buffer;
 }
 
-void* SPVM_STRING_BUFFER_add(SPVM_STRING_BUFFER* string_buffer, char* string) {
+char* SPVM_STRING_BUFFER_get_buffer(SPVM_STRING_BUFFER* string_buffer) {
+  return string_buffer->buffer;
+}
+
+void SPVM_STRING_BUFFER_add(SPVM_STRING_BUFFER* string_buffer, char* string) {
+  
+  int32_t string_length = strlen(string);
   
   assert(byte_size > 0);
   
-  if (byte_size > string_buffer->page_byte_size) {
-    fprintf(stderr, "Very lerge memory byte size is specified(SPVM_STRING_BUFFER_alloc())\n");
-    abort();
-  }
+  int32_t new_length = string_buffer->length + string_length;
   
-  // Adjust alignment
-  int32_t aligned_byte_size = (byte_size - 1) + ((int32_t)sizeof(SPVM_VALUE) - ((byte_size - 1) % (int32_t)sizeof(SPVM_VALUE)));
-  
-  // Move to next page
-  if (string_buffer->current_offset + aligned_byte_size > string_buffer->page_byte_size) {
-    // Next page is not yet allocated
-    string_buffer->current_page++;
-    string_buffer->current_offset = 0;
+  // Extend
+  if (new_length > string_buffer->capacity) {
+    char* new_buffer = SPVM_UTIL_ALLOCATOR_safe_malloc_zero(new_length + 1);
+    memcpy(new_buffer, string_buffer->buffer, string_buffer->length);
     
-    if (string_buffer->current_page == string_buffer->pages_length) {
-      int32_t new_string_buffer_pages_length = string_buffer->pages_length * 2;
-      
-      int64_t new_pages_byte_size = (int64_t)new_string_buffer_pages_length * (int64_t)sizeof(char*);
-      char** new_pages = SPVM_UTIL_ALLOCATOR_safe_malloc_zero(new_pages_byte_size);
-      memcpy(new_pages, string_buffer->pages, string_buffer->pages_length * sizeof(char*));
-      free(string_buffer->pages);
-      string_buffer->pages = new_pages;
-      
-      {
-        int32_t i;
-        for (i = string_buffer->pages_length; i < new_string_buffer_pages_length; i++) {
-          string_buffer->pages[i] = SPVM_UTIL_ALLOCATOR_safe_malloc_zero(string_buffer->page_byte_size);
-        }
-      }
-      
-      string_buffer->pages_length = new_string_buffer_pages_length;
-    }
+    string_buffer->length = new_length;
+    string_buffer->buffer = new_buffer;
   }
   
-  // Allocated address
-  char* alloc_address = string_buffer->pages[string_buffer->current_page] + string_buffer->current_offset;
+  memcpy(string_buffer->buffer + string_buffer_length, string, string_length);
   
-  string_buffer->current_offset += aligned_byte_size;
-  
-  return alloc_address;
+  return;
 }
 
 void SPVM_STRING_BUFFER_free(SPVM_STRING_BUFFER* string_buffer) {
