8f00c9c75f17321022fc444847cee824a0ca4cd4
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-11-03 08:45:53 +0900

    add GT_BYTE bytecode

--- spvm_bytecode_builder.c
+++ spvm_bytecode_builder.c
@@ -776,11 +776,8 @@ void SPVM_BYTECODE_BUILDER_build_bytecode_array(SPVM_COMPILER* compiler) {
                   
                   SPVM_TYPE* type = SPVM_OP_get_type(compiler, op_condition_target->first);
                   if (type->code == SPVM_TYPE_C_CODE_BYTE) {
-                    SPVM_BYTECODE_ARRAY_push_int(compiler, bytecode_array, SPVM_BYTECODE_C_CODE_CMP_BYTE);
-                    
-                    
-                    
-                    SPVM_BYTECODE_ARRAY_push_int(compiler, bytecode_array, SPVM_BYTECODE_C_CODE_IF_GT_ZERO);
+                    SPVM_BYTECODE_ARRAY_push_int(compiler, bytecode_array, SPVM_BYTECODE_C_CODE_GT_BYTE);
+                    SPVM_BYTECODE_ARRAY_push_int(compiler, bytecode_array, SPVM_BYTECODE_C_CODE_IF_NE_ZERO);
                   }
                   else if (type->code == SPVM_TYPE_C_CODE_SHORT) {
                     SPVM_BYTECODE_ARRAY_push_int(compiler, bytecode_array, SPVM_BYTECODE_C_CODE_CMP_SHORT);
