b9fa8cebe3d41e83cd202cf7c62fc8e7322c113b
Yuki Kimoto <kimoto.yuki@gmail.com>
2017-11-10 16:52:29 +0900

    create tmp var of return term

--- spvm_op_checker.c
+++ spvm_op_checker.c
@@ -1708,6 +1708,14 @@ void SPVM_OP_CHECKER_check(SPVM_COMPILER* compiler) {
                     }
                   }
                 }
+                else if (op_cur->code == SPVM_OP_C_CODE_RETURN) {
+                  if (op_cur->first) {
+                    if (op_cur->first->code == SPVM_OP_C_CODE_UNDEF) {
+                      create_tmp_var = 1;
+                      op_cur = op_cur->first;
+                    }
+                  }
+                }
                 // CALL_SUB which return value don't void
                 else if (op_cur->code == SPVM_OP_C_CODE_CALL_SUB) {
                   if (tmp_var_type->code != SPVM_TYPE_C_CODE_VOID) {
