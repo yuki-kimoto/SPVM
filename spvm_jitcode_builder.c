c427cfb31c1079e8ba1fd5533ebbfbf645ba05fc
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-01-12 10:53:14 +0900

    simplify jitcode object

--- spvm_jitcode_builder.c
+++ spvm_jitcode_builder.c
@@ -1492,23 +1492,24 @@ void SPVM_JITCODE_BUILDER_build_jitcode() {
               int32_t field_byte_offset = constant_pool_field->byte_offset;
               
               SPVM_STRING_BUFFER_add(string_buffer, "  // GET_FIELD\n");
-              SPVM_STRING_BUFFER_add(string_buffer, "  {\n");
-              SPVM_STRING_BUFFER_add(string_buffer, "    SPVM_API_OBJECT* object = var");
+              SPVM_STRING_BUFFER_add(string_buffer, "  if (__builtin_expect(");
+              SPVM_STRING_BUFFER_add(string_buffer, "var");
               SPVM_STRING_BUFFER_add_int(string_buffer, opcode->operand1);
-              SPVM_STRING_BUFFER_add(string_buffer, "  ;\n");
-              SPVM_STRING_BUFFER_add(string_buffer, "    if (__builtin_expect(object == NULL, 0)) {\n");
-              SPVM_STRING_BUFFER_add(string_buffer, "      api->set_exception(api, api->new_string(api, \"Object must be not undef.\", 0));\n");
+              SPVM_STRING_BUFFER_add(string_buffer, " == NULL, 0)) {\n");
+              SPVM_STRING_BUFFER_add(string_buffer, "    api->set_exception(api, api->new_string(api, \"Object must be not undef.\", 0));\n");
               SPVM_JITCODE_BUILDER_add_string_buffer_croak(string_buffer, sub_opcode_base, eval_stack, &eval_stack_top, sub_is_void);
-              SPVM_STRING_BUFFER_add(string_buffer, "    }\n");
-              SPVM_STRING_BUFFER_add(string_buffer, "    else {\n");
-              SPVM_STRING_BUFFER_add(string_buffer, "      var");
+              SPVM_STRING_BUFFER_add(string_buffer, "  }\n");
+              SPVM_STRING_BUFFER_add(string_buffer, "  else {\n");
+              SPVM_STRING_BUFFER_add(string_buffer, "    var");
               SPVM_STRING_BUFFER_add_int(string_buffer, opcode->operand0);
               SPVM_STRING_BUFFER_add(string_buffer, " = *(");
               SPVM_STRING_BUFFER_add(string_buffer, field_type);
-              SPVM_STRING_BUFFER_add(string_buffer, "*)((intptr_t)object + SPVM_JITCODE_C_OBJECT_HEADER_BYTE_SIZE + ");
+              SPVM_STRING_BUFFER_add(string_buffer, "*)((intptr_t)");
+              SPVM_STRING_BUFFER_add(string_buffer, "var");
+              SPVM_STRING_BUFFER_add_int(string_buffer, opcode->operand1);
+              SPVM_STRING_BUFFER_add(string_buffer, " + SPVM_JITCODE_C_OBJECT_HEADER_BYTE_SIZE + ");
               SPVM_STRING_BUFFER_add_int(string_buffer, field_byte_offset);
               SPVM_STRING_BUFFER_add(string_buffer, ");\n");
-              SPVM_STRING_BUFFER_add(string_buffer, "    }\n");
               SPVM_STRING_BUFFER_add(string_buffer, "  }\n");
               
               break;
