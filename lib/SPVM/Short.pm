c8d8da5227faf22f0d77ef7a68887248391a9894
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-01-15 17:56:46 +0900

    finish version 1.0 SPVM::Integer implementation and document

--- lib/SPVM/Short.pm
+++ lib/SPVM/Short.pm
@@ -10,24 +10,24 @@ SPVM::Short - Short Utility
 
 =head2 MIN_VALUE
 
-B<short MIN_VALUE()>
+B<sub MIN_VALUE() : short>
 
 A constant holding the minimum value a short can have, C<-32768>.
 
 =head2 MAX_VALUE
 
-B<short MAX_VALUE()>
+B<sub MAX_VALUE() : short>
 
 A constant holding the maximum value a short can have, C<32767>.
 
 =head2 BYTES
 
-B<int BYTES()>
+B<sub BYTES() : int>
 
 The number of shorts used to represent a short value in two's complement binary form, C<2>.
 
 =head2 SIZE
 
-B<int SIZE>
+B<sub SIZE() : int>
 
 The number of bits used to represent a short value in two's complement binary form, C<16>.
