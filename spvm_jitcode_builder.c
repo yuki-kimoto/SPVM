42fe11224779ae0d74f38d0e77ebe03acab91ecc
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-01-24 11:18:19 +0900

    add jitcode set field type

--- spvm_jitcode_builder.c
+++ spvm_jitcode_builder.c
@@ -1611,7 +1611,6 @@ void SPVM_JITCODE_BUILDER_build_jitcode() {
             case SPVM_OPCODE_C_CODE_SET_FIELD_LONG:
             case SPVM_OPCODE_C_CODE_SET_FIELD_FLOAT:
             case SPVM_OPCODE_C_CODE_SET_FIELD_DOUBLE:
-            case SPVM_OPCODE_C_CODE_SET_FIELD_OBJECT:
             {
               int32_t field_id = opcode->operand1;
               SPVM_CONSTANT_POOL_FIELD* constant_pool_field = (SPVM_CONSTANT_POOL_FIELD*)&constant_pool[field_id];
@@ -1624,56 +1623,69 @@ void SPVM_JITCODE_BUILDER_build_jitcode() {
               SPVM_STRING_BUFFER_add(string_buffer, "    croak_flag = 1;\n");
               SPVM_STRING_BUFFER_add(string_buffer, "  }\n");
               SPVM_STRING_BUFFER_add(string_buffer, "  else {\n");
-              if (opcode->code == SPVM_OPCODE_C_CODE_SET_FIELD_OBJECT) {
-                SPVM_STRING_BUFFER_add(string_buffer, "    SPVM_API_OBJECT** field_address = (SPVM_API_OBJECT**)((intptr_t)");
-                SPVM_JITCODE_BUILDER_add_var(string_buffer, opcode->operand0);
-                SPVM_STRING_BUFFER_add(string_buffer, " + SPVM_JITCODE_C_OBJECT_HEADER_BYTE_SIZE + ");
-                SPVM_STRING_BUFFER_add_int(string_buffer, field_byte_offset);
-                SPVM_STRING_BUFFER_add(string_buffer, ");\n");
-                SPVM_STRING_BUFFER_add(string_buffer, "    if(*field_address != NULL) {\n");
-                SPVM_STRING_BUFFER_add(string_buffer, "      if (SPVM_JITCODE_INLINE_ISWEAK(*field_address)) {\n");
-                SPVM_STRING_BUFFER_add(string_buffer, "        api->unweaken(api, field_address);\n");
-                SPVM_STRING_BUFFER_add(string_buffer, "      }\n");
-                SPVM_STRING_BUFFER_add(string_buffer, "      if (SPVM_JITCODE_INLINE_GET_REF_COUNT(*field_address) > 1) { SPVM_JITCODE_INLINE_DEC_REF_COUNT_ONLY(*field_address); }\n");
-                SPVM_STRING_BUFFER_add(string_buffer, "      else { api->dec_ref_count(api, *field_address); }\n");
-                SPVM_STRING_BUFFER_add(string_buffer, "    }\n");
-                SPVM_STRING_BUFFER_add(string_buffer, "    *field_address = ");
-                SPVM_JITCODE_BUILDER_add_var(string_buffer, opcode->operand2);
-                SPVM_STRING_BUFFER_add(string_buffer, "  ;\n");
-                SPVM_STRING_BUFFER_add(string_buffer, "    if(*field_address != NULL) { SPVM_JITCODE_INLINE_INC_REF_COUNT(*field_address); }\n");
+              char* field_type = NULL;
+              switch (opcode->code) {
+                case SPVM_OPCODE_C_CODE_SET_FIELD_BYTE:
+                  field_type = "SPVM_API_byte";
+                  break;
+                case SPVM_OPCODE_C_CODE_SET_FIELD_SHORT:
+                  field_type = "SPVM_API_short";
+                  break;
+                case SPVM_OPCODE_C_CODE_SET_FIELD_INT:
+                  field_type = "SPVM_API_int";
+                  break;
+                case SPVM_OPCODE_C_CODE_SET_FIELD_LONG:
+                  field_type = "SPVM_API_long";
+                  break;
+                case SPVM_OPCODE_C_CODE_SET_FIELD_FLOAT:
+                  field_type = "float";
+                  break;
+                case SPVM_OPCODE_C_CODE_SET_FIELD_DOUBLE:
+                  field_type = "double";
+                  break;
               }
-              else {
-                char* field_type = NULL;
-                switch (opcode->code) {
-                  case SPVM_OPCODE_C_CODE_SET_FIELD_BYTE:
-                    field_type = "SPVM_API_byte";
-                    break;
-                  case SPVM_OPCODE_C_CODE_SET_FIELD_SHORT:
-                    field_type = "SPVM_API_short";
-                    break;
-                  case SPVM_OPCODE_C_CODE_SET_FIELD_INT:
-                    field_type = "SPVM_API_int";
-                    break;
-                  case SPVM_OPCODE_C_CODE_SET_FIELD_LONG:
-                    field_type = "SPVM_API_long";
-                    break;
-                  case SPVM_OPCODE_C_CODE_SET_FIELD_FLOAT:
-                    field_type = "float";
-                    break;
-                  case SPVM_OPCODE_C_CODE_SET_FIELD_DOUBLE:
-                    field_type = "double";
-                    break;
-                }
-                SPVM_STRING_BUFFER_add(string_buffer, "    *(");
-                SPVM_STRING_BUFFER_add(string_buffer, field_type);
-                SPVM_STRING_BUFFER_add(string_buffer, "*)((intptr_t)");
-                SPVM_JITCODE_BUILDER_add_var(string_buffer, opcode->operand0);
-                SPVM_STRING_BUFFER_add(string_buffer, " + SPVM_JITCODE_C_OBJECT_HEADER_BYTE_SIZE + ");
-                SPVM_STRING_BUFFER_add_int(string_buffer, field_byte_offset);
-                SPVM_STRING_BUFFER_add(string_buffer, ") = ");
-                SPVM_JITCODE_BUILDER_add_var(string_buffer, opcode->operand2);
-                SPVM_STRING_BUFFER_add(string_buffer, ";\n");
-              }
+              SPVM_STRING_BUFFER_add(string_buffer, "    *(");
+              SPVM_STRING_BUFFER_add(string_buffer, field_type);
+              SPVM_STRING_BUFFER_add(string_buffer, "*)((intptr_t)");
+              SPVM_JITCODE_BUILDER_add_var(string_buffer, opcode->operand0);
+              SPVM_STRING_BUFFER_add(string_buffer, " + SPVM_JITCODE_C_OBJECT_HEADER_BYTE_SIZE + ");
+              SPVM_STRING_BUFFER_add_int(string_buffer, field_byte_offset);
+              SPVM_STRING_BUFFER_add(string_buffer, ") = ");
+              SPVM_JITCODE_BUILDER_add_var(string_buffer, opcode->operand2);
+              SPVM_STRING_BUFFER_add(string_buffer, ";\n");
+              SPVM_STRING_BUFFER_add(string_buffer, "  }\n");
+              
+              break;
+            }
+            case SPVM_OPCODE_C_CODE_SET_FIELD_OBJECT:
+            {
+              int32_t field_id = opcode->operand1;
+              SPVM_CONSTANT_POOL_FIELD* constant_pool_field = (SPVM_CONSTANT_POOL_FIELD*)&constant_pool[field_id];
+              int32_t field_byte_offset = constant_pool_field->byte_offset;
+              
+              SPVM_STRING_BUFFER_add(string_buffer, "  if (__builtin_expect(");
+              SPVM_JITCODE_BUILDER_add_operand(string_buffer, "SPVM_API_OBJECT*", opcode->operand0);
+              SPVM_STRING_BUFFER_add(string_buffer, " == NULL, 0)) {\n");
+              SPVM_STRING_BUFFER_add(string_buffer, "    api->set_exception(api, api->new_string(api, \"Object must be not undef.\", 0));\n");
+              SPVM_STRING_BUFFER_add(string_buffer, "    croak_flag = 1;\n");
+              SPVM_STRING_BUFFER_add(string_buffer, "  }\n");
+              SPVM_STRING_BUFFER_add(string_buffer, "  else {\n");
+              SPVM_STRING_BUFFER_add(string_buffer, "    SPVM_API_OBJECT** field_address = (SPVM_API_OBJECT**)((intptr_t)");
+              SPVM_JITCODE_BUILDER_add_operand(string_buffer, "SPVM_API_OBJECT*", opcode->operand0);
+              SPVM_STRING_BUFFER_add(string_buffer, " + SPVM_JITCODE_C_OBJECT_HEADER_BYTE_SIZE + ");
+              SPVM_STRING_BUFFER_add_int(string_buffer, field_byte_offset);
+              SPVM_STRING_BUFFER_add(string_buffer, ");\n");
+              SPVM_STRING_BUFFER_add(string_buffer, "    if(*field_address != NULL) {\n");
+              SPVM_STRING_BUFFER_add(string_buffer, "      if (SPVM_JITCODE_INLINE_ISWEAK(*field_address)) {\n");
+              SPVM_STRING_BUFFER_add(string_buffer, "        api->unweaken(api, field_address);\n");
+              SPVM_STRING_BUFFER_add(string_buffer, "      }\n");
+              SPVM_STRING_BUFFER_add(string_buffer, "      if (SPVM_JITCODE_INLINE_GET_REF_COUNT(*field_address) > 1) { SPVM_JITCODE_INLINE_DEC_REF_COUNT_ONLY(*field_address); }\n");
+              SPVM_STRING_BUFFER_add(string_buffer, "      else { api->dec_ref_count(api, *field_address); }\n");
+              SPVM_STRING_BUFFER_add(string_buffer, "    }\n");
+              SPVM_STRING_BUFFER_add(string_buffer, "    *field_address = ");
+              SPVM_JITCODE_BUILDER_add_operand(string_buffer, "SPVM_API_OBJECT*", opcode->operand2);
+              SPVM_STRING_BUFFER_add(string_buffer, "  ;\n");
+              SPVM_STRING_BUFFER_add(string_buffer, "    if(*field_address != NULL) { SPVM_JITCODE_INLINE_INC_REF_COUNT(*field_address); }\n");
               SPVM_STRING_BUFFER_add(string_buffer, "  }\n");
               
               break;
