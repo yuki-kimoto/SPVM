f2ef859574760372d4209c325967cc797c5f46cc
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-01-24 11:57:14 +0900

    cleanup set_field

--- spvm_jitcode_builder.c
+++ spvm_jitcode_builder.c
@@ -1617,41 +1617,41 @@ void SPVM_JITCODE_BUILDER_build_jitcode() {
               int32_t field_byte_offset = constant_pool_field->byte_offset;
               
               SPVM_STRING_BUFFER_add(string_buffer, "  if (__builtin_expect(");
-              SPVM_JITCODE_BUILDER_add_var(string_buffer, opcode->operand0);
+              SPVM_JITCODE_BUILDER_add_operand(string_buffer, "SPVM_API_OBJECT*", opcode->operand0);
               SPVM_STRING_BUFFER_add(string_buffer, " == NULL, 0)) {\n");
               SPVM_STRING_BUFFER_add(string_buffer, "    api->set_exception(api, api->new_string(api, \"Object must be not undef.\", 0));\n");
               SPVM_STRING_BUFFER_add(string_buffer, "    croak_flag = 1;\n");
               SPVM_STRING_BUFFER_add(string_buffer, "  }\n");
               SPVM_STRING_BUFFER_add(string_buffer, "  else {\n");
-              char* field_type = NULL;
+              char* field_type_name = NULL;
               switch (opcode->code) {
                 case SPVM_OPCODE_C_CODE_SET_FIELD_BYTE:
-                  field_type = "SPVM_API_byte";
+                  field_type_name = "SPVM_API_byte";
                   break;
                 case SPVM_OPCODE_C_CODE_SET_FIELD_SHORT:
-                  field_type = "SPVM_API_short";
+                  field_type_name = "SPVM_API_short";
                   break;
                 case SPVM_OPCODE_C_CODE_SET_FIELD_INT:
-                  field_type = "SPVM_API_int";
+                  field_type_name = "SPVM_API_int";
                   break;
                 case SPVM_OPCODE_C_CODE_SET_FIELD_LONG:
-                  field_type = "SPVM_API_long";
+                  field_type_name = "SPVM_API_long";
                   break;
                 case SPVM_OPCODE_C_CODE_SET_FIELD_FLOAT:
-                  field_type = "float";
+                  field_type_name = "float";
                   break;
                 case SPVM_OPCODE_C_CODE_SET_FIELD_DOUBLE:
-                  field_type = "double";
+                  field_type_name = "double";
                   break;
               }
               SPVM_STRING_BUFFER_add(string_buffer, "    *(");
-              SPVM_STRING_BUFFER_add(string_buffer, field_type);
+              SPVM_STRING_BUFFER_add(string_buffer, field_type_name);
               SPVM_STRING_BUFFER_add(string_buffer, "*)((intptr_t)");
-              SPVM_JITCODE_BUILDER_add_var(string_buffer, opcode->operand0);
+              SPVM_JITCODE_BUILDER_add_operand(string_buffer, "SPVM_API_OBJECT*", opcode->operand0);
               SPVM_STRING_BUFFER_add(string_buffer, " + SPVM_JITCODE_C_OBJECT_HEADER_BYTE_SIZE + ");
               SPVM_STRING_BUFFER_add_int(string_buffer, field_byte_offset);
               SPVM_STRING_BUFFER_add(string_buffer, ") = ");
-              SPVM_JITCODE_BUILDER_add_var(string_buffer, opcode->operand2);
+              SPVM_JITCODE_BUILDER_add_operand(string_buffer, field_type_name, opcode->operand2);
               SPVM_STRING_BUFFER_add(string_buffer, ";\n");
               SPVM_STRING_BUFFER_add(string_buffer, "  }\n");
               
