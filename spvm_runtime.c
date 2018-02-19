cf7f2c7c00ce324ea9391e0ae786ed305d3807a8
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-02-19 11:01:35 +0900

    cleanup auto_dec_ref_count_stack

--- spvm_runtime.c
+++ spvm_runtime.c
@@ -211,7 +211,7 @@ SPVM_API_VALUE SPVM_RUNTIME_call_sub(SPVM_API* api, int32_t sub_id, SPVM_API_VAL
   int32_t call_sub_arg_stack_top = -1;
   
   // Auto decrement reference count variable index stack
-  SPVM_API_OBJECT* auto_dec_ref_count_stack[65535];
+  SPVM_API_VALUE auto_dec_ref_count_stack[65535];
   
   // Auto decrement reference count variable index stack top
   int32_t auto_dec_ref_count_stack_top = -1;
@@ -1022,14 +1022,14 @@ SPVM_API_VALUE SPVM_RUNTIME_call_sub(SPVM_API* api, int32_t sub_id, SPVM_API_VAL
       }
       case SPVM_OPCODE_C_CODE_PUSH_AUTO_DEC_REF_COUNT: {
         auto_dec_ref_count_stack_top++;
-        auto_dec_ref_count_stack[auto_dec_ref_count_stack_top] = opcode->operand0;
+        auto_dec_ref_count_stack[auto_dec_ref_count_stack_top].int_value = opcode->operand0;
         break;
       }
       case SPVM_OPCODE_C_CODE_LEAVE_SCOPE: {
         int32_t auto_dec_ref_count_stack_base = opcode->operand0;
         int32_t auto_dec_ref_count_index;
         for (auto_dec_ref_count_index = auto_dec_ref_count_stack_base; auto_dec_ref_count_index <= auto_dec_ref_count_stack_top; auto_dec_ref_count_index++) {
-          int32_t var_index = auto_dec_ref_count_stack[auto_dec_ref_count_index];
+          int32_t var_index = auto_dec_ref_count_stack[auto_dec_ref_count_index].int_value;
           
           if (*(SPVM_API_OBJECT**)&call_stack[var_index] != NULL) {
             if (SPVM_INLINE_GET_REF_COUNT(*(SPVM_API_OBJECT**)&call_stack[var_index]) > 1) { SPVM_INLINE_DEC_REF_COUNT_ONLY(*(SPVM_API_OBJECT**)&call_stack[var_index]); }
@@ -1813,7 +1813,7 @@ SPVM_API_VALUE SPVM_RUNTIME_call_sub(SPVM_API* api, int32_t sub_id, SPVM_API_VAL
     {
       int32_t auto_dec_ref_count_index;
       for (auto_dec_ref_count_index = 0; auto_dec_ref_count_index <= auto_dec_ref_count_stack_top; auto_dec_ref_count_index++) {
-        int32_t var_index = auto_dec_ref_count_stack[auto_dec_ref_count_index];
+        int32_t var_index = auto_dec_ref_count_stack[auto_dec_ref_count_index].int_value;
         
         if (*(SPVM_API_OBJECT**)&call_stack[var_index] != NULL) {
           if (SPVM_INLINE_GET_REF_COUNT(*(SPVM_API_OBJECT**)&call_stack[var_index]) > 1) { SPVM_INLINE_DEC_REF_COUNT_ONLY(*(SPVM_API_OBJECT**)&call_stack[var_index]); }
