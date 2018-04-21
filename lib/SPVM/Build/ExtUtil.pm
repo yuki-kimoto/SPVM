524b6e9045442f3cde97d0c2fa8dc8f31ad3ea8d
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-04-21 09:10:24 +0900

    only print warning message when SPVM_TEST_ENABLE_WARNINGS is defined

--- lib/SPVM/Build/ExtUtil.pm
+++ lib/SPVM/Build/ExtUtil.pm
@@ -20,8 +20,14 @@ sub new {
   
   my $self = {};
   
-  $self->{extra_compiler_flags} = '-std=c99 -Wall -Wextra -Wno-unused-label -Wno-unused-function -Wno-unused-label -Wno-unused-parameter -Wno-unused-variable';
-
+  $self->{extra_compiler_flags} = '-std=c99';
+  
+  # I want to print warnings, but if gcc version is different, can't suppress no needed warning message.
+  # so I dicide not to print warning in release version
+  if ($ENV{SPVM_TEST_ENABLE_WARNINGS}) {
+    $self->{extra_compiler_flags} .= " -Wall -Wextra -Wno-unused-label -Wno-unused-function -Wno-unused-label -Wno-unused-parameter -Wno-unused-variable";
+  }
+  
   $self->{optimize} = '-O3';
   
   return bless $self, $class;
