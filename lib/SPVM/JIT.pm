cae1b47d9ca252696752a0d57721baea3aa5fe85
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-05-22 07:39:56 +0900

    add SPVM::JIT

---
+++ lib/SPVM/JIT.pm
@@ -0,0 +1,34 @@
+package SPVM::JIT;
+
+use 5.008007;
+use strict;
+use warnings;
+
+use SPVM;
+
+sub import {
+  my ($class, $package_name) = @_;
+  
+  # Enable runtime JIT compile
+  $SPVM::JIT{$package_name} = 1;
+}
+
+1;
+
+=encoding UTF-8
+
+=head1 NAME
+
+SPVM::JIT - Compile SPVM module to machine code in runtime
+
+=head1 SYNOPSIS
+
+  use SPVM::JIT 'Point';
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2017-2018 by Yuki Kimoto
+
+MIT License
+
+=cut
