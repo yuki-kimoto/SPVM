e1e5782605bfc453d17b0a9c7a76f9f14a1ad203
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-05-23 16:58:23 +0900

    add get_basic_type_id

--- spvm_runtime_api.h
+++ spvm_runtime_api.h
@@ -79,6 +79,7 @@ int32_t SPVM_RUNTIME_API_get_sub_id(SPVM_API* api, const char* name);
 int32_t SPVM_RUNTIME_API_get_sub_id_interface_method(SPVM_API* api, SPVM_OBJECT* object, int32_t decl_sub_id);
 int32_t SPVM_RUNTIME_API_get_method_sub_id(SPVM_API* api, SPVM_OBJECT* object, const char* sub_name);
 int32_t SPVM_RUNTIME_API_get_class_method_sub_id(SPVM_API* api, const char* package_name, const char* sub_name);
+int32_t SPVM_RUNTIME_API_get_basic_type_id(SPVM_API* api, const char* name);
 
 SPVM_OBJECT* SPVM_RUNTIME_API_new_object(SPVM_API* api, int32_t package_id);
 
