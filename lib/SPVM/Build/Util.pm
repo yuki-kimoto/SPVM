f85d04593ce2036a876af38333722970837b0279
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-05-31 06:55:20 +0900

    add SPVM::Build::Util

---
+++ lib/SPVM/Build/Util.pm
@@ -0,0 +1,7 @@
+package SPVM::Build::Util;
+
+# SPVM::Build::tUtil is used from Makefile.PL and SPVM::Build, SPVM::Build::Precompile, SPVM::Build::Native
+# so this module must be wrote as pure per script, not contain XS and don't use any other SPVM modules.
+
+1;
+
