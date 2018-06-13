9c0d42196738595cf0656031de5f26b24a5e1830
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-06-13 09:01:24 +0900

    fix bug : [rt.cpan.org #125541] System perl used in build process
        I receive a advice
    
          "
           It seems that the system perl is used here (/usr/bin/perl),
           not the perl used for building the distribution. Typically this can
           be fixed by using something like $^X.
          "
    
        This bug is fixed by using $^X.

--- lib/SPVM/Build/Util.pm
+++ lib/SPVM/Build/Util.pm
@@ -120,7 +120,7 @@ sub create_package_make_rule {
   $make_rule
     .= "$shared_lib_file :: @deps\n\n";
   $make_rule
-    .= "\tperl -Mblib -MSPVM::Build -e \"SPVM::Build->new(build_dir => 'spvm_build')->create_shared_lib_${category}_dist('$package_name')\"\n\n";
+    .= "\t$^X -Mblib -MSPVM::Build -e \"SPVM::Build->new(build_dir => 'spvm_build')->create_shared_lib_${category}_dist('$package_name')\"\n\n";
   
   return $make_rule;
 }
