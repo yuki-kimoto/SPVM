ad38348b4a08a8a2a78695fa0a2f47c1836e8736
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-06-21 10:19:53 +0900

    cleanup

--- spvm_op.c
+++ spvm_op.c
@@ -1616,10 +1616,10 @@ SPVM_OP* SPVM_OP_build_package(SPVM_COMPILER* compiler, SPVM_OP* op_package, SPV
         compiler->fatal_error = 1;
       }
       else {
-        const char* package_var_name_abs = SPVM_OP_create_package_var_abs_name(compiler, package_name, package_var_name);
+        const char* package_var_abs_name = SPVM_OP_create_package_var_abs_name(compiler, package_name, package_var_name);
         our->id = compiler->op_ours->length;
         SPVM_LIST_push(compiler->op_ours, op_our);
-        SPVM_HASH_insert(compiler->op_our_symtable, package_var_name_abs, strlen(package_var_name_abs), op_our);
+        SPVM_HASH_insert(compiler->op_our_symtable, package_var_abs_name, strlen(package_var_abs_name), op_our);
 
         SPVM_HASH_insert(package->op_our_symtable, package_var_name, strlen(package_var_name), op_our);
         
