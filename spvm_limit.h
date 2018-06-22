df815580c6ce92d8e1187515b290a9581b4abfcd
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-06-23 07:54:39 +0900

    cleanup limit

--- spvm_limit.h
+++ spvm_limit.h
@@ -4,12 +4,6 @@
 #include "spvm_base.h"
 
 enum {
-  /* Type dimension limit */
-  SPVM_LIMIT_C_DIMENSION = UINT8_MAX,
-  
-  /* Basic type name length limit */
-  SPVM_LIMIT_C_BASIC_TYPE_NAME = INT16_MAX,
-
   // OP code operand max value
   SPVM_LIMIT_C_OPCODE_OPERAND_VALUE_MAX = UINT16_MAX,
 };
