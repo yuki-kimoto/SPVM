5c4ae9044c1cb8dd658be21f1589a640a521bbb4
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-06-30 09:17:27 +0900

    check set filed undef in prcompile

--- spvm_csource_builder.c
+++ spvm_csource_builder.c
@@ -1898,23 +1898,44 @@ void SPVM_CSOURCE_BUILDER_build_sub_implementation(SPVM_COMPILER* compiler, SPVM
       {
         int32_t rel_id = opcode->operand1;
         SPVM_OP* op_field_access = SPVM_LIST_fetch(package->op_field_accesses, rel_id);
+        SPVM_FIELD* field = op_field_access->uv.field_access->field;
+        const char* package_name = field->op_package->uv.package->op_name->uv.name;
+        const char* field_signature = field->signature;
         int32_t field_index = op_field_access->uv.field_access->field->index;
 
-        SPVM_STRING_BUFFER_add(string_buffer, "  if (__builtin_expect(");
+        SPVM_STRING_BUFFER_add(string_buffer, "  {\n");
+        SPVM_STRING_BUFFER_add(string_buffer, "    int32_t field_index = -1;\n");
+        SPVM_STRING_BUFFER_add(string_buffer, "    if (field_index == -1) { field_index = env->get_field_index(env, \"");
+        SPVM_STRING_BUFFER_add(string_buffer, (char*)package_name);
+        SPVM_STRING_BUFFER_add(string_buffer, "\", \"");
+        SPVM_STRING_BUFFER_add(string_buffer, (char*)field_signature);
+        SPVM_STRING_BUFFER_add(string_buffer, "\"); }\n");
+
+        SPVM_STRING_BUFFER_add(string_buffer, "    if (field_index < 0) {\n");
+        SPVM_STRING_BUFFER_add(string_buffer, "      void* exception = env->new_string_raw(env, \"Field not found ");
+        SPVM_STRING_BUFFER_add(string_buffer, (char*)package_name);
+        SPVM_STRING_BUFFER_add(string_buffer, " ");
+        SPVM_STRING_BUFFER_add(string_buffer, (char*)field_signature);
+        SPVM_STRING_BUFFER_add(string_buffer, "\", 0);\n");
+        SPVM_STRING_BUFFER_add(string_buffer, "      env->set_exception(env, exception);\n");
+        SPVM_STRING_BUFFER_add(string_buffer, "      return SPVM_EXCEPTION;\n");
+        SPVM_STRING_BUFFER_add(string_buffer, "    }\n");
+        SPVM_STRING_BUFFER_add(string_buffer, "    if (__builtin_expect(");
         SPVM_CSOURCE_BUILDER_add_operand(string_buffer, "void*", opcode->operand0);
         SPVM_STRING_BUFFER_add(string_buffer, " == NULL, 0)) {\n");
         SPVM_STRING_BUFFER_add(string_buffer, "    env->set_exception(env, env->new_string_raw(env, \"Object must be not undef.\", 0));\n");
-        SPVM_STRING_BUFFER_add(string_buffer, "    exception_flag = 1;\n");
-        SPVM_STRING_BUFFER_add(string_buffer, "  }\n");
-        SPVM_STRING_BUFFER_add(string_buffer, "  else {\n");
-        SPVM_STRING_BUFFER_add(string_buffer, "    SPVM_RUNTIME_C_INLINE_OBJECT_ASSIGN(\n");
-        SPVM_STRING_BUFFER_add(string_buffer, "      (void**)((intptr_t)");
+        SPVM_STRING_BUFFER_add(string_buffer, "      exception_flag = 1;\n");
+        SPVM_STRING_BUFFER_add(string_buffer, "    }\n");
+        SPVM_STRING_BUFFER_add(string_buffer, "    else {\n");
+        SPVM_STRING_BUFFER_add(string_buffer, "      SPVM_RUNTIME_C_INLINE_OBJECT_ASSIGN(\n");
+        SPVM_STRING_BUFFER_add(string_buffer, "        (void**)((intptr_t)");
         SPVM_CSOURCE_BUILDER_add_operand(string_buffer, "void*", opcode->operand0);
         SPVM_STRING_BUFFER_add(string_buffer, " + (intptr_t)env->object_header_byte_size + sizeof(SPVM_VALUE) * ");
         SPVM_STRING_BUFFER_add_int(string_buffer, field_index);
         SPVM_STRING_BUFFER_add(string_buffer, "),\n");
-        SPVM_STRING_BUFFER_add(string_buffer, "      NULL");
-        SPVM_STRING_BUFFER_add(string_buffer, "    );\n");
+        SPVM_STRING_BUFFER_add(string_buffer, "        NULL");
+        SPVM_STRING_BUFFER_add(string_buffer, "      );\n");
+        SPVM_STRING_BUFFER_add(string_buffer, "    }\n");
         SPVM_STRING_BUFFER_add(string_buffer, "  }\n");
         
         break;
