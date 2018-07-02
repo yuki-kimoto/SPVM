e5d375c11e4daa923d7c4cd7b9c4c8ecda5b1ad6
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-07-02 17:48:56 +0900

    use more cached field index

--- spvm_csource_builder.c
+++ spvm_csource_builder.c
@@ -1958,31 +1958,14 @@ void SPVM_CSOURCE_BUILDER_build_sub_implementation(SPVM_COMPILER* compiler, SPVM
         int32_t rel_id = opcode->operand1;
         SPVM_OP* op_field_access = SPVM_LIST_fetch(sub->op_field_accesses, rel_id);
         SPVM_FIELD* field = op_field_access->uv.field_access->field;
-        const char* package_name = field->op_package->uv.package->op_name->uv.name;
-        const char* field_signature = field->signature;
+        const char* field_name = field->op_name->uv.name;
 
         SPVM_STRING_BUFFER_add(string_buffer, "  {\n");
-        SPVM_STRING_BUFFER_add(string_buffer, "    int32_t field_index = -1;\n");
-        SPVM_STRING_BUFFER_add(string_buffer, "    if (field_index < 0) {\n");
-        SPVM_STRING_BUFFER_add(string_buffer, "      field_index = env->get_field_index(env, \"");
-        SPVM_STRING_BUFFER_add(string_buffer, (char*)package_name);
-        SPVM_STRING_BUFFER_add(string_buffer, "\", \"");
-        SPVM_STRING_BUFFER_add(string_buffer, (char*)field_signature);
-        SPVM_STRING_BUFFER_add(string_buffer, "\");\n");
-        SPVM_STRING_BUFFER_add(string_buffer, "      if (field_index < 0) {\n");
-        SPVM_STRING_BUFFER_add(string_buffer, "        void* exception = env->new_string_raw(env, \"Field not found ");
-        SPVM_STRING_BUFFER_add(string_buffer, (char*)package_name);
-        SPVM_STRING_BUFFER_add(string_buffer, " ");
-        SPVM_STRING_BUFFER_add(string_buffer, (char*)field_signature);
-        SPVM_STRING_BUFFER_add(string_buffer, "\", 0);\n");
-        SPVM_STRING_BUFFER_add(string_buffer, "        env->set_exception(env, exception);\n");
-        SPVM_STRING_BUFFER_add(string_buffer, "        return SPVM_EXCEPTION;\n");
-        SPVM_STRING_BUFFER_add(string_buffer, "      }\n");
-        SPVM_STRING_BUFFER_add(string_buffer, "    }\n");
-
         SPVM_STRING_BUFFER_add(string_buffer, "    env->weaken_object_field(env, ");
         SPVM_CSOURCE_BUILDER_add_operand(string_buffer, "void*", opcode->operand0);
-        SPVM_STRING_BUFFER_add(string_buffer, ", field_index);\n");
+        SPVM_STRING_BUFFER_add(string_buffer, ", field_index_");
+        SPVM_STRING_BUFFER_add(string_buffer, (char*)field_name);
+        SPVM_STRING_BUFFER_add(string_buffer, ");\n");
         SPVM_STRING_BUFFER_add(string_buffer, "    if (env->get_exception(env)) {\n");
         SPVM_STRING_BUFFER_add(string_buffer, "      exception_flag = 1;\n");
         SPVM_STRING_BUFFER_add(string_buffer, "    }\n");
