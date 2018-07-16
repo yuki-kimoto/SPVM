d7238f2240cd2d154b90b4945dc6cf2c772739c7
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-07-16 15:16:12 +0900

    cleanup

--- lib/SPVM/Build/src/spvm_csource_builder.c
+++ lib/SPVM/Build/src/spvm_csource_builder.c
@@ -1726,9 +1726,10 @@ void SPVM_CSOURCE_BUILDER_build_sub_implementation(SPVM_COMPILER* compiler, SPVM
         SPVM_STRING_BUFFER_add(string_buffer, "        exception_flag = 1;\n");
         SPVM_STRING_BUFFER_add(string_buffer, "      } \n");
         SPVM_STRING_BUFFER_add(string_buffer, "      else { \n");
+        SPVM_STRING_BUFFER_add(string_buffer, "        void* object = *(void**)((intptr_t)array + (intptr_t)env->object_header_byte_size + sizeof(void*) * index);\n");
         SPVM_STRING_BUFFER_add(string_buffer, "        SPVM_RUNTIME_C_INLINE_OBJECT_ASSIGN(&");
         SPVM_CSOURCE_BUILDER_add_operand(string_buffer, "void*", opcode->operand0);
-        SPVM_STRING_BUFFER_add(string_buffer, ", *(void**)((intptr_t)array + (intptr_t)env->object_header_byte_size + sizeof(void*) * index)); \n");
+        SPVM_STRING_BUFFER_add(string_buffer, ", object); \n");
         SPVM_STRING_BUFFER_add(string_buffer, "      } \n");
         SPVM_STRING_BUFFER_add(string_buffer, "    } \n");
         SPVM_STRING_BUFFER_add(string_buffer, "  } \n");
