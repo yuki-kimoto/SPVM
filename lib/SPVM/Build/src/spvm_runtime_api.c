d920dd52a39d2588eedc26f93a76eafe35a8eefb
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-07-16 09:13:25 +0900

    add int array body

--- lib/SPVM/Build/src/spvm_runtime_api.c
+++ lib/SPVM/Build/src/spvm_runtime_api.c
@@ -614,8 +614,14 @@ SPVM_OBJECT* SPVM_RUNTIME_API_new_int_array_raw(SPVM_ENV* env, int32_t length) {
   (void)env;
   SPVM_RUNTIME* runtime = SPVM_RUNTIME_API_get_runtime();
 
-  int64_t array_byte_size = (int64_t)(intptr_t)env->object_header_byte_size + ((int64_t)length) * (int64_t)sizeof(int32_t);
+  int64_t array_byte_size = (int64_t)(intptr_t)env->object_header_byte_size + ((int64_t)length) * (int64_t)sizeof(SPVM_VALUE_int);
   SPVM_OBJECT* object = SPVM_RUNTIME_ALLOCATOR_alloc_memory_block_zero(runtime, array_byte_size);
+  
+  if (length > 0) {
+    SPVM_VALUE_int* body = SPVM_RUNTIME_ALLOCATOR_alloc_memory_block_zero(runtime, length * sizeof(SPVM_VALUE_int));
+    *(SPVM_VALUE_int**)&object->body = body;
+    SPVM_RUNTIME_ALLOCATOR_free_memory_block(runtime, body);
+  }
   
   object->dimension = 1;
   object->basic_type_id = SPVM_BASIC_TYPE_C_ID_INT;
