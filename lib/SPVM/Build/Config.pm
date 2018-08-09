f681c469eabb3e3af8adb377ace64493271adc9e
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-08-09 12:48:28 +0900

    add some SPVM::Build::Config document

--- lib/SPVM/Build/Config.pm
+++ lib/SPVM/Build/Config.pm
@@ -28,6 +28,20 @@ sub to_hash {
   return $hash_config;
 }
 
+sub get_config {
+  my ($self, $name) = @_;
+  
+  return $self->{config}{$name};
+}
+
+sub set_config {
+  my ($self, $name, $value) = @_;
+  
+  $self->{config}{$name} = $value;
+  
+  return $self;
+}
+
 sub set_ccflags {
   my ($self, $ccflags) = @_;
   
@@ -50,20 +64,6 @@ sub add_ccflags {
   $ccflags .= " $new_ccflags";
   
   $self->set_config('ccflags' => $ccflags);
-  
-  return $self;
-}
-
-sub get_config {
-  my ($self, $name) = @_;
-  
-  return $self->{config}{$name};
-}
-
-sub set_config {
-  my ($self, $name, $value) = @_;
-  
-  $self->{config}{$name} = $value;
   
   return $self;
 }
@@ -93,7 +93,7 @@ sub set_cc {
 sub get_cc {
   my ($self, $cc) = @_;
   
-  return $self->get_config(cc => $cc);
+  return $self->get_config('cc');
 }
 
 sub set_optimize {
@@ -105,7 +105,7 @@ sub set_optimize {
 sub get_optimize {
   my ($self, $optimize) = @_;
   
-  return $self->get_config(optimize => $optimize);
+  return $self->get_config('optimize');
 }
 
 sub set_ld {
@@ -117,7 +117,7 @@ sub set_ld {
 sub get_ld {
   my ($self, $ld) = @_;
   
-  return $self->get_config(ld => $ld);
+  return $self->get_config('ld');
 }
 
 sub set_ldflags {
@@ -146,34 +146,141 @@ sub add_ldflags {
   return $self;
 }
 
-=pod
- Compiler:
-    cc='cc', ccflags ='-fno-strict-aliasing -pipe -fstack-protector -I/usr/local/include -D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64',
-    optimize='-O2',
-    cppflags='-fno-strict-aliasing -pipe -fstack-protector -I/usr/local/include'
-    ccversion='', gccversion='4.1.2 20080704 (Red Hat 4.1.2-52)', gccosandvers=''
-    intsize=4, longsize=8, ptrsize=8, doublesize=8, byteorder=12345678
-    d_longlong=define, longlongsize=8, d_longdbl=define, longdblsize=16
-    ivtype='long', ivsize=8, nvtype='double', nvsize=8, Off_t='off_t', lseeksize=8
-    alignbytes=8, prototype=define
-    
-  Linker and Libraries:
-    ld='cc', ldflags =' -fstack-protector -L/usr/local/lib'
-    libpth=/usr/include/oracle/11.2/client64 /usr/local/lib /usr/lib /lib /lib64 /usr/lib64 /usr/local/lib64
-    libs=-lnsl -ldl -lm -lcrypt -lutil -lc
-    perllibs=-lnsl -ldl -lm -lcrypt -lutil -lc
-    libc=libc-2.5.so, so=so, useshrplib=false, libperl=libperl.a
-    gnulibc_version='2.5'
-=cut
-
-
 =head1 NAME
 
-SPVM::Build::Config;
+SPVM::Build::Config - build config
 
 =head1 DESCRIPTION
 
-L<ExtUtils::CBuilder> config of C<new>, C<compile>, C<link> methods.
+L<SPVM::Build::Config> is configuration of c/c++ compile and link.
+
+=head1 Methods
+
+=head2 new
+
+  my $build_config = SPVM::Build::Config->new;
+  
+Create L<SPVM::Build::Config> object.
+
+=head2 replace_all_config
+
+  my $config = {cc => 'g++', ld => 'g++'};
+  $build_config->replace_all_config($config);
+
+Replace all config.
+
+All of old configs is removed and added new config.
+
+=head2 to_hash
+
+  my $config = $build_config->to_hash;
+
+Convert configs to hash reference.
+
+=head2 get_config
+
+  my $cc = $build_config->get_config('cc');
+
+Get a config value.
+
+=head2 set_config
+
+  $build_config->set_config(cc => $cc);
+
+Set a config value.
+
+=head2 set_ccflags
+
+  $build_config->set_ccflags($ccflags);
+
+Set C<ccflags>.
+
+=head2 get_ccflags
+
+  my $ccflags = $build_config->get_ccflags;
+
+Get C<ccflags>.
+
+sub add_ccflags {
+  my ($self, $new_ccflags) = @_;
+  
+  my $ccflags = $build_config->get_config('ccflags');
+  
+  $ccflags .= " $new_ccflags";
+  
+  $build_config->set_config('ccflags' => $ccflags);
+  
+  return $self;
+}
+
+sub set_std {
+  my ($self, $spec) = @_;
+  
+  my $ccflags = $build_config->get_ccflags;
+  
+  # Remove -std=foo section
+  $ccflags =~ s/-std=[^ ]+//g;
+  
+  $ccflags .= " -std=$spec";
+  
+  # Add -std=foo section
+  $build_config->set_ccflags($ccflags);
+  
+  return $self;
+}
+
+sub set_cc {
+  my ($self, $cc) = @_;
+  
+  return $build_config->set_config(cc => $cc);
+}
+
+sub get_cc {
+  my ($self, $cc) = @_;
+  
+  return $build_config->get_config(cc => $cc);
+}
+
+sub set_optimize {
+  my ($self, $optimize) = @_;
+  
+  return $build_config->set_config(optimize => $optimize);
+}
+
+sub get_optimize {
+  my ($self, $optimize) = @_;
+  
+  return $build_config->get_config(optimize => $optimize);
+}
+
+sub set_ld {
+  my ($self, $ld) = @_;
+  
+  return $build_config->set_config(ld => $ld);
+}
+
+sub get_ld {
+  my ($self, $ld) = @_;
+  
+  return $build_config->get_config(ld => $ld);
+}
+
+sub set_ldflags {
+  my ($self, $ldflags) = @_;
+  
+  $build_config->set_config(ldflags => $ldflags);
+  
+  return $self;
+}
+
+sub get_ldflags {
+  my $self = shift;
+  
+  return $build_config->get_config('ldflags');
+}
+
+sub add_ldflags {
+
 
 =cut
 
