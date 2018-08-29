e06da0a96a36814ac85768eb26503a73f757a8d0
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-08-29 09:17:17 +0900

    add sub->info_long_pool_ids sub->info_string_pool_ids

--- lib/SPVM/Build/src/spvm_sub.c
+++ lib/SPVM/Build/src/spvm_sub.c
@@ -24,6 +24,9 @@ SPVM_SUB* SPVM_SUB_new(SPVM_COMPILER* compiler) {
   sub->info_types = SPVM_COMPILER_ALLOCATOR_alloc_list(compiler, 0);
   sub->info_constants = SPVM_COMPILER_ALLOCATOR_alloc_list(compiler, 0);
   sub->info_switch_infos = SPVM_COMPILER_ALLOCATOR_alloc_list(compiler, 0);
+
+  sub->info_long_pool_ids = SPVM_COMPILER_ALLOCATOR_alloc_list(compiler, 0);
+  sub->info_string_pool_ids = SPVM_COMPILER_ALLOCATOR_alloc_list(compiler, 0);
   
   return sub;
 }
