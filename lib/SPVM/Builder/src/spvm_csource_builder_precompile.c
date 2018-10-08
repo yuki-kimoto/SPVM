8f133c45800ac322fe05578cca3a54cf5a6b5cf6
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-10-08 10:35:20 +0900

    cleanup

--- lib/SPVM/Builder/src/spvm_csource_builder_precompile.c
+++ lib/SPVM/Builder/src/spvm_csource_builder_precompile.c
@@ -3265,15 +3265,6 @@ void SPVM_CSOURCE_BUILDER_PRECOMPILE_build_sub_implementation(SPVM_ENV* env, SPV
             SPVM_STRING_BUFFER_add(string_buffer, ";\n");
             break;
           }
-          case SPVM_OPCODE_C_ID_CALL_SUB:
-          case SPVM_OPCODE_C_ID_CALL_METHOD: {
-            SPVM_STRING_BUFFER_add(string_buffer, "      ");
-            SPVM_CSOURCE_BUILDER_PRECOMPILE_add_operand(env, string_buffer, SPVM_CSOURCE_BUILDER_PRECOMPILE_C_CTYPE_ID_INT, var_id);
-            SPVM_STRING_BUFFER_add(string_buffer, " = ");
-            SPVM_CSOURCE_BUILDER_PRECOMPILE_add_stack(env, string_buffer, SPVM_CSOURCE_BUILDER_PRECOMPILE_C_CTYPE_ID_INT, 0);
-            SPVM_STRING_BUFFER_add(string_buffer, ";\n");
-            break;
-          }
           case SPVM_OPCODE_C_ID_CALL_SUB_INT_TMP:
           case SPVM_OPCODE_C_ID_CALL_METHOD_INT_TMP: {
             SPVM_STRING_BUFFER_add(string_buffer, "      ");
