e511035932d3260bf921c17728418b98218cdd10
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-10-16 12:50:59 +0900

    reduce sub and my memory size

--- lib/SPVM/Builder/include/spvm_runtime_info_type.h
+++ lib/SPVM/Builder/include/spvm_runtime_info_type.h
@@ -6,8 +6,8 @@
 // Field information
 struct SPVM_runtime_info_type {
   int32_t basic_type_id;
-  int32_t dimension;
-  int32_t flag;
+  uint16_t flag;
+  uint8_t dimension;
 };
 
 SPVM_RUNTIME_INFO_TYPE* SPVM_RUNTIME_INFO_TYPE_new();
--- lib/SPVM/Builder/include/spvm_runtime_my.h
+++ lib/SPVM/Builder/include/spvm_runtime_my.h
@@ -6,10 +6,10 @@
 // Field information
 struct SPVM_runtime_my {
   int32_t basic_type_id;
-  int32_t type_dimension;
-  int32_t type_flag;
-  int32_t var_id;
-  int32_t value_field_basic_type_id;
+  uint16_t var_id;
+  uint16_t type_flag;
+  uint8_t type_dimension;
+  uint8_t value_field_basic_type_id;
 };
 
 SPVM_RUNTIME_MY* SPVM_RUNTIME_MY_new();
