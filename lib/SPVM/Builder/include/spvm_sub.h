887bdacd760def967d488d07f0c98ae1ad88367b
Yuki Kimoto <kimoto.yuki@gmail.com>
2018-11-01 12:30:15 +0900

    add have_vaarg to sub

--- lib/SPVM/Builder/include/spvm_sub.h
+++ lib/SPVM/Builder/include/spvm_sub.h
@@ -60,6 +60,7 @@ struct SPVM_sub {
   int32_t object_vars_alloc_length;
   int32_t ref_vars_alloc_length;
   int32_t return_runtime_type;
+  int8_t have_vaarg;
 };
 
 SPVM_SUB* SPVM_SUB_new(SPVM_COMPILER* compiler);
