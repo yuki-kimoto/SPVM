f7a8d3878524e33b9b415b23ef0009c0ea60989f
Yuki Kimoto <kimoto.yuki@gmail.com>
2019-03-08 13:19:59 +0900

    in_addr become int

--- lib/SPVM/sockaddr_in.c
+++ lib/SPVM/sockaddr_in.c
@@ -34,9 +34,9 @@ int32_t SPNATIVE__SPVM__sockaddr_in__sin_family(SPVM_ENV* env, SPVM_VALUE* stack
   
   struct sockaddr_in* sin = env->pointer(env, obj_sin);
   
-  int32_t sin_family = sin->sin_family;
+  int8_t sin_family = sin->sin_family;
   
-  stack[0].ival = sin_family;
+  stack[0].bval = sin_family;
   
   return SPVM_SUCCESS;
 }
@@ -44,7 +44,7 @@ int32_t SPNATIVE__SPVM__sockaddr_in__sin_family(SPVM_ENV* env, SPVM_VALUE* stack
 int32_t SPNATIVE__SPVM__sockaddr_in__set_sin_port(SPVM_ENV* env, SPVM_VALUE* stack) {
   void* obj_sin = stack[0].oval;
   
-  int32_t sin_port = stack[1].sval;
+  int16_t sin_port = stack[1].sval;
   
   struct sockaddr_in* sin = env->pointer(env, obj_sin);
   
@@ -58,7 +58,7 @@ int32_t SPNATIVE__SPVM__sockaddr_in__sin_port(SPVM_ENV* env, SPVM_VALUE* stack) 
   
   struct sockaddr_in* sin = env->pointer(env, obj_sin);
   
-  int32_t sin_port = sin->sin_port;
+  int16_t sin_port = sin->sin_port;
   
   stack[0].sval = sin_port;
   
@@ -68,34 +68,21 @@ int32_t SPNATIVE__SPVM__sockaddr_in__sin_port(SPVM_ENV* env, SPVM_VALUE* stack) 
 int32_t SPNATIVE__SPVM__sockaddr_in__set_sin_addr(SPVM_ENV* env, SPVM_VALUE* stack) {
   void* obj_sin = stack[0].oval;
   
-  void* obj_sin_addr_arg = stack[1].oval;
+  int32_t sin_addr = stack[1].ival;
   
   struct sockaddr_in* sin = env->pointer(env, obj_sin);
-
-  struct in_addr* sin_addr_arg = env->pointer(env, obj_sin_addr_arg);
-
-  sin->sin_addr = *sin_addr_arg;
+  
+  sin->sin_addr.s_addr = sin_addr;
   
   return SPVM_SUCCESS;
 }
 
 int32_t SPNATIVE__SPVM__sockaddr_in__sin_addr(SPVM_ENV* env, SPVM_VALUE* stack) {
-  void* obj_sockin = stack[0].oval;
+  void* obj_sin = stack[0].oval;
   
-  struct sockaddr_in* sockin = env->pointer(env, obj_sockin);
+  struct sockaddr_in* sin = env->pointer(env, obj_sin);
   
-  int32_t in_addr_basic_type_id = env->basic_type_id(env, "SPVM::in_addr");
-  assert(in_addr_basic_type_id >= 0);
-  
-  void* obj_sin_addr = env->new_obj(env, in_addr_basic_type_id);
-  
-  struct in_addr* sin_addr = env->alloc_memory_block_zero(env, sizeof(struct in_addr));
-  
-  *sin_addr = sockin->sin_addr;
-  
-  env->set_pointer(env, obj_sin_addr, sin_addr);
-  
-  stack[0].oval = obj_sin_addr;
+  stack[0].ival = sin->sin_addr.s_addr;
   
   return SPVM_SUCCESS;
 }
