12267bbb4b6d2b1aaf3d33d11e86b1206c74a2c0
motxx <motohiko.ave@gmail.com>
2019-03-26 03:37:10 +0900

    add SPVM::Time::Format->time_to_str

--- lib/SPVM/Time/Format.c
+++ lib/SPVM/Time/Format.c
@@ -1,6 +1,8 @@
 #include "spvm_native.h"
 
+#include <memory.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 
 int32_t SPNATIVE__SPVM__Time__Format__epoch_by_strptime(SPVM_ENV* env, SPVM_VALUE* stack) {
@@ -13,6 +15,34 @@ int32_t SPNATIVE__SPVM__Time__Format__epoch_by_strptime(SPVM_ENV* env, SPVM_VALU
   }
 
   stack[0].lval = mktime(&tm);
+
+  return SPVM_SUCCESS;
+}
+
+int32_t SPNATIVE__SPVM__Time__Format__strftime(SPVM_ENV* env, SPVM_VALUE* stack) {
+  const char* format = (const char*)env->belems(env, stack[0].oval);
+  time_t epoch = stack[1].lval;
+
+  struct tm dt;
+  gmtime_r(&epoch, &dt); // FIXME: only support UTC for now
+
+  int32_t capacity = 100;
+  void* obuffer = env->new_barray_raw(env, capacity);
+  env->inc_ref_count(env, obuffer);
+  char* buffer = (char *)(env->belems(env, obuffer));
+
+  if (!strftime(buffer, capacity, format, &dt)) {
+    env->dec_ref_count(env, obuffer);
+    SPVM_CROAK("Can't write as format", "SPVM/Time/Format.c", __LINE__);
+  }
+
+  int32_t length = sprintf(buffer, "%s", buffer);
+  void* oline = env->new_barray_raw(env, length);
+  int8_t* line = env->belems(env, oline);
+  memcpy(line, buffer, length);
+
+  env->dec_ref_count(env, obuffer);
+  stack[0].oval = oline;
 
   return SPVM_SUCCESS;
 }
