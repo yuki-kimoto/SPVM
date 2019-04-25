ab320cf9cf4b9a5e02596e4b4029be3306cf6dda
Yuki Kimoto <kimoto.yuki@gmail.com>
2019-04-25 12:18:24 +0900

    add SPVM::Time::Info

--- lib/SPVM/Time.c
+++ lib/SPVM/Time.c
@@ -13,3 +13,18 @@ int32_t SPNATIVE__SPVM__Time__time(SPVM_ENV* env, SPVM_VALUE* stack) {
   return SPVM_SUCCESS;
 }
 
+/*
+int32_t SPNATIVE__SPVM__Time__localtime(SPVM_ENV* env, SPVM_VALUE* stack) {
+
+  time_t time;
+  struct tm resultp;
+
+#ifdef __CYGWIN__ || __MINGW32__
+  localtime_s(&time, &resultp);
+#else
+  localtime_r(&time, &resultp);
+#endif
+
+  return SPVM_SUCCESS;
+}
+*/
