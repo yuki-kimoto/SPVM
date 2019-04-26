39bbe57a2f7465698493a0b22ef16f24d46ba2de
Yuki Kimoto <kimoto.yuki@gmail.com>
2019-04-27 07:57:17 +0900

    remove gets_chomp

--- lib/SPVM/CORE.c
+++ lib/SPVM/CORE.c
@@ -20,9 +20,11 @@ static const char* MFILE = "SPVM/CORE.c";
 int32_t SPNATIVE__SPVM__CORE__chomp(SPVM_ENV* env, SPVM_VALUE* stack) {
 
   void* obj_str = stack[0].oval;
-  if (!obj_str) { SPVM_DIE("String must be defined", MFILE, __LINE__); }
+  if (!obj_str) {
+    return SPVM_SUCCESS;
+  }
   char* str = (char*)env->belems(env, obj_str);
-  int32_t len = env->len(env, str);
+  int32_t len = env->len(env, obj_str);
   
   if (len == 0) {
     return SPVM_SUCCESS;
@@ -31,6 +33,7 @@ int32_t SPNATIVE__SPVM__CORE__chomp(SPVM_ENV* env, SPVM_VALUE* stack) {
   if (str[len - 1] != '\n') {
     return SPVM_SUCCESS;
   }
+
   
   str[len - 1] = '\0';
   *(int32_t*)((intptr_t)obj_str + (intptr_t)env->object_length_offset) = len - 1;
