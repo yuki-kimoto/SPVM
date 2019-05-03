4cbbdaae6d2d366499b7f8c406f9fea3fd7fb01c
Yuki Kimoto <kimoto.yuki@gmail.com>
2019-05-03 17:48:14 +0900

    implement some of SPVM::IO::Select

--- lib/SPVM/IO/Select.c
+++ lib/SPVM/IO/Select.c
@@ -156,38 +156,3 @@ int32_t SPNATIVE__SPVM__IO__Select___has_exception(SPVM_ENV* env, SPVM_VALUE* st
   
   return SPVM_SUCCESS;
 }
-
-/*
-sub can_read
-{
- my $vec = shift;
- my $timeout = shift;
- my $r = $vec->[VEC_BITS];
- 
- defined($r) && (select($r,undef,undef,$timeout) > 0)
-    ? handles($vec, $r)
-    : ();
-}
- 
-sub can_write
-{
- my $vec = shift;
- my $timeout = shift;
- my $w = $vec->[VEC_BITS];
- 
- defined($w) && (select(undef,$w,undef,$timeout) > 0)
-    ? handles($vec, $w)
-    : ();
-}
- 
-sub has_exception
-{
- my $vec = shift;
- my $timeout = shift;
- my $e = $vec->[VEC_BITS];
- 
- defined($e) && (select(undef,undef,$e,$timeout) > 0)
-    ? handles($vec, $e)
-    : ();
-}
-*/
