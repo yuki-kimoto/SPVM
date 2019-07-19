fa88f7168c81d41a4ff196651e222bf42d702782
Yuki Kimoto <kimoto.yuki@gmail.com>
2019-07-19 13:07:52 +0900

    add Base64 doc

---
+++ lib/SPVM/MIME/Base64.pm
@@ -0,0 +1,37 @@
+package SPVM::MIME::Base64;
+
+1;
+
+=head1 NAME
+
+SPVM::MIME::Base64 - Base64 encode/decode
+
+=head1 SYNOPSYS
+
+  my $b64 = SPVM::MIME::Base64->new;
+  my $text = $b64->encode($binary);
+  my $binary = $b64->decode($text);
+
+=head1 DESCRIPTION
+
+Base64 encode/decode
+
+=head1 METHODS
+
+=head2 new
+
+  sub new : SPVM::MIME::Base64 ()
+
+Create new L<SPVM::MIME::Base64> object.
+
+=head2 encode
+
+  sub encode : string ($self : self, $binary : string)
+
+Encode binary data to base64 text.
+
+=head2 decode
+
+  sub decode : string ($self : self, $base64_text : string)
+
+Decode encoded Base64 text to binary data.
