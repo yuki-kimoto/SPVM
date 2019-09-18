c334bb286bf42dfccf2be60f4329823373d4a688
Yuki Kimoto <kimoto.yuki@gmail.com>
2019-09-18 15:36:52 +0900

    add SPVM::Math doc#

--- lib/SPVM/Math.pm
+++ lib/SPVM/Math.pm
@@ -6,82 +6,201 @@ use SPVM 'SPVM::Math';
 
 =head1 NAME
 
-SPVM::Math - Error number
+SPVM::Math - Mathmatical functions
 
 =head1 SYNOPSYS
 
   use SPVM::Math;
   
-  my $sin = SPVM::Math->sin(
+  my $pi = SPVM::Math->pi;
+  my $sin = SPVM::Math->sin($pi / 4);
   
 =head1 DESCRIPTION
 
-L<SPVM::Math> gets and sets a error number, and L<SPVM::Math> defines various error numbers.
+L<SPVM::Math> defines mathmatical functions. 
 
-  native sub pi : double();
+=head1 CLASS METHODS
 
-  native sub INFINITYF : float ();
-  native sub NANF : float ();
-  
-  native sub isinff : int($x : float);
-  native sub isfinitef : int($x : float);
-  native sub isnanf : int ($x : float);
-  
-  native sub INFINITY : double ();
-  native sub NAN : double ();
-  
-  native sub isinf : int ($x : double);
-  native sub isfinite : int ($x : double);
-  native sub isnan : int ($x : double);
+=head2 pi
 
-  
-  # Trigonometric functions
-  native sub acos : double ($x : double);
-  native sub asin : double($x : double);
-  native sub atan : double ($x : double);
-  native sub atan2 : double ($y : double, $x : double);
-  native sub cos : double ($x : double);
-  native sub sin : double ($x : double);
-  native sub tan : double ($x : double);
-  
-  # Hyperbolic functions
-  native sub acosh : double ($x : double);
-  native sub asinh : double ($x : double);
-  native sub atanh : double ($x : double);
-  native sub cosh : double ($x : double);
-  native sub sinh : double ($x : double);
-  native sub tanh : double ($x : double);
-  
-  # Exponential functions and logarithmic functions
-  native sub exp : double ($x : double);
-  native sub exp2 : double ($x : double);
-  native sub expm1 : double ($x : double);
-  native sub log : double ($x : double);
-  native sub log10 : double ($x : double);
-  native sub log1p : double ($x : double);
-  
-  # Exponentiation functions and Absolute value functions
-  native sub cbrt : double ($x : double);
-  native sub fabs : double ($x : double);
-  native sub hypot : double ($x : double, $y : double);
-  native sub pow : double ($x : double, $y : double);
-  native sub sqrt : double ($x : double);
-  
-  # Error function and Gamma function
-  native sub erf : double ($x : double);
-  native sub erfc : double ($x : double);
-  native sub lgamma : double ($x : double);
-  native sub tgamma : double ($x : double);
+  sub pi : double()
 
-  # Nearest integer function
-  native sub ceil : double ($x : double);
-  native sub floor : double ($x : double);
-  native sub nearbyint : double ($x : double);
-  native sub round : double ($x : double);
-  
-  # Surplus functions
-  native sub remainder : double ($x1 : double, $x2 : double);
+=head2 INFINITYF
 
-  native sub signbitf : int ($num : float);
-  native sub signbit : int ($num : double);
+  sub INFINITYF : float ()
 
+=head2 NANF
+
+  sub NANF : float ()
+
+=head2 isinff
+
+  sub isinff : int($x : float)
+
+=head2 isfinitef
+
+  sub isfinitef : int($x : float)
+
+=head2 isnanf
+
+  sub isnanf : int ($x : float)
+
+=head2 INFINITY
+
+  sub INFINITY : double ()
+
+=head2 NAN
+
+  sub NAN : double ()
+
+=head2 isinf
+
+  sub isinf : int ($x : double)
+
+=head2 isfinite
+
+  sub isfinite : int ($x : double)
+
+=head2 isnan
+
+  sub isnan : int ($x : double)
+
+=head2 acos
+
+  sub acos : double ($x : double)
+
+=head2 asin
+
+  sub asin : double($x : double)
+
+=head2 atan
+
+  sub atan : double ($x : double)
+
+=head2 atan2
+
+  sub atan2 : double ($y : double, $x : double)
+
+=head2 cos
+
+  sub cos : double ($x : double)
+
+=head2 sin
+
+  sub sin : double ($x : double)
+
+=head2 tan
+
+  sub tan : double ($x : double)
+
+=head2 acosh
+
+  sub acosh : double ($x : double)
+
+=head2 asinh
+
+  sub asinh : double ($x : double)
+
+=head2 atanh
+
+  sub atanh : double ($x : double)
+
+=head2 cosh
+
+  sub cosh : double ($x : double)
+
+=head2 sinh
+
+  sub sinh : double ($x : double)
+
+=head2 tanh
+
+  sub tanh : double ($x : double)
+
+=head2 exp
+
+  sub exp : double ($x : double)
+
+=head2 exp2
+
+  sub exp2 : double ($x : double)
+
+=head2 expm1
+
+  sub expm1 : double ($x : double)
+
+=head2 log
+
+  sub log : double ($x : double)
+
+=head2 log10
+
+  sub log10 : double ($x : double)
+
+=head2 log1p
+
+  sub log1p : double ($x : double)
+
+=head2 cbrt
+
+  sub cbrt : double ($x : double)
+
+=head2 fabs
+
+  sub fabs : double ($x : double)
+
+=head2 hypot
+
+  sub hypot : double ($x : double, $y : double)
+
+=head2 pow
+
+  sub pow : double ($x : double, $y : double)
+
+=head2 sqrt
+
+  sub sqrt : double ($x : double)
+
+=head2 erf
+
+  sub erf : double ($x : double)
+
+=head2 erfc
+
+  sub erfc : double ($x : double)
+
+=head2 lgamma
+
+  sub lgamma : double ($x : double)
+
+=head2 tgamma
+
+  sub tgamma : double ($x : double)
+
+=head2 ceil
+
+  sub ceil : double ($x : double)
+
+=head2 floor
+
+  sub floor : double ($x : double)
+
+=head2 nearbyint
+
+  sub nearbyint : double ($x : double)
+
+=head2 round
+
+  sub round : double ($x : double)
+
+=head2 remainder
+
+  sub remainder : double ($x1 : double, $x2 : double)
+
+=head2 signbitf
+
+  sub signbitf : int ($num : float)
+
+=head2 signbit
+
+  sub signbit : int ($num : double)
