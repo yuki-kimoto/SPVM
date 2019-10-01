8f42787ee065010b9b3c9ba8c00abede97d546c6
Yuki Kimoto <kimoto.yuki@gmail.com>
2019-10-01 17:20:43 +0900

    improve regex pod

---
+++ lib/SPVM/Regex.pm
@@ -0,0 +1,41 @@
+package SPVM::Regex;
+
+use SPVM 'SPVM::Regex';
+
+1;
+
+=head1 NAME
+
+SPVM::Regex - Regular expression
+
+=head1 SYNOPSYS
+  
+  use SPVM::Regex;
+  
+=head1 DESCRIPTION
+
+L<SPVM::Regex> provides regular expression functions.
+
+=head1 CLASS METHODS
+
+  my $re = SPVM::Regex->new("^ab+c");
+
+=head1 INSTANCE METHODS
+
+=head2 caps
+
+  sub caps : string[] ()
+
+Get the strings captured by "match" method.
+
+=head2  match_start
+
+  sub match_start : int ()
+
+Get the start byte offset of the string matched by "match" method.
+
+=head2 replace_count
+
+  sub replace_count : int ();
+
+Get the replace count of the strings replaced by "replace_all" method.
