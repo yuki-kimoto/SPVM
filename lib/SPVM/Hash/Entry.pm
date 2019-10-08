8b669f3e951d989c04933f6cf03fba862162a7b6
Yuki Kimoto <kimoto.yuki@gmail.com>
2019-10-08 17:17:39 +0900

    separate SPVM::Hash::Entry

---
+++ lib/SPVM/Hash/Entry.pm
@@ -0,0 +1,17 @@
+package SPVM::Hash::Entry;
+
+use SPVM 'SPVM::Hash::Entry';
+
+1;
+
+=head1 NAME
+
+SPVM::Hash::Entry - Hash entry
+
+=head1 SYNOPSYS
+  
+  use SPVM::Hash::Entry;
+
+=head1 DESCRIPTION
+
+L<SPVM::Hash::Entry> is a internal data structure for L<SPVM::Hash>.
