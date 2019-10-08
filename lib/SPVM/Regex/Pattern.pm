e22616ab912a7a17966e8b3695c6421af666fb0f
Yuki Kimoto <kimoto.yuki@gmail.com>
2019-10-08 16:26:24 +0900

    separate SPVM::Regex::Pattern

---
+++ lib/SPVM/Regex/Pattern.pm
@@ -0,0 +1,17 @@
+package SPVM::Regex::Pattern;
+
+use SPVM 'SPVM::Regex::Pattern';
+
+1;
+
+=head1 NAME
+
+SPVM::Regex::Pattern - Regular expression pattern
+
+=head1 SYNOPSYS
+  
+  use SPVM::Regex::Pattern;
+
+=head1 DESCRIPTION
+
+This is SPVM::Regex internal data structure.
