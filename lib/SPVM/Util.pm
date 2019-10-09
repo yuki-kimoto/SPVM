2be3161e15bbc4db6497953e37afe73001288bbb
Yuki Kimoto <kimoto.yuki@gmail.com>
2019-10-09 17:11:13 +0900

    improve SPVM::Util doc

--- lib/SPVM/Util.pm
+++ lib/SPVM/Util.pm
@@ -12,6 +12,14 @@ SPVM::Util - Variouse utilities
   
   use SPVM::Util;
   
+  # Copy object array
+  my $objects = [(object)SPVM::Int->new(1), SPVM::Int->new(2), SPVM::Int->new(3)];
+  my $objects_copy = copy_oarray($objects, sub : object ($self : self, $obj : object) {
+    my $int_obj = (SPVM::Int)$obj;
+    my $new_int_obj = SPVM::Int->new($int_obj->val);
+    return $new_int_obj;
+  });
+  
   # Stringify all object and join them by the specific separator
   my $objects = new Foo[3];
   my $str = SPVM::Util->joino(",", $objects, sub : string ($self : self, $obj : object) {
@@ -34,12 +42,18 @@ Unix standard library.
 
 =head1 CLASS METHODS
 
+=head2 copy_oarray
+
+  sub copy_oarray : object[] ($objects : object[], $cloner : SPVM::Cloner)
+
+Copy object array. You must specify a L<SPVM::Cloner> object to copy each element.
+
 =head2 joino
 
   sub joino : string ($sep : string, $objects : oarray, $stringer : SPVM::Stringer)
 
 Stringify all objects and join them by specific separator.
-Each object must have to_str method defined in L<SPVM::Stringer>. otherwise a exception occur.
+You must specify a L<SPVM::Stringer> object to stringify each element.
 
 =head2 split
 
