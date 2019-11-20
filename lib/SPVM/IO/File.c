9e3ae57ff409964c34ac6462cbedc8679937ab2b
Yuki Kimoto <kimoto.yuki@gmail.com>
2019-11-20 10:36:19 +0900

    STDOUT, STDIN, STDERR become package variable

--- lib/SPVM/IO/File.c
+++ lib/SPVM/IO/File.c
@@ -19,6 +19,10 @@ int32_t SPNATIVE__SPVM__IO__File__init_package_vars(SPVM_ENV* env, SPVM_VALUE* s
   SPVM_SET_PACKAGE_VAR_INT(env, "SPVM::IO::File", "$SEEK_CUR", SEEK_CUR, MFILE, __LINE__);
   SPVM_SET_PACKAGE_VAR_INT(env, "SPVM::IO::File", "$SEEK_END", SEEK_END, MFILE, __LINE__);
   
+  SPVM_SET_PACKAGE_VAR_INT(env, "SPVM::IO::File", "$STDIN", fileno(stdin), MFILE, __LINE__);
+  SPVM_SET_PACKAGE_VAR_INT(env, "SPVM::IO::File", "$STDOUT", fileno(stdout), MFILE, __LINE__);
+  SPVM_SET_PACKAGE_VAR_INT(env, "SPVM::IO::File", "$STDERR", fileno(stderr), MFILE, __LINE__);
+  
   return SPVM_SUCCESS;
 }
 
@@ -393,23 +397,5 @@ int32_t SPNATIVE__SPVM__IO__File__open(SPVM_ENV* env, SPVM_VALUE* stack) {
     SPVM_DIE("Can't open file \"%s\": %s", file_name, errstr, MFILE, __LINE__);
   }
   
-  return SPVM_SUCCESS;
-}
-
-int32_t SPNATIVE__SPVM__IO__File__STDIN(SPVM_ENV* env, SPVM_VALUE* stack) {
-  stack[0].ival = fileno(stdin);
-
-  return SPVM_SUCCESS;
-}
-
-int32_t SPNATIVE__SPVM__IO__File__STDOUT(SPVM_ENV* env, SPVM_VALUE* stack) {
-  stack[0].ival = fileno(stdout);
-
-  return SPVM_SUCCESS;
-}
-
-int32_t SPNATIVE__SPVM__IO__File__STDERR(SPVM_ENV* env, SPVM_VALUE* stack) {
-  stack[0].ival = fileno(stderr);
-
   return SPVM_SUCCESS;
 }
