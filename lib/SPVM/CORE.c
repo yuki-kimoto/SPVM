d86cd424a267ed18143c1a8b0afc02e3a60fdf00
Yuki Kimoto <kimoto.yuki@gmail.com>
2019-11-30 08:22:11 +0900

    remove no needed include

--- lib/SPVM/CORE.c
+++ lib/SPVM/CORE.c
@@ -3,12 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <string.h>
 #include <inttypes.h>
 #include <float.h>
 #include <complex.h>
-#include <memory.h>
 #include <ctype.h>
-#include <errno.h>
-
-static const char* MFILE = "SPVM/CORE.c";
