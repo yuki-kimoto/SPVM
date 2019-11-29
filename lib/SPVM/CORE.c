b1e457035b130295939e0584065e1720e0c7e382
Yuki Kimoto <kimoto.yuki@gmail.com>
2019-11-30 08:17:44 +0900

    remove no needed SPVM::CORE function

--- lib/SPVM/CORE.c
+++ lib/SPVM/CORE.c
@@ -12,12 +12,3 @@
 #include <errno.h>
 
 static const char* MFILE = "SPVM/CORE.c";
-
-int32_t SPNATIVE__SPVM__CORE__DBL_MAX(SPVM_ENV* env, SPVM_VALUE* stack) {
-  (void)env;
-  (void)stack;
-  
-  stack[0].dval = DBL_MAX;
-  
-  return SPVM_SUCCESS;
-}
