90372f169bc118b8b6d25520b73c64ea2ad71a0e
Yuki Kimoto <kimoto.yuki@gmail.com>
2020-01-13 11:45:40 +0900

    add backuped exe code to solo

---
+++ solo/exebak/SPVM/Builder/include/spvm_csource_builder_exe.h
@@ -0,0 +1,11 @@
+#ifndef SPVM_CSOURCE_BUILDER_EXE_H
+#define SPVM_CSOURCE_BUILDER_EXE_H
+
+#include <stdio.h>
+
+#include "spvm_base.h"
+#include "spvm_native.h"
+
+void SPVM_CSOURCE_BUILDER_EXE_build_exe_csource(SPVM_ENV* env, SPVM_STRING_BUFFER* string_buffer, SPVM_PORTABLE* portable, const char* package_name);
+
+#endif
