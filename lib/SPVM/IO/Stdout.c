3c0215e4e67ac9e1749d8981e5b41941164be54e
Yuki Kimoto <kimoto.yuki@gmail.com>
2020-01-20 12:23:47 +0900

    add exception to SPVM::IO::Stdout print

--- lib/SPVM/IO/Stdout.c
+++ lib/SPVM/IO/Stdout.c
@@ -8,32 +8,28 @@ int32_t SPNATIVE__SPVM__IO__Stdout__print(SPVM_ENV* env, SPVM_VALUE* stack) {
 
   void* string = stack[0].oval;
   
+  if (!string) {
+    SPVM_DIE("String must be defined", MFILE, __LINE__);
+  }
+  
   const char* bytes = (const char*)env->get_elems_byte(env, string);
   int32_t string_length = env->length(env, string);
   
   // Print
   int32_t error = 0;
-  for (int32_t i = 0; i < string_length; i++) {
-    int32_t ret = fputc(bytes[i], stdout);
-    if (ret == EOF) {
-      error = 1;
-      break;
-    }
+  int32_t write_length = fwrite(bytes, 1, string_length, stdout);
+  if (write_length =! string_length) {
+    SPVM_DIE("Can't print string to stdout", MFILE, __LINE__);
   }
   
-  // Auto flush
+  // Flush buffer to stdout if auto flush is true
   int8_t auto_flush;
   SPVM_GET_PACKAGE_VAR_BYTE(env, auto_flush, "SPVM::IO::Stdout", "$AUTO_FLUSH", MFILE, __LINE__);
-  
   if (auto_flush) {
-    fflush(stdout);//SPVM::IO::Stdout::print (Don't remove this comment for tests)
-  }
-  
-  if (error) {
-    stack[0].ival = -1;
-  }
-  else {
-    stack[0].ival = string_length;
+    int32_t ret = fflush(stdout);//SPVM::IO::Stdout::print (Don't remove this comment for tests)
+    if (ret != 0) {
+      SPVM_DIE("Can't flush buffer to stdout", MFILE, __LINE__);
+    }
   }
   
   return SPVM_SUCCESS;
@@ -45,7 +41,7 @@ int32_t SPNATIVE__SPVM__IO__Stdout__flush(SPVM_ENV* env, SPVM_VALUE* stack) {
   int32_t ret  = (int32_t)fflush(stdout);
   
   if (ret != 0) {
-    SPVM_DIE("Can't flush to stdout", MFILE, __LINE__);
+    SPVM_DIE("Can't flush buffer to stdout", MFILE, __LINE__);
   }
   
   return SPVM_SUCCESS;
