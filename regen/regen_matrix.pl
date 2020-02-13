53280ce4bcc3796f5de6de46a643fc0c0bbcdc51
Yuki Kimoto <kimoto.yuki@gmail.com>
2020-02-13 12:15:48 +0900

    add matrix data structures

--- regen/regen_matrix.pl
+++ regen/regen_matrix.pl
@@ -12,17 +12,21 @@ sub prefix {
 for my $dim (@dims) {
   for my $type (@types) {
     my $prefix = prefix($type);
+    my $fields_length = $dim * $dim;
+    my $package_name = "SPVM::Matrix${dim}_$fields_length$prefix";
     
-    my $package_name = "SPVM::Vector${dim}_${dim}${prefix}";
-    
-    my $package_content = "# $package_name is created by regen/regen_vector.pl\n";
+    my $package_content = "# $package_name is created by regen/regen_matrix.pl\n";
     $package_content .= "package $package_name : public mulnum_t {\n";
-    for(my $i = 0; $i < $dim; $i++) {
-      $package_content .= "  has x$i : public $type;\n";
+    
+    for (my $i = 0; $i < $dim; $i++) {
+      for (my $k = 0; $k < $dim; $k++) {
+        $package_content .= "  has x$i$k : public $type;\n";
+      }
     }
+    
     $package_content .= "}\n";
     
-    my $module_file = "lib/SPVM/Vector${dim}_${dim}${prefix}.spvm";
+    my $module_file = "lib/SPVM/Matrix${dim}_$fields_length$prefix.spvm";
     open my $module_fh, '>', $module_file
       or die "Can't open $module_file: $!";
     print $module_fh $package_content;
