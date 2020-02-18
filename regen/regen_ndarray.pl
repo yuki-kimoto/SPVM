60fb06022a24b66681dee370e9b962935ee28797
Yuki Kimoto <kimoto.yuki@gmail.com>
2020-02-18 18:08:09 +0900

    add ndarray tests

--- regen/regen_ndarray.pl
+++ regen/regen_ndarray.pl
@@ -8,30 +8,23 @@ sub prefix {
   my $prefix = substr($type, 0, 1);
   return $prefix;
 }
-sub base_type {
-  my ($type) = @_;
-  
-  my $base_type;
-  if ($type eq 'string') {
-    $base_type = 'String';
-  }
-  else {
-    $base_type = $type;
-  }
-  return $base_type;
-}
 
 for my $type (@types) {
   my $prefix = prefix($type);
   
   my $package_name = "SPVM::NDArray::" . ucfirst($type);
-  my $base_type = base_type $type;
   
   my $package_content = "# $package_name is created by regen/regen_ndarray.pl\n";
   $package_content .= "package $package_name : public {\n";
   $package_content .= <<"EOS";
-  has base : public ${base_type}[];
-  has base_im : public ${base_type}[];
+  has base : public ${type}[];
+EOS
+  
+  if ($type ne 'string') {
+    $package_content .=  "  has base_im : public ${type}[];\n";
+  }
+  
+  $package_content .= <<"EOS";
   has offset : public int;
   has length : public int;
   has T : public byte;
