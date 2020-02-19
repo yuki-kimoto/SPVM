d761a0a24c5f2e55acf822f445432bee4c2cb192
Yuki Kimoto <kimoto.yuki@gmail.com>
2020-02-19 10:15:07 +0900

    add SPVM::NDArray::ComplexX(X is Byte, Short, Int, Long, Float, Double)

--- regen/regen_ndarray_complex.pl
+++ regen/regen_ndarray_complex.pl
@@ -12,29 +12,21 @@ sub prefix {
 for my $type (@types) {
   my $prefix = prefix($type);
   
-  my $package_name = "SPVM::NDArray::" . ucfirst($type);
+  my $package_name = "SPVM::NDArray::Complex" . ucfirst($type);
   
-  my $package_content = "# $package_name is created by regen/regen_ndarray_complex.pl\n";
-  $package_content .= "package $package_name : public {\n";
-  $package_content .= <<"EOS";
-  has base : public ${type}[];
-EOS
-  
-  if ($type ne 'string') {
-    $package_content .=  "  has base_im : public ${type}[];\n";
-  }
-  
-  $package_content .= <<"EOS";
+  my $package_content = <<"EOS";
+# $package_name is created by regen/regen_ndarray_complex.pl
+package $package_name : public {
+  has base : public Complex_2${prefix}[];
   has offset : public int;
   has length : public int;
   has T : public byte;
-  has is_complex : public byte;
   has shape : public int[];
   has na_flags : public byte[];
+}
 EOS
-  $package_content .= "}\n";
   
-  my $module_file = "lib/SPVM/NDArray/"  . ucfirst($type) . ".spvm";
+  my $module_file = "lib/SPVM/NDArray/Complex"  . ucfirst($type) . ".spvm";
   open my $module_fh, '>', $module_file
     or die "Can't open $module_file: $!";
   print $module_fh $package_content;
