0a1d470f98a090b729ad567ea99d5ed5e1b25255
Yuki Kimoto <kimoto.yuki@gmail.com>
2020-02-19 09:31:02 +0900

    add SPVM::Quaternion

--- regen/regen_quaternion.pl
+++ regen/regen_quaternion.pl
@@ -11,17 +11,19 @@ sub prefix {
 for my $type (@types) {
   my $prefix = prefix($type);
   
-  my $package_name = "SPVM::Complex_2$prefix";
+  my $package_name = "SPVM::Quaternion_4$prefix";
   
-  my $package_content = "# $package_name is created by regen/regen_complex.pl\n";
+  my $package_content = "# $package_name is created by regen/regen_quaternion.pl\n";
   $package_content .= "package $package_name : mulnum_t {\n";
     $package_content .= <<"EOS";
+  has t : $type;
   has x : $type;
   has y : $type;
+  has z : $type;
 EOS
   $package_content .= "}\n";
   
-  my $module_file = "lib/SPVM/Complex_2$prefix.spvm";
+  my $module_file = "lib/SPVM/Quaternion_4$prefix.spvm";
   open my $module_fh, '>', $module_file
     or die "Can't open $module_file: $!";
   print $module_fh $package_content;
