f5d6114bcd1f27709f1e389702ea213520d4395e
Yuki Kimoto <kimoto.yuki@gmail.com>
2020-06-15 17:42:29 +0900

    Revert "remove regen ndarray"
    This reverts commit 66355ad48a45b0e3819b795c583c6425bbffa519.

---
+++ regen/regen_ndarray.pl
@@ -0,0 +1,33 @@
+use strict;
+use warnings;
+
+my @types = qw(byte short int long float double string);
+
+sub prefix {
+  my ($type) = @_;
+  my $prefix = substr($type, 0, 1);
+  return $prefix;
+}
+
+for my $type (@types) {
+  my $prefix = prefix($type);
+  
+  my $package_name = "SPVM::NDArray::" . ucfirst($type);
+  
+  my $package_content = <<"EOS";
+# $package_name is created by regen/regen_ndarray.pl
+package $package_name : public {
+  has base : public ${type}[];
+  has offset : public int;
+  has length : public int;
+  has T : public byte;
+  has shape : public int[];
+  has na_flags : public byte[];
+}
+EOS
+  
+  my $module_file = "lib/SPVM/NDArray/"  . ucfirst($type) . ".spvm";
+  open my $module_fh, '>', $module_file
+    or die "Can't open $module_file: $!";
+  print $module_fh $package_content;
+}
