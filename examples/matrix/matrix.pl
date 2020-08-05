370dab8adea0fb2889fa547caf4cba0a29fdb02d
Yuki Kimoto <kimoto.yuki@gmail.com>
2020-08-05 09:36:23 +0900

    add matrix example

---
+++ examples/matrix/matrix.pl
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+
+use FindBin;
+use lib "$FindBin::Bin/lib";
+
+use SPVM 'MyMatrix';
+
+MyMatrix->add_float_matrix;
+
+=pod
+MyMatrix->sub_float_matrix;
+
+MyMatrix->scamal_float_matrix;
+
+MyMatrix->mal_float_matrix;
+=cut
