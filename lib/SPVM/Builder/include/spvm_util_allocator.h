ef6aa4a1f48b0a9cf83bcae742bc8289e193472b
Yuki Kimoto <kimoto.yuki@gmail.com>
2021-03-19 09:36:39 +0900

    remove not used SPVM_UTIL_ALLOCATOR_safe_malloc

--- lib/SPVM/Builder/include/spvm_util_allocator.h
+++ lib/SPVM/Builder/include/spvm_util_allocator.h
@@ -1,7 +1,6 @@
 #ifndef SPVM_UTIL_ALLOCATOR_H
 #define SPVM_UTIL_ALLOCATOR_H
 
-void* SPVM_UTIL_ALLOCATOR_safe_malloc(size_t byte_size);
 void* SPVM_UTIL_ALLOCATOR_safe_malloc_zero(size_t byte_size);
 
 #endif
--- lib/SPVM/Builder/src/spvm_util_allocator.c
+++ lib/SPVM/Builder/src/spvm_util_allocator.c
@@ -6,28 +6,6 @@
 
 #include "spvm_util_allocator.h"
 
-void* SPVM_UTIL_ALLOCATOR_safe_malloc(size_t byte_size) {
-  
-  if (byte_size < 1) {
-    fprintf(stderr, "Failed to allocate memory. Size must be more than 0(SPVM_UTIL_ALLOCATOR_safe_malloc)\n");
-    abort();
-  }
-  
-  if ((size_t)byte_size > SIZE_MAX) {
-    fprintf(stderr, "Failed to allocate memory. Size is too big(SPVM_UTIL_ALLOCATOR_safe_malloc())\n");
-    abort();
-  }
-  
-  void* block = malloc((size_t)byte_size);
-  
-  if (block == NULL) {
-    fprintf(stderr, "Failed to allocate memory. malloc function return NULL(SPVM_UTIL_ALLOCATOR_safe_malloc())\n");
-    abort();
-  }
-  
-  return block;
-}
-
 void* SPVM_UTIL_ALLOCATOR_safe_malloc_zero(size_t byte_size) {
   
   if (byte_size < 1) {
