1a5dccf0b5141796dfa5c598c41697e2b81db1c8
Yuki Kimoto <kimoto.yuki@gmail.com>
2021-05-01 10:02:31 +0900

    SPVMの関数を50音順に並べ替えるスクリプトを追加

---
+++ t/sort_sub_name_spvm.pl
@@ -0,0 +1,34 @@
+# SPVMモジュールの関数名を50音順に並べ替える
+
+my $is_sub;
+
+my $sub_defs_h = {};
+
+my $cur_name = '';
+my $cur_sub = '';
+while (my $line = <>) {
+  if ($line =~ /^\}/) {
+    $is_sub = 0;
+  }
+  if ($is_sub) {
+    if ($line =~ /\bsub\s+(\w+)\b/) {
+      $cur_name = $1;
+      $sub_defs_h->{$cur_name} = $line;
+    }
+    else {
+      $sub_defs_h->{$cur_name} .= $line;
+    }
+  }
+  else {
+    if ($line =~ /\bsub\s+(\w+)\b/) {
+      $is_sub = 1;
+      $cur_name = $1;
+      $sub_defs_h->{$cur_name} = $line;
+    }
+  }
+}
+
+for my $sub_name (sort { lc $a cmp lc $b } keys %$sub_defs_h) {
+  my $sub_def = $sub_defs_h->{$sub_name};
+  print $sub_def;
+}
