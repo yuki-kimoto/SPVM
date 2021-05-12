f4a46a1b80cefad2af28e4e743014bb34a700880
Yuki Kimoto <kimoto.yuki@gmail.com>
2021-05-12 13:37:53 +0900

    fix native api name. object_type_runtime_type_offset is changed to object_type_category_offset. object_type_flag_offset is changed to object_flag_offset. this was mistake.

--- lib/SPVM/Builder/include/spvm_native.h
+++ lib/SPVM/Builder/include/spvm_native.h
@@ -103,8 +103,8 @@ struct spvm_env {
   void* object_ref_count_offset;
   void* object_basic_type_id_offset;
   void* object_type_dimension_offset;
-  void* object_type_runtime_type_offset;
-  void* object_type_flag_offset;
+  void* object_type_category_offset;
+  void* object_flag_offset;
   void* object_length_offset;
   void* byte_object_basic_type_id;
   void* short_object_basic_type_id;
