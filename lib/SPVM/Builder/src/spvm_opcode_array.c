9a2074565a7997f19e91494b9311d80bbd69714e
Yuki Kimoto <kimoto.yuki@gmail.com>
2021-12-27 12:23:14 +0900

    Revert "SPVM_OPCODE_ARRAY  memory blocks is managed by the compiler"
    This reverts commit a7ad435fc90566d1e9f768cf4b5e39f1a942bdeb.

--- lib/SPVM/Builder/src/spvm_opcode_array.c
+++ lib/SPVM/Builder/src/spvm_opcode_array.c
@@ -3,19 +3,19 @@
 #include <assert.h>
 
 #include "spvm_opcode_array.h"
-#include "spvm_compiler_allocator.h"
+#include "spvm_util_allocator.h"
 #include "spvm_compiler.h"
 #include "spvm_opcode.h"
 
 SPVM_OPCODE_ARRAY* SPVM_OPCODE_ARRAY_new(SPVM_COMPILER* compiler) {
   (void)compiler;
   
-  SPVM_OPCODE_ARRAY* opcodes = SPVM_COMPILER_ALLOCATOR_safe_malloc_zero_tmp(compiler, sizeof(SPVM_OPCODE_ARRAY));
+  SPVM_OPCODE_ARRAY* opcodes = SPVM_UTIL_ALLOCATOR_safe_malloc_zero(sizeof(SPVM_OPCODE_ARRAY));
   opcodes->capacity = 64;
   opcodes->length = 0;
   
   int64_t values_byte_size = (int64_t)opcodes->capacity * (int64_t)sizeof(SPVM_OPCODE);
-  SPVM_OPCODE* values = SPVM_COMPILER_ALLOCATOR_safe_malloc_zero_tmp(compiler, values_byte_size);
+  SPVM_OPCODE* values = SPVM_UTIL_ALLOCATOR_safe_malloc_zero(values_byte_size);
   opcodes->values = values;
   
   // Add 1 opcode because 0 mean no opcode
@@ -36,9 +36,9 @@ void SPVM_OPCODE_ARRAY_push_opcode(SPVM_COMPILER* compiler, SPVM_OPCODE_ARRAY* o
     int32_t new_capacity = capacity * 2;
     
     int64_t new_values_byte_size = (int64_t)new_capacity * (int64_t)sizeof(SPVM_OPCODE);
-    SPVM_OPCODE* new_values = SPVM_COMPILER_ALLOCATOR_safe_malloc_zero_tmp(compiler, new_values_byte_size);
+    SPVM_OPCODE* new_values = SPVM_UTIL_ALLOCATOR_safe_malloc_zero(new_values_byte_size);
     memcpy(new_values, opcodes->values, capacity * sizeof(SPVM_OPCODE));
-    SPVM_COMPILER_ALLOCATOR_free_tmp(compiler, opcodes->values);
+    free(opcodes->values);
     opcodes->values = new_values;
     
     opcodes->capacity = new_capacity;
@@ -53,6 +53,6 @@ void SPVM_OPCODE_ARRAY_push_opcode(SPVM_COMPILER* compiler, SPVM_OPCODE_ARRAY* o
 void SPVM_OPCODE_ARRAY_free(SPVM_COMPILER* compiler, SPVM_OPCODE_ARRAY* opcodes) {
   (void)compiler;
   
-  SPVM_COMPILER_ALLOCATOR_free_tmp(compiler, opcodes->values);
-  SPVM_COMPILER_ALLOCATOR_free_tmp(compiler, opcodes);
+  free(opcodes->values);
+  free(opcodes);
 }
