531a1c6c0e1c151cebd3d930fee380e64576d7de
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-01-21 08:42:13 +0900

    Cleanup create_spvm_module_sources

--- lib/SPVM/Builder/Exe.pm
+++ lib/SPVM/Builder/Exe.pm
@@ -670,35 +670,38 @@ sub create_spvm_module_sources {
     $module_source_base =~ s|::|/|g;
     my $module_source_csource_file = "$build_src_dir/$module_source_base.modsrc.c";
     
+    # Source creating callback
+    my $create_cb = sub {
+      # This source is UTF-8 binary
+      my $module_source = $builder->get_module_source($class_name);
+      my $module_source_c_hex = $module_source;
+      
+      # Escape to Hex C launguage string literal
+      $module_source_c_hex =~ s/(.)/$_ = sprintf("\\x%02X", ord($1));$_/ges;
+      
+      # native class name
+      my $class_cname = $class_name;
+      $class_cname =~ s/::/__/g;
+
+      my $get_module_source_csource = <<"EOS";
+static const char* module_source = "$module_source_c_hex";
+const char* SPMODSRC__${class_cname}__get_module_source() {
+return module_source;
+}
+EOS
+      mkpath dirname $module_source_csource_file;
+      
+      open my $module_source_csource_fh, '>', $module_source_csource_file
+        or die "Can't open file $module_source_csource_file:$!";
+
+      print $module_source_csource_fh $get_module_source_csource;
+    };
+    
     # Create source file
     $self->create_source_file({
       input_file => $module_file,
       output_file => $module_source_csource_file,
-      create_cb => sub {
-        # This source is UTF-8 binary
-        my $module_source = $builder->get_module_source($class_name);
-        my $module_source_c_hex = $module_source;
-        
-        # Escape to Hex C launguage string literal
-        $module_source_c_hex =~ s/(.)/$_ = sprintf("\\x%02X", ord($1));$_/ges;
-        
-        # native class name
-        my $class_cname = $class_name;
-        $class_cname =~ s/::/__/g;
-
-        my $get_module_source_csource = <<"EOS";
-static const char* module_source = "$module_source_c_hex";
-const char* SPMODSRC__${class_cname}__get_module_source() {
-  return module_source;
-}
-EOS
-        mkpath dirname $module_source_csource_file;
-        
-        open my $module_source_csource_fh, '>', $module_source_csource_file
-          or die "Can't open file $module_source_csource_file:$!";
-
-        print $module_source_csource_fh $get_module_source_csource;
-      }
+      create_cb => $create_cb,
     });
   }
 }
