e08cf20443ec2b29b885a0b8037ec156ed9b2604
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-01-22 07:30:40 +0900

    Rename solo Main to MyExe

--- solo/myexe.c
+++ solo/myexe.c
@@ -17,7 +17,7 @@
 int32_t main(int32_t argc, const char *argv[]) {
   
   // Class name
-  const char* class_name = "Main";
+  const char* class_name = "MyExe";
   
   // Create compiler
   SPVM_COMPILER* compiler = SPVM_COMPILER_new();
@@ -31,29 +31,9 @@ int32_t main(int32_t argc, const char *argv[]) {
   SPVM_OP_build_use(compiler, op_use_start, op_type_start, NULL, 0);
   SPVM_LIST_push(compiler->op_use_stack, op_use_start);
   
-  // Get script directory
-  char* cur_script_dir = "solo/SPVM";
-  /*
-  const char* cur_script_name = argv[0];
-  int32_t cur_script_name_length = (int32_t)strlen(argv[0]);
-  char* cur_script_dir = malloc(cur_script_name_length + 1);
-  memcpy(cur_script_dir, cur_script_name, cur_script_name_length);
-  cur_script_dir[cur_script_name_length] = '\0';
-  int32_t found_sep = 0;
-  for (int32_t i = cur_script_name_length - 1; i >= 0; i--) {
-    if (cur_script_dir[i] == '/' || cur_script_dir[i] == '\\') {
-      cur_script_dir[i] = '\0';
-      found_sep = 1;
-      break;
-    }
-  }
-  if (!found_sep) {
-    cur_script_dir[0] = '.';
-    cur_script_dir[1] = '\0';
-  }
-  */
-  
-  SPVM_LIST_push(compiler->module_dirs, cur_script_dir);
+  // Add module directory
+  char* module_dir = "solo/SPVM";
+  SPVM_LIST_push(compiler->module_dirs, module_dir);
   
   SPVM_COMPILER_compile(compiler);
   
