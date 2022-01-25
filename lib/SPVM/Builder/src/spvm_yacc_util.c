287a9e348bad6653348067baf6c261957100dccc
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-01-25 15:59:26 +0900

    Use SPVM_COMPILER_error in SPVM_yyerror

--- lib/SPVM/Builder/src/spvm_yacc_util.c
+++ lib/SPVM/Builder/src/spvm_yacc_util.c
@@ -38,17 +38,8 @@ void SPVM_yyerror(SPVM_COMPILER* compiler, const char* message_not_used) {
   token[length] = '\0';
   
   int32_t char_pos = (int32_t)(compiler->befbufptr + empty_count + 1 - compiler->line_start_ptr);
-  
-  const char* error_format = "[CompileError]Unexpected token \"%s\" at %s line %d:%d\n";
-  
-  int32_t error_message_max_size = strlen(error_format) + strlen(token) + strlen(compiler->cur_file) + 30 + 30 + 1;
-  
-  fprintf(stderr, error_format, token, compiler->cur_file, compiler->cur_line, char_pos);
-  
-  char* message = SPVM_ALLOCATOR_new_block_compile_eternal(compiler, error_message_max_size);
-  sprintf(message, error_format, token, compiler->cur_file, compiler->cur_line, char_pos);
-  
-  SPVM_LIST_push(compiler->error_messages, message);
+
+  SPVM_COMPILER_error(compiler, "Unexpected token \"%s\" at %s line %d:%d\n", token, compiler->cur_file, compiler->cur_line, char_pos);
 
   SPVM_ALLOCATOR_free_block_compile_tmp(compiler, token);
 }
