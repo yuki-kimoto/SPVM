4bde5c705359206704c6ce43d41f23bfcfbe2b92
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-01-26 10:40:22 +0900

    Add SPVM::Builder::API module. The implementation is not yet done

---
+++ lib/SPVM/Builder/API.pm
@@ -0,0 +1,19 @@
+package SPVM::Builder::API;
+
+1;
+
+=head1 NAME
+
+SPVM::Builder::API - SPVM Builder Public APIs
+
+=head1 DESCRIPTION
+
+B<SPVM::Builder::API> is the public APIs of SPVM Builder. 
+
+L<SPVM::Builder>, L<SPVM::Builder::CC>, L<SPVM::Builder::Util> is a private modules of L<SPVM>.
+
+The names and arguments of the methods are changed without warnings in the future release.
+
+However, the methods is useful to get the information of SPVM modules.
+
+B<SPVM::Builder::API> provides the public APIs to call the methods. These APIs is public and stable.
