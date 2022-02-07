3676139ec1c936af4c3bad17d5bcd3a09e16987d
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-02-07 12:11:43 +0900

    Add new_string_raw("abc",0) test

--- t/default/lib/SPVM/TestCase/NativeAPI.c
+++ t/default/lib/SPVM/TestCase/NativeAPI.c
@@ -1848,7 +1848,7 @@ int32_t SPVM__TestCase__NativeAPI__new_string_raw(SPVM_ENV* env, SPVM_VALUE* sta
 
   int32_t ret;
   
-  // Basic
+  // Length is shorten than the string
   {
     void* string = env->new_string_raw(env, "abc", 1);
 
@@ -1861,6 +1861,29 @@ int32_t SPVM__TestCase__NativeAPI__new_string_raw(SPVM_ENV* env, SPVM_VALUE* sta
     
     const char* string_chars = env->get_chars(env, string);
     if (strcmp(string_chars, "a") != 0) {
+      stack[0].ival = 0;
+      env->inc_ref_count(env, string);
+      env->dec_ref_count(env, string);
+      return 0;
+    }
+    
+    env->inc_ref_count(env, string);
+    env->dec_ref_count(env, string);
+  }
+
+  // Length is shorten than the string 0
+  {
+    void* string = env->new_string_raw(env, "abc", 0);
+
+    if (env->length(env, string) != 0) {
+      stack[0].ival = 0;
+      env->inc_ref_count(env, string);
+      env->dec_ref_count(env, string);
+      return 0;
+    }
+    
+    const char* string_chars = env->get_chars(env, string);
+    if (strcmp(string_chars, "") != 0) {
       stack[0].ival = 0;
       env->inc_ref_count(env, string);
       env->dec_ref_count(env, string);
