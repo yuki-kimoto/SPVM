5e1129aa4a3c8b2b29a711526cb2d1531b4d0b4f
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-02-25 09:00:19 +0900

    Remove unneeded string buffer access id functions

--- lib/SPVM/Builder/include/spvm_string_buffer.h
+++ lib/SPVM/Builder/include/spvm_string_buffer.h
@@ -17,13 +17,6 @@ SPVM_STRING_BUFFER* SPVM_STRING_BUFFER_new(SPVM_COMPILER* compiler, int32_t capa
 char* SPVM_STRING_BUFFER_get_buffer(SPVM_STRING_BUFFER* string_buffer);
 void SPVM_STRING_BUFFER_maybe_extend(SPVM_STRING_BUFFER* string_buffer, int32_t new_length);
 
-int32_t SPVM_STRING_BUFFER_add_field_access_id_name(SPVM_STRING_BUFFER* string_buffer, const char* cur_class_name, const char* access_class_name, const char* field_name);
-int32_t SPVM_STRING_BUFFER_add_field_access_offset_name(SPVM_STRING_BUFFER* string_buffer, const char* cur_class_name, const char* access_class_name, const char* field_name);
-int32_t SPVM_STRING_BUFFER_add_method_access_id_name(SPVM_STRING_BUFFER* string_buffer, const char* cur_class_name, const char* access_class_name, const char* method_name);
-int32_t SPVM_STRING_BUFFER_add_basic_type_access_id_name(SPVM_STRING_BUFFER* string_buffer, const char* cur_class_name, const char* basic_type_name);
-int32_t SPVM_STRING_BUFFER_add_class_var_access_id_name(SPVM_STRING_BUFFER* string_buffer, const char* cur_class_name, const char* access_class_name, const char* class_var_name);
-
-int32_t SPVM_STRING_BUFFER_add_class_name(SPVM_STRING_BUFFER* string_buffer, const char* class_name);
 int32_t SPVM_STRING_BUFFER_add(SPVM_STRING_BUFFER* string_buffer, const char* string);
 int32_t SPVM_STRING_BUFFER_add_len(SPVM_STRING_BUFFER* string_buffer, char* string, int32_t string_length);
 int32_t SPVM_STRING_BUFFER_add_hex_char(SPVM_STRING_BUFFER* string_buffer, char ch);
