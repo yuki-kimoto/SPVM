365623229b3b8c436fe132d04605cb4a7248791d
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-03-01 08:19:52 +0900

    Fix SPVM_STRING_new

--- lib/SPVM/Builder/src/spvm_string.c
+++ lib/SPVM/Builder/src/spvm_string.c
@@ -5,6 +5,7 @@
 #include "spvm_hash.h"
 
 SPVM_STRING* SPVM_STRING_new(SPVM_COMPILER* compiler, const char* value, int32_t length) {
+  
   SPVM_STRING* found_string = SPVM_HASH_fetch(compiler->string_symtable2, value, length);
   if (found_string) {
     return found_string;
@@ -12,14 +13,15 @@ SPVM_STRING* SPVM_STRING_new(SPVM_COMPILER* compiler, const char* value, int32_t
   else {
     char* new_value = SPVM_ALLOCATOR_new_block_compile_eternal(compiler, length + 1);
     memcpy(new_value, value, length);
-
+    new_value[length] = '\0';
+    
     SPVM_STRING* string = SPVM_ALLOCATOR_new_block_compile_eternal(compiler, sizeof(SPVM_STRING));
-    string->value = value;
+    string->value = new_value;
     string->length = length;
     string->id = compiler->strings2->length;
 
     SPVM_LIST_push(compiler->strings2, string);
-    SPVM_HASH_insert(compiler->string_symtable2, value, length, string);
+    SPVM_HASH_insert(compiler->string_symtable2, new_value, length, string);
     
     return string;
   }
