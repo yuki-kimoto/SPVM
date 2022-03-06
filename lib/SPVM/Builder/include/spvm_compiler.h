1a0f414098d2b460b6b04dbc85cebbe25e4edf13
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-03-07 08:31:38 +0900

    Add runtime fields to compiler

--- lib/SPVM/Builder/include/spvm_compiler.h
+++ lib/SPVM/Builder/include/spvm_compiler.h
@@ -109,24 +109,6 @@ struct spvm_compiler {
   // Basic type symbol table
   SPVM_HASH* basic_type_symtable;
 
-  // Runtime Basic types
-  SPVM_LIST* runtime_basic_types;
-  
-  // Runtime Basic type symbol table
-  SPVM_HASH* runtime_basic_type_symtable;
-
-  // Runtime Basic types
-  SPVM_LIST* runtime_types;
-  
-  // Runtime Basic type symbol table
-  SPVM_HASH* runtime_type_symtable;
-
-  // Runtime class types
-  SPVM_LIST* runtime_classes;
-  
-  // Runtime class symbol table
-  SPVM_HASH* runtime_class_symtable;
-
   // Types
   SPVM_LIST* types;
   
@@ -153,6 +135,42 @@ struct spvm_compiler {
   
   // module source symtable
   SPVM_HASH* module_source_symtable;
+
+  // Strings - string literals and symbol names
+  SPVM_LIST* runtime_strings;
+  
+  // String symtable
+  SPVM_HASH* runtime_string_symtable;
+
+  // Runtime Basic types
+  SPVM_LIST* runtime_basic_types;
+  
+  // Runtime Basic type symbol table
+  SPVM_HASH* runtime_basic_type_symtable;
+
+  // Runtime Basic types
+  SPVM_LIST* runtime_types;
+  
+  // Runtime Basic type symbol table
+  SPVM_HASH* runtime_type_symtable;
+
+  // Runtime class types
+  SPVM_LIST* runtime_classes;
+  
+  // Runtime class symbol table
+  SPVM_HASH* runtime_class_symtable;
+
+  // Runtime methods
+  SPVM_LIST* runtime_methods;
+
+  // Runtime class variables
+  SPVM_LIST* runtime_class_vars;
+
+  // Runtime fields
+  SPVM_LIST* runtime_fields;
+  
+  // Runtime switch infos
+  SPVM_LIST* runtime_switch_infos;
 };
 
 SPVM_COMPILER* SPVM_COMPILER_new();
