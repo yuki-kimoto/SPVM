1b4a5570fd789ee5eb3a0038bace3a75f0808172
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-03-08 07:58:20 +0900

    Reorder spvm_class

--- lib/SPVM/Builder/include/spvm_class.h
+++ lib/SPVM/Builder/include/spvm_class.h
@@ -17,20 +17,20 @@ enum {
 };
 
 struct spvm_class {
-  const char* name;
-  SPVM_TYPE* type;
-  SPVM_LIST* class_vars;
-  SPVM_LIST* methods;
-  SPVM_HASH* method_symtable;
-  SPVM_LIST* fields;
   int32_t id;
-  const char* module_file;
   int32_t method_destructor_id;
   int32_t flag;
   int32_t object_fields_length;
   int32_t object_fields_offset;
   int8_t has_init_block;
   int8_t is_anon;
+  const char* name;
+  const char* module_file;
+  SPVM_TYPE* type;
+  SPVM_LIST* class_vars;
+  SPVM_LIST* methods;
+  SPVM_HASH* method_symtable;
+  SPVM_LIST* fields;
   SPVM_OP* op_class;
   SPVM_OP* op_name;
   SPVM_HASH* class_var_symtable;
--- lib/SPVM/Builder/include/spvm_runtime_class.h
+++ lib/SPVM/Builder/include/spvm_runtime_class.h
@@ -4,20 +4,20 @@
 #include "spvm_typedecl.h"
 
 struct spvm_runtime_class {
-  const char* name;
-  SPVM_TYPE* type;
-  SPVM_LIST* class_vars;
-  SPVM_LIST* methods;
-  SPVM_HASH* method_symtable;
-  SPVM_LIST* fields;
   int32_t id;
-  const char* module_file;
   int32_t method_destructor_id;
   int32_t flag;
   int32_t object_fields_length;
   int32_t object_fields_offset;
   int8_t has_init_block;
   int8_t is_anon;
+  const char* name;
+  const char* module_file;
+  SPVM_TYPE* type;
+  SPVM_LIST* class_vars;
+  SPVM_LIST* methods;
+  SPVM_HASH* method_symtable;
+  SPVM_LIST* fields;
 };
 
 #endif
