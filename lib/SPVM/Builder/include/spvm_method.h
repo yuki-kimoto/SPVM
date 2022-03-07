5fa454a46cab0c5febee462842a7302cd7b0e9e5
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-03-07 16:01:38 +0900

    Reorder spvm_method.h and spvm_runtime_method.h

--- lib/SPVM/Builder/include/spvm_method.h
+++ lib/SPVM/Builder/include/spvm_method.h
@@ -21,6 +21,28 @@ enum {
 
 // Method information
 struct spvm_method {
+  void* precompile_address;
+  void* native_address;
+  int32_t return_type_id;
+  SPVM_LIST* arg_type_ids;
+  SPVM_LIST* arg_mem_ids;
+  const char* name;
+  const char* signature;
+  int32_t opcodes_base;
+  int32_t opcodes_length;
+  int32_t id;
+  int32_t class_id;
+  int32_t flag;
+  int8_t is_class_method;
+  int32_t byte_vars_alloc_length;
+  int32_t short_vars_alloc_length;
+  int32_t int_vars_alloc_length;
+  int32_t long_vars_alloc_length;
+  int32_t float_vars_alloc_length;
+  int32_t double_vars_alloc_length;
+  int32_t object_vars_alloc_length;
+  int32_t ref_vars_alloc_length;
+  int32_t mortal_stack_length;
   SPVM_OP* op_method;
   SPVM_OP* op_name;
   SPVM_OP* op_block;
@@ -30,36 +52,18 @@ struct spvm_method {
   SPVM_LIST* object_arg_ids;
   SPVM_LIST* mys;
   SPVM_LIST* captures;
-  void* precompile_address;
-  void* native_address;
   SPVM_TYPE* return_type;
   SPVM_LIST* args;
   SPVM_LIST* arg_types;
-  SPVM_LIST* arg_mem_ids;
-  const char* name;
   const char* abs_name;
-  const char* signature;
   SPVM_CLASS* class;
   const char* accessor_original_name;
   const char* anon_method_defined_class_name;
-  int32_t opcodes_base;
-  int32_t opcodes_length;
   int32_t call_method_arg_stack_max;
   int32_t rel_id;
   int32_t eval_stack_max_length;
-  int32_t mortal_stack_length;
-  int32_t id;
-  int32_t flag;
   int32_t args_alloc_length;
   int32_t vars_alloc_length;
-  int32_t byte_vars_alloc_length;
-  int32_t short_vars_alloc_length;
-  int32_t int_vars_alloc_length;
-  int32_t long_vars_alloc_length;
-  int32_t float_vars_alloc_length;
-  int32_t double_vars_alloc_length;
-  int32_t object_vars_alloc_length;
-  int32_t ref_vars_alloc_length;
   int32_t tmp_vars_length;
   int8_t have_vaarg;
   int8_t is_class_var_setter;
@@ -69,7 +73,6 @@ struct spvm_method {
   int8_t is_simple_constructor;
   int8_t is_constant;
   int8_t is_init;
-  int8_t is_class_method;
   int8_t can_precompile;
 };
 
