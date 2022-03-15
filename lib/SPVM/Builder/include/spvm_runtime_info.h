af22d81a791953549db9e227a21b9a51ededcf90
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-03-15 09:06:38 +0900

    Reorder runtime_info fields

--- lib/SPVM/Builder/include/spvm_runtime_info.h
+++ lib/SPVM/Builder/include/spvm_runtime_info.h
@@ -4,6 +4,9 @@
 #include "spvm_typedecl.h"
 
 struct spvm_runtime_info {
+  // Allocator
+  SPVM_ALLOCATOR* allocator;
+
   // Operation codes
   SPVM_OPCODE* opcodes;
 
@@ -19,27 +22,15 @@ struct spvm_runtime_info {
   // Strings - string literals and symbol names
   int32_t strings_length;
   
-  // String symtable
-  SPVM_HASH* string_symtable;
-
   // Runtime Basic types
   SPVM_LIST* basic_types;
   
-  // Runtime Basic type symbol table
-  SPVM_HASH* basic_type_symtable;
-
   // Runtime Basic types
   SPVM_LIST* types;
   
-  // Runtime Basic type symbol table
-  SPVM_HASH* type_symtable;
-
   // Runtime class types
   SPVM_LIST* classes;
   
-  // Runtime class symbol table
-  SPVM_HASH* class_symtable;
-
   // Runtime methods
   SPVM_LIST* methods;
 
@@ -58,7 +49,18 @@ struct spvm_runtime_info {
   // Runtime class variables of a class
   SPVM_RUNTIME_CLASS_VARS_OF_CLASS* class_vars_of_class;
   
-  SPVM_ALLOCATOR* allocator;
+  // String symtable
+  SPVM_HASH* string_symtable;
+
+  // Runtime Basic type symbol table
+  SPVM_HASH* basic_type_symtable;
+
+  // Runtime Basic type symbol table
+  SPVM_HASH* type_symtable;
+
+  // Runtime class symbol table
+  SPVM_HASH* class_symtable;
+
 };
 
 #endif
