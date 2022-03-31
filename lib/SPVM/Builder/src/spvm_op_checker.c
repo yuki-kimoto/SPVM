a5bc848a3e88177a1f70abddb286ccf58be4d578
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-03-31 15:32:19 +0900

    Cleanup SPVM_OP_CHECKER_check_assign string

--- lib/SPVM/Builder/src/spvm_op_checker.c
+++ lib/SPVM/Builder/src/spvm_op_checker.c
@@ -4450,34 +4450,30 @@ SPVM_OP* SPVM_OP_CHECKER_check_assign(SPVM_COMPILER* compiler, SPVM_TYPE* dist_t
       can_assign = 0;
     }
   }
+  // Dist type is string type
+  else if (SPVM_TYPE_is_string_type(compiler, dist_type->basic_type->id, dist_type->dimension, dist_type->flag)) {
+    // Source type is string
+    if (SPVM_TYPE_is_string_type(compiler, src_type->basic_type->id, src_type->dimension, src_type->flag)) {
+      can_assign = 1;
+    }
+    // Source type is number
+    else if (SPVM_TYPE_is_numeric_type(compiler, src_type->basic_type->id, src_type->dimension, src_type->flag)) {
+      can_assign = 1;
+      need_implicite_convertion = 1;
+    }
+    else if (SPVM_TYPE_is_undef_type(compiler, src_type->basic_type->id, src_type->dimension, src_type->flag)) {
+      can_assign = 1;
+    }
+    else {
+      can_assign = 0;
+    }
+  }
   // Dist type is object type
   else if (SPVM_TYPE_is_object_type(compiler, dist_type->basic_type->id, dist_type->dimension, dist_type->flag)) {
     // Dist type is any object array type
     if (SPVM_TYPE_is_any_object_array_type(compiler, dist_type->basic_type->id, dist_type->dimension, dist_type->flag)) {
       if (SPVM_TYPE_is_object_array_type(compiler, src_type->basic_type->id, src_type->dimension, src_type->flag)) {
         can_assign = 1;
-      }
-      else if (SPVM_TYPE_is_undef_type(compiler, src_type->basic_type->id, src_type->dimension, src_type->flag)) {
-        can_assign = 1;
-      }
-      else {
-        can_assign = 0;
-      }
-    }
-    // Dist type is string type
-    else if (SPVM_TYPE_is_string_type(compiler, dist_type->basic_type->id, dist_type->dimension, dist_type->flag)) {
-      // Source type is number
-      if (SPVM_TYPE_is_numeric_type(compiler, src_type->basic_type->id, src_type->dimension, src_type->flag)) {
-        can_assign = 1;
-        need_implicite_convertion = 1;
-      }
-      // Source type is string
-      else if (SPVM_TYPE_is_string_type(compiler, src_type->basic_type->id, src_type->dimension, src_type->flag)) {
-        can_assign = 1;
-      }
-      // Source type is byte array
-      else if (SPVM_TYPE_is_byte_array_type(compiler, src_type->basic_type->id, src_type->dimension, src_type->flag)) {
-        can_assign = 0;
       }
       else if (SPVM_TYPE_is_undef_type(compiler, src_type->basic_type->id, src_type->dimension, src_type->flag)) {
         can_assign = 1;
