21549c14a951f350a890129f20f95f7f16026fff
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-04-04 08:59:26 +0900

    Reorder spvm_runtime_basic_type members

--- lib/SPVM/Builder/include/spvm_runtime_basic_type.h
+++ lib/SPVM/Builder/include/spvm_runtime_basic_type.h
@@ -4,10 +4,10 @@
 #include "spvm_typedecl.h"
 
 struct spvm_runtime_basic_type {
-  int32_t name_id;
-  int32_t class_id;
   int32_t id;
   int32_t category;
+  int32_t name_id;
+  int32_t class_id;
 };
 
 #endif
