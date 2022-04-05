2888fef8bbff95350f079938afa4c78f05771512
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-04-06 08:43:21 +0900

    Cleanup SPVM_TYPE_is_numeric_array_type

--- lib/SPVM/Builder/src/spvm_type.c
+++ lib/SPVM/Builder/src/spvm_type.c
@@ -993,13 +993,17 @@ int32_t SPVM_TYPE_is_any_object_array_type(SPVM_COMPILER* compiler, int32_t basi
 int32_t SPVM_TYPE_is_numeric_array_type(SPVM_COMPILER* compiler, int32_t basic_type_id, int32_t dimension, int32_t flag) {
   (void)compiler;
   
-  if (dimension == 1 && (basic_type_id >= SPVM_NATIVE_C_BASIC_TYPE_ID_BYTE && basic_type_id <= SPVM_NATIVE_C_BASIC_TYPE_ID_DOUBLE) && !(flag & SPVM_TYPE_C_FLAG_REF))
-  {
-    return 1;
+  int32_t is_numeric_array_type;
+  int32_t basic_type_is_numeric_type = SPVM_BASIC_TYPE_is_numeric_type(compiler, basic_type_id);
+  if (dimension == 1 && basic_type_is_numeric_type && !(flag & SPVM_TYPE_C_FLAG_REF)) {
+    is_numeric_array_type = 1;
   }
+  // Array
   else {
-    return 0;
+    is_numeric_array_type = 0;
   }
+  
+  return is_numeric_array_type;
 }
 
 int32_t SPVM_TYPE_is_undef_type(SPVM_COMPILER* compiler, int32_t basic_type_id, int32_t dimension, int32_t flag) {
