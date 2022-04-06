3631a4bf301b01a4308f25368b980569c90b7b3d
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-04-06 11:33:00 +0900

    Support muldim array upcast

--- lib/SPVM/Builder/src/spvm_type.c
+++ lib/SPVM/Builder/src/spvm_type.c
@@ -1361,7 +1361,12 @@ int32_t SPVM_TYPE_can_assign(
           can_assign = 1;
         }
         else {
-          can_assign = 0;
+          if (SPVM_BASIC_TYPE_is_interface_type(compiler, dist_type_basic_type_id)) {
+            can_assign = SPVM_BASIC_TYPE_has_interface(compiler, src_type_basic_type_id, dist_type_basic_type_id);
+          }
+          else {
+            can_assign = 0;
+          }
         }
       }
       else {
