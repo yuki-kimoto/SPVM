3f812251e8403c4cfd1645229d0ed8d2df62c4a0
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-04-06 11:28:30 +0900

    Cleanup SPVM_API_can_assign_array_element

--- lib/SPVM/Builder/src/spvm_api.c
+++ lib/SPVM/Builder/src/spvm_api.c
@@ -8853,27 +8853,27 @@ int32_t SPVM_API_can_assign_array_element(SPVM_ENV* env, SPVM_OBJECT* array, SPV
       can_assign = 0;
     }
     else if (array_type_dimension == 1) {
-      switch (array_basic_type_category) {
-        case SPVM_NATIVE_C_BASIC_TYPE_CATEGORY_ANY_OBJECT:
-        {
-          can_assign = 1;
-          break;
-        }
-        case SPVM_NATIVE_C_BASIC_TYPE_CATEGORY_INTERFACE:
-        {
-          can_assign = SPVM_API_has_interface_by_id(env, array_basic_type_id, array_type_dimension - 1, element_basic_type_id, element_type_dimension);
-          break;
-        }
-        case SPVM_NATIVE_C_BASIC_TYPE_CATEGORY_CALLBACK:
-        {
-          can_assign = SPVM_API_has_callback_by_id(env, array_basic_type_id, array_type_dimension - 1, element_basic_type_id, element_type_dimension);
-          break;
-        }
-        default: {
-          if (array_basic_type_id == element_basic_type_id && array_type_dimension == element_type_dimension + 1) {
+      if (array_basic_type_id == element_basic_type_id && array_type_dimension == element_type_dimension + 1) {
+        can_assign = 1;
+      }
+      else {
+        switch (array_basic_type_category) {
+          case SPVM_NATIVE_C_BASIC_TYPE_CATEGORY_ANY_OBJECT:
+          {
             can_assign = 1;
+            break;
           }
-          else {
+          case SPVM_NATIVE_C_BASIC_TYPE_CATEGORY_INTERFACE:
+          {
+            can_assign = SPVM_API_has_interface_by_id(env, array_basic_type_id, array_type_dimension - 1, element_basic_type_id, element_type_dimension);
+            break;
+          }
+          case SPVM_NATIVE_C_BASIC_TYPE_CATEGORY_CALLBACK:
+          {
+            can_assign = SPVM_API_has_callback_by_id(env, array_basic_type_id, array_type_dimension - 1, element_basic_type_id, element_type_dimension);
+            break;
+          }
+          default: {
             can_assign = 0;
           }
         }
