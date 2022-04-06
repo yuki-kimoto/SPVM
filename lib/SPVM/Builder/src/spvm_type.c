059d8743802363d41811a55d97db4f7cfc587726
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-04-06 11:05:48 +0900

    Cleanup SPVM_TYPE_can_assign Dist type is Dist type is muldim array type

--- lib/SPVM/Builder/src/spvm_type.c
+++ lib/SPVM/Builder/src/spvm_type.c
@@ -1408,7 +1408,7 @@ int32_t SPVM_TYPE_can_assign(
   // Dist type is muldim array type
   else if (SPVM_TYPE_is_muldim_array_type(compiler, dist_type_basic_type_id, dist_type_dimension, dist_type_flag)) {
     // Source type is muldim
-    if (SPVM_TYPE_is_muldim_array_type(compiler, src_type_basic_type_id, src_type_dimension, src_type_flag)) {
+    if (SPVM_TYPE_is_object_type(compiler, src_type_basic_type_id, src_type_dimension, src_type_flag)) {
       if (dist_type_basic_type_id == src_type_basic_type_id && dist_type_dimension == src_type_dimension) {
         can_assign = 1;
       }
