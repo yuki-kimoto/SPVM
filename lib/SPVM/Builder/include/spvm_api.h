a186874897a64f275396d78040d9c7db89af1a55
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-04-07 14:27:41 +0900

    Remove SPVM_API_string_buffer_xxx

--- lib/SPVM/Builder/include/spvm_api.h
+++ lib/SPVM/Builder/include/spvm_api.h
@@ -353,11 +353,6 @@ int32_t SPVM_API_object_get_type_dimension(SPVM_OBJECT* object);
 
 int32_t SPVM_API_get_arg_type_id(SPVM_ENV* env, int32_t arg_id);
 
-SPVM_STRING_BUFFER* SPVM_API_string_buffer_new_tmp(SPVM_ALLOCATOR* allocator, int32_t capacity);
-const char* SPVM_API_string_buffer_get_value(SPVM_STRING_BUFFER* string_buffer);
-int32_t SPVM_API_string_buffer_get_length(SPVM_STRING_BUFFER* string_buffer);
-void SPVM_API_string_buffer_free(SPVM_STRING_BUFFER* string_buffer);
-
 const char* SPVM_API_precompile_create_precompile_source(SPVM_ENV* env, SPVM_STRING_BUFFER* string_buffer, const char* class_name);
 
 int32_t SPVM_API_get_method_name_id(SPVM_ENV* env, int32_t method_id);
--- lib/SPVM/Builder/src/spvm_api.c
+++ lib/SPVM/Builder/src/spvm_api.c
@@ -8445,24 +8445,6 @@ int32_t SPVM_API_get_anon_method_method_id(SPVM_ENV* env, int32_t anon_method_id
   return anon_method_method_id;
 }
 
-SPVM_STRING_BUFFER* SPVM_API_string_buffer_new_tmp(SPVM_ALLOCATOR* allocator, int32_t capacity) {
-  SPVM_STRING_BUFFER* string_buffer = SPVM_STRING_BUFFER_new_tmp(allocator, capacity);
-  
-  return string_buffer;
-}
-
-const char* SPVM_API_string_buffer_get_value(SPVM_STRING_BUFFER* string_buffer) {
-  return string_buffer->value;
-}
-
-int32_t SPVM_API_string_buffer_get_length(SPVM_STRING_BUFFER* string_buffer) {
-  return string_buffer->length;
-}
-
-void SPVM_API_string_buffer_free(SPVM_STRING_BUFFER* string_buffer) {
-  SPVM_STRING_BUFFER_free(string_buffer);
-}
-
 const char* SPVM_API_precompile_create_precompile_source(SPVM_ENV* env, SPVM_STRING_BUFFER* string_buffer, const char* class_name) {
   SPVM_PRECOMPILE_create_precompile_source(env, string_buffer, class_name);
 }
