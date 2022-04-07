c65a1b1b43997b3fdd5c27cf9f2d02fc8963e41c
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-04-07 12:59:48 +0900

    Remove env from allocator API

--- lib/SPVM/Builder/include/spvm_api_allocator.h
+++ lib/SPVM/Builder/include/spvm_api_allocator.h
@@ -4,9 +4,7 @@
 #include "spvm_typedecl.h"
 #include "spvm_native.h"
 
-
-SPVM_ENV_ALLOCATOR* SPVM_API_ALLOCATOR_new_env_raw();
-SPVM_ENV_ALLOCATOR* SPVM_API_ALLOCATOR_new();
-void SPVM_API_ALLOCATOR_free(SPVM_ENV_ALLOCATOR* env_allocator);
+SPVM_ALLOCATOR* SPVM_API_ALLOCATOR_new();
+void SPVM_API_ALLOCATOR_free(SPVM_ALLOCATOR* allocator);
 
 #endif
--- lib/SPVM/Builder/src/spvm_api_allocator.c
+++ lib/SPVM/Builder/src/spvm_api_allocator.c
@@ -3,20 +3,12 @@
 #include "spvm_allocator.h"
 #include "spvm_api_allocator.h"
 
-SPVM_ENV_ALLOCATOR* SPVM_API_ALLOCATOR_new_env_raw() {
-  
-  return NULL;
-}
-
-SPVM_ENV_ALLOCATOR* SPVM_API_ALLOCATOR_new() {
+SPVM_ALLOCATOR* SPVM_API_ALLOCATOR_new() {
   SPVM_ALLOCATOR* allocator = SPVM_ALLOCATOR_new(allocator);
 
-  SPVM_ENV_ALLOCATOR* env_allocator = SPVM_API_ALLOCATOR_new_env_raw();
-  env_allocator->allocator = allocator;
-  
-  return env_allocator;
+  return allocator;
 }
 
-void SPVM_API_ALLOCATOR_free(SPVM_ENV_ALLOCATOR* env_allocator) {
+void SPVM_API_ALLOCATOR_free(SPVM_ALLOCATOR* allocator) {
   SPVM_ALLOCATOR_free(allocator);
 }
