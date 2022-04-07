eb2f9e676ee863a044efdf7d32bdd6e8fea8e2d3
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-04-07 13:29:45 +0900

    Fix the compilation error of lib/SPVM/Builder/src/spvm_api_string_buffer.c

--- lib/SPVM/Builder/src/spvm_api_string_buffer.c
+++ lib/SPVM/Builder/src/spvm_api_string_buffer.c
@@ -11,14 +11,14 @@ SPVM_STRING_BUFFER* SPVM_API_STRING_BUFFER_new_tmp(SPVM_ALLOCATOR* allocator, in
   return string_buffer;
 }
 
-const char* SPVM_API_STRING_BUFFER_get_value(SPVM_STRING_BUFFER* env_string_buffer) {
+const char* SPVM_API_STRING_BUFFER_get_value(SPVM_STRING_BUFFER* string_buffer) {
   return string_buffer->value;
 }
 
-int32_t SPVM_API_STRING_BUFFER_get_length(SPVM_STRING_BUFFER* env_string_buffer) {
+int32_t SPVM_API_STRING_BUFFER_get_length(SPVM_STRING_BUFFER* string_buffer) {
   return string_buffer->length;
 }
 
-void SPVM_API_STRING_BUFFER_free(SPVM_STRING_BUFFER* env_string_buffer) {
+void SPVM_API_STRING_BUFFER_free(SPVM_STRING_BUFFER* string_buffer) {
   SPVM_STRING_BUFFER_free(string_buffer);
 }
