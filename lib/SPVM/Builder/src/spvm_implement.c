d47636bc31494abc286a9dbac1c093e2e4f4df15
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-04-07 13:29:02 +0900

    deleted:    lib/SPVM/Builder/include/spvm_implement.h deleted:    lib/SPVM/Builder/src/spvm_implement.c

--- lib/SPVM/Builder/src/spvm_implement.c
+++
@@ -1,9 +0,0 @@
-#include "spvm_interface.h"
-#include "spvm_allocator.h"
-#include "spvm_compiler.h"
-
-SPVM_INTERFACE* SPVM_INTERFACE_new(SPVM_COMPILER* compiler) {
-  SPVM_INTERFACE* interface = SPVM_ALLOCATOR_alloc_memory_block_tmp(compiler->allocator, sizeof(SPVM_INTERFACE));
-  
-  return interface;
-}
