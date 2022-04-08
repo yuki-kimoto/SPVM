450bd1b2958d58116aea6d911095986db90ec915
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-04-08 10:57:52 +0900

    Use env compiler api more

--- solo/myexe.c
+++ solo/myexe.c
@@ -75,24 +75,24 @@ SPVM_ENV* SPVM_NATIVE_new_env_prepared() {
   SPVM_ENV* env = SPVM_NATIVE_new_env_raw();
   
   // Create compiler
-  void* compiler = SPVM_API_COMPILER_new_compiler();
+  void* compiler = env->api->compiler->new_compiler();
   
   // compiler->debug = 1;
   
-  SPVM_API_COMPILER_set_start_file(compiler, class_name);
+  env->api->compiler->set_start_file(compiler, class_name);
 
-  SPVM_API_COMPILER_set_start_line(compiler, 0);
+  env->api->compiler->set_start_line(compiler, 0);
   
   // Add module directory
   char* module_dir = "solo/SPVM";
-  SPVM_API_COMPILER_add_module_dir(compiler, module_dir);
+  env->api->compiler->add_module_dir(compiler, module_dir);
 
-  int32_t compile_error_code = SPVM_API_COMPILER_compile_spvm(compiler, class_name);
+  int32_t compile_error_code = env->api->compiler->compile_spvm(compiler, class_name);
   
   if (compile_error_code != 0) {
-    int32_t error_messages_length = SPVM_API_COMPILER_get_error_messages_length(compiler);
+    int32_t error_messages_length = env->api->compiler->get_error_messages_length(compiler);
     for (int32_t i = 0; i < error_messages_length; i++) {
-      const char* error_message = SPVM_API_COMPILER_get_error_message(compiler, i);
+      const char* error_message = env->api->compiler->get_error_message(compiler, i);
       fprintf(stderr, "%s\n", error_message);
     }
     exit(255);
@@ -100,9 +100,9 @@ SPVM_ENV* SPVM_NATIVE_new_env_prepared() {
 
   // Build runtime information
   void* runtime = SPVM_API_RUNTIME_new_runtime(env);
-  SPVM_API_COMPILER_build_runtime(compiler, runtime);
+  env->api->compiler->build_runtime(compiler, runtime);
   
-  SPVM_API_COMPILER_free_compiler(compiler);
+  env->api->compiler->free_compiler(compiler);
   
   // Prepare runtime
   SPVM_API_RUNTIME_prepare(runtime);
