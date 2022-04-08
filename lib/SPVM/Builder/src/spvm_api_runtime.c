2a348e76e4ea549f3be75a31575b1cfa42468bcf
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-04-08 14:48:52 +0900

    Add type apis to runtime api

--- lib/SPVM/Builder/src/spvm_api_runtime.c
+++ lib/SPVM/Builder/src/spvm_api_runtime.c
@@ -8,6 +8,9 @@
 #include <stdarg.h>
 
 #include "spvm_native.h"
+
+#include "spvm_type.h"
+
 #include "spvm_list.h"
 #include "spvm_hash.h"
 
@@ -132,3 +135,100 @@ SPVM_RUNTIME_BASIC_TYPE* SPVM_API_RUNTIME_get_basic_type(SPVM_RUNTIME* runtime, 
   
   return basic_type;
 }
+
+SPVM_RUNTIME_TYPE* SPVM_API_RUNTIME_get_type(SPVM_RUNTIME* runtime, int32_t type_id) {
+  
+  if (type_id < 0) {
+    return NULL;
+  }
+  
+  if (type_id >= runtime->types_length) {
+    return NULL;
+  }
+
+  SPVM_RUNTIME_TYPE* type = &runtime->types[type_id];
+  
+  return type;
+}
+
+int32_t SPVM_API_RUNTIME_get_type_basic_type_id(SPVM_RUNTIME* runtime, int32_t type_id) {
+  
+  SPVM_RUNTIME_TYPE* type = SPVM_API_RUNTIME_get_type(runtime, type_id);
+  
+  assert(type);
+  
+  int32_t type_basic_type_id = type->basic_type_id;
+  
+  return type_basic_type_id;
+}
+
+int32_t SPVM_API_RUNTIME_get_type_dimension(SPVM_RUNTIME* runtime, int32_t type_id) {
+  
+  SPVM_RUNTIME_TYPE* type = SPVM_API_RUNTIME_get_type(runtime, type_id);
+  
+  assert(type);
+  
+  int32_t type_dimension = type->dimension;
+  
+  return type_dimension;
+}
+
+int32_t SPVM_API_RUNTIME_get_type_width(SPVM_RUNTIME* runtime, int32_t type_id) {
+  
+  SPVM_RUNTIME_TYPE* type = SPVM_API_RUNTIME_get_type(runtime, type_id);
+  
+  assert(type);
+  
+  int32_t type_width = type->width;
+  
+  return type_width;
+}
+
+int32_t SPVM_API_RUNTIME_get_type_is_ref(SPVM_RUNTIME* runtime, int32_t type_id) {
+  
+  SPVM_RUNTIME_TYPE* type = SPVM_API_RUNTIME_get_type(runtime, type_id);
+  
+  assert(type);
+  
+  int32_t is_ref = type->flag & SPVM_TYPE_C_FLAG_REF;
+  
+  return is_ref;
+}
+
+int32_t SPVM_API_RUNTIME_get_type_is_object(SPVM_RUNTIME* runtime, int32_t type_id) {
+  
+  SPVM_RUNTIME_TYPE* type = SPVM_API_RUNTIME_get_type(runtime, type_id);
+  int32_t type_dimension = type->dimension;
+  int32_t basic_type_id = type->basic_type_id;
+  int32_t basic_type_category = SPVM_API_RUNTIME_get_basic_type_category(runtime, basic_type_id);
+  
+  assert(type);
+  
+  int32_t is_object;
+  
+  if (type_dimension == 0) {
+    switch (basic_type_category) {
+      case SPVM_NATIVE_C_BASIC_TYPE_CATEGORY_STRING:
+      case SPVM_NATIVE_C_BASIC_TYPE_CATEGORY_CLASS:
+      case SPVM_NATIVE_C_BASIC_TYPE_CATEGORY_INTERFACE:
+      case SPVM_NATIVE_C_BASIC_TYPE_CATEGORY_CALLBACK:
+      case SPVM_NATIVE_C_BASIC_TYPE_CATEGORY_ANY_OBJECT:
+      {
+        is_object = 1;
+        break;
+      }
+      default: {
+        is_object = 0;
+      }
+    }
+  }
+  else if (type_dimension > 0) {
+    is_object = 1;
+  }
+  else {
+    assert(0);
+  }
+  
+  return is_object;
+}
+
