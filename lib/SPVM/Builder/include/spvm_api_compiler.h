0a2241ff83c13b5a94979b9d8306fe839256c393
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-04-13 08:25:04 +0900

    Remove not used native compiler declarations

--- lib/SPVM/Builder/include/spvm_api_compiler.h
+++ lib/SPVM/Builder/include/spvm_api_compiler.h
@@ -4,27 +4,6 @@
 #include "spvm_typedecl.h"
 #include "spvm_native.h"
 
-int32_t SPVM_API_COMPILER_get_next_method_id_flag(SPVM_COMPILER* compiler, const char* method_abs_name, int32_t start_index, int32_t flag);
-int32_t SPVM_API_COMPILER_get_class_id(SPVM_COMPILER* compiler, const char* class_name);
-int32_t SPVM_API_COMPILER_get_classes_length(SPVM_COMPILER* compiler);
-const char* SPVM_API_COMPILER_get_class_name(SPVM_COMPILER* compiler, int32_t class_id);
-const char* SPVM_API_COMPILER_get_class_module_file(SPVM_COMPILER* compiler, int32_t class_id);
-int32_t SPVM_API_COMPILER_get_method_class_id(SPVM_COMPILER* compiler, int32_t method_id);
-int32_t SPVM_API_COMPILER_is_anon_class(SPVM_COMPILER* compiler, int32_t class_id);
-int32_t SPVM_API_COMPILER_get_methods_length(SPVM_COMPILER* compiler, int32_t class_id);
-int32_t SPVM_API_COMPILER_get_method_id(SPVM_COMPILER* compiler, int32_t class_id, int32_t method_index_of_class);
-int32_t SPVM_API_COMPILER_get_method_id_by_name(SPVM_COMPILER* compiler, const char* class_name, const char* method_name);
-const char* SPVM_API_COMPILER_get_method_name(SPVM_COMPILER* compiler, int32_t method_id);
-int32_t SPVM_API_COMPILER_is_anon_method(SPVM_COMPILER* compiler, int32_t method_id);
-int32_t SPVM_API_COMPILER_is_init_block_method(SPVM_COMPILER* compiler, int32_t method_id);
-int32_t SPVM_API_COMPILER_is_native_method(SPVM_COMPILER* compiler, int32_t method_id);
-int32_t SPVM_API_COMPILER_is_precompile_method(SPVM_COMPILER* compiler, int32_t method_id);
-const char* SPVM_API_COMPILER_get_method_signature(SPVM_COMPILER* compiler, int32_t method_id);
-void* SPVM_API_COMPILER_get_native_method_address(SPVM_COMPILER* compiler, int32_t method_id);
-void* SPVM_API_COMPILER_get_precompile_method_address(SPVM_COMPILER* compiler, int32_t method_id);
-void SPVM_API_COMPILER_set_native_method_address(SPVM_COMPILER* compiler, int32_t method_id, void* address);
-void SPVM_API_COMPILER_set_precompile_method_address(SPVM_COMPILER* compiler, int32_t method_id, void* address);
-const char* SPVM_API_COMPILER_get_method_abs_name(SPVM_COMPILER* compiler, int32_t method_id);
 SPVM_COMPILER* SPVM_API_COMPILER_new_compiler();
 void SPVM_API_COMPILER_set_start_line(SPVM_COMPILER* compiler, int32_t start_line);
 int32_t SPVM_API_COMPILER_get_start_line(SPVM_COMPILER* compiler);
