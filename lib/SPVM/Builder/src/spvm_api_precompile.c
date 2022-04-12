b78110480797a1f95e74d8c61454f7595c10e081
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-04-13 08:41:45 +0900

    Create spvm_api_precompile.h and add SPVM_API_PRECOMPILE_create_precompile_source

--- lib/SPVM/Builder/src/spvm_api_precompile.c
+++ lib/SPVM/Builder/src/spvm_api_precompile.c
@@ -3,20 +3,23 @@
 #include "spvm_precompile.h"
 #include "spvm_api_precompile.h"
 
-SPVM_PRECOMPILE* SPVM_API_PRECOMPILE_new_precompile() {
-  SPVM_PRECOMPILE* precompile = SPVM_PRECOMPILE_new(precompile);
-
+SPVM_API_PRECOMPILE* SPVM_API_PRECOMPILE_new_precompile() {
+  SPVM_API_PRECOMPILE* precompile = SPVM_API_PRECOMPILE_new(precompile);
   return precompile;
 }
 
-void SPVM_API_PRECOMPILE_free_precompile(SPVM_PRECOMPILE* precompile) {
-  SPVM_PRECOMPILE_free(precompile);
+void SPVM_API_PRECOMPILE_free_precompile(SPVM_API_PRECOMPILE* precompile) {
+  SPVM_API_PRECOMPILE_free(precompile);
 }
 
-void SPVM_PRECOMPILE_set_runtime(SPVM_PRECOMPILE* precompile, SPVM_RUNTIME* runtime) {
-  SPVM_PRECOMPILE_set_runtime(precompile, runtime);
+void SPVM_API_PRECOMPILE_set_runtime(SPVM_API_PRECOMPILE* precompile, SPVM_RUNTIME* runtime) {
+  SPVM_API_PRECOMPILE_set_runtime(precompile, runtime);
 }
 
-SPVM_RUNTIME* SPVM_PRECOMPILE_get_runtime(SPVM_PRECOMPILE* precompile) {
-  return SPVM_PRECOMPILE_get_runtime(precompile, runtime);
+SPVM_RUNTIME* SPVM_API_PRECOMPILE_get_runtime(SPVM_API_PRECOMPILE* precompile) {
+  return SPVM_API_PRECOMPILE_get_runtime(precompile, runtime);
+}
+
+void SPVM_API_PRECOMPILE_create_precompile_source(SPVM_API_PRECOMPILE* precompile, SPVM_STRING_BUFFER* string_buffer, const char* class_name) {
+  SPVM_PRECOMPILE_create_precompile_source(precompile, string_buffer, class_name);
 }
