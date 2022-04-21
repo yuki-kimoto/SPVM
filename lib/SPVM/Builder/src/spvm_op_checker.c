fbb4a062c9e8e569af66e8b981244f717b622fae
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-04-22 08:00:44 +0900

    Remove can_precompile from spvm_method

--- lib/SPVM/Builder/src/spvm_op_checker.c
+++ lib/SPVM/Builder/src/spvm_op_checker.c
@@ -4927,7 +4927,7 @@ void SPVM_OP_CHECKER_resolve_classes(SPVM_COMPILER* compiler) {
       SPVM_METHOD* method = SPVM_LIST_get(class->methods, i);
       
       // Set method precompile flag if class have precompile descriptor
-      if (class->is_precompile && method->can_precompile) {
+      if (class->is_precompile && method->is_precompile) {
         method->is_precompile = 1;
       }
 
