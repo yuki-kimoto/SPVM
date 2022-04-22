e4e8bfe1177f05ba6382e6ded5f855eddb1cf207
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-04-23 06:03:53 +0900

    Add config dependent tests

--- lib/SPVM/Builder/Exe.pm
+++ lib/SPVM/Builder/Exe.pm
@@ -179,6 +179,7 @@ sub new {
     unless ($config->is_exe) {
       confess "Config file \"$config_file\" is not the config to create the executable file";
     }
+    use D;du $config->dependent_files;
   }
   else {
     $config = SPVM::Builder::Config::Exe->new_gnu99;
