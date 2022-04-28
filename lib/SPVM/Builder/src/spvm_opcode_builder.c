719d955f8c9a69d0d62d17f12dc1113d5bbff687
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-04-28 16:17:37 +0900

    Cleanup type cast to Double object

--- lib/SPVM/Builder/src/spvm_opcode_builder.c
+++ lib/SPVM/Builder/src/spvm_opcode_builder.c
@@ -3111,6 +3111,27 @@ void SPVM_OPCODE_BUILDER_build_opcode_array(SPVM_COMPILER* compiler) {
                                 assert(0);
                               }
                             }
+                            // To Double object
+                            else if (SPVM_TYPE_is_double_object_type(compiler, cast_type->basic_type->id, cast_type->dimension, cast_type->flag)) {
+                              if (SPVM_TYPE_is_double_object_type(compiler, src_type->basic_type->id, src_type->dimension, src_type->flag)) {
+                                SPVM_OPCODE_BUILDER_set_opcode_id(compiler, &opcode, SPVM_OPCODE_C_ID_MOVE_OBJECT);
+                                use_new_logic = 1;
+                              }
+                              else if (SPVM_TYPE_is_double_type(compiler, src_type->basic_type->id, src_type->dimension, src_type->flag)) {
+                                SPVM_OPCODE_BUILDER_set_opcode_id(compiler, &opcode, SPVM_OPCODE_C_ID_TYPE_CAST_DOUBLE_TO_DOUBLE_OBJECT);
+                                use_new_logic = 1;
+                              }
+                              else if (SPVM_TYPE_is_any_object_type(compiler, src_type->basic_type->id, src_type->dimension, src_type->flag)) {
+                                SPVM_OPCODE_BUILDER_set_opcode_id(compiler, &opcode, SPVM_OPCODE_C_ID_TYPE_CAST_EQUAL_OBJECT);
+                                opcode.operand2 = op_cast_type->uv.type->basic_type->id;
+                                opcode.operand3 = op_cast_type->uv.type->dimension;
+                                throw_exception = 1;
+                                use_new_logic = 1;
+                              }
+                              else {
+                                assert(0);
+                              }
+                            }
                             
                             if (!use_new_logic) {
 
