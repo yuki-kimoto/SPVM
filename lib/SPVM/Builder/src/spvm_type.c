aadaa52b89f6c4a651f74e76fe740945703e05a1
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-05-01 07:46:37 +0900

    Cleanup SPVM_TYPE_check_castability: Fix Cast type is callback type. More strict

--- lib/SPVM/Builder/src/spvm_type.c
+++ lib/SPVM/Builder/src/spvm_type.c
@@ -1559,6 +1559,27 @@ int32_t SPVM_TYPE_check_castability(
       castability = 0;
     }
   }
+  // Cast type is callback type
+  else if (SPVM_TYPE_is_callback_type(compiler, cast_type_basic_type_id, cast_type_dimension, cast_type_flag)) {
+    if (SPVM_TYPE_is_class_type(compiler, src_type_basic_type_id, src_type_dimension, src_type_flag)) {
+      castability = SPVM_BASIC_TYPE_has_callback(compiler, src_type_basic_type_id, cast_type_basic_type_id);
+    }
+    else if (SPVM_TYPE_is_interface_type(compiler, src_type_basic_type_id, src_type_dimension, src_type_flag)) {
+      castability = 0;
+    }
+    else if (SPVM_TYPE_is_callback_type(compiler, src_type_basic_type_id, src_type_dimension, src_type_flag)) {
+      castability = 1;
+    }
+    else if (SPVM_TYPE_is_any_object_type(compiler, src_type_basic_type_id, src_type_dimension, src_type_flag)) {
+      castability = 1;
+    }
+    else if (SPVM_TYPE_is_undef_type(compiler, src_type_basic_type_id, src_type_dimension, src_type_flag)) {
+      castability = 1;
+    }
+    else {
+      castability = 0;
+    }
+  }
   // Cast type is object type
   else if (SPVM_TYPE_is_object_type(compiler, cast_type_basic_type_id, cast_type_dimension, cast_type_flag)) {
     // Cast type is any object array type
