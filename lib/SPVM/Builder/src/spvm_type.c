da3856b4e32cbde049b2b7fe63be6ba4f4a400ae
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-05-01 08:07:25 +0900

    Cleanup SPVM_TYPE_check_castability: Cleanup Cast type is any object array type. Simplify the logic

--- lib/SPVM/Builder/src/spvm_type.c
+++ lib/SPVM/Builder/src/spvm_type.c
@@ -1741,39 +1741,36 @@ int32_t SPVM_TYPE_check_castability(
       castability = 0;
     }
   }
+  // Cast type is any object array type
+  else if (SPVM_TYPE_is_any_object_array_type(compiler, cast_type_basic_type_id, cast_type_dimension, cast_type_flag)) {
+    if (SPVM_TYPE_is_any_object_type(compiler, src_type_basic_type_id, src_type_dimension, src_type_flag)) {
+      castability = 1;
+    }
+    else if (SPVM_TYPE_is_object_array_type(compiler, src_type_basic_type_id, src_type_dimension, src_type_flag)) {
+      castability = 1;
+    }
+    else if (SPVM_TYPE_is_undef_type(compiler, src_type_basic_type_id, src_type_dimension, src_type_flag)) {
+      castability = 1;
+    }
+    else {
+      castability = 0;
+    }
+  }
   // Cast type is object type
   else if (SPVM_TYPE_is_object_type(compiler, cast_type_basic_type_id, cast_type_dimension, cast_type_flag)) {
-    // Cast type is any object array type
-    if (SPVM_TYPE_is_any_object_array_type(compiler, cast_type_basic_type_id, cast_type_dimension, cast_type_flag)) {
-      if (SPVM_TYPE_is_object_array_type(compiler, src_type_basic_type_id, src_type_dimension, src_type_flag)) {
-        castability = 1;
-      }
-      else if (SPVM_TYPE_is_any_object_type(compiler, src_type_basic_type_id, src_type_dimension, src_type_flag)) {
-        castability = 1;
-      }
-      else if (SPVM_TYPE_is_undef_type(compiler, src_type_basic_type_id, src_type_dimension, src_type_flag)) {
-        castability = 1;
-      }
-      else {
-        castability = 0;
-      }
+    // Source type is object type
+    if (SPVM_TYPE_is_object_type(compiler, src_type_basic_type_id, src_type_dimension, src_type_flag)) {
+      castability = 1;
     }
-    // Cast type is ohter object types
+    // Source type is undef type
+    else if (SPVM_TYPE_is_undef_type(compiler, src_type_basic_type_id, src_type_dimension, src_type_flag)) {
+      castability = 1;
+    }
+    else if (SPVM_TYPE_is_undef_type(compiler, src_type_basic_type_id, src_type_dimension, src_type_flag)) {
+      castability = 1;
+    }
     else {
-      // Source type is object type
-      if (SPVM_TYPE_is_object_type(compiler, src_type_basic_type_id, src_type_dimension, src_type_flag)) {
-        castability = 1;
-      }
-      // Source type is undef type
-      else if (SPVM_TYPE_is_undef_type(compiler, src_type_basic_type_id, src_type_dimension, src_type_flag)) {
-        castability = 1;
-      }
-      else if (SPVM_TYPE_is_undef_type(compiler, src_type_basic_type_id, src_type_dimension, src_type_flag)) {
-        castability = 1;
-      }
-      else {
-        castability = 0;
-      }
+      castability = 0;
     }
   }
   else {
