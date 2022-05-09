4fd27b986ef9045472d502ca70aa1f4f9c69093b
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-05-10 08:13:40 +0900

    Improve EqualityChecker doc

--- lib/SPVM/EqualityChecker.pm
+++ lib/SPVM/EqualityChecker.pm
@@ -4,13 +4,14 @@ package SPVM::EqualityChecker;
 
 =head1 NAME
 
-SPVM::EqualityChecker - A Callback Type to Check the Object Equality
+SPVM::EqualityChecker - Interface Type for Object Equality Checking Callback
 
 =head1 SYNOPSYS
   
   use EqualityChecker;
+  use Point;
   
-  my $equality_checker : EqualityChecker = method : int ($object1 : object, $object2 : object) {
+  my $equality_checker = (EqualityChecker)method : int ($object1 : object, $object2 : object) {
     my $point1 = (Point)$object1;
     my $point2 = (Point)$object2;
     
@@ -22,17 +23,17 @@ SPVM::EqualityChecker - A Callback Type to Check the Object Equality
     }
   };
   
-  my $point1 = Point->new(1, 2);
-  my $point2 = Point->new(5, 6);
+  my $point1 = Point->new_xy(1, 2);
+  my $point2 = Point->new_xy(5, 6);
   
   my $is_equal = $equality_checker->($point1, $point2);
 
 =head1 DESCRIPTION
 
-L<EqualityChecker|SPVM::EqualityChecker> is an interface type for the callback to check the object equality.
+L<EqualityChecker|SPVM::EqualityChecker> is the interface type for the object equality checking callback.
 
-=head1 INTERFACE METHOD
+=head1 INTERFACE METHODS
 
   required method : int ($object1 : object, $object2 : object)
 
-The implementation must receive two objects and if they are equals, return C<1>, otherwise return C<0>.
+The implementation must receive two objects and if they are equal, return C<1>, otherwise return C<0>.
