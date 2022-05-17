6c6962684daf975164e99be5ecce1284ddcf4749
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-05-17 13:05:56 +0900

    Use native sprintf logic in d specifier

--- lib/SPVM/Fn.c
+++ lib/SPVM/Fn.c
@@ -204,13 +204,28 @@ int32_t SPVM__Fn___snsprintf_double(SPVM_ENV* env, SPVM_VALUE* stack) {
   return 0;
 }
 
+int32_t SPVM__Fn___native_snprintf_d(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+
+  int32_t value = stack[0].ival;
+  
+  // "-2147483648"
+  char formatted_string[12] = {0};
+
+  int32_t length = snprintf(formatted_string, 12, "%"PRId32, value);
+
+  stack[0].oval = env->new_string(env, formatted_string, length);
+
+  return 0;
+}
+
 int32_t SPVM__Fn___native_snprintf_x(SPVM_ENV* env, SPVM_VALUE* stack) {
   (void)env;
 
   int32_t value = stack[0].ival;
   
   // "ffffffff"
-  char formatted_string[9] = {};
+  char formatted_string[9] = {0};
 
   int32_t length = snprintf(formatted_string, 9, "%"PRIx32, (uint32_t)value);
 
@@ -225,7 +240,7 @@ int32_t SPVM__Fn___native_snprintf_X(SPVM_ENV* env, SPVM_VALUE* stack) {
   int32_t value = stack[0].ival;
   
   // "FFFFFFFF"
-  char formatted_string[9] = {};
+  char formatted_string[9] = {0};
 
   int32_t length = snprintf(formatted_string, 9, "%"PRIx32, (uint32_t)value);
 
