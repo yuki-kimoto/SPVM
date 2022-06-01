518819bbcd752c2d0c39a534fe2fd8a5e821100d
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-06-02 07:29:43 +0900

    Rename Regex::Pattern to Regex::Opcode

---
+++ lib/SPVM/Regex/Opcode.pm
@@ -0,0 +1,15 @@
+package SPVM::Regex::Opcode;
+
+1;
+
+=head1 NAME
+
+SPVM::Regex::Opcode - Regular expression pattern
+
+=head1 SYNOPSYS
+  
+  use Regex::Opcode;
+
+=head1 DESCRIPTION
+
+L<Regex::Opcode|SPVM::Regex::Opcode> is a internal data structure which represents regular expression patterns for L<Regex|SPVM::Regex>.
