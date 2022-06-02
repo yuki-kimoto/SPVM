71b8d97931cdfde1dfbfe6277b42fcae8d080532
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-06-02 17:22:37 +0900

    Update Regex doc

--- lib/SPVM/Regex.pm
+++ lib/SPVM/Regex.pm
@@ -119,8 +119,6 @@ L<Regex|SPVM::Regex> provides the methodset of Perl regular expression. The targ
   *     more than or equals to 0 repeats
   ?     0 or 1 repeats
   {m,n} repeats between m and n
-  {m,n} repeats between m and n
-  {m,n} repeats between m and n
   
   # Regular expression character
   ^    first of string
