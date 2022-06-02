9744e4cf42d1aaca250a9e297f3b45f1c5411fc1
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-06-02 15:33:52 +0900

    renamed:    lib/SPVM/Regex/Opcode.spvm -> lib/SPVM/Regex/Expression.spvm

---
+++ lib/SPVM/Regex/Expression.pm
@@ -0,0 +1,15 @@
+package SPVM::Regex::Expression;
+
+1;
+
+=head1 NAME
+
+SPVM::Regex::Expression - Regular expression pattern
+
+=head1 SYNOPSYS
+  
+  use Regex::Expression;
+
+=head1 DESCRIPTION
+
+L<Regex::Expression|SPVM::Regex::Expression> is a internal data structure which represents regular expression patterns for L<Regex|SPVM::Regex>.
