59212bd774df631a043265e9be8a61ae00985015
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-06-14 14:24:04 +0900

    lib/SPVM/Builder/CC.pm:dl_func_list logic move forward

--- lib/SPVM/Builder/CC.pm
+++ lib/SPVM/Builder/CC.pm
@@ -617,6 +617,30 @@ sub link {
   # Category
   my $category = $self->category;
 
+  # dl_func_list
+  # This option is needed Windows DLL file
+  my $dl_func_list = [];
+  my $method_names = $self->builder->get_method_names($class_name, $category);
+  for my $method_name (@$method_names) {
+    my $cfunc_name = SPVM::Builder::Util::create_cfunc_name($class_name, $method_name, $category);
+    push @$dl_func_list, $cfunc_name;
+  }
+  
+  if ($category eq 'precompile') {
+    # Add anon class sub names to dl_func_list
+    my $anon_class_names = $self->builder->get_anon_class_names_by_parent_class_name($class_name);
+    
+    for my $anon_class_name (@$anon_class_names) {
+      my $anon_method_cfunc_name = SPVM::Builder::Util::create_cfunc_name($anon_class_name, "", $category);
+      push @$dl_func_list, $anon_method_cfunc_name;
+    }
+  }
+
+  # This is bad hack to suppress boot strap function error.
+  unless (@$dl_func_list) {
+    push @$dl_func_list, '';
+  }
+
   # Build directory
   my $build_dir = $self->builder->build_dir;
   if (defined $build_dir) {
@@ -711,30 +735,6 @@ sub link {
     }
   }
   
-  # dl_func_list
-  # This option is needed Windows DLL file
-  my $dl_func_list = [];
-  my $method_names = $self->builder->get_method_names($class_name, $category);
-  for my $method_name (@$method_names) {
-    my $cfunc_name = SPVM::Builder::Util::create_cfunc_name($class_name, $method_name, $category);
-    push @$dl_func_list, $cfunc_name;
-  }
-  
-  if ($category eq 'precompile') {
-    # Add anon class sub names to dl_func_list
-    my $anon_class_names = $self->builder->get_anon_class_names_by_parent_class_name($class_name);
-    
-    for my $anon_class_name (@$anon_class_names) {
-      my $anon_method_cfunc_name = SPVM::Builder::Util::create_cfunc_name($anon_class_name, "", $category);
-      push @$dl_func_list, $anon_method_cfunc_name;
-    }
-  }
-
-  # This is bad hack to suppress boot strap function error.
-  unless (@$dl_func_list) {
-    push @$dl_func_list, '';
-  }
-
   # Linker
   my $ld = $config->ld;
 
