6b41f586f02282c3e5690e26fa79870dee85bc0d
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-06-19 08:34:10 +0900

    Do the following replacement find * | grep -P '\.(spvm|pm|pod)$' | grep -v ppport | grep -v 'Fn.c' | xargs perl -pi -e 's/errno/error_code/g' And rename errno keyword to error_code

--- lib/SPVM/Builder/src/spvm_toke.c
+++ lib/SPVM/Builder/src/spvm_toke.c
@@ -2048,7 +2048,7 @@ int SPVM_yylex(SPVM_YYSTYPE* yylvalp, SPVM_COMPILER* compiler) {
                   yylvalp->opval = SPVM_TOKE_new_op(compiler, SPVM_OP_C_ID_STRING_EQ);
                   keyword_token = STREQ;
                 }
-                else if (strcmp(symbol_name, "errno") == 0) {
+                else if (strcmp(symbol_name, "error_code") == 0) {
                   yylvalp->opval = SPVM_TOKE_new_op(compiler, SPVM_OP_C_ID_ERROR_CODE);
                   keyword_token = ERROR_CODE;
                 }
