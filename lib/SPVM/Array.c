7639628eab4c65c743a17ff58816c445bcda31a3
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-06-27 10:51:05 +0900

    Cleanup headers

--- lib/SPVM/Array.c
+++ lib/SPVM/Array.c
@@ -1,27 +1,6 @@
 #include "spvm_native.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
-#include <inttypes.h>
-#include <float.h>
-#include <complex.h>
-#include <memory.h>
-#include <ctype.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-#include <inttypes.h>
-#include <float.h>
-#include <complex.h>
-#include <memory.h>
-#include <ctype.h>
-#include <errno.h>
-#include <stddef.h>
-#include <assert.h>
 
 static const char* MFILE = "SPVM/Array.c";
 
