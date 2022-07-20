a6f0b42e5b7958d0ee03abe137f4168a181c85df
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-07-20 13:29:10 +0900

    Add Error::System class.

---
+++ lib/SPVM/Error/System.pm
@@ -0,0 +1,15 @@
+package SPVM::Error::System;
+
+1;
+
+=head1 Name
+
+SPVM::Error::System - System Error
+
+=head1 Usage
+
+  use Error::System;
+
+=head1 Description
+
+C<Error::System> is the system error class.
