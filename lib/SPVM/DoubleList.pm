71b9c57e14fb8378c4488d5467a52e15499ea6d7
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-07-24 09:00:17 +0900

    Improve FloatList error message and tests

--- lib/SPVM/DoubleList.pm
+++ lib/SPVM/DoubleList.pm
@@ -4,52 +4,52 @@ package SPVM::DoubleList;
 
 =head1 Name
 
-SPVM::DoubleList - Dynamic double array
+SPVM::DoubleList - Dynamic double Array
 
 =head1 Usage
   
   use DoubleList;
   
-  # Create a double list with array
-  my $double_list = DoubleList->new;
-  my $double_list = DoubleList->new([1.5, 2.5, 3.5]);
-  
   # Create a double list
-  my $double_list = DoubleList->new_len(3);
-
+  my $list = DoubleList->new;
+  my $list = DoubleList->new([(double)1, 2, 3]);
+  
+  # Create a double list with array length
+  my $list = DoubleList->new_len(10);
+  
   # Get list length
-  my $length = $double_list->length;
+  my $length = $list->length;
   
-  # Push double value
-  $double_list->push(3.5);
-
-  # Pop double value.
-  my $double_value = $double_list->pop;
-
-  # Unshift double value.
-  $double_list->unshift(3.5);
+  # Push value
+  $list->push((double)3);
   
-  # Shift double value.
-  my $double_value = $double_list->shift;
+  # Pop value.
+  my $value = $list->pop;
   
-  # Set double value.
-  $double_list->set(2, 3.5);
-
-  # Get double value.
-  my $double_value = $double_list->get(2);
-
-  # Insert double value
-  $double_list->insert(1, 3);
-
-  # Remove double value
-  my $double_value = $double_list->remove(1);
-
-  # Convert DoubleList to double array.
-  my $double_array = $double_list->to_array;
-
+  # Unshift value.
+  $list->unshift((double)3);
+  
+  # Shift value.
+  my $value = $list->shift;
+  
+  # Set value.
+  $list->set(2, (double)3);
+  
+  # Get value.
+  my $value = $list->get(2);
+  
+  # Insert value
+  $list->insert(1, 3);
+  
+  # Remove value
+  my $value = $list->remove(1);
+  
+  # Convert list to array.
+  my $array = $list->to_array;
+  
 =head1 Description
 
-L<DoubleList|SPVM::DoubleList> is dynamic double array.
+L<DoubleList|SPVM::DoubleList> is a dynamic C<double> array.
 
 =head1 Fields
 
@@ -63,7 +63,10 @@ The length.
 
   has values : ro double[];
 
-The values.
+The values. This is the internally used array, but it can be manipulated directly.
+
+  my $values = $list->values;
+  $valeus->[0] = 5;
 
 =head1 Class Methods
 
@@ -71,95 +74,128 @@ The values.
 
     static method new : DoubleList ($array : double[]...)
 
-Create a new L<DoubleList|SPVM::DoubleList> object with specific C<double> array.
+Create a new L<DoubleList|SPVM::DoubleList> object with a C<double> array.
 
-Internally, new array is created, and each element of argument array is copied to internal array.
+Internally, a new array is created, and each element of the specified array is copied to the new array.
 
-If the array of the argument is C<undef>, 0-length internal array is created.
+If the array is C<undef>, a new array that length is C<0> is created.
 
-  my $double_list = DoubleList->new;
-  my $double_list = DoubleList->new([1.5, 2.5, 3.5]);
+B<Examples:>
+
+  my $list = DoubleList->new;
+  my $list = DoubleList->new([(double)1, 2, 3]);
+  my $list = DoubleList->new(undef);
 
 =head2 new_len
 
     static method new_len : DoubleList ($length : int)
 
-Create a new L<DoubleList|SPVM::DoubleList> object with array length.
+Create a new L<DoubleList|SPVM::DoubleList> object with the length.
+
+The length must be greater than or equal to C<0>. Otherwise an excpetion will be thrown.
 
 =head1 Instance Methods
-
-=head2 unshift
-
-  method unshift : void ($value : double)
-
-Appending the value to the top of list.
 
 =head2 get
 
   method get : double ($index : int)
 
-Get the value with index.
+Get the element of the position of the index.
+
+The index must be greater than or equal to 0. Otherwise an excpetion will be thrown.
+
+The index must be less than the length of the list. Otherwise an excpetion will be thrown.
 
 =head2 insert
 
   method insert : void ($index : int, $value : double)
 
-Insert a element to the specific index.
+Insert a element to the position of the index.
 
-=head2 remove
+The index must be greater than or equal to C<0>. Otherwise an excpetion will be thrown.
 
-  method remove : double ($index : int)
-
-Remove and return the element which is specified by the index.
-  
-=head2 resize
-
-  method resize : void ($new_length : int)
-
-Resize this list. If the new length is shorter than the current length, the list is truncated to the new length. If the new length is shorter than the current length, the list is truncated to the new length. If the new length is same as the current length, there is nothing to do. If the new length is longer than the current length, the list grows to the new length, and the values of the added elements are set to 0.
-
-New length must be greater than or equal to 0, otherwise a exception occur.
-
-=head2 set
-
-  method set : void ($index : int, $value : double)
-
-Set the value with index.
-
-=head2 set_array
-
-  method set_array : void ($array : double[])
-
-Set a array. Each elements of the array is copied to the correspoinding index of the array this list has.
-
-Array must be defined, otherwise a exception occurs.
-
-The length of argument array must be same as the length of current list array, otherwise a exception occures.
-
-=head2 shift
-
-  method shift : double ()
-
-Shifts the first value of the list off and returns it, shortening
-the array by 1 and moving everything down.
-If there are no elements in the list, exception occur.
-
-=head2 to_array
-
-  method to_array : double[] ()
-
-Convert L<DoubleList|SPVM::DoubleList> to double array.
+The index must be less than or equal to the length of the list. Otherwise an excpetion will be thrown.
 
 =head2 pop
 
   method pop : double ()
 
-Pops and returns the last value of the list, shortening the array by one element
-If there are no elements in the list, exception occur.
+Remove the last element and return it.
+
+The length of the list must be greater than C<0>. Otherwise an excpetion will be thrown.
 
 =head2 push
   
   method push : void ($value : double)
 
-Appending the value to the end of list.
+Add a element after the end of the list.
 
+=head2 remove
+
+  method remove : double ($index : int)
+
+Remove the element at the position of the index and return it.
+
+The index must be greater than or equal to C<0>. Otherwise an excpetion will be thrown.
+
+The index must be less than the length of the list. Otherwise an excpetion will be thrown.
+
+=head2 replace
+
+  method replace : void ($offset : int, $remove_length : int, $replace : double[])
+
+Replace the elements of the range specified by the offset and the lenght with the replacement array.
+
+The offset must be greater than or equal to C<0>. Otherwise an excpetion will be thrown.
+
+The removing length must be greater than or equal to C<0>. Otherwise an excpetion will be thrown.
+
+The offset + the removing lenght must be less than or equal to the length of the list. Otherwise an excpetion will be thrown.
+
+=head2 resize
+
+  method resize : void ($new_length : int)
+
+Resize the list.
+
+The new length must be greater than or equal to C<0>. Otherwise an excpetion will be thrown.
+
+=head2 set
+
+  method set : void ($index : int, $value : double)
+
+Set the element at the position of the index.
+
+The index must be greater than or equal to C<0>. Otherwise an excpetion will be thrown.
+
+The index must be less than the length of the list. Otherwise an excpetion will be thrown.
+
+=head2 set_array
+
+  method set_array : void ($array : double[])
+
+Set a array. Each element of the array is copied to the element of the list.
+
+The array must be defined. Otherwise an excpetion will be thrown.
+
+The length of the array must be the same as the length of the list. Otherwise an excpetion will be thrown.
+
+=head2 shift
+
+  method shift : double ()
+
+Remove the first element and return it.
+
+The length of the list must be greater than C<0>. Otherwise an excpetion will be thrown.
+
+=head2 to_array
+
+  method to_array : double[] ()
+
+Convert the list to an array.
+
+=head2 unshift
+
+  method unshift : void ($value : double)
+
+Insert a element at the beginning of the list.
--- t/default/module/DoubleList.t
+++ t/default/module/DoubleList.t
@@ -8,27 +8,26 @@ use Test::More;
 
 use SPVM 'TestCase::Module::DoubleList';
 
-
-
 # Start objects count
 my $start_memory_blocks_count = SPVM::get_memory_blocks_count();
 
-# SPVM::DoubleList
+# DoubleList
 {
-  ok(SPVM::TestCase::Module::DoubleList->push);
-  ok(SPVM::TestCase::Module::DoubleList->pop);
+  ok(SPVM::TestCase::Module::DoubleList->fields);
   ok(SPVM::TestCase::Module::DoubleList->new);
   ok(SPVM::TestCase::Module::DoubleList->new_len);
-  ok(SPVM::TestCase::Module::DoubleList->unshift);
-  ok(SPVM::TestCase::Module::DoubleList->shift);
-  ok(SPVM::TestCase::Module::DoubleList->length);
   ok(SPVM::TestCase::Module::DoubleList->get);
-  ok(SPVM::TestCase::Module::DoubleList->set);
-  ok(SPVM::TestCase::Module::DoubleList->set_array);
-  ok(SPVM::TestCase::Module::DoubleList->to_array);
   ok(SPVM::TestCase::Module::DoubleList->insert);
+  ok(SPVM::TestCase::Module::DoubleList->pop);
+  ok(SPVM::TestCase::Module::DoubleList->push);
   ok(SPVM::TestCase::Module::DoubleList->remove);
   ok(SPVM::TestCase::Module::DoubleList->replace);
+  ok(SPVM::TestCase::Module::DoubleList->resize);
+  ok(SPVM::TestCase::Module::DoubleList->set);
+  ok(SPVM::TestCase::Module::DoubleList->set_array);
+  ok(SPVM::TestCase::Module::DoubleList->shift);
+  ok(SPVM::TestCase::Module::DoubleList->to_array);
+  ok(SPVM::TestCase::Module::DoubleList->unshift);
 }
 
 # All object is freed
