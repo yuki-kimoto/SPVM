4d112b1b2df22ed7f911590fd79c1cdb5cecd762
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-07-29 09:30:27 +0900

    Update modules links

--- lib/SPVM/Document/Modules.pm
+++ lib/SPVM/Document/Modules.pm
@@ -10,83 +10,75 @@ The list of SPVM Standard Modules. All Standard modules are yet experimental sta
 
 =over 2
 
-=item * L<Array|SPVM::Array> - Array Utilities
+=item * L<Array|SPVM::Array>
 
-=item * L<Byte|SPVM::Byte> - byte Object
+=item * L<ByteList|SPVM::ByteList>
 
-=item * L<ByteList|SPVM::ByteList> - Dynamic byte Array
+=item * L<Cloneable|SPVM::Cloneable>
 
-=item * L<Cloner|SPVM::Cloner> - Interface Type for the Callback to Clone a Object
+=item * L<Cloner|SPVM::Cloner>
 
-=item * L<Cloneable|SPVM::Cloneable> - Interface Type to Clone a Object
+=item * L<Comparator::Byte|SPVM::Comparator::Byte>
 
-=item * L<Comparator::Byte|SPVM::Comparator::Byte> - Interface Type for byte Comparation Callback
+=item * L<Comparator::Double|SPVM::Comparator::Double>
 
-=item * L<Comparator::Short|SPVM::Comparator::Short> - Interface Type for short Comparation Callback
+=item * L<Comparator::Float|SPVM::Comparator::Float>
 
-=item * L<Comparator::Int|SPVM::Comparator::Int> - Interface Type for int Comparation Callback
+=item * L<Comparator::Int|SPVM::Comparator::Int>
 
-=item * L<Comparator::Long|SPVM::Comparator::Long> - Interface Type for long Comparation Callback
+=item * L<Comparator::Long|SPVM::Comparator::Long>
 
-=item * L<Comparator::Float|SPVM::Comparator::Float> - Interface Type for float Comparation Callback
+=item * L<Comparator::Short|SPVM::Comparator::Short>
 
-=item * L<Comparator::Double|SPVM::Comparator::Double> - Interface Type for double Comparation Callback
+=item * L<Comparator|SPVM::Comparator>
 
-=item * L<Comparator::String|SPVM::Comparator::String> - Interface Type for string Comparation Callback
+=item * L<Comparator::String|SPVM::Comparator::String>
 
-=item * L<Comparator|SPVM::Comparator> - Interface Type for object Comparation Callback
+=item * L<Complex_2d|SPVM::Complex_2d>
 
-=item * L<Complex_2d|SPVM::Complex_2d> - double Complex Type
+=item * L<Complex_2f|SPVM::Complex_2f>
 
-=item * L<Complex_2f|SPVM::Complex_2f> - float Complex Type
+=item * L<DoubleList|SPVM::DoubleList>
 
-=item * L<Double|SPVM::Double> - double Object
+=item * L<EqualityChecker::Address|SPVM::EqualityChecker::Address>
 
-=item * L<DoubleList|SPVM::DoubleList> - Dynamic double Array
+=item * L<EqualityChecker|SPVM::EqualityChecker>
 
-=item * L<EqualityChecker|SPVM::EqualityChecker> - Interface Type for Object Equality Checking Callback
+=item * L<FloatList|SPVM::FloatList>
 
-=item * L<EqualityChecker::Address|SPVM::EqualityChecker::Address> - A Implementation of L<EqualityChecker|SPVM::EqualityChecker> to Check Addresses Equality.
+=item * L<Fn|SPVM::Fn>
 
-=item * L<Float|SPVM::Float> - float Object
+=item * L<Format|SPVM::Format>
 
-=item * L<FloatList|SPVM::FloatList> - Dynamic float Array
+=item * L<Hash::Entry|SPVM::Hash::Entry>
 
-=item * L<Fn|SPVM::Fn> - Standard Functions
+=item * L<Hash|SPVM::Hash>
 
-=item * L<Hash|SPVM::Hash> - Hash Data Structure
+=item * L<IntList|SPVM::IntList>
 
-=item * L<Hash::Entry|SPVM::Hash::Entry> - Hash Entry
+=item * L<List|SPVM::List>
 
-=item * L<Int|SPVM::Int> - int Object
+=item * L<LongList|SPVM::LongList>
 
-=item * L<IntList|SPVM::IntList> - Dynamic int Array
+=item * L<Point3D|SPVM::Point3D>
 
-=item * L<List|SPVM::List> - Dynamic Object Array
+=item * L<Point|SPVM::Point>
 
-=item * L<Long|SPVM::Long> - long Object
+=item * L<ShortList|SPVM::ShortList>
 
-=item * L<LongList|SPVM::LongList> - Dynamic long Array
+=item * L<Sort|SPVM::Sort>
 
-=item * L<Short|SPVM::Short> - short Object
+=item * L<Stringable|SPVM::Stringable>
 
-=item * L<ShortList|SPVM::ShortList> - Dynamic short Array
+=item * L<StringBuffer|SPVM::StringBuffer>
 
-=item * L<Sort|SPVM::Sort> - Sorting Utilities
+=item * L<Stringer|SPVM::Stringer>
 
-=item * L<Format|SPVM::Format> - Format Utilities
+=item * L<StringList|SPVM::StringList>
 
-=item * L<StringBuffer|SPVM::StringBuffer> - String Buffer
+=item * L<Time::Info|SPVM::Time::Info>
 
-=item * L<Stringable|SPVM::Stringable> - Interface Type to Stringify a Object
-
-=item * L<Stringer|SPVM::Stringer> - Interface Type for Stringing Callback
-
-=item * L<StringList|SPVM::StringList> - Dynamic string Array
-
-=item * L<Time|SPVM::Time> - Time Manipulation
-
-=item * L<Time::Info|SPVM::Time::Info> - Time Information
+=item * L<Time|SPVM::Time>
 
 =back
 
