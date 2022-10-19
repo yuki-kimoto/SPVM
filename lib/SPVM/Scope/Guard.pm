70af60bfb27474e15141697d7bff03c31e13269e
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-10-19 13:43:33 +0900

    Added the Scope::Guard class.
      [New Features]
        * Added the Scope::Guard class.

---
+++ lib/SPVM/Scope/Guard.pm
@@ -0,0 +1,78 @@
+package SPVM::Scope::Guard;
+
+1;
+
+=head1 Name
+
+SPVM::Scope::Guard - Executing Handler at End of Scope
+
+=head1 Usage
+  
+  use Scope::Guard;
+  
+  # Executing the handler at the end of the scope
+  {
+    my $guard = Scope::Guard->new(method : void () {
+      print "End of Scope\n";
+    });
+    
+    print "Foo";
+  }
+  
+  # With a capture
+  {
+    my $value = 1;
+    my $guard = Scope::Guard->new([$value : int] method : void () {
+      print "$value\n";
+    });
+    
+    print "Foo";
+  }
+
+=head1 Description
+
+C<Scope::Guard> provides a feature to execue a hander at the end of the scope.
+
+=head1 Fields
+
+=head2 handler
+
+  has handler : ro Scope::Guard::Handler;
+
+A handler. The type is L<Scope::Guard::Handler|SPVM::Scope::Guard::Handler>.
+
+=head2 dismiss
+
+  has dismiss : rw byte;
+
+Gets and sets C<dismiss> field. See the L</"DESTROY"> method about the behavior.
+
+=head1 Class Methods
+
+=head2 new
+
+  static method new : Scope::Guard ($handler : Scope::Guard::Handler)
+
+Creates a new C<Scope::Guard> object and returns it.
+
+The C<$handler> is set to the L</"handler"> field.
+
+The C<$handler> is a L<Scope::Guard::Handler|SPVM::Scope::Guard::Handler> object.
+
+The C<$handler> must be defined. Otherwise an exception will be thrown.
+
+=head1 Instance Methods
+
+=head2 DESTROY
+
+  method DESTROY : void ()
+
+Execute the L</"handler">.
+
+If L</"dismiss"> is true, the handler is not executed.
+
+=head1 See Also
+
+=head2 Scope::Guard
+
+C<SPVM::Scope::Guard> is a port of Perl's L<Scope::Guard> to L<SPVM>.
---
+++ lib/SPVM/Scope/Guard/Handler.pm
@@ -0,0 +1,23 @@
+package SPVM::Scope::Guard::Handler;
+
+1;
+
+=head1 Name
+
+SPVM::Scope::Guard::Handler - Handler of Scope::Guard
+
+=head1 Usage
+  
+  use Scope::Guard::Handler;
+
+=head1 Description
+
+C<Scope::Guard::Handler> is an L<interface|SPVM::Document::Language/"Interface"> for the handler of L<Scope::Guard|SPVM::Scope::Guard>.
+
+=head1 Interface Methods
+
+=head2 Anon Method
+
+  required method : void ();
+
+The handler of L<Scope::Guard|SPVM::Scope::Guard>.
