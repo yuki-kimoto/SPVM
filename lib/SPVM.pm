ab22b88f276383328a17ee0aa4b8215e5b0e063f
Yuki Kimoto <kimoto.yuki@gmail.com>
2022-12-15 15:02:15 +0900

    Fixed the compile order

--- lib/SPVM.pm
+++ lib/SPVM.pm
@@ -77,11 +77,11 @@ sub import {
     for my $added_class_name (@$added_class_names) {
       next if $added_class_name =~ /::anon/;
       
-      # Build Precompile classs - Compile C source codes and link them to SPVM precompile method
-      $BUILDER->build_and_bind_dynamic_lib_at_runtime($added_class_name, 'precompile');
-      
       # Build native classs - Compile C source codes and link them to SPVM native method
       $BUILDER->build_and_bind_dynamic_lib_at_runtime($added_class_name, 'native');
+      
+      # Build Precompile classs - Compile C source codes and link them to SPVM precompile method
+      $BUILDER->build_and_bind_dynamic_lib_at_runtime($added_class_name, 'precompile');
     }
   }
 }
