1f93fe681c13219461b4ab5bc414df7c9e183a37
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-01-05 06:42:26 +0900

    Added Compiler class. This is only templates

---
+++ lib/SPVM/Compiler.c
@@ -0,0 +1,14 @@
+#include "spvm_native.h"
+
+
+
+static const char* FILE_NAME = "Compiler.c";
+
+int32_t SPVM__Compiler__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+
