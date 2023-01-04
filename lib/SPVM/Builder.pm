059bbeb9a9bd3c84371e3699265f259610ba0c5b
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-01-04 09:07:35 +0900

    Cleanup bind_methods

--- lib/SPVM/Builder.pm
+++ lib/SPVM/Builder.pm
@@ -229,40 +229,41 @@ sub bind_methods {
   my ($self, $dynamic_lib_file, $class_name, $category) = @_;
 
   my $method_names = $self->get_method_names($class_name, $category);
-  my $method_infos = [];
-  for my $method_name (@$method_names) {
-    my $method_info = {};
-    $method_info->{class_name} = $class_name;
-    $method_info->{method_name} = $method_name;
-    push @$method_infos, $method_info;
-  }
-  
-  # Add anon class sub names if precompile
-  if ($category eq 'precompile') {
-    my $anon_class_names = $self->get_anon_class_names_by_parent_class_name($class_name);
-    for my $anon_class_name (@$anon_class_names) {
+  if (@$method_names) {
+    my $method_infos = [];
+    for my $method_name (@$method_names) {
       my $method_info = {};
-      $method_info->{class_name} = $anon_class_name;
-      $method_info->{method_name} = "";
+      $method_info->{class_name} = $class_name;
+      $method_info->{method_name} = $method_name;
       push @$method_infos, $method_info;
     }
-  }
-  
-  for my $method_info (@$method_infos) {
-    my $class_name = $method_info->{class_name};
-    my $method_name = $method_info->{method_name};
+    
+    # Add anon class sub names if precompile
+    if ($category eq 'precompile') {
+      my $anon_class_names = $self->get_anon_class_names_by_parent_class_name($class_name);
+      for my $anon_class_name (@$anon_class_names) {
+        my $method_info = {};
+        $method_info->{class_name} = $anon_class_name;
+        $method_info->{method_name} = "";
+        push @$method_infos, $method_info;
+      }
+    }
+    
+    for my $method_info (@$method_infos) {
+      my $class_name = $method_info->{class_name};
+      my $method_name = $method_info->{method_name};
 
-    my $cfunc_address;
-    if ($dynamic_lib_file) {
-      my $dynamic_lib_libref = DynaLoader::dl_load_file($dynamic_lib_file);
-      
-      if ($dynamic_lib_libref) {
+      my $cfunc_address;
+      if ($dynamic_lib_file) {
+        my $dynamic_lib_libref = DynaLoader::dl_load_file($dynamic_lib_file);
+        
+        if ($dynamic_lib_libref) {
 
-        my $cfunc_name = SPVM::Builder::Util::create_cfunc_name($class_name, $method_name, $category);
-        $cfunc_address = DynaLoader::dl_find_symbol($dynamic_lib_libref, $cfunc_name);
-        unless ($cfunc_address) {
-          my $dl_error = DynaLoader::dl_error();
-          my $error = <<"EOS";
+          my $cfunc_name = SPVM::Builder::Util::create_cfunc_name($class_name, $method_name, $category);
+          $cfunc_address = DynaLoader::dl_find_symbol($dynamic_lib_libref, $cfunc_name);
+          unless ($cfunc_address) {
+            my $dl_error = DynaLoader::dl_error();
+            my $error = <<"EOS";
 Can't find native function \"$cfunc_name\" corresponding to ${class_name}->$method_name in \"$dynamic_lib_file\"
 
 You must write the following definition.
@@ -277,23 +278,24 @@ int32_t $cfunc_name(SPVM_ENV* env, SPVM_VALUE* stack) {
 
 $dl_error
 EOS
-          confess $error;
+            confess $error;
+          }
+        }
+        else {
+          my $dl_error = DynaLoader::dl_error();
+          confess "The DynaLoader::dl_load_file function failed:Can't load the \"$dynamic_lib_file\" file for the $category methods in the $class_name class: $dl_error";
         }
       }
       else {
-        my $dl_error = DynaLoader::dl_error();
-        confess "The DynaLoader::dl_load_file function failed:Can't load the \"$dynamic_lib_file\" file for the $category methods in the $class_name class: $dl_error";
+        confess "DLL file is not specified";
       }
-    }
-    else {
-      confess "DLL file is not specified";
-    }
-    
-    if ($category eq 'native') {
-      $self->native_address_info->{$class_name}{$method_name} = $cfunc_address;
-    }
-    elsif ($category eq 'precompile') {
-      $self->precompile_address_info->{$class_name}{$method_name} = $cfunc_address;
+      
+      if ($category eq 'native') {
+        $self->native_address_info->{$class_name}{$method_name} = $cfunc_address;
+      }
+      elsif ($category eq 'precompile') {
+        $self->precompile_address_info->{$class_name}{$method_name} = $cfunc_address;
+      }
     }
   }
 }
