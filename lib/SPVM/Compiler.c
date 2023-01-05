9b96874e6a6c369e027a5eb01a189f3f90b2866f
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-01-05 10:20:07 +0900

    Added empty methods in the Compiler class

--- lib/SPVM/Compiler.c
+++ lib/SPVM/Compiler.c
@@ -12,3 +12,79 @@ int32_t SPVM__Compiler__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
 }
 
 
+int32_t SPVM__Compiler__new_native_compiler(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__set_native_method_address(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__get_precompile_method_address(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__set_precompile_method_address(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__build_class_source(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__build_method_source(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__new_env(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__new_stack(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__call_init_blocks(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__set_command_info(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__compile(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
