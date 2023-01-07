d38c46350c3a19c4aa85624cac10714531340f29
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-01-08 08:32:14 +0900

    Revert "Temporary revmoed the Compiler classes for the release"
    This reverts commit 2c5cd02fff40201a8e23ff08881e44893ddec55d.

---
+++ lib/SPVM/Compiler.c
@@ -0,0 +1,102 @@
+#include "spvm_native.h"
+
+
+
+static const char* FILE_NAME = "Compiler.c";
+
+int32_t SPVM__Compiler__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+
+int32_t SPVM__Compiler__new_native_compiler(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__set_native_method_address(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__get_precompile_method_address(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__set_precompile_method_address(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__build_class_source(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__build_method_source(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__new_env(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__new_stack(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__call_init_blocks(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  void* obj_my_env = stack[0].oval;
+  SPVM_ENV* my_env = env->get_pointer(env, stack, obj_my_env);
+  
+  my_env->call_init_blocks(my_env);
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__set_command_info(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__call_method(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
+
+int32_t SPVM__Compiler__compile(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  return 0;
+}
---
+++ lib/SPVM/Compiler.pm
@@ -0,0 +1,45 @@
+package SPVM::Compiler;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Compiler - foo
+
+=head1 Description
+
+C<SPVM::Compiler> is the L<SPVM>'s C<Compiler> class.
+
+=head1 Usage
+
+  use Compiler;
+  
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+[--user-name] C<[--user-email]>
+
+=head1 Copyright & License
+
+Copyright 2023-2023 [--user-name], all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
