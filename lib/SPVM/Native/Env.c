41f79fa54ccb911bfb0837bd8f380ec3029c1474
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-01-09 09:06:59 +0900

    Implemented Native::Env

--- lib/SPVM/Native/Env.c
+++ lib/SPVM/Native/Env.c
@@ -1,14 +1,53 @@
 #include "spvm_native.h"
 
-
-
 static const char* FILE_NAME = "Native/Env.c";
 
-int32_t SPVM__Native__Env__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+int32_t SPVM__Native__Env__new(SPVM_ENV* env, SPVM_VALUE* stack) {
   (void)env;
   (void)stack;
+  
+  int32_t e = 0;
+  
+  void* obj_runtime = stack[0].oval;
+  
+  void* runtime = env->get_pointer(env, stack, obj_runtime);
+  
+  SPVM_ENV* my_env = SPVM_NATIVE_new_env_raw();
+  
+  // Set runtime information
+  my_env->runtime = runtime;
+  
+  // Initialize env
+  my_env->init_env(my_env);
+  
+  // Call INIT blocks
+  my_env->call_init_blocks(my_env);
+
+  void* obj_self= env->new_pointer_object_by_name(env, stack, "Native::Env", my_env, &e, FILE_NAME, __LINE__);
+  if (e) { return e; }
+  
+  env->set_field_object_by_name(env, stack, obj_self, "runtime", obj_runtime, &e, FILE_NAME, __LINE__);
+  if (e) { return e; }
+  
+  stack[0].oval = obj_self;
   
   return 0;
 }
 
+int32_t SPVM__Native__Env__DESTROY(SPVM_ENV* env, SPVM_VALUE* stack) {
+  (void)env;
+  (void)stack;
+  
+  int32_t e = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  SPVM_ENV* my_env= env->get_pointer(env, stack, obj_self);
+  
+  // Cleanup global varialbes
+  my_env->cleanup_global_vars(my_env);
 
+  my_env->free_env_raw(my_env);
+  
+  return 0;
+}
