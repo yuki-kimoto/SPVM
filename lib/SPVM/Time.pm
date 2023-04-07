a52641edf05c5aaa52afdd9ea1f42a3b6c656468
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-04-07 12:52:47 +0900

    Removed SPVM::Time. Use SPVM::Sys::Time instead and etc.
      [Incompatible Changes]
        * Removed SPVM::Time. Use SPVM::Sys::Time instead.
        * Removed SPVM::Time::Info. Use SPVM::Sys::Time::Tm instead.

--- lib/SPVM/Time.pm
+++ lib/SPVM/Time.pm
@@ -4,65 +4,8 @@ package SPVM::Time;
 
 =head1 Name
 
-SPVM::Time - Time Manipulation
+SPVM::Time - Time Manipulation (Removed)
 
 =head1 Description
 
-(Deprecated) The methods of SPVM::Time will be implemented in SPVM::Sys::Time
-
-C<Time> is a module to manipulate time.
-
-=head1 Usage
-  
-  use Time;
-  
-  # Get the current epoch time
-  my $epoch = Time->time;
-  
-  # Convert a epoch time to the Time::Info object that is local time
-  my $time_info_local = Time->localtime($epoch);
-  
-  # Convert a epoch time to the Time::Info object that is UTC
-  my $time_info_utc = Time->gmtime($epoch);
-  
-=head1 Class Methods
-
-=head2 time
-
-  static method time : long ();
-
-Gets the current epoch time. 
-
-This method is the same as C<time> function of C<Linux>.
-
-  my $epoch = Time->time;
-
-=head2 localtime
-
-  static method localtime : Time::Info ($time : long);
-
-Converts an epoch $time to the L<Time::Info|SPVM::Time::Info> object that is local time.
-
-This method is the same as C<localtime> function of C<Linux>.
-
-  my $time_info = Time->localtime($epoch);
-
-=head2 gmtime
-
-  static method gmtime : Time::Info ($time : long);
-
-Converts an epoch $time to the L<Time::Info|SPVM::Time::Info> object that is C<UTC>.
-
-This method is the same as C<gmtime> function of C<Linux>.
-
-  my $time_info = Time->gmtime($epoch);
-
-=head1 See Also
-
-See L<Time::Local|SPVM::Time::Local> about C<timelocal> and C<timegm> methods.
-
-=head1 Copyright & License
-
-Copyright (c) 2023 Yuki Kimoto
-
-MIT License
+This class is removed. Use the L<Sys::Time|SPVM::Sys::Time> class instead.
