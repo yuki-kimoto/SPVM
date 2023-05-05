5b9a6bc3d177fe6bb41d9539e39187c5b11ffa00
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-05-06 08:37:45 +0900

    Fixed float default

--- t/02_vm/lib/SPVM/TestCase/NativeAPI.c
+++ t/02_vm/lib/SPVM/TestCase/NativeAPI.c
@@ -3002,7 +3002,7 @@ int32_t SPVM__TestCase__NativeAPI__default_all_types_native(SPVM_ENV* env, SPVM_
     }
   }
   else {
-    if (!(value_float == 1.5f)) {
+    if (!(value_float == 1.02f)) {
       stack[0].ival = 0;
       spvm_warn("line %d", __LINE__);
       return 0;
