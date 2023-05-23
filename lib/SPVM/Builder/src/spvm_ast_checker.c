0e6ed8a0e8a171678816c55b085a3b5a9ca5d62a
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-05-23 13:04:50 +0900

    Reorder op code

--- lib/SPVM/Builder/src/spvm_ast_checker.c
+++ lib/SPVM/Builder/src/spvm_ast_checker.c
@@ -2496,6 +2496,29 @@ void SPVM_AST_CHECKER_traversal_ast_check_syntax(SPVM_COMPILER* compiler, SPVM_O
               
               break;
             }
+            case SPVM_OP_C_ID_CLASS_VAR_ACCESS: {
+              
+              SPVM_OP* op_class_var_access = op_cur;
+              
+              // Check field name
+              SPVM_AST_CHECKER_resolve_class_var_access(compiler, op_class_var_access, class->op_class);
+              if (SPVM_COMPILER_get_error_messages_length(compiler) > 0) {
+                return;
+              }
+              
+              SPVM_CLASS_VAR_ACCESS* class_var_access = op_class_var_access->uv.class_var_access;
+              SPVM_CLASS_VAR* class_var = class_var_access->class_var;
+              SPVM_CLASS* class_var_access_class = class_var->class;
+              
+              if (!SPVM_OP_is_allowed(compiler, method->class, class_var_access_class)) {
+                if (!SPVM_AST_CHECKER_can_access(compiler, method->class, class_var_access_class, class_var_access->class_var->access_control_type)) {
+                  SPVM_COMPILER_error(compiler, "The %s \"%s\" class variable of the \"%s\" class cannnot be accessed from the current class \"%s\".\n  at %s line %d", SPVM_ATTRIBUTE_get_name(compiler, class_var_access->class_var->access_control_type), class_var->name, class_var_access_class->name,  method->class->name, op_class_var_access->file, op_class_var_access->line);
+                  return;
+                }
+              }
+              
+              break;
+            }
             case SPVM_OP_C_ID_CALL_METHOD: {
               
               SPVM_OP* op_call_method = op_cur;
@@ -2584,29 +2607,6 @@ void SPVM_AST_CHECKER_traversal_ast_check_syntax(SPVM_COMPILER* compiler, SPVM_O
                 SPVM_OP_replace_op(compiler, op_stab, op_constant);
                 
                 op_cur = op_constant;
-              }
-              
-              break;
-            }
-            case SPVM_OP_C_ID_CLASS_VAR_ACCESS: {
-              
-              SPVM_OP* op_class_var_access = op_cur;
-              
-              // Check field name
-              SPVM_AST_CHECKER_resolve_class_var_access(compiler, op_class_var_access, class->op_class);
-              if (SPVM_COMPILER_get_error_messages_length(compiler) > 0) {
-                return;
-              }
-              
-              SPVM_CLASS_VAR_ACCESS* class_var_access = op_class_var_access->uv.class_var_access;
-              SPVM_CLASS_VAR* class_var = class_var_access->class_var;
-              SPVM_CLASS* class_var_access_class = class_var->class;
-              
-              if (!SPVM_OP_is_allowed(compiler, method->class, class_var_access_class)) {
-                if (!SPVM_AST_CHECKER_can_access(compiler, method->class, class_var_access_class, class_var_access->class_var->access_control_type)) {
-                  SPVM_COMPILER_error(compiler, "The %s \"%s\" class variable of the \"%s\" class cannnot be accessed from the current class \"%s\".\n  at %s line %d", SPVM_ATTRIBUTE_get_name(compiler, class_var_access->class_var->access_control_type), class_var->name, class_var_access_class->name,  method->class->name, op_class_var_access->file, op_class_var_access->line);
-                  return;
-                }
               }
               
               break;
