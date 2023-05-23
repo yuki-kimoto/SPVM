c200aae4a0564bba5c083ca4890890597d237088
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-05-23 12:51:25 +0900

    constant_pool_id is removed

--- lib/SPVM/Builder/include/spvm_class_var_access.h
+++ lib/SPVM/Builder/include/spvm_class_var_access.h
@@ -9,7 +9,6 @@
 struct spvm_class_var_access {
   SPVM_OP* op_name;
   SPVM_CLASS_VAR* class_var;
-  int32_t constant_pool_id;
 };
 
 SPVM_CLASS_VAR_ACCESS* SPVM_CLASS_VAR_ACCESS_new(SPVM_COMPILER* compiler);
