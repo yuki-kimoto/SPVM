5d422e04508ec2cf1b11a18938fd4c2379a78907
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-05 09:53:42 +0900

    The SPVM_API_set_object_info function was removed

--- lib/SPVM/Builder/src/spvm_api.c
+++ lib/SPVM/Builder/src/spvm_api.c
@@ -2625,12 +2625,6 @@ SPVM_OBJECT* SPVM_API_new_string_nolen(SPVM_ENV* env, SPVM_VALUE* stack, const c
   return object;
 }
 
-void SPVM_API_set_object_info (SPVM_ENV* env, SPVM_VALUE* stack, SPVM_OBJECT* object, int32_t basic_type_id, int32_t type_dimension, int32_t length) {
-  object->basic_type_id = basic_type_id;
-  object->type_dimension = type_dimension;
-  object->length = length;
-}
-
 SPVM_OBJECT* SPVM_API_new_object_common (SPVM_ENV* env, SPVM_VALUE* stack, size_t alloc_size, int32_t basic_type_id, int32_t type_dimension, int32_t length, int32_t flag) {
   
   SPVM_OBJECT* object = SPVM_API_new_memory_stack(env, stack, alloc_size);
