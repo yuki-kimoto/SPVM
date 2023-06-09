48a2fe5789ba7896e210d165ff81c2643f53f22e
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-10 07:55:28 +0900

    The get_field_type_id was reduced.

--- lib/SPVM/Builder/src/spvm_api.c
+++ lib/SPVM/Builder/src/spvm_api.c
@@ -3169,8 +3169,11 @@ void SPVM_API_dec_ref_count(SPVM_ENV* env, SPVM_VALUE* stack, SPVM_OBJECT* objec
         int32_t object_fields_base = SPVM_API_RUNTIME_get_class_fields_base_id(runtime, class->id);
         int32_t object_fields_length = SPVM_API_RUNTIME_get_class_fields_length(runtime, class->id);
         for (int32_t field_id = object_fields_base; field_id < object_fields_base + object_fields_length; field_id++) {
-          int32_t field_type_id = SPVM_API_RUNTIME_get_field_type_id(runtime, field_id);
-          int32_t field_type_is_object = SPVM_API_RUNTIME_get_type_is_object(runtime, field_type_id);
+          int32_t field_basic_type_id = SPVM_API_RUNTIME_get_field_basic_type_id(runtime, field_id);
+          int32_t field_type_dimension = SPVM_API_RUNTIME_get_field_type_dimension(runtime, field_id);
+          int32_t field_type_flag = SPVM_API_RUNTIME_get_field_type_flag(runtime, field_id);
+          int32_t field_type_is_object = SPVM_API_RUNTIME_is_object_type(runtime, field_basic_type_id, field_type_dimension, field_type_flag);
+          
           SPVM_RUNTIME_FIELD* field = SPVM_API_RUNTIME_get_field(runtime, field_id);
           if (field_type_is_object) {
             SPVM_OBJECT** get_field_object_address = (SPVM_OBJECT**)((intptr_t)object + (size_t)env->object_header_size + field->offset);
