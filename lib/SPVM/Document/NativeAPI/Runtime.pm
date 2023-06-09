e6f9dc4d9c011eec7e453b9f1d8d30fe3db9ae91
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-09 11:12:46 +0900

    Tests and doc of the Native Runtime API was fixed.

--- lib/SPVM/Document/NativeAPI/Runtime.pm
+++ lib/SPVM/Document/NativeAPI/Runtime.pm
@@ -106,7 +106,8 @@ Runtime native APIs have its IDs.
   82 get_type_flag
   83 is_object_type
   84 get_class_version_string_id
-  85 can_assign
+  85 class_init_flags
+  86 can_assign
 
 =head1 Runtime Native APIs
 
@@ -451,6 +452,10 @@ Runtime native APIs have its IDs.
   int32_t (*get_class_version_string_id)(void* runtime, int32_t class_id);
 
 Gets the ID of the constant string of the version number.
+
+=head2 class_init_flags
+
+Internally used.
 
 =head2 can_assign
 
