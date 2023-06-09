b721d9637e63fb54d7e0b41d0aa75771a5b1ac52
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-09 14:44:16 +0900

    All members of the spvm_runtime_class.h were copied to the spvm_runtime_basic_type.h

--- lib/SPVM/Builder/include/spvm_runtime_basic_type.h
+++ lib/SPVM/Builder/include/spvm_runtime_basic_type.h
@@ -11,6 +11,29 @@ struct spvm_runtime_basic_type {
   int32_t category;
   int32_t name_id;
   int32_t class_id;
+  int32_t class_name_id;
+  int32_t class_rel_file_id;
+  int32_t class_path_id;
+  int32_t init_method_id;
+  int32_t destructor_method_id;
+  int32_t required_method_id;
+  int32_t type_id;
+  int32_t methods_base_id;
+  int32_t methods_length;
+  int32_t anon_methods_base_id;
+  int32_t anon_methods_length;
+  int32_t fields_base_id;
+  int32_t fields_length;
+  int32_t class_vars_base_id;
+  int32_t class_vars_length;
+  int32_t interfaces_base_id;
+  int32_t interfaces_length;
+  int32_t fields_size;
+  int32_t parent_class_id;
+  int32_t version_string_id;
+  int8_t has_init_block;
+  int8_t is_anon;
+  int8_t is_pointer;
 };
 
 #endif
