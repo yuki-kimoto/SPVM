6bb98e386648a9e9b550a67e9cf9147fc141d1d7
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-14 10:57:53 +0900

    Using compiler->classes was reduced.

--- lib/SPVM/Builder/src/spvm_ast_checker.c
+++ lib/SPVM/Builder/src/spvm_ast_checker.c
@@ -978,8 +978,10 @@ void SPVM_AST_CHECKER_resolve_classes(SPVM_COMPILER* compiler) {
   }
 
   // Replace fields
-  for (int32_t class_index = compiler->cur_class_base; class_index < compiler->classes->length; class_index++) {
-    SPVM_CLASS* class = SPVM_LIST_get(compiler->classes, class_index);
+  for (int32_t basic_type_index = compiler->cur_basic_type_base; basic_type_index < compiler->basic_types->length; basic_type_index++) {
+    SPVM_BASIC_TYPE* class_basic_type = SPVM_LIST_get(compiler->basic_types, basic_type_index);
+    SPVM_CLASS* class = class_basic_type->class;
+    if (!class) { continue; }
 
     for (int32_t field_index = 0; field_index < class->fields->length; field_index++) {
       SPVM_FIELD* field = SPVM_LIST_get(class->fields, field_index);
