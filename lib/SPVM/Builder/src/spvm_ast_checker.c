960ebec2e0362f65d07d1681c0ff6297e1ade3a8
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-14 10:54:34 +0900

    Using compiler->classes was reduced.

--- lib/SPVM/Builder/src/spvm_ast_checker.c
+++ lib/SPVM/Builder/src/spvm_ast_checker.c
@@ -649,8 +649,10 @@ void SPVM_AST_CHECKER_resolve_classes(SPVM_COMPILER* compiler) {
   }
   
   // Add anon methods
-  for (int32_t class_index = compiler->cur_class_base; class_index < compiler->classes->length; class_index++) {
-    SPVM_CLASS* class = SPVM_LIST_get(compiler->classes, class_index);
+  for (int32_t basic_type_index = compiler->cur_basic_type_base; basic_type_index < compiler->basic_types->length; basic_type_index++) {
+    SPVM_BASIC_TYPE* class_basic_type = SPVM_LIST_get(compiler->basic_types, basic_type_index);
+    SPVM_CLASS* class = class_basic_type->class;
+    if (!class) { continue; }
     
     // Add the anon method
     for (int32_t anon_methods_index = 0; anon_methods_index < class->anon_methods->length; anon_methods_index++) {
