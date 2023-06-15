1c851ecf17d84d22e2ecbd969d3c95991e62ac28
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-15 10:32:19 +0900

    Using SPVM_CLASS was reduced.

--- lib/SPVM/Builder/src/spvm_ast_checker.c
+++ lib/SPVM/Builder/src/spvm_ast_checker.c
@@ -1100,8 +1100,7 @@ void SPVM_AST_CHECKER_traverse_ast_resolve_op_types(SPVM_COMPILER* compiler, SPV
             if (op_type->uv.type->resolved_in_ast) {
               const char* class_name_maybe = op_type->uv.type->basic_type->name;
               
-              SPVM_CLASS* class_current = class_basic_type->class;
-              const char* class_name = SPVM_HASH_get(class_current->type->basic_type->class_alias_symtable, class_name_maybe, strlen(class_name_maybe));
+              const char* class_name = SPVM_HASH_get(class_basic_type->class_alias_symtable, class_name_maybe, strlen(class_name_maybe));
               if (class_name == NULL) {
                 class_name = class_name_maybe;
               }
