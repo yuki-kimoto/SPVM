a5825fb00f31ced333e87262e128dc651125e347
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-15 11:00:37 +0900

    Using SPVM_CLASS was reduced.

--- lib/SPVM/Builder/src/spvm_ast_checker.c
+++ lib/SPVM/Builder/src/spvm_ast_checker.c
@@ -650,9 +650,8 @@ void SPVM_AST_CHECKER_resolve_classes(SPVM_COMPILER* compiler) {
 
   for (int32_t basic_type_index = compiler->cur_basic_type_base; basic_type_index < compiler->basic_types->length; basic_type_index++) {
     SPVM_BASIC_TYPE* class_basic_type = SPVM_LIST_get(compiler->basic_types, basic_type_index);
-    SPVM_CLASS* class = class_basic_type->class;
     
-    if (!class) { continue; }
+    if (!class_basic_type->is_class) { continue; }
     // Add interfaces
     for (int32_t i = 0; i < class_basic_type->interface_decls->length; i++) {
       SPVM_INTERFACE* interface_decl = SPVM_LIST_get(class_basic_type->interface_decls, i);
