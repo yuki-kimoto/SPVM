56466cc8057121c0981318f8827ac95a19c9f665
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-18 08:53:58 +0900

    The version was now 0.989006.

--- lib/SPVM.pm
+++ lib/SPVM.pm
@@ -7,7 +7,7 @@ use Carp 'cluck';
 
 use SPVM::Global;
 
-our $VERSION = "0.989005";
+our $VERSION = "0.989006";
 
 require XSLoader;
 XSLoader::load('SPVM', $VERSION);
--- lib/SPVM/Builder/include/spvm_api.h
+++ lib/SPVM/Builder/include/spvm_api.h
@@ -4,7 +4,7 @@
 #ifndef SPVM_API_H
 #define SPVM_API_H
 
-#define SPVM_VERSION "0.989005"
+#define SPVM_VERSION "0.989006"
 
 #include "spvm_typedecl.h"
 #include "spvm_native.h"
