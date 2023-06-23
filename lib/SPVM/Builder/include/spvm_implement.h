d284d76e550c9c75a220f552aacf773203e14019
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-23 09:53:53 +0900

    SPVM_IMPLEMENT_GET_CLASS_VAR_BYTE_V2 was added and used.

--- lib/SPVM/Builder/include/spvm_implement.h
+++ lib/SPVM/Builder/include/spvm_implement.h
@@ -1420,6 +1420,8 @@ static inline void SPVM_IMPLEMENT_SET_FIELD_UNDEF(SPVM_ENV* env, SPVM_VALUE* sta
 #define SPVM_IMPLEMENT_SET_CLASS_VAR_OBJECT(env, stack, basic_type_id, class_var_index, in) (SPVM_IMPLEMENT_OBJECT_ASSIGN(env, stack, (void**)&((SPVM_VALUE*)env->class_vars_heap)[env->api->runtime->get_basic_type_class_vars_base_address_id(env->runtime, basic_type_id) + class_var_index], in))
 #define SPVM_IMPLEMENT_SET_CLASS_VAR_UNDEF(env, stack, basic_type_id, class_var_index) (SPVM_IMPLEMENT_OBJECT_ASSIGN(env, stack, (void**)&((SPVM_VALUE*)env->class_vars_heap)[env->api->runtime->get_basic_type_class_vars_base_address_id(env->runtime, basic_type_id) + class_var_index], NULL))
 
+#define SPVM_IMPLEMENT_GET_CLASS_VAR_BYTE_V2(env, stack, out, class_var) (out = env->get_class_var_byte(env, stack, class_var))
+
 #define SPVM_IMPLEMENT_GET_EXCEPTION_VAR(env, stack, out) (SPVM_IMPLEMENT_OBJECT_ASSIGN(env, stack, out, env->get_exception(env, stack)))
 #define SPVM_IMPLEMENT_SET_EXCEPTION_VAR(env, stack, in) (env->set_exception(env, stack, in))
 #define SPVM_IMPLEMENT_SET_EXCEPTION_VAR_UNDEF(env, stack) (env->set_exception(env, stack, NULL))
