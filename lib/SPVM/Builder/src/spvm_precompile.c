f140bc0e5c74fe5b10d37e68c760e062c553ee2c
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-28 10:10:01 +0900

    get_basic_type_module_rel_file was reduced.

--- lib/SPVM/Builder/src/spvm_precompile.c
+++ lib/SPVM/Builder/src/spvm_precompile.c
@@ -2794,9 +2794,8 @@ void SPVM_PRECOMPILE_build_method_source(SPVM_PRECOMPILE* precompile, SPVM_STRIN
       case SPVM_OPCODE_C_ID_WARN: {
         int32_t line = opcode->operand1;
         
-        int32_t module_rel_file_id = SPVM_API_RUNTIME_get_basic_type_module_rel_file_id(runtime, current_basic_type_id);
         int32_t module_dir_id = SPVM_API_RUNTIME_get_basic_type_module_dir_id(runtime, current_basic_type_id);
-        const char* module_rel_file = SPVM_API_RUNTIME_get_constant_string_value(runtime, module_rel_file_id, NULL);
+        const char* module_rel_file = SPVM_API_RUNTIME_get_basic_type_module_rel_file(runtime, current_basic_type_id);
         const char* module_dir = NULL;
         const char* module_dir_sep;
         if (module_dir_id >= 0) {
