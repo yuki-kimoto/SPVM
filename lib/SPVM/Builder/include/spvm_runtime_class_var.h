3ed2c74a57b980e334d631e8c424ecf71d0a4d68
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-28 13:12:09 +0900

    string_pool_index were added.

--- lib/SPVM/Builder/include/spvm_runtime_class_var.h
+++ lib/SPVM/Builder/include/spvm_runtime_class_var.h
@@ -9,6 +9,7 @@
 struct spvm_runtime_class_var {
   int32_t index;
   int32_t name_string_address_id;
+  int32_t name_string_index;
   int32_t current_basic_type_id;
   int32_t basic_type_id;
   int32_t type_dimension;
--- lib/SPVM/Builder/include/spvm_runtime_constant_string.h
+++ lib/SPVM/Builder/include/spvm_runtime_constant_string.h
@@ -10,7 +10,9 @@ struct spvm_runtime_constant_string {
   const char* value;
   int32_t length;
   int32_t address_id;
+  int32_t index;
   int32_t string_pool_address_id;
+  int32_t string_pool_index;
 };
 
 #endif
