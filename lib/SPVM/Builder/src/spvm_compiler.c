1dcdb26fe9f91e7fd709a1028c567c8d6e77670a
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-29 14:11:06 +0900

    runtime_string->index was set.

--- lib/SPVM/Builder/src/spvm_compiler.c
+++ lib/SPVM/Builder/src/spvm_compiler.c
@@ -903,6 +903,7 @@ int32_t* SPVM_COMPILER_create_runtime_codes(SPVM_COMPILER* compiler, SPVM_ALLOCA
     SPVM_RUNTIME_CONSTANT_STRING* runtime_string = (SPVM_RUNTIME_CONSTANT_STRING*)constant_string_runtime_codes_ptr;
     
     runtime_string->address_id = string->address_id;
+    runtime_string->index = string->index;
     runtime_string->length = string->length;
     runtime_string->string_pool_address_id = string->string_pool_address_id;
     runtime_string->string_pool_index = string->string_pool_index;
