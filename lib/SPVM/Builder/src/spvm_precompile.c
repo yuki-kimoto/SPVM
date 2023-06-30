231ad0ab6fcd19255c607e668b82c4ced5f164f7
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-30 13:21:53 +0900

    Native Runtime API v2 were used more.

--- lib/SPVM/Builder/src/spvm_precompile.c
+++ lib/SPVM/Builder/src/spvm_precompile.c
@@ -1927,9 +1927,13 @@ void SPVM_PRECOMPILE_build_method_source(SPVM_PRECOMPILE* precompile, SPVM_STRIN
         
         SPVM_STRING_BUFFER_add(string_buffer, "  assert(basic_type_id >= 0);\n");
         
-        SPVM_STRING_BUFFER_add(string_buffer, "  SPVM_IMPLEMENT_NEW_MULNUM_ARRAY(env, stack, ");
+        SPVM_STRING_BUFFER_add(string_buffer, "  basic_type = ");
+        SPVM_PRECOMPILE_add_basic_type(precompile, string_buffer, basic_type_name);
+        SPVM_STRING_BUFFER_add(string_buffer, ";\n");
+        
+        SPVM_STRING_BUFFER_add(string_buffer, "  SPVM_IMPLEMENT_NEW_MULNUM_ARRAY_V2(env, stack, ");
         SPVM_PRECOMPILE_add_operand_address(precompile, string_buffer, SPVM_PRECOMPILE_C_CTYPE_ID_OBJECT, opcode->operand0);
-        SPVM_STRING_BUFFER_add(string_buffer, ", basic_type_id, length, &error_id, object_ref_count_offset);\n");
+        SPVM_STRING_BUFFER_add(string_buffer, ", basic_type, length, &error_id, object_ref_count_offset);\n");
         
         break;
       }
