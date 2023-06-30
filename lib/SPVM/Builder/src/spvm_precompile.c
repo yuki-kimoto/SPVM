7fb1cb3d3155fc6c28146285f02dde7da769f67d
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-30 13:18:06 +0900

    Native Runtime API v2 were used more.

--- lib/SPVM/Builder/src/spvm_precompile.c
+++ lib/SPVM/Builder/src/spvm_precompile.c
@@ -937,16 +937,20 @@ void SPVM_PRECOMPILE_build_method_source(SPVM_PRECOMPILE* precompile, SPVM_STRIN
         SPVM_STRING_BUFFER_add(string_buffer, ";\n");
         
         SPVM_STRING_BUFFER_add(string_buffer, "  assert(basic_type_id >= 0);\n");
+        
+        SPVM_STRING_BUFFER_add(string_buffer, "  basic_type = ");
+        SPVM_PRECOMPILE_add_basic_type(precompile, string_buffer, cast_basic_type_name);
+        SPVM_STRING_BUFFER_add(string_buffer, ";\n");
                                               
         SPVM_STRING_BUFFER_add(string_buffer, "  cast_type_dimension = ");
         SPVM_STRING_BUFFER_add_int(string_buffer, opcode->operand3);
         SPVM_STRING_BUFFER_add(string_buffer, ";\n");
         
-        SPVM_STRING_BUFFER_add(string_buffer, "  SPVM_IMPLEMENT_MOVE_OBJECT_WITH_TYPE_CHECKING(env, stack, ");
+        SPVM_STRING_BUFFER_add(string_buffer, "  SPVM_IMPLEMENT_MOVE_OBJECT_WITH_TYPE_CHECKING_V2(env, stack, ");
         SPVM_PRECOMPILE_add_operand_address(precompile, string_buffer, SPVM_PRECOMPILE_C_CTYPE_ID_OBJECT, opcode->operand0);
         SPVM_STRING_BUFFER_add(string_buffer, ", ");
         SPVM_PRECOMPILE_add_operand(precompile, string_buffer, SPVM_PRECOMPILE_C_CTYPE_ID_OBJECT, opcode->operand1);
-        SPVM_STRING_BUFFER_add(string_buffer, ", basic_type_id, cast_type_dimension, &error_id, object_ref_count_offset);\n");
+        SPVM_STRING_BUFFER_add(string_buffer, ", basic_type, cast_type_dimension, &error_id, object_ref_count_offset);\n");
         break;
       }
       case SPVM_OPCODE_C_ID_MOVE_OBJECT_CHECK_READ_ONLY: {
