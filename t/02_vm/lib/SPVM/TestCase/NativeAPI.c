7ae5b90c85f1cef9d1a40df1710146b075542bd6
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-30 16:23:14 +0900

    The before commit b56e81e11713fcaab9d6cbfb13b3434913e19a73 was fixed.

--- t/02_vm/lib/SPVM/TestCase/NativeAPI.c
+++ t/02_vm/lib/SPVM/TestCase/NativeAPI.c
@@ -2335,7 +2335,7 @@ int32_t SPVM__TestCase__NativeAPI__check_native_api_runtime_indexes(SPVM_ENV* en
   if ((void*)&env->api->runtime->reserved84 != &env_array[84]) { stack[0].ival = 0; return 0; }
   if ((void*)&env->api->runtime->reserved85 != &env_array[85]) { stack[0].ival = 0; return 0; }
   if ((void*)&env->api->runtime->can_assign != &env_array[86]) { stack[0].ival = 0; return 0; }
-  if ((void*)&env->api->runtime->get_field_basic_type_id != &env_array[100]) { stack[0].ival = 0; return 0; }
+  if ((void*)&env->api->runtime->get_field_basic_type != &env_array[100]) { stack[0].ival = 0; return 0; }
   if ((void*)&env->api->runtime->get_field_type_dimension != &env_array[101]) { stack[0].ival = 0; return 0; }
   if ((void*)&env->api->runtime->get_field_type_flag != &env_array[102]) { stack[0].ival = 0; return 0; }
   if ((void*)&env->api->runtime->get_class_var_basic_type_id != &env_array[103]) { stack[0].ival = 0; return 0; }
@@ -2348,7 +2348,7 @@ int32_t SPVM__TestCase__NativeAPI__check_native_api_runtime_indexes(SPVM_ENV* en
   if ((void*)&env->api->runtime->get_arg_type_dimension != &env_array[110]) { stack[0].ival = 0; return 0; }
   if ((void*)&env->api->runtime->get_arg_type_flag != &env_array[111]) { stack[0].ival = 0; return 0; }
   if ((void*)&env->api->runtime->get_method_current_basic_type != &env_array[112]) { stack[0].ival = 0; return 0; }
-  if ((void*)&env->api->runtime->get_field_current_basic_type_id != &env_array[113]) { stack[0].ival = 0; return 0; }
+  if ((void*)&env->api->runtime->get_field_current_basic_type != &env_array[113]) { stack[0].ival = 0; return 0; }
   if ((void*)&env->api->runtime->get_class_var_current_basic_type_id != &env_array[114]) { stack[0].ival = 0; return 0; }
   
   spvm_warn("Foo %s %d", "aaa", 3);
