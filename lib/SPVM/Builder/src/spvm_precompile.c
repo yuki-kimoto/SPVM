db73b30f6285ccbdfe0fa7081bd5936ff8b2b5f3
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-06-30 18:07:28 +0900

    Simplified.

--- lib/SPVM/Builder/src/spvm_precompile.c
+++ lib/SPVM/Builder/src/spvm_precompile.c
@@ -45,7 +45,7 @@ void SPVM_PRECOMPILE_build_source(SPVM_PRECOMPILE* precompile, SPVM_STRING_BUFFE
   SPVM_RUNTIME* runtime = precompile->runtime;
   
   SPVM_RUNTIME_BASIC_TYPE* basic_type = SPVM_API_RUNTIME_get_basic_type_by_name(runtime, basic_type_name);
-  int32_t basic_type_id = SPVM_API_RUNTIME_get_basic_type_id_by_name(runtime, basic_type_name);
+  int32_t basic_type_id = basic_type->id;
   int32_t basic_type_methods_base = SPVM_API_RUNTIME_get_basic_type_methods_base(runtime, basic_type);
   int32_t basic_type_methods_length = SPVM_API_RUNTIME_get_basic_type_methods_length(runtime, basic_type);
 
