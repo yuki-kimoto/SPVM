ac20ba079b06578e7f21d95ae7d479499c8345d1
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-06 15:42:54 +0900

    string_index reduced.

--- lib/SPVM/Builder/src/spvm_compiler.c
+++ lib/SPVM/Builder/src/spvm_compiler.c
@@ -1105,9 +1105,7 @@ SPVM_RUNTIME* SPVM_COMPILER_build_runtime(SPVM_COMPILER* compiler) {
     runtime->basic_type_symtable = SPVM_HASH_new_hash_permanent(allocator, runtime->basic_types_length);
     for (int32_t basic_type_id = 0; basic_type_id < runtime->basic_types_length; basic_type_id++) {
       SPVM_RUNTIME_BASIC_TYPE* basic_type = &runtime->basic_types[basic_type_id];
-      SPVM_RUNTIME_STRING* basic_type_name_string = (SPVM_RUNTIME_STRING*)&basic_type->constant_strings[basic_type->name_string_index];
-      const char* basic_type_name = basic_type_name_string->value;
-      SPVM_HASH_set(runtime->basic_type_symtable, basic_type_name, strlen(basic_type_name), basic_type);
+      SPVM_HASH_set(runtime->basic_type_symtable, basic_type->name, strlen(basic_type->name), basic_type);
     }
   }
   
