1e42f49d3f9357c3d0062a0cdd40dc0ed427fe1d
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-06 13:19:28 +0900

    Runtime info was fixed.

--- lib/SPVM/Builder/src/spvm_compiler.c
+++ lib/SPVM/Builder/src/spvm_compiler.c
@@ -808,6 +808,7 @@ SPVM_RUNTIME* SPVM_COMPILER_build_runtime(SPVM_COMPILER* compiler) {
     
     if (basic_type->init_method) {
       runtime_basic_type->init_method_index = basic_type->init_method->index;
+      runtime_basic_type->init_method = &runtime_basic_type->methods[basic_type->init_method->index];
     }
     else {
       runtime_basic_type->init_method_index = -1;
@@ -815,17 +816,15 @@ SPVM_RUNTIME* SPVM_COMPILER_build_runtime(SPVM_COMPILER* compiler) {
     
     if (basic_type->destructor_method) {
       runtime_basic_type->destructor_method_index = basic_type->destructor_method->index;
+      runtime_basic_type->destructor_method = &runtime_basic_type->methods[basic_type->destructor_method->index];
     }
     else {
       runtime_basic_type->destructor_method_index = -1;
     }
     
-    if (basic_type->category == SPVM_NATIVE_C_BASIC_TYPE_CATEGORY_INTERFACE) {
-      assert(basic_type->required_method);
-    }
-    
     if (basic_type->required_method) {
       runtime_basic_type->required_method_index = basic_type->required_method->index;
+      runtime_basic_type->required_method = &runtime_basic_type->methods[basic_type->required_method->index];
     }
     else {
       runtime_basic_type->required_method_index = -1;
