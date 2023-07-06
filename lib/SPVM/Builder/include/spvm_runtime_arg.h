3c008a10a8125d3661de368b223f57fcedc41637
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-06 14:53:29 +0900

    basic_type_id was removed from spvm_runtime_arg

--- lib/SPVM/Builder/include/spvm_runtime_arg.h
+++ lib/SPVM/Builder/include/spvm_runtime_arg.h
@@ -7,11 +7,10 @@
 #include "spvm_typedecl.h"
 
 struct spvm_runtime_arg {
+  SPVM_RUNTIME_BASIC_TYPE* basic_type;
   int32_t index;
-  int32_t basic_type_id;
   int32_t type_dimension;
   int32_t type_flag;
-  SPVM_RUNTIME_BASIC_TYPE* basic_type;
 };
 
 #endif
