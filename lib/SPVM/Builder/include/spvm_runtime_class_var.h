8c3d7460e51db512fefd079b4a61eadc7a4e9b74
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-06 10:28:10 +0900

    The following members were added to struct spvm_runtime_class_var.
    +  const char* name;
    +  SPVM_RUNTIME_BASIC_TYPE* current_basic_type;
    +  SPVM_RUNTIME_BASIC_TYPE* basic_type;

--- lib/SPVM/Builder/include/spvm_runtime_class_var.h
+++ lib/SPVM/Builder/include/spvm_runtime_class_var.h
@@ -15,6 +15,9 @@ struct spvm_runtime_class_var {
   int32_t type_dimension;
   int32_t type_flag;
   SPVM_VALUE data;
+  const char* name;
+  SPVM_RUNTIME_BASIC_TYPE* current_basic_type;
+  SPVM_RUNTIME_BASIC_TYPE* basic_type;
 };
 
 #endif
