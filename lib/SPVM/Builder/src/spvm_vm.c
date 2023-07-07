9d9c5cb1127ec629e71ea2d6f5bf7b65c293dc2d
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-07 14:06:36 +0900

    Cleaned up.

--- lib/SPVM/Builder/src/spvm_vm.c
+++ lib/SPVM/Builder/src/spvm_vm.c
@@ -2398,10 +2398,9 @@ int32_t SPVM_VM_call_method(SPVM_ENV* env, SPVM_VALUE* stack, SPVM_RUNTIME_METHO
   label_END_OF_METHOD: {
   
     if (error_id == 0) {
-      void* current_method_return_basic_type = SPVM_API_RUNTIME_get_method_return_basic_type(runtime, current_method);
-      int32_t current_method_return_type_dimension = SPVM_API_RUNTIME_get_method_return_type_dimension(runtime, current_method);
-      int32_t current_method_return_type_flag = SPVM_API_RUNTIME_get_method_return_type_flag(runtime, current_method);
-      
+      SPVM_RUNTIME_BASIC_TYPE* current_method_return_basic_type = current_method->return_basic_type;
+      int32_t current_method_return_type_dimension = current_method->return_type_dimension;
+      int32_t current_method_return_type_flag =current_method->return_type_flag;
       int32_t method_return_type_is_object = SPVM_API_RUNTIME_is_object_type(runtime, current_method_return_basic_type, current_method_return_type_dimension, current_method_return_type_flag);
       if (method_return_type_is_object) {
         if (*(void**)&stack[0] != NULL) {
