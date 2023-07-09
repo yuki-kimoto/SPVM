a7e53580f780bf60c20dadcb6163545961bbec5c
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-10 07:55:53 +0900

    SPVM::Document::NativeAPI::Argument was completed.

--- lib/SPVM/Document/NativeAPI/Argument.pm
+++ lib/SPVM/Document/NativeAPI/Argument.pm
@@ -1,30 +1,52 @@
 =head1 Name
 
-SPVM::Document::NativeAPI::Argument - SPVM Argument Native APIs
+SPVM::Document::NativeAPI::Argument - Argument Native APIs
 
 =head1 Description
 
-SPVM Argument Native APIs are the APIs for arguments.
+The argument native APIs of L<SPVM> are the APIs to get information of arguments.
 
 =head1 Usage
 
-  SPVM_API_Arg* api_arg = env->api->arg;
+  SPVM_API_ARG* api_arg = env->api->arg;
   
-  void* basic_type = api_arg->get_basic_type(runtime, arg);
+  # ...
   
+  void* basic_type = api_arg->get_basic_type(env->runtime, arg);
+
+The C<arg> is got by the L<get_arg|SPVM::Document::NativeAPI::Method/"get_arg"> method native API.
+
 =head1 Native APIs
 
 =head2 get_basic_type
 
   void* (*get_basic_type)(void* runtime, void* arg);
 
+Returns the L<basic type|SPVM::Document::NativeAPI::BasicType> of the argument.
+
+The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
+
+The C<arg> argument is a L<arg|SPVM::Document::NativeAPI::Arg> object.
+
 =head2 get_type_dimension
 
   int32_t (*get_type_dimension)(void* runtime, void* arg);
 
+Returns the type dimention of the argument.
+
+The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
+
+The C<arg> argument is a L<arg|SPVM::Document::NativeAPI::Arg> object.
+
 =head2 get_type_flag
 
   int32_t (*get_type_flag)(void* runtime, void* arg);
+
+Returns the type flag of the argument.
+
+The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
+
+The C<arg> argument is a L<arg|SPVM::Document::NativeAPI::Arg> object.
 
 =head1 Native API IDs
 
