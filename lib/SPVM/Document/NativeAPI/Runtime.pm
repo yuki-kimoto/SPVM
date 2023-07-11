9a39a6e5da6c7ee9860f5f801927cc16947c82e5
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-11 14:30:30 +0900

    The doc of the runtime native API was improved.

--- lib/SPVM/Document/NativeAPI/Runtime.pm
+++ lib/SPVM/Document/NativeAPI/Runtime.pm
@@ -32,7 +32,9 @@ Frees a runtime object.
 
   int32_t (*get_object_data_offset)(void* runtime);
 
-Returns the offset of the data header size of the object.
+Returns the offset of the data of the C<SPVM_OBJECT> type.
+
+The C<runtime> argument is a runtime object.
 
 =head2 get_object_ref_count_offset
 
@@ -40,40 +42,82 @@ Returns the offset of the data header size of the object.
 
 Returns the offset of the C<ref_count> member variable of the C<SPVM_OBJECT> type.
 
+The C<runtime> argument is a runtime object.
+
 =head2 get_object_length_offset
 
   int32_t (*get_object_length_offset)(void* runtime);
 
 Returns the offset of the C<length> member variable of the C<SPVM_OBJECT> type.
 
+The C<runtime> argument is a runtime object.
+
 =head2 get_basic_type_by_id
 
   void* (*get_basic_type_by_id)(void* runtime, int32_t basic_type_id);
+
+Gets a L<basic type|SPVM::Document::NativeAPI::BasicType> object by a basic type ID and returns it.
+
+The C<runtime> argument is a runtime object.
 
 =head2 get_basic_type_by_name
 
   void* (*get_basic_type_by_name)(void* runtime, const char* basic_type_name);
 
+Gets a L<basic type|SPVM::Document::NativeAPI::BasicType> object by a basic type name and returns it.
+
+The C<runtime> argument is a runtime object.
+
 =head2 get_basic_types_length
 
   int32_t (*get_basic_types_length)(void* runtime);
+
+Gets the length of the basic types.
+
+The C<runtime> argument is a runtime object.
 
 =head2 is_object_type
 
   int32_t (*is_object_type)(void* runtime, void* basic_type, int32_t type_dimension, int32_t flag);
 
+If the type is object type, returns 1. Otherwise returns 0.
+
+The C<runtime> argument is a runtime object.
+
+The C<basic_type> argument is a L<basic type|SPVM::Document::NativeAPI::BasicType> object.
+
 =head2 can_assign
 
   int32_t (*can_assign)(void* runtime, void* dist_basic_type, int32_t dist_type_dimension, int32_t dist_type_flag, void* src_basic_type, int32_t src_type_dimension, int32_t src_type_flag);
+
+If a source type can be assigned to a dist type, return 1. Otherwise returns 0.
+
+The C<runtime> argument is a runtime object.
+
+The C<dist_basic_type> argument is a L<basic type|SPVM::Document::NativeAPI::BasicType> object.
+
+The C<src_basic_type> argument is a L<basic type|SPVM::Document::NativeAPI::BasicType> object.
 
 =head2 build_precompile_module_source
 
   void (*build_precompile_module_source)(void* runtime, void* string_buffer, const char* module_name);
 
+Builds the C source code of a precompiled module.
+
+The C<runtime> argument is a runtime object.
+
+The C<string_buffer> argument is a L<string buffer|SPVM::Document::NativeAPI::StringBuffer> object.
+
 =head2 build_precompile_method_source
 
   void (*build_precompile_method_source)(void* runtime, void* string_buffer, const char* module_name, const char* method_name);
   
+Builds the C source code of a precompiled method.
+
+The C<runtime> argument is a runtime object.
+
+The C<string_buffer> argument is a L<string buffer|SPVM::Document::NativeAPI::StringBuffer> object.
+
 =head1 Native API IDs
 
   0 new_instance
