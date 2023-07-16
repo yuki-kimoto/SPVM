a399fae32ed688282d2548289c05ded8db6a51c1
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-17 08:39:11 +0900

    Native::Field was implemented.

--- lib/SPVM/Native/Field.pm
+++ lib/SPVM/Native/Field.pm
@@ -6,40 +6,69 @@ package SPVM::Native::Field;
 
 =head1 Name
 
-SPVM::Native::Field - Short Description
+SPVM::Native::Field - Information of Fields
 
 =head1 Description
 
-The Native::Field class of L<SPVM> has methods for someting.
+The Native::Field class of L<SPVM> has methods to manipulate information of fields.
 
 =head1 Usage
 
   use Native::Field;
 
-=head1 Fields
-
-
-
-=head1 Class Methods
-
-
-
 =head1 Instance Methods
 
+=head2 get_index
 
+  method get_index : int ();
 
+Gets the index.
 
-=head1 Repository
+If a memory error occurs, an exception is thrown.
 
+=head2 get_name
 
+  method get_name : string ();
 
-=head1 Author
+Gets the name.
 
-[--user-name] C<[--user-email]>
+If a memory error occurs, an exception is thrown.
+
+=head2 get_basic_type
+
+  method get_basic_type : Native::BasicType ();
+
+Gets the L<Native::BasicType|SPVM::Native::BasicType> object.
+
+If a memory error occurs, an exception is thrown.
+
+=head2 get_type_dimension
+
+  method get_type_dimension : int ();
+
+Gets the type dimension.
+
+If a memory error occurs, an exception is thrown.
+
+=head2 get_type_flag
+
+  method get_type_flag : int ();
+
+Gets the type flag.
+
+If a memory error occurs, an exception is thrown.
+
+=head2 get_current_basic_type
+
+  method get_current_basic_type : Native::BasicType ();
+
+Gets the current L<Native::BasicType|SPVM::Native::BasicType> object that defines this field.
+
+If a memory error occurs, an exception is thrown.
 
 =head1 Copyright & License
 
-Copyright (c) 2023 [--user-name]
+Copyright (c) 2023 Yuki Kimoto
 
 MIT License
 
