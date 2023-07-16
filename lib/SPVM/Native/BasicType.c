823546b404d940227535ddf3b883a111aadf4e51
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-17 08:01:22 +0900

    The get_parent method was added to the Native::BasicType class.

--- lib/SPVM/Native/BasicType.c
+++ lib/SPVM/Native/BasicType.c
@@ -125,3 +125,33 @@ int32_t SPVM__Native__BasicType__get_module_rel_file(SPVM_ENV* env, SPVM_VALUE* 
   return 0;
 }
 
+int32_t SPVM__Native__BasicType__get_parent(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  void* basic_type = env->get_pointer(env, stack, obj_self);
+  
+  if (!basic_type) {
+    return env->die(env, stack, "The basic type was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  void* obj_runtime = env->get_field_object_by_name(env, stack, obj_self, "runtime", &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  void* runtime = env->get_pointer(env, stack, obj_runtime);
+  
+  if (!runtime) {
+    return env->die(env, stack, "The runtime was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  void* parent = env->api->basic_type->get_parent(runtime, basic_type);
+  
+  void* obj_parent = env->new_pointer_object_by_name(env, stack, "Native::BasicType", parent, &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  stack[0].oval = obj_parent;
+  
+  return 0;
+}
