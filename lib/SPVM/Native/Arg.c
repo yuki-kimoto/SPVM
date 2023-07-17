f44a7fa63789eccce6595b7c0b5d9386fadb4e60
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-17 10:07:22 +0900

    Native::Arg was implemented.

--- lib/SPVM/Native/Arg.c
+++ lib/SPVM/Native/Arg.c
@@ -7,11 +7,118 @@
 
 static const char* FILE_NAME = "Native/Arg.c";
 
-int32_t SPVM__Native__Arg__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
-  (void)env;
-  (void)stack;
+int32_t SPVM__Native__Arg__get_index(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  void* arg = env->get_pointer(env, stack, obj_self);
+  
+  if (!arg) {
+    return env->die(env, stack, "The argument was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  void* obj_runtime = env->get_field_object_by_name(env, stack, obj_self, "runtime", &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  void* runtime = env->get_pointer(env, stack, obj_runtime);
+  
+  if (!runtime) {
+    return env->die(env, stack, "The runtime was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  int32_t index = env->api->arg->get_index(runtime, arg);
+  
+  stack[0].ival = index;
   
   return 0;
 }
 
+int32_t SPVM__Native__Arg__get_basic_type(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  void* arg = env->get_pointer(env, stack, obj_self);
+  
+  if (!arg) {
+    return env->die(env, stack, "The argument was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  void* obj_runtime = env->get_field_object_by_name(env, stack, obj_self, "runtime", &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  void* runtime = env->get_pointer(env, stack, obj_runtime);
+  
+  if (!runtime) {
+    return env->die(env, stack, "The runtime was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  void* basic_type = env->api->arg->get_basic_type(runtime, arg);
+  
+  void* obj_basic_type = env->new_pointer_object_by_name(env, stack, "Native::BasicType", basic_type, &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  stack[0].oval = obj_basic_type;
+  
+  return 0;
+}
+
+int32_t SPVM__Native__Arg__get_type_dimension(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  void* arg = env->get_pointer(env, stack, obj_self);
+  
+  if (!arg) {
+    return env->die(env, stack, "The argument was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  void* obj_runtime = env->get_field_object_by_name(env, stack, obj_self, "runtime", &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  void* runtime = env->get_pointer(env, stack, obj_runtime);
+  
+  if (!runtime) {
+    return env->die(env, stack, "The runtime was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  int32_t type_dimension = env->api->arg->get_type_dimension(runtime, arg);
+  
+  stack[0].ival = type_dimension;
+  
+  return 0;
+}
+
+int32_t SPVM__Native__Arg__get_type_flag(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  void* arg = env->get_pointer(env, stack, obj_self);
+  
+  if (!arg) {
+    return env->die(env, stack, "The argument was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  void* obj_runtime = env->get_field_object_by_name(env, stack, obj_self, "runtime", &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  void* runtime = env->get_pointer(env, stack, obj_runtime);
+  
+  if (!runtime) {
+    return env->die(env, stack, "The runtime was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  int32_t type_flag = env->api->arg->get_type_flag(runtime, arg);
+  
+  stack[0].ival = type_flag;
+  
+  return 0;
+}
 
--- lib/SPVM/Native/Arg.pm
+++ lib/SPVM/Native/Arg.pm
@@ -6,40 +6,53 @@ package SPVM::Native::Arg;
 
 =head1 Name
 
-SPVM::Native::Arg - Short Description
+SPVM::Native::Arg - Information of Arguments
 
 =head1 Description
 
-The Native::Arg class of L<SPVM> has methods for someting.
+The Native::Arg class of L<SPVM> has methods to manipulate information of arguments.
 
 =head1 Usage
 
   use Native::Arg;
 
-=head1 Fields
-
-
-
-=head1 Class Methods
-
-
-
 =head1 Instance Methods
 
+=head2 get_index
 
+  method get_index : int ();
 
+Gets the index.
 
-=head1 Repository
+If a memory error occurs, an exception is thrown.
 
+=head2 get_basic_type
 
+  method get_basic_type : Native::BasicType ();
 
-=head1 Author
+Gets the L<Native::BasicType|SPVM::Native::BasicType> object.
 
-[--user-name] C<[--user-email]>
+If a memory error occurs, an exception is thrown.
+
+=head2 get_type_dimension
+
+  method get_type_dimension : int ();
+
+Gets the type dimension.
+
+If a memory error occurs, an exception is thrown.
+
+=head2 get_type_flag
+
+  method get_type_flag : int ();
+
+Gets the type flag.
+
+If a memory error occurs, an exception is thrown.
 
 =head1 Copyright & License
 
-Copyright (c) 2023 [--user-name]
+Copyright (c) 2023 Yuki Kimoto
 
 MIT License
 
