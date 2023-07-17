abcdb8fd4d5053593e76aa603ed71691f57a140f
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-17 10:13:52 +0900

    Native::Method was implemented.

--- lib/SPVM/Native/Method.c
+++ lib/SPVM/Native/Method.c
@@ -7,11 +7,178 @@
 
 static const char* FILE_NAME = "Native/Method.c";
 
-int32_t SPVM__Native__Method__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
-  (void)env;
-  (void)stack;
+int32_t SPVM__Native__Method__get_index(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  void* method = env->get_pointer(env, stack, obj_self);
+  
+  if (!method) {
+    return env->die(env, stack, "The method was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  void* obj_runtime = env->get_field_object_by_name(env, stack, obj_self, "runtime", &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  void* runtime = env->get_pointer(env, stack, obj_runtime);
+  
+  if (!runtime) {
+    return env->die(env, stack, "The runtime was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  int32_t index = env->api->method->get_index(runtime, method);
+  
+  stack[0].ival = index;
   
   return 0;
 }
 
+int32_t SPVM__Native__Method__get_name(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  void* method = env->get_pointer(env, stack, obj_self);
+  
+  if (!method) {
+    return env->die(env, stack, "The method was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  void* obj_runtime = env->get_field_object_by_name(env, stack, obj_self, "runtime", &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  void* runtime = env->get_pointer(env, stack, obj_runtime);
+  
+  if (!runtime) {
+    return env->die(env, stack, "The runtime was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  const char* name = env->api->method->get_name(runtime, method);
+  
+  void* obj_name = env->new_string_nolen(env, stack, name);
+  
+  stack[0].oval = obj_name;
+  
+  return 0;
+}
 
+int32_t SPVM__Native__Method__get_return_basic_type(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  void* method = env->get_pointer(env, stack, obj_self);
+  
+  if (!method) {
+    return env->die(env, stack, "The method was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  void* obj_runtime = env->get_field_object_by_name(env, stack, obj_self, "runtime", &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  void* runtime = env->get_pointer(env, stack, obj_runtime);
+  
+  if (!runtime) {
+    return env->die(env, stack, "The runtime was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  void* basic_type = env->api->method->get_return_basic_type(runtime, method);
+  
+  void* obj_basic_type = env->new_pointer_object_by_name(env, stack, "Native::BasicType", basic_type, &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  stack[0].oval = obj_basic_type;
+  
+  return 0;
+}
+
+int32_t SPVM__Native__Method__get_return_type_dimension(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  void* method = env->get_pointer(env, stack, obj_self);
+  
+  if (!method) {
+    return env->die(env, stack, "The method was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  void* obj_runtime = env->get_field_object_by_name(env, stack, obj_self, "runtime", &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  void* runtime = env->get_pointer(env, stack, obj_runtime);
+  
+  if (!runtime) {
+    return env->die(env, stack, "The runtime was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  int32_t type_dimension = env->api->method->get_return_type_dimension(runtime, method);
+  
+  stack[0].ival = type_dimension;
+  
+  return 0;
+}
+
+int32_t SPVM__Native__Method__get_return_type_flag(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  void* method = env->get_pointer(env, stack, obj_self);
+  
+  if (!method) {
+    return env->die(env, stack, "The method was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  void* obj_runtime = env->get_field_object_by_name(env, stack, obj_self, "runtime", &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  void* runtime = env->get_pointer(env, stack, obj_runtime);
+  
+  if (!runtime) {
+    return env->die(env, stack, "The runtime was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  int32_t type_flag = env->api->method->get_return_type_flag(runtime, method);
+  
+  stack[0].ival = type_flag;
+  
+  return 0;
+}
+
+int32_t SPVM__Native__Method__get_current_basic_type(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_self = stack[0].oval;
+  
+  void* method = env->get_pointer(env, stack, obj_self);
+  
+  if (!method) {
+    return env->die(env, stack, "The method was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  void* obj_runtime = env->get_field_object_by_name(env, stack, obj_self, "runtime", &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  void* runtime = env->get_pointer(env, stack, obj_runtime);
+  
+  if (!runtime) {
+    return env->die(env, stack, "The runtime was already destroyed.", __func__, FILE_NAME, __LINE__);
+  }
+  
+  void* current_basic_type = env->api->method->get_current_basic_type(runtime, method);
+  
+  void* obj_current_basic_type = env->new_pointer_object_by_name(env, stack, "Native::BasicType", current_basic_type, &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  stack[0].oval = obj_current_basic_type;
+  
+  return 0;
+}
--- lib/SPVM/Native/Method.pm
+++ lib/SPVM/Native/Method.pm
@@ -6,40 +6,69 @@ package SPVM::Native::Method;
 
 =head1 Name
 
-SPVM::Native::Method - Short Description
+SPVM::Native::Method - Information of Methods
 
 =head1 Description
 
-The Native::Method class of L<SPVM> has methods for someting.
+The Native::Method class of L<SPVM> has methods to manipulate information of methods.
 
 =head1 Usage
 
   use Native::Method;
 
-=head1 Fields
-
-
-
-=head1 Class Methods
-
-
-
 =head1 Instance Methods
 
+=head2 get_index
 
+  method get_index : int ();
 
+Gets the index.
 
-=head1 Repository
+If a memory error occurs, an exception is thrown.
 
+=head2 get_name
 
+  method get_name : string ();
 
-=head1 Author
+Gets the name.
 
-[--user-name] C<[--user-email]>
+If a memory error occurs, an exception is thrown.
+
+=head2 get_return_basic_type
+
+  method get_return_basic_type : Native::BasicType ();
+
+Gets the L<Native::BasicType|SPVM::Native::BasicType> object of the return value.
+
+If a memory error occurs, an exception is thrown.
+
+=head2 get_return_type_dimension
+
+  method get_return_type_dimension : int ();
+
+Gets the type dimension of the return value.
+
+If a memory error occurs, an exception is thrown.
+
+=head2 get_return_type_flag
+
+  method get_return_type_flag : int ();
+
+Gets the type flag of the return value.
+
+If a memory error occurs, an exception is thrown.
+
+=head2 get_current_basic_type
+
+  method get_current_basic_type : Native::BasicType ();
+
+Gets the current L<Native::BasicType|SPVM::Native::BasicType> object that defines this method.
+
+If a memory error occurs, an exception is thrown.
 
 =head1 Copyright & License
 
-Copyright (c) 2023 [--user-name]
+Copyright (c) 2023 Yuki Kimoto
 
 MIT License
 
