4be6998d25e70a62a867e7b1fc59d628a5293841
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-18 09:26:54 +0900

    A replacement done.
    find * | grep -v 'ppport\.h' | grep -P '\.(spvm|c|h|xs|y|pm|pod|t)$' | xargs perl -pi -e 's|remove_module_name_part_from_file|remove_basic_type_name_part_from_file|g'

--- lib/SPVM/Builder/Util.pm
+++ lib/SPVM/Builder/Util.pm
@@ -228,7 +228,7 @@ sub convert_basic_type_name_to_rel_file {
   return $rel_file_with_ext;
 }
 
-sub remove_module_name_part_from_file {
+sub remove_basic_type_name_part_from_file {
   my ($file, $basic_type_name) = @_;
 
   $basic_type_name =~ s/^SPVM:://;
