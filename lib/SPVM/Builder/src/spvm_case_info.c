7ec9193b66efbc5ea7c3f615dae1faf80a547b70
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-24 07:58:21 +0900

    compiler->current_each_compile_allocator was used more.

--- lib/SPVM/Builder/src/spvm_case_info.c
+++ lib/SPVM/Builder/src/spvm_case_info.c
@@ -7,7 +7,7 @@
 #include "spvm_compiler.h"
 
 SPVM_CASE_INFO* SPVM_CASE_INFO_new(SPVM_COMPILER* compiler) {
-  SPVM_CASE_INFO* case_info = SPVM_ALLOCATOR_alloc_memory_block_permanent(compiler->global_allocator, sizeof(SPVM_CASE_INFO));
+  SPVM_CASE_INFO* case_info = SPVM_ALLOCATOR_alloc_memory_block_permanent(compiler->current_each_compile_allocator, sizeof(SPVM_CASE_INFO));
   
   return case_info;
 }
