1946e24fa07a728cfe053b811bba905402582bea
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-24 08:28:15 +0900

    compiler->current_each_compile_allocator was used more.

--- lib/SPVM/Builder/src/spvm_method.c
+++ lib/SPVM/Builder/src/spvm_method.c
@@ -15,10 +15,10 @@
 #include "spvm_opcode_list.h"
 
 SPVM_METHOD* SPVM_METHOD_new(SPVM_COMPILER* compiler) {
-  SPVM_METHOD* method = SPVM_ALLOCATOR_alloc_memory_block_permanent(compiler->global_allocator, sizeof(SPVM_METHOD));
+  SPVM_METHOD* method = SPVM_ALLOCATOR_alloc_memory_block_permanent(compiler->current_each_compile_allocator, sizeof(SPVM_METHOD));
   
-  method->var_decls = SPVM_LIST_new_list_permanent(compiler->global_allocator, 0);
-  method->anon_method_fields = SPVM_LIST_new_list_permanent(compiler->global_allocator, 0);
+  method->var_decls = SPVM_LIST_new_list_permanent(compiler->current_each_compile_allocator, 0);
+  method->anon_method_fields = SPVM_LIST_new_list_permanent(compiler->current_each_compile_allocator, 0);
   
   method->opcode_list = SPVM_OPCODE_LIST_new(compiler);
   
