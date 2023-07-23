38c0ac6860a80b47a39444bcf5996bb7b827c273
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-24 08:36:05 +0900

    compiler->current_each_compile_allocator was used more.

--- lib/SPVM/Builder/src/spvm_var_decl.c
+++ lib/SPVM/Builder/src/spvm_var_decl.c
@@ -16,7 +16,7 @@ SPVM_VAR_DECL* SPVM_VAR_DECL_new(SPVM_COMPILER* compiler) {
 }
 
 SPVM_VAR_DECL* SPVM_VAR_DECL_new_arg(SPVM_COMPILER* compiler) {
-  SPVM_VAR_DECL* var_decl = SPVM_ALLOCATOR_alloc_memory_block_permanent(compiler->global_allocator, sizeof(SPVM_VAR_DECL));
+  SPVM_VAR_DECL* var_decl = SPVM_ALLOCATOR_alloc_memory_block_permanent(compiler->current_each_compile_allocator, sizeof(SPVM_VAR_DECL));
 
   var_decl->index = -1;
   var_decl->call_stack_index = -1;
