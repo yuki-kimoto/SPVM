59bb3057b9a82b6d5c98ebe6b7e526703f1f86f7
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-24 14:10:36 +0900

    The doc of the compiler native APIs was improved.

--- lib/SPVM/Document/NativeAPI/Compiler.pm
+++ lib/SPVM/Document/NativeAPI/Compiler.pm
@@ -1,47 +1,22 @@
 =head1 Name
 
-SPVM::Document::NativeAPI::Compiler - SPVM Compiler Native APIs
+SPVM::Document::NativeAPI::Compiler - Compiler Native APIs
 
 =head1 Usage
 
-  // New compiler
   void* compiler = env->api->compiler->new_instance();
   
-  // @INC
   env->api->compiler->add_include_dir(compiler, "lib");
   
-  // Compile SPVM
   env->api->compiler->set_start_file(compiler, __FILE__);
   env->api->compiler->get_start_line(compiler, __LINE__ + 1);
   int32_t status = env->api->compiler->compile(compiler, "MyClass");
   
-  // Free compiler
   env->api->compiler->free_instance(compiler);
 
 =head1 Description
 
-SPVM Compiler Native APIs are the APIs to compile SPVM modules.
-
-=head1 IDs of Compiler Native APIs
-
-Compiler native APIs have its IDs.
-
-  0 new_instance,
-  1 free_instance,
-  2 get_start_line,
-  3 set_start_line,
-  4 get_start_file,
-  5 set_start_file,
-  6 get_include_dirs_length,
-  7 get_include_dir,
-  8 add_include_dir,
-  9 clear_include_dirs,
-  10 get_module_file,
-  11 set_module_file,
-  12 compile,
-  13 get_error_message,
-  14 get_error_messages_length,
-  15 get_runtime,
+The compiler native APIs of L<SPVM> are the APIs to compile SPVM modules.
 
 =head1 Compiler Native APIs
 
@@ -57,53 +32,71 @@ Creates a compiler object.
 
 Frees a compiler.
 
-=head2 set_start_line
-  
-  void (*set_start_line)(void* compiler, int32_t start_line);
-
-Sets the start line of the caller.
-
 =head2 get_start_line
   
   int32_t (*get_start_line)(void* compiler);
 
-Gets the start line of the caller.
+Gets the start line.
 
-=head2 set_start_file
+=head2 set_start_line
   
-  void (*set_start_file)(void* compiler, const char* start_file);
+  void (*set_start_line)(void* compiler, int32_t start_line);
 
-Set the start file of the caller. C<start_file> is copied.
+Sets a start line.
 
 =head2 get_start_file
   
   const char* (*get_start_file)(void* compiler);
 
-Gets the start file of the caller.
+Gets the start file.
 
-=head2 add_include_dir
+=head2 set_start_file
   
-  void (*add_include_dir)(void* compiler, const char* include_dir);
+  void (*set_start_file)(void* compiler, const char* start_file);
 
-Adds a class searching directory. C<include_dir> is copied.
+Set a start file.
 
 =head2 get_include_dirs_length
   
   int32_t (*get_include_dirs_length)(void* compiler);
 
-Gets the length of the class searching directories.
+Gets the length of the directories for searching modules.
 
 =head2 get_include_dir
 
   const char* (*get_include_dir)(void* compiler, int32_t index);
 
-Gets a searching directory.
+Gets a directory for searching modules by an index.
+
+=head2 add_include_dir
+  
+  void (*add_include_dir)(void* compiler, const char* include_dir);
+
+Adds a directory for searching modules.
+
+=head2 clear_include_dirs
+  
+  void (*clear_include_dirs)(SPVM_COMPILER* compiler);
+
+Clears the directories for searching modules.
+
+=head2 get_module_file
+
+  void* (*get_module_file)(void* compiler, const char* module_name);
+
+Gets the L<module file|SPVM::Document::NativeAPI::ModuleFile> object for a module.
+
+=head2 set_module_file
+
+  void (*set_module_file)(void* compiler, const char* module_name, void* module_file);
+
+Sets a L<module file|SPVM::Document::NativeAPI::ModuleFile> object for a module.
 
 =head2 compile
   
-  int32_t (*compile)(void* compiler, const char* basic_type_name);
+  int32_t (*compile)(void* compiler, const char* module_name);
 
-Compile SPVM classes.
+Compiles SPVM modules.
 
 =head2 get_error_messages_length
   
@@ -115,25 +108,32 @@ Gets the length of the compilation error messages.
   
   const char* (*get_error_message)(void* compiler, int32_t index);
 
-Gets the compiler error messages.
-
-=head2 clear_include_dirs
-  
-  void (*clear_include_dirs)(SPVM_COMPILER* compiler);
-
-Clear the class searching directories. The class searching directories are freed.
-
-=head2 get_module_file
-
-  void* (*get_module_file)(void* compiler, const char* module_name);
-
-=head2 set_module_file
-
-  void (*set_module_file)(void* compiler, const char* module_name, void* module_file);
+Gets a compiler error message by an index.
 
 =head2 get_runtime
 
-void* (*get_runtime)(void* compiler);
+  void* (*get_runtime)(void* compiler);
+
+Gets the runtime.
+
+=head1 Compiler Native API IDs
+
+  0 new_instance 
+  1 free_instance 
+  2 get_start_line 
+  3 set_start_line 
+  4 get_start_file 
+  5 set_start_file 
+  6 get_include_dirs_length 
+  7 get_include_dir 
+  8 add_include_dir 
+  9 clear_include_dirs 
+  10 get_module_file 
+  11 set_module_file 
+  12 compile 
+  13 get_error_message 
+  14 get_error_messages_length 
+  15 get_runtime 
 
 =head1 Copyright & License
 
