e005c1130627028aaa5d23ce842bf385b26f65cd
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-27 08:37:21 +0900

    get_field_native_object_by_name is added.

--- lib/SPVM/Native/BasicType.c
+++ lib/SPVM/Native/BasicType.c
@@ -7,6 +7,28 @@
 
 static const char* FILE_NAME = "Native/BasicType.c";
 
+static void* get_field_native_object_by_name(SPVM_ENV* env, SPVM_VALUE* stack, void* object, const char* field_name, int32_t* error_id, const char* func_name, const char* file_name, int32_t line) {
+  
+  *error_id = 0;
+  
+  void* obj_runtime = env->get_field_object_by_name(env, stack, object, field_name, error_id, func_name, file_name, line);
+  if (error_id) { return NULL; }
+  
+  if (!obj_runtime) {
+    *error_id = env->die(env, stack, "The %s cannot be got.", field_name, func_name, file_name, line);
+    return NULL;
+  }
+  
+  void* runtime = env->get_pointer(env, stack, obj_runtime);
+  
+  if (!runtime) {
+    *error_id = env->die(env, stack, "The pointer to the native %s cannot be got.", field_name, func_name, file_name, line);
+    return NULL;
+  }
+  
+  return obj_runtime;
+}
+
 int32_t SPVM__Native__BasicType__get_id(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   int32_t error_id = 0;
