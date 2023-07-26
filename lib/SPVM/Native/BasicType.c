914875ebe5a64cd13954660a13738b45dc36c9e0
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-27 08:38:53 +0900

    get_field_native_object_by_name is used more.

--- lib/SPVM/Native/BasicType.c
+++ lib/SPVM/Native/BasicType.c
@@ -41,14 +41,10 @@ int32_t SPVM__Native__BasicType__get_id(SPVM_ENV* env, SPVM_VALUE* stack) {
     return env->die(env, stack, "The basic type was already destroyed.", __func__, FILE_NAME, __LINE__);
   }
   
-  void* obj_runtime = env->get_field_object_by_name(env, stack, obj_self, "runtime", &error_id, __func__, FILE_NAME, __LINE__);
+  void* obj_runtime = get_field_native_object_by_name(env, stack, obj_self, "runtime", &error_id, __func__, FILE_NAME, __LINE__);
   if (error_id) { return error_id; }
   
   void* runtime = env->get_pointer(env, stack, obj_runtime);
-  
-  if (!runtime) {
-    return env->die(env, stack, "The runtime was already destroyed.", __func__, FILE_NAME, __LINE__);
-  }
   
   int32_t id = env->api->basic_type->get_id(runtime, basic_type);
   
