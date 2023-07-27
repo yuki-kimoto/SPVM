a4e7693d13259370ea640f0f862e3cc55b4e0a07
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-27 16:38:30 +0900

    Native::MethodCall: empty native methods are added.

--- lib/SPVM/Native/MethodCall.c
+++ lib/SPVM/Native/MethodCall.c
@@ -7,9 +7,163 @@
 
 static const char* FILE_NAME = "Native/MethodCall.c";
 
-int32_t SPVM__Native__MethodCall__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+int32_t SPVM__Native__MethodCall__new_class_method(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   return 0;
 }
 
+int32_t SPVM__Native__MethodCall__new_instance_method_static(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__new_instance_method(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__init(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_byte(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_short(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_int(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_long(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_float(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_double(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_object(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_byte_ref(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_short_ref(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_int_ref(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_long_ref(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_float_ref(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_double_ref(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_mulnum_byte_ref(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_mulnum_short_ref(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_mulnum_int_ref(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_mulnum_long_ref(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_mulnum_float_ref(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_mulnum_double_ref(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__set_stack_length(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__call(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__get_stack_byte(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__get_stack_short(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__get_stack_int(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__get_stack_long(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__get_stack_float(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__get_stack_double(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+int32_t SPVM__Native__MethodCall__get_stack_object(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
 
