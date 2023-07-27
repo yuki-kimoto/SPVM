5ec6f2111405302286e95a1a6d51f93fad2ecc20
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-27 13:29:00 +0900

    The empty Native::MethodCall module is added.

---
+++ lib/SPVM/Native/MethodCall.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2023 Yuki Kimoto
+// MIT License
+
+#include "spvm_native.h"
+
+
+
+static const char* FILE_NAME = "Native/MethodCall.c";
+
+int32_t SPVM__Native__MethodCall__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+
