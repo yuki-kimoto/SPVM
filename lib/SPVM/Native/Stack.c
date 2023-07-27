d7234b2e84498e54d0ef67ce335f9969c9c9dffe
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-27 09:44:30 +0900

    env->get_field_object_defined_and_has_pointer_by_name is used more.

--- lib/SPVM/Native/Stack.c
+++ lib/SPVM/Native/Stack.c
@@ -5,36 +5,13 @@
 
 static const char* FILE_NAME = "Native::Stack.c";
 
-static void* get_field_native_object_by_name(SPVM_ENV* env, SPVM_VALUE* stack, void* object, const char* field_name, int32_t* error_id, const char* func_name, const char* file_name, int32_t line) {
-  
-  *error_id = 0;
-  
-  void* obj_runtime = env->get_field_object_by_name(env, stack, object, field_name, error_id, func_name, file_name, line);
-  
-  if (*error_id) { return NULL; }
-  
-  if (!obj_runtime) {
-    *error_id = env->die(env, stack, "The %s cannot be got.", field_name, func_name, file_name, line);
-    return NULL;
-  }
-  
-  void* runtime = env->get_pointer(env, stack, obj_runtime);
-  
-  if (!runtime) {
-    *error_id = env->die(env, stack, "The pointer to the native %s cannot be got.", field_name, func_name, file_name, line);
-    return NULL;
-  }
-  
-  return obj_runtime;
-}
-
 int32_t SPVM__Native__Stack__DESTROY(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   int32_t e = 0;
   
   void* obj_self = stack[0].oval;
   
-  void* obj_my_env = get_field_native_object_by_name(env, stack, obj_self, "env", &e, __func__, FILE_NAME, __LINE__);
+  void* obj_my_env = env->get_field_object_defined_and_has_pointer_by_name(env, stack, obj_self, "env", &e, __func__, FILE_NAME, __LINE__);
   if (e) { return e; }
   
   SPVM_ENV* my_env = env->get_pointer(env, stack, obj_my_env);
