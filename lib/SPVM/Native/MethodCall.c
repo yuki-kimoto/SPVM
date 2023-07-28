edb1a837781990d8e94a0a903167e206e52339f3
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-07-28 09:01:10 +0900

    new_instance_method is fixed.

--- lib/SPVM/Native/MethodCall.c
+++ lib/SPVM/Native/MethodCall.c
@@ -103,8 +103,6 @@ int32_t SPVM__Native__MethodCall__new_instance_method(SPVM_ENV* env, SPVM_VALUE*
     return env->die(env, stack, "The $invocant must be defined.", __func__, FILE_NAME, __LINE__);
   }
   
-  const char* invocant = env->get_chars(env, stack, obj_invocant);
-  
   void* obj_method_name = stack[0].oval;
   
   if (!obj_method_name) {
@@ -113,15 +111,24 @@ int32_t SPVM__Native__MethodCall__new_instance_method(SPVM_ENV* env, SPVM_VALUE*
   
   const char* method_name = env->get_chars(env, stack, obj_method_name);
   
-  void* method = env->get_instance_method(env, stack, invocant, method_name);
+  void* method = env->get_instance_method(env, stack, obj_invocant, method_name);
   if (!method) {
-    return env->die(env, stack, "The \"%s\" method in the \"%s\" class cannot be found.", method_name, invocant, __func__, FILE_NAME, __LINE__);
+    return env->die(env, stack, "The \"%s\" method cannot be found.", method_name, __func__, FILE_NAME, __LINE__);
   }
+  
+  void* obj_method = env->new_pointer_object_by_name(env, stack, "Native::Method", method, &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  env->set_field_object_by_name(env, stack, obj_method, "runtime", obj_runtime, &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
   
   void* obj_method_call = env->new_pointer_object_by_name(env, stack, "Native::MethodCall", method, &error_id, __func__, FILE_NAME, __LINE__);
   if (error_id) { return error_id; }
   
   env->set_field_object_by_name(env, stack, obj_method_call, "runtime", obj_runtime, &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  env->set_field_object_by_name(env, stack, obj_method_call, "method", obj_method, &error_id, __func__, FILE_NAME, __LINE__);
   if (error_id) { return error_id; }
   
   return 0;
