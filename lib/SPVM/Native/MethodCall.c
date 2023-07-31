84d474540278dcb442f149ba94e19f5a21d13dbf
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-08-01 08:38:02 +0900

    Methods in Native::MethodCall chained.
    -  native static method new_class_method : Native::MethodCall ($runtime : Native::Runtime, $basic_type_name : string, $method_name : string);
    +  native static method new_class_method : Native::MethodCall ($basic_type_name : string, $method_name : string, $env : Native::Env = undef, $stack : Native::Stack = undef);
    
    -  native static method new_instance_method_static : Native::MethodCall($runtime : Native::Runtime, $basic_type_name : string, $method_name : string);
    +  native static method new_instance_method_static : Native::MethodCall($basic_type_name : string, $method_name : string, $env : Native::Env = undef, $stack : Native::Stack = undef);
    
    -  native static method new_instance_method : Native::MethodCall($runtime : Native::Runtime, $invocant : object, $method_name : string);
    +  native static method new_instance_method : Native::MethodCall($invocant : object, $method_name : string, $env : Native::Env = undef, $stack : Native::Stack = undef);

--- lib/SPVM/Native/MethodCall.c
+++ lib/SPVM/Native/MethodCall.c
@@ -11,12 +11,6 @@ int32_t SPVM__Native__MethodCall__new_class_method(SPVM_ENV* env, SPVM_VALUE* st
   
   int32_t error_id = 0;
   
-  void* obj_runtime = stack[0].oval;
-  
-  if (!obj_runtime) {
-    return env->die(env, stack, "The $runtime must be defined.", __func__, FILE_NAME, __LINE__);
-  }
-  
   void* obj_basic_type_name = stack[0].oval;
   
   if (!obj_basic_type_name) {
@@ -25,7 +19,7 @@ int32_t SPVM__Native__MethodCall__new_class_method(SPVM_ENV* env, SPVM_VALUE* st
   
   const char* basic_type_name = env->get_chars(env, stack, obj_basic_type_name);
   
-  void* obj_method_name = stack[0].oval;
+  void* obj_method_name = stack[1].oval;
   
   if (!obj_method_name) {
     return env->die(env, stack, "The $method_name must be defined.", __func__, FILE_NAME, __LINE__);
@@ -41,13 +35,7 @@ int32_t SPVM__Native__MethodCall__new_class_method(SPVM_ENV* env, SPVM_VALUE* st
   void* obj_method = env->new_pointer_object_by_name(env, stack, "Native::Method", method, &error_id, __func__, FILE_NAME, __LINE__);
   if (error_id) { return error_id; }
   
-  env->set_field_object_by_name(env, stack, obj_method, "runtime", obj_runtime, &error_id, __func__, FILE_NAME, __LINE__);
-  if (error_id) { return error_id; }
-  
   void* obj_method_call = env->new_pointer_object_by_name(env, stack, "Native::MethodCall", method, &error_id, __func__, FILE_NAME, __LINE__);
-  if (error_id) { return error_id; }
-  
-  env->set_field_object_by_name(env, stack, obj_method_call, "runtime", obj_runtime, &error_id, __func__, FILE_NAME, __LINE__);
   if (error_id) { return error_id; }
   
   env->set_field_object_by_name(env, stack, obj_method_call, "method", obj_method, &error_id, __func__, FILE_NAME, __LINE__);
@@ -60,12 +48,6 @@ int32_t SPVM__Native__MethodCall__new_instance_method_static(SPVM_ENV* env, SPVM
   
   int32_t error_id = 0;
   
-  void* obj_runtime = stack[0].oval;
-  
-  if (!obj_runtime) {
-    return env->die(env, stack, "The $runtime must be defined.", __func__, FILE_NAME, __LINE__);
-  }
-  
   void* obj_basic_type_name = stack[0].oval;
   
   if (!obj_basic_type_name) {
@@ -74,7 +56,7 @@ int32_t SPVM__Native__MethodCall__new_instance_method_static(SPVM_ENV* env, SPVM
   
   const char* basic_type_name = env->get_chars(env, stack, obj_basic_type_name);
   
-  void* obj_method_name = stack[0].oval;
+  void* obj_method_name = stack[1].oval;
   
   if (!obj_method_name) {
     return env->die(env, stack, "The $method_name must be defined.", __func__, FILE_NAME, __LINE__);
@@ -90,13 +72,7 @@ int32_t SPVM__Native__MethodCall__new_instance_method_static(SPVM_ENV* env, SPVM
   void* obj_method = env->new_pointer_object_by_name(env, stack, "Native::Method", method, &error_id, __func__, FILE_NAME, __LINE__);
   if (error_id) { return error_id; }
   
-  env->set_field_object_by_name(env, stack, obj_method, "runtime", obj_runtime, &error_id, __func__, FILE_NAME, __LINE__);
-  if (error_id) { return error_id; }
-  
   void* obj_method_call = env->new_pointer_object_by_name(env, stack, "Native::MethodCall", method, &error_id, __func__, FILE_NAME, __LINE__);
-  if (error_id) { return error_id; }
-  
-  env->set_field_object_by_name(env, stack, obj_method_call, "runtime", obj_runtime, &error_id, __func__, FILE_NAME, __LINE__);
   if (error_id) { return error_id; }
   
   env->set_field_object_by_name(env, stack, obj_method_call, "method", obj_method, &error_id, __func__, FILE_NAME, __LINE__);
@@ -109,19 +85,13 @@ int32_t SPVM__Native__MethodCall__new_instance_method(SPVM_ENV* env, SPVM_VALUE*
   
   int32_t error_id = 0;
   
-  void* obj_runtime = stack[0].oval;
-  
-  if (!obj_runtime) {
-    return env->die(env, stack, "The $runtime must be defined.", __func__, FILE_NAME, __LINE__);
-  }
-  
   void* obj_invocant = stack[0].oval;
   
   if (!obj_invocant) {
     return env->die(env, stack, "The $invocant must be defined.", __func__, FILE_NAME, __LINE__);
   }
   
-  void* obj_method_name = stack[0].oval;
+  void* obj_method_name = stack[1].oval;
   
   if (!obj_method_name) {
     return env->die(env, stack, "The $method_name must be defined.", __func__, FILE_NAME, __LINE__);
@@ -137,13 +107,7 @@ int32_t SPVM__Native__MethodCall__new_instance_method(SPVM_ENV* env, SPVM_VALUE*
   void* obj_method = env->new_pointer_object_by_name(env, stack, "Native::Method", method, &error_id, __func__, FILE_NAME, __LINE__);
   if (error_id) { return error_id; }
   
-  env->set_field_object_by_name(env, stack, obj_method, "runtime", obj_runtime, &error_id, __func__, FILE_NAME, __LINE__);
-  if (error_id) { return error_id; }
-  
   void* obj_method_call = env->new_pointer_object_by_name(env, stack, "Native::MethodCall", method, &error_id, __func__, FILE_NAME, __LINE__);
-  if (error_id) { return error_id; }
-  
-  env->set_field_object_by_name(env, stack, obj_method_call, "runtime", obj_runtime, &error_id, __func__, FILE_NAME, __LINE__);
   if (error_id) { return error_id; }
   
   env->set_field_object_by_name(env, stack, obj_method_call, "method", obj_method, &error_id, __func__, FILE_NAME, __LINE__);
