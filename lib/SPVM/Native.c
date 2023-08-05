028584df1db76fa898313974a579f94f5876c9f5
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-08-06 08:32:49 +0900

    get_current_env and get_current_stack methods are added to Native class.

--- lib/SPVM/Native.c
+++ lib/SPVM/Native.c
@@ -7,9 +7,33 @@
 
 static const char* FILE_NAME = "Native.c";
 
-int32_t SPVM__Native__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+int32_t SPVM__Native__get_current_env(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_env = env->new_pointer_object_by_name(env, stack, "Native::Env", env, &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  env->set_field_byte_by_name(env, stack, obj_env, "no_destroy", 1, &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  stack[0].oval = obj_env;
   
   return 0;
 }
 
+int32_t SPVM__Native__get_current_stack(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t error_id = 0;
+  
+  void* obj_stack = env->new_pointer_object_by_name(env, stack, "Native::Stack", stack, &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  env->set_field_byte_by_name(env, stack, obj_stack, "no_destroy", 1, &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
+  stack[0].oval = obj_stack;
+  
+  return 0;
+}
 
--- lib/SPVM/Native.pm
+++ lib/SPVM/Native.pm
@@ -6,40 +6,33 @@ package SPVM::Native;
 
 =head1 Name
 
-SPVM::Native - Short Description
+SPVM::Native - Native Module Utilities
 
 =head1 Description
 
-The Native class of L<SPVM> has methods for someting.
+The Native class of L<SPVM> has methods for native module utilities.
 
 =head1 Usage
 
   use Native;
 
-=head1 Fields
-
-
-
 =head1 Class Methods
 
+=head2 get_current_env
 
+  static get_current_env : Native::Env ();
 
-=head1 Instance Methods
+Gets the current execution environemnt. This is a L<Native::Env|SPVM::Native::Env> object.
 
+=head2 get_current_stack
 
+  static get_current_stack : Native::Stack ();
 
-
-=head1 Repository
-
-
-
-=head1 Author
-
-[--user-name] C<[--user-email]>
+Gets the current execution stack. This is a L<Native::Stack|SPVM::Native::Stack> object.
 
 =head1 Copyright & License
 
-Copyright (c) 2023 [--user-name]
+Copyright (c) 2023 Yuki Kimoto
 
 MIT License
 
