a0258c598d014cef639a25c85e9cd7f0e8e2f3ee
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-08-07 10:10:52 +0900

    MethodCall::EnvStack is implemented.

--- lib/SPVM/Native/MethodCall/EnvStack.pm
+++ lib/SPVM/Native/MethodCall/EnvStack.pm
@@ -6,36 +6,25 @@ package SPVM::Native::MethodCall::EnvStack;
 
 =head1 Name
 
-SPVM::Native::MethodCall::EnvStack - Short Description
+SPVM::Native::MethodCall::EnvStack - Method Call on Environment/Stack
 
 =head1 Description
 
-The Native::MethodCall::EnvStack class of L<SPVM> has methods for someting.
+The Native::MethodCall::EnvStack class of L<SPVM> has methods to call methods on an execution environment and an execution stack.
 
 =head1 Usage
 
   use Native::MethodCall::EnvStack;
 
-=head1 Fields
-
-
-
 =head1 Class Methods
 
+  static method call_class_method : Native::MethodCall::EnvStack ($env : Native::Env, $stack : Native::Stack, $basic_type_name : string, $method_name : string);
+  
+Calls a class method with an execution environment and an execution stack.
 
+The execution environment is a L<Native::Env|SPVM::Native::Env> object.
 
-=head1 Instance Methods
-
-
-
-
-=head1 Repository
-
-
-
-=head1 Author
-
-Yuki Kimoto C<kimoto.yuki@gmail.com>
+The execution stack is a L<Native::Stack|SPVM::Native::Stack> object.
 
 =head1 Copyright & License
 
