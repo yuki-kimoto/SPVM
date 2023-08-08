6232eb48ba95eaf6c763f95af17dc9a65cb7de1c
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-08-08 13:26:48 +0900

    Native::MethodCall::Stack and Native::MethodCall::EnvStack are removed.

--- lib/SPVM/Builder/Util.pm
+++ lib/SPVM/Builder/Util.pm
@@ -197,11 +197,7 @@ sub get_spvm_compiler_and_runtime_module_file_names {
     SPVM/Native/Method.c
     SPVM/Native/MethodCall.c
     SPVM/Native/MethodCall/Callback.spvm
-    SPVM/Native/MethodCall/EnvStack.c
-    SPVM/Native/MethodCall/EnvStack.spvm
     SPVM/Native/MethodCall.spvm
-    SPVM/Native/MethodCall/Stack.c
-    SPVM/Native/MethodCall/Stack.spvm
     SPVM/Native/Method.spvm
     SPVM/Native/ModuleFile.c
     SPVM/Native/ModuleFile.spvm
