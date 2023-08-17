b40bc32cad7aa75e98000033724635d292d00147
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-08-17 09:29:46 +0900

    The doc of the type native APIs is improved.

--- lib/SPVM/Document/NativeAPI/Type.pm
+++ lib/SPVM/Document/NativeAPI/Type.pm
@@ -9,12 +9,20 @@ The type native APIs of L<SPVM> are the APIs to manipulate types.
 =head1 Usage
 
   SPVM_API_TYPE* api_type = env->api->type;
-  
+
 =head1 Native APIs
 
 =head2 can_assign
 
   int32_t (*can_assign)(void* runtime, void* dist_basic_type, int32_t dist_type_dimension, int32_t dist_type_flag, void* src_basic_type, int32_t src_type_dimension, int32_t src_type_flag);
+
+If a source type can be assigned to a dist type, returns 1. Otherwise returns 0.
+
+The C<runtime> argument is a runtime object.
+
+The C<dist_basic_type> argument is a L<basic type|SPVM::Document::NativeAPI::BasicType> object.
+
+The C<src_basic_type> argument is a L<basic type|SPVM::Document::NativeAPI::BasicType> object.
 
 =head2 get_type_width
 
@@ -30,17 +38,41 @@ Gets the width of a type.
 
   int32_t (*is_object_type)(void* runtime, void* basic_type, int32_t type_dimension, int32_t flag);
 
+If the type is object type, returns 1. Otherwise returns 0.
+
+The C<runtime> argument is a runtime object.
+
+The C<basic_type> argument is a L<basic type|SPVM::Document::NativeAPI::BasicType> object.
+
 =head2 is_any_object_type
 
   int32_t (*is_any_object_type)(void* runtime, void* basic_type, int32_t type_dimension, int32_t flag);
+
+If the type is any object type, returns 1. Otherwise returns 0.
+
+The C<runtime> argument is a runtime object.
+
+The C<basic_type> argument is a L<basic type|SPVM::Document::NativeAPI::BasicType> object.
 
 =head2 is_object_array_type
 
   int32_t (*is_object_array_type)(void* runtime, void* basic_type, int32_t dimension, int32_t flag);
 
+If the type is object array type, returns 1. Otherwise returns 0.
+
+The C<runtime> argument is a runtime object.
+
+The C<basic_type> argument is a L<basic type|SPVM::Document::NativeAPI::BasicType> object.
+
 =head2 is_any_object_array_type
 
   int32_t (*is_any_object_array_type)(void* runtime, void* basic_type, int32_t type_dimension, int32_t flag);
+
+If the type is any object array type, returns 1. Otherwise returns 0.
+
+The C<runtime> argument is a runtime object.
+
+The C<basic_type> argument is a L<basic type|SPVM::Document::NativeAPI::BasicType> object.
 
 =head1 Native API IDs
 
