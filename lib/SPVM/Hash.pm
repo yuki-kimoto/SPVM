29730a4771b16362470806318c342b875b396c6c
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-08-26 08:04:57 +0900

    The doc of Hash is improved.

--- lib/SPVM/Hash.pm
+++ lib/SPVM/Hash.pm
@@ -4,13 +4,16 @@ package SPVM::Hash;
 
 =head1 Name
 
-SPVM::Hash - Hash Data Structure
+SPVM::Hash - Hash (Associative Array)
+
+=head1 Description
+
+The Hash class of L<SPVM> has methods to manipulate hashes(associative arrays).
 
 =head1 Usage
   
   use Hash;
   
-  # Create hash
   my $book = Hash->new;
   my $book = Hash->new({});
   my $book = Hash->new({id => 4, name => "Perl", price => 3000.0});
@@ -18,14 +21,14 @@ SPVM::Hash - Hash Data Structure
   $book->set_int(id => 4);
   $book->set_string(name => "Perl");
   $book->set_double(price => 3000.0);
+  $book->set(point => Point->new(1, 2));
   
-  my $id = (int)$book->get_int("id");
-  my $key = (string)$book->get_string("name");
-  my $price = (double)$book->get_double("price");
-  
-=head1 Description
+  my $id = $book->get_int("id");
+  my $name = $book->get_string("name");
+  my $price = $book->get_double("price");
+  my $point = $book->get("point");
 
-C<Hash> is Hash Data Structure. This is generally called associative array.
+=head2 Details
 
 The hash function is C<siphash-1-3>.
 
@@ -43,7 +46,9 @@ The hash function is C<siphash-1-3>.
 
   static method new : Hash ($key_values : object[] = undef);
 
-Create a new L<Hash|SPVM::Hash> object with key value pairs.
+Creates a new L<Hash|SPVM::Hash> object with key value pairs.
+
+Examples:
 
   my $book = Hash->new;
   my $book = Hash->new({});
@@ -53,13 +58,13 @@ Create a new L<Hash|SPVM::Hash> object with key value pairs.
 
 =head2 count
 
-  count : int ()
+  method count : int ();
 
 Counts keys in the hash.
 
 =head2 copy
 
-  copy : Hash ()
+  method copy : Hash ();
 
 Copies hash.
 
@@ -73,115 +78,149 @@ The alias for the L</"copy">.
 
 =head2 delete
 
-  delete : object ($key : string)
+  method delete : object ($key : string);
 
 Deletes a key value pair. Deleted value is returned.
 
 =head2 exists
 
-  exists : int ($key : string)
+  method exists : int ($key : string);
 
 Specify the key and check if the value exists. If exists, return 1, otherwise 0.
 
 =head2 keys
 
-  keys : string[] ()
+  method keys : string[] ();
 
 Gets keys. This method do not copy the strings.
 
 =head2 values
 
-  values : object[] ()
+  method values : object[] ();
 
-Gets values.
+Gets all the values contained in this hash.
 
 =head2 get
 
-  get : object ($key : string)
+  method get : object ($key : string);
 
-Gets a value.
+Gets the value corresponding to key $key, and returns it.
 
 =head2 get_byte
 
-  get_byte : int ($key : string)
+  method get_byte : int ($key : string);
 
-Gets the value with a $key from a L<Byte|SPVM::Byte> object.
+Gets the value corresponding to key $key, and casts it to the byte type, and returns it as the int type.
+
+Exceptions:
+
+The value of the $key must be a L<Byte|SPVM::Byte> object. Otherwise an exception is thrown.
 
 =head2 get_short
 
-  get_short : int ($key : string)
+  method get_short : int ($key : string);
 
-Gets the value with a $key from a L<Short|SPVM::Short> object.
-  
+Gets the value corresponding to key $key, and casts it to the short type, and returns it as the int type.
+
+Exceptions:
+
+The value of the $key must be a L<Short|SPVM::Short> object. Otherwise an exception is thrown.
+
+=head2 get_string
+
+  method get_string : string ($key : string)
+
+Gets the value corresponding to key $key, and casts it to the string type, and returns it.
+
+Exceptions:
+
+The value of the $key must be a string. Otherwise an exception is thrown.
+
 =head2 get_int
 
-  get_int : int ($key : string)
+  method get_int : int ($key : string);
 
-Gets the value with a $key from a L<Int|SPVM::Int> object.
-  
+Gets the value corresponding to key $key, and casts it to the int type, and returns it.
+
+Exceptions:
+
+The value of the $key must be a L<Int|SPVM::Int> object. Otherwise an exception is thrown.
+
 =head2 get_long
 
-  get_long : long ($key : string)
+  method get_long : long ($key : string);
 
-Gets the value with a $key from a L<Long|SPVM::Long> object.
+Gets the value corresponding to key $key, and casts it to the long type, and returns it.
+
+Exceptions:
+
+The value of the $key must be a L<Long|SPVM::Long> object. Otherwise an exception is thrown.
 
 =head2 get_float
 
-  get_float : float ($key : string)
+  method get_float : float ($key : string);
 
-Gets the value with a $key from a L<Float|SPVM::Float> object.
-  
+Gets the value corresponding to key $key, and casts it to the float type, and returns it.
+
+Exceptions:
+
+The value of the $key must be a L<Float|SPVM::Float> object. Otherwise an exception is thrown.
+
 =head2 get_double
 
-  get_double : double ($key : string)
+  method get_double : double ($key : string);
 
-Gets the value with a $key from a L<Double|SPVM::Double> object.
+Gets the value corresponding to key $key, and casts it to the double type, and returns it.
+
+Exceptions:
+
+The value of the $key must be a L<Double|SPVM::Double> object. Otherwise an exception is thrown.
 
 =head2 set
   
-  set : void ($key : string, $val : object)
+  method set : void ($key : string, $val : object);
 
 Sets the object $value with the $key.
 
 =head2 set_byte
 
-  set_byte : void ($key : string, $value : int)
+  method set_byte : void ($key : string, $value : int);
 
 Sets the C<byte> $value with the $key. the $value is converted to L<Byte|SPVM::Byte> object.
 
 =head2 set_short
 
-  set_short : void ($key : string, $value : int)
+  method set_short : void ($key : string, $value : int);
 
 Sets the C<short> $value with the $key. the $value is converted to L<Short|SPVM::Short> object.
 
 =head2 set_int
 
-  set_int : void ($key : string, $value : int)
+  method set_int : void ($key : string, $value : int);
 
 Sets the C<int> $value with the $key. the $value is converted to L<Int|SPVM::Int> object.
 
 =head2 set_long
 
-  set_long : void ($key : string, $value : long)
+  method set_long : void ($key : string, $value : long);
 
 Sets the C<long> $value with the $key. the $value is converted to L<Long|SPVM::Long> object.
 
 =head2 set_float
 
-  set_float : void ($key : string, $value : float)
+  method set_float : void ($key : string, $value : float);
 
 Sets the C<float> $value with the $key. the $value is converted to L<Float|SPVM::Float> object.
 
 =head2 set_double
 
-  set_double : void ($key : string, $value : double)
+  method set_double : void ($key : string, $value : double);
 
 Sets the C<double> $value with the $key. the $value is converted to L<Double|SPVM::Double> object.
 
 =head2 set_string
 
-  set_string : void ($key : string, $value : string)
+  method set_string : void ($key : string, $value : string);
 
 Sets the string $value with the $key.
 
