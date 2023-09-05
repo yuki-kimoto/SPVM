83e26bed4a0006d39a17a0339e8b539f36610a58
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-09-05 14:53:28 +0900

    A native API test is simplified.

--- t/02_vm/lib/SPVM/TestCase/NativeAPI2.c
+++ t/02_vm/lib/SPVM/TestCase/NativeAPI2.c
@@ -5,9 +5,7 @@
 #include "baz/baz.h"
 
 int32_t SPVM__TestCase__NativeAPI2__mul(SPVM_ENV* env, SPVM_VALUE* stack) {
-  (void)env;
-  (void)stack;
-
+  
   int32_t total = stack[0].ival * stack[1].ival;
   
   stack[0].ival = total;
@@ -16,8 +14,6 @@ int32_t SPVM__TestCase__NativeAPI2__mul(SPVM_ENV* env, SPVM_VALUE* stack) {
 }
 
 int32_t SPVM__TestCase__NativeAPI2__src_foo(SPVM_ENV* env, SPVM_VALUE* stack) {
-  (void)env;
-  (void)stack;
   
   if (foo() == 3) {
     stack[0].ival = 1;
@@ -30,9 +26,7 @@ int32_t SPVM__TestCase__NativeAPI2__src_foo(SPVM_ENV* env, SPVM_VALUE* stack) {
 }
 
 int32_t SPVM__TestCase__NativeAPI2__src_bar(SPVM_ENV* env, SPVM_VALUE* stack) {
-  (void)env;
-  (void)stack;
-
+  
   if (bar() == 4) {
     stack[0].ival = 1;
   }
@@ -44,9 +38,7 @@ int32_t SPVM__TestCase__NativeAPI2__src_bar(SPVM_ENV* env, SPVM_VALUE* stack) {
 }
 
 int32_t SPVM__TestCase__NativeAPI2__src_baz(SPVM_ENV* env, SPVM_VALUE* stack) {
-  (void)env;
-  (void)stack;
-
+  
   if (baz() == 5) {
     stack[0].ival = 1;
   }
