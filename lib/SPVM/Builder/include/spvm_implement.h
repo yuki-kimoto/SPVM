fd1ab2a0f2e074fde1bdfbb58fbdd34e2a3c5a6b
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-09-15 13:48:55 +0900

    Remove SPVM_IMPLEMENT_GET_OBJECT_NO_WEAKEN_ADDRESS.

--- lib/SPVM/Builder/include/spvm_implement.h
+++ lib/SPVM/Builder/include/spvm_implement.h
@@ -151,9 +151,6 @@ static inline void* SPVM_IMPLEMENT_GET_METHOD_BY_NAME(SPVM_ENV* env, SPVM_VALUE*
   return method;
 }
 
-//  "& ~(intptr_t)1" means dropping weaken flag
-#define SPVM_IMPLEMENT_GET_OBJECT_NO_WEAKEN_ADDRESS(env, stack, object) ((void*)((intptr_t)object & ~(intptr_t)1))
-
 static inline void SPVM_IMPLEMENT_OBJECT_ASSIGN(SPVM_ENV* env, SPVM_VALUE* stack, void** dist_address, void* src_object, int32_t object_ref_count_offset) {
   void* tmp_object = (void*)((intptr_t)src_object & ~(intptr_t)1);
   if (tmp_object != NULL) {
