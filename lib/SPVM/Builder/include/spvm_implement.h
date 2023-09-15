dba0d5eb7371d0cf0378a86d8978f8bf14c8aac9
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-09-15 09:47:10 +0900

    Remove SPVM_IMPLEMENT_INC_REF_COUNT_ONLY.

--- lib/SPVM/Builder/include/spvm_implement.h
+++ lib/SPVM/Builder/include/spvm_implement.h
@@ -154,8 +154,6 @@ static inline void* SPVM_IMPLEMENT_GET_METHOD_BY_NAME(SPVM_ENV* env, SPVM_VALUE*
 //  "& ~(intptr_t)1" means dropping weaken flag
 #define SPVM_IMPLEMENT_GET_OBJECT_NO_WEAKEN_ADDRESS(env, stack, object) ((void*)((intptr_t)object & ~(intptr_t)1))
 
-#define SPVM_IMPLEMENT_INC_REF_COUNT_ONLY(env, stack, object, object_ref_count_offset) ((*(int32_t*)((intptr_t)object + object_ref_count_offset))++)
-
 #define SPVM_IMPLEMENT_ISWEAK(dist_address) (((intptr_t)*(void**)dist_address) & 1)
 
 static inline void SPVM_IMPLEMENT_OBJECT_ASSIGN(SPVM_ENV* env, SPVM_VALUE* stack, void** dist_address, void* src_object, int32_t object_ref_count_offset) {
