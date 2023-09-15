495b36def8f2b36af4bbd4a0b5c7acd700c34918
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-09-16 08:20:39 +0900

    SPVM_API_isweak returns 0 if the object is NULL.

--- lib/SPVM/Builder/src/spvm_api.c
+++ lib/SPVM/Builder/src/spvm_api.c
@@ -2915,8 +2915,11 @@ int32_t SPVM_API_is_read_only(SPVM_ENV* env, SPVM_VALUE* stack, SPVM_OBJECT* str
 
 int32_t SPVM_API_isweak(SPVM_ENV* env, SPVM_VALUE* stack, SPVM_OBJECT** object_address) {
   
+  int32_t isweak = 0;
   
-  int32_t isweak = (intptr_t)*object_address & 1;
+  if (*object_address) {
+    isweak = (intptr_t)*object_address & 1;
+  }
   
   return isweak;
 }
