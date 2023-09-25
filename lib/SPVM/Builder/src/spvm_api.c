dc9c6e3bea43bcfa7d6f8f9451079fcd75666cd5
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-09-25 12:14:59 +0900

    Remove warnings.

--- lib/SPVM/Builder/src/spvm_api.c
+++ lib/SPVM/Builder/src/spvm_api.c
@@ -3683,15 +3683,9 @@ SPVM_OBJECT* SPVM_API_new_object_common(SPVM_ENV* env, SPVM_VALUE* stack, size_t
   
   SPVM_MUTEX_init(mutex);
   
-  warn("SPVM_API_new_object_common %d", __LINE__);
-  
   SPVM_MUTEX_lock(mutex);
   
-  warn("SPVM_API_new_object_common %d", __LINE__);
-  
   SPVM_MUTEX_unlock(mutex);
-  
-  warn("SPVM_API_new_object_common %d", __LINE__);
   
   return object;
 }
