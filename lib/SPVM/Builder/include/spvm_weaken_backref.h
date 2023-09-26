c76057944d3dd56c030c78ab0ff78048c7f48091
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-09-26 09:01:28 +0900

    Remove mutex member from struct spvm_weaken_backref.

--- lib/SPVM/Builder/include/spvm_weaken_backref.h
+++ lib/SPVM/Builder/include/spvm_weaken_backref.h
@@ -4,16 +4,11 @@
 #ifndef SPVM_WEAKEN_BACKREF_H
 #define SPVM_WEAKEN_BACKREF_H
 
-#include <stdio.h>
-
 #include "spvm_typedecl.h"
 
 struct spvm_weaken_backref {
   SPVM_OBJECT** ref;
-  SPVM_MUTEX* mutex;
   SPVM_WEAKEN_BACKREF* next;
 };
-
-SPVM_WEAKEN_BACKREF* SPVM_WEAKEN_BACKREF_new();
 
 #endif
