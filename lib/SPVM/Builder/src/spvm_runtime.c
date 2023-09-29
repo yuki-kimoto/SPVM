441999328bdca9e8cc757487aa2937aae489bc25
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-09-30 07:43:44 +0900

    Revert "Cleanup stderr."
    This reverts commit 605daf286e0d0eb328168fe55455b7a2ed8a2a8e.

--- lib/SPVM/Builder/src/spvm_runtime.c
+++ lib/SPVM/Builder/src/spvm_runtime.c
@@ -97,28 +97,15 @@ void SPVM_RUNTIME_init_stdio(SPVM_RUNTIME* runtime) {
   
   // stderr
   {
-    int32_t stderr_fileno = fileno(stderr);
-    
-    assert(stderr_fileno >= 0);
-    
-    int32_t stderr_fileno_dup = dup(stderr_fileno);
-    
-    assert(stderr_fileno_dup >= 2);
-    
-    FILE* spvm_stderr = fdopen(stderr_fileno_dup, "w");
-    
-    assert(spvm_stderr);
+    runtime->spvm_stderr = fdopen(dup(fileno(stderr)), "w");
     
 #ifdef _WIN32  
     
-    setmode(fileno(spvm_stderr), _O_BINARY);
-    
+    setmode(fileno(runtime->spvm_stderr), _O_BINARY);
+
 #endif
     
     setvbuf(runtime->spvm_stderr, NULL, _IONBF, 0);
-    
-    runtime->spvm_stderr = spvm_stderr;
-    
   }
   
 }
