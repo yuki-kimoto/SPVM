eb3136bd04f3bbbaa015b6437c98b96c321d8926
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-10-05 07:35:25 +0900

    Add empty Sync::Channel class.

---
+++ lib/SPVM/Sync/Channel.pm
@@ -0,0 +1,45 @@
+package SPVM::Sync::Channel;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Sync::Channel - Short Description
+
+=head1 Description
+
+Sync::Channel class of L<SPVM> has methods for someting.
+
+=head1 Usage
+
+  use Sync::Channel;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
