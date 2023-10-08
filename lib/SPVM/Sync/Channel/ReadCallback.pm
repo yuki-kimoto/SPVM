4554661029cf1d8b14c9fbf68fed09625ffddae6
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-10-09 07:52:01 +0900

    Add empty Go class.

---
+++ lib/SPVM/Sync/Channel/ReadCallback.pm
@@ -0,0 +1,37 @@
+package SPVM::Sync::Channel::ReadCallback;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Sync::Channel::ReadCallback - Short Description
+
+=head1 Description
+
+Sync::Channel::ReadCallback interface of L<SPVM> has interface methods for someting.
+
+=head1 Usage
+
+  interface Sync::Channel::ReadCallback;
+
+=head1 Interface Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
