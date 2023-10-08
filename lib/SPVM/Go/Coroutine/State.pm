645d79462b7eaee3b0a86afac4b964227d672aaa
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-10-09 08:19:07 +0900

    Add Go::Coroutine.

---
+++ lib/SPVM/Go/Coroutine/State.pm
@@ -0,0 +1,45 @@
+package SPVM::Go::Coroutine::State;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Go::Coroutine::State - Short Description
+
+=head1 Description
+
+Go::Coroutine::State class of L<SPVM> has methods for someting.
+
+=head1 Usage
+
+  use Go::Coroutine::State;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
