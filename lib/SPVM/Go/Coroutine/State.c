fa08fb1c6492e18f650d45d3c12c00b876b75610
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-10-09 08:19:59 +0900

    Add Go::Coroutine::State c files.

---
+++ lib/SPVM/Go/Coroutine/State.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2023 Yuki Kimoto
+// MIT License
+
+#include "spvm_native.h"
+
+
+
+static const char* FILE_NAME = "Go/Coroutine/State.c";
+
+int32_t SPVM__Go__Coroutine__State__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+
