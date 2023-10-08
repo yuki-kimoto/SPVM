ad081cd78c5698075e7459d6853921dbd1189845
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-10-09 08:28:21 +0900

    Add Go::Event.

---
+++ lib/SPVM/Go/Event.pm
@@ -0,0 +1,45 @@
+package SPVM::Go::Event;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Go::Event - Short Description
+
+=head1 Description
+
+Go::Event class of L<SPVM> has methods for someting.
+
+=head1 Usage
+
+  use Go::Event;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
---
+++ lib/SPVM/Go/Event/Coroutine.pm
@@ -0,0 +1,45 @@
+package SPVM::Go::Event::Coroutine;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Go::Event::Coroutine - Short Description
+
+=head1 Description
+
+Go::Event::Coroutine class of L<SPVM> has methods for someting.
+
+=head1 Usage
+
+  use Go::Event::Coroutine;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
