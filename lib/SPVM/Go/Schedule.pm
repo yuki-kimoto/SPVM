b2e336a162386d6bad18884ef2acb2de4f88017e
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-10-09 08:17:54 +0900

    Add Go::Schedule.

---
+++ lib/SPVM/Go/Schedule.pm
@@ -0,0 +1,45 @@
+package SPVM::Go::Schedule;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Go::Schedule - Short Description
+
+=head1 Description
+
+Go::Schedule class of L<SPVM> has methods for someting.
+
+=head1 Usage
+
+  use Go::Schedule;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
+
