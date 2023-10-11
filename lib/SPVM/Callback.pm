5217f3fc95a6b4dacacbcb02554acd9bfa43d362
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-10-11 13:27:27 +0900

    Add the Callback class.
      [New Feature]
        * Add the Callback class.

---
+++ lib/SPVM/Callback.pm
@@ -0,0 +1,29 @@
+package SPVM::Callback;
+
+1;
+
+=head1 Name
+
+SPVM::Callback - Callback interface
+
+=head1 Usage
+  
+  use Callback;
+
+=head1 Description
+
+The Callback interface is an L<interface|SPVM::Document::Language/"Interface"> for callbacks.
+
+=head1 Interface Methods
+
+=head2 Anon Method
+
+C<required method : void ();>
+
+A callback.
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
