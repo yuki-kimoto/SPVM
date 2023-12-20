9cea067f53cc66500d83ac62043d4c6866c07305
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-12-20 13:08:38 +0900

    Improve the doc of the Cloneable interface.
      [Document Improvement]
        * Improve the doc of the Cloneable interface.

--- lib/SPVM/Cloneable.pm
+++ lib/SPVM/Cloneable.pm
@@ -6,25 +6,24 @@ package SPVM::Cloneable;
 
 SPVM::Cloneable - Interface Type to Clone Object
 
+=head1 Description
+
+The Cloneable interface in L<SPVM> represents a cloneable interface.
+
 =head1 Usage
   
   class Point {
     interface Cloneable;
     
-C<method clone : L<Point|SPVM::Point> ()>
+    method clone : Point () {
+      
       my $new_point = Point->new($self->x, $self->y);
       
       return $new_point;
     }
   }
 
-=head1 Description
-
-L<Cloneable|SPVM::Cloneable> is a callback interface to clone a object.
-
 =head1 Interface Methods
-
-Interface methods.
 
 =head2 clone
 
