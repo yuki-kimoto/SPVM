454554afb74a8fbbbf00acc069fe33f82cd9ad17
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-02-21 09:20:11 +0900

    Improve the doc of basic type Native APIs.
      [Document Improvement]
        * Improve the doc of basic type Native APIs.

--- lib/SPVM/Document/NativeAPI/BasicType.pm
+++ lib/SPVM/Document/NativeAPI/BasicType.pm
@@ -4,247 +4,164 @@ SPVM::Document::NativeAPI::BasicType - Basic Type Native APIs
 
 =head1 Description
 
-The basic type native APIs are the APIs to manipulate information of basic types.
+The basic type native APIs are the APIs for basic types.
 
 =head1 Usage
 
+  int32_t error_id
+  
   SPVM_API_BASIC_TYPE* api_basic_type = env->api->basic_type;
   
-  const char* basic_type_name = api_basic_type->get_name(runtime, basic_type);
-
-The C<class_var> is got by the L<get_basic_type_by_id|SPVM::Document::NativeAPI::Runtime/"get_basic_type_by_id"> and the L<get_basic_type_by_name|SPVM::Document::NativeAPI::Runtime/"get_basic_type_by_name"> runtime native API.
+  void* basic_type = env->get_basic_type(env, stack, "Int");
+  
+  const char* basic_type_name = api_basic_type->get_name(env->runtime, basic_type);
 
 =head1 Native APIs
 
 =head2 get_name
 
-  const char* (*get_name)(void* runtime, void* basic_type);
+C<const char* (*get_name)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>);>
 
 Returns the name of the basic type.
 
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
-
 =head2 get_id
 
-  int32_t (*get_id)(void* runtime, void* basic_type);
+C<int32_t (*get_id)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>);>
 
 Returns the ID of the basic type.
 
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
-
 =head2 get_category
 
-  int32_t (*get_category)(void* runtime, void* basic_type);
+C<int32_t (*get_category)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>);>
 
-Returns the category of the basic type.
-
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
+Returns the L<base type category ID|SPVM::Document::NativeAPI/"Basic Type Category IDs"> of the basic type.
 
 =head2 get_parent
 
-  void* (*get_parent)(void* runtime, void* basic_type);
+C<void* (*get_parent)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>);>
 
-Returns the parent L<basic type|SPVM::Document::NativeAPI::BasicType> object of the basic type.
-
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
+Returns the parent L<basic type|SPVM::Document::NativeAPI::BasicType> object of the basic type I<basic_type>.
 
 =head2 get_version_string
 
-  const char* (*get_version_string)(void* runtime, void* basic_type);
+C<const char* (*get_version_string)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>);>
 
 Returns the version string of the basic type.
 
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
-
 =head2 get_class_dir
 
-  const char* (*get_class_dir)(void* runtime, void* basic_type);
+C<const char* (*get_class_dir)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>);>
 
 Returns the class directory of the basic type.
 
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
-
 =head2 get_class_rel_file
 
-  const char* (*get_class_rel_file)(void* runtime, void* basic_type);
+C<const char* (*get_class_rel_file)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>);>
 
 Returns the class relative file of the basic type.
 
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
-
 =head2 is_pointer
 
-  int32_t (*is_pointer)(void* runtime, void* basic_type);
+C<int32_t (*is_pointer)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>);>
 
 If the basic type is a pointer class, returns 1. Otherwise returns 0.
 
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
-
 =head2 is_anon
 
-  int32_t (*is_anon)(void* runtime, void* basic_type);
+C<int32_t (*is_anon)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>);>
 
 If the basic type is an anon class, returns 1. Otherwise returns 0.
 
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
-
 =head2 get_class_var_by_index
 
-  void* (*get_class_var_by_index)(void* runtime, void* basic_type, int32_t class_var_index);
+C<void* (*get_class_var_by_index)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>, int32_t class_var_index);>
 
-Gets a class variable by a class variable index, and returns it.
-
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
+Gets a L<class variable|SPVM::Document::NativeAPI::ClassVariable> defined in the basic type I<basic_type> given a class variable index I<class_var_index>, and returns it.
 
 =head2 get_class_var_by_name
 
-  void* (*get_class_var_by_name)(void* runtime, void* basic_type, const char* class_var_name);
+C<void* (*get_class_var_by_name)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>, const char* class_var_name);>
 
-Gets a class variable by a class variable name, and returns it.
-
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
+Gets a L<class variable|SPVM::Document::NativeAPI::ClassVariable> defined in the basic type I<basic_type> given a class variable name I<class_var_name>, and returns it.
 
 =head2 get_class_vars_length
 
-  int32_t (*get_class_vars_length)(void* runtime, void* basic_type);
+C<int32_t (*get_class_vars_length)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>);>
 
-Returns the length of the class variables of the basic type.
-
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
+Returns the length of the class variables defined in the basic type I<basic_type>.
 
 =head2 get_field_by_index
 
-  void* (*get_field_by_index)(void* runtime, void* basic_type, int32_t field_index);
+C<void* (*get_field_by_index)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>, int32_t field_index);>
 
-Gets a field by a field index, and returns it.
-
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
+Gets a L<field|SPVM::Document::NativeAPI::Field> defined in the basic type I<basic_type> given a field index I<field_index>, and returns it.
 
 =head2 get_field_by_name
 
-  void* (*get_field_by_name)(void* runtime, void* basic_type, const char* field_name);
+C<void* (*get_field_by_name)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>, const char* field_name);>
 
-Gets a field by a field name, and returns it.
-
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
+Gets a L<field|SPVM::Document::NativeAPI::Field> defined in the basic type I<basic_type> given a field name I<field_name>, and returns it.
 
 =head2 get_fields_length
 
-  int32_t (*get_fields_length)(void* runtime, void* basic_type);
+C<int32_t (*get_fields_length)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>);>
 
-Returns the length of the fields of the basic type.
-
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
+Returns the length of the fields defined in the basic type I<basic_type>.
 
 =head2 get_method_by_index
 
-  void* (*get_method_by_index)(void* runtime, void* basic_type, int32_t method_index);
+C<void* (*get_method_by_index)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>, int32_t method_index);>
 
-Gets a method by a method index, and returns it.
-
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
+Gets a L<method|SPVM::Document::NativeAPI::Method> defined in the basic type I<basic_type> given a method index I<method_index>, and returns it.
 
 =head2 get_method_by_name
 
-  void* (*get_method_by_name)(void* runtime, void* basic_type, const char* method_name);
+C<void* (*get_method_by_name)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>, const char* method_name);>
 
-Gets a method by a method name, and returns it.
-
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
+Gets a L<method|SPVM::Document::NativeAPI::Method> defined in the basic type I<basic_type> given a method name I<method_name>, and returns it.
 
 =head2 get_methods_length
 
-  int32_t (*get_methods_length)(void* runtime, void* basic_type);
+C<int32_t (*get_methods_length)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>);>
 
-Returns the length of the methods of the basic type.
-
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
+Returns the length of the methods defined in the basic type I<basic_type>.
 
 =head2 get_anon_basic_type_by_index
 
-  void* (*get_anon_basic_type_by_index)(void* runtime, void* basic_type, int32_t anon_basic_type_index);
+C<void* (*get_anon_basic_type_by_index)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>, int32_t anon_basic_type_index);>
 
-Gets a anon basic type used by the basic type by an anon basic type index, and returns it.
-
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
+Gets a anon L<basic type|SPVM::Document::NativeAPI::BasicType> defined in the basic type I<basic_type> given an anon basic type index, and returns it.
 
 =head2 get_anon_basic_types_length
 
-    int32_t (*get_anon_basic_types_length)(void* runtime, void* basic_type);
+C<int32_t (*get_anon_basic_types_length)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>);>
 
-Returns the length of the anon basic types used by the basic type.
-
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
+Returns the length of the anon basic types defined in the basic type I<basic_type>.
 
 =head2 has_interface
 
-  int32_t (*has_interface)(void* runtime, void* basic_type, void* interface_basic_type);
+C<int32_t (*has_interface)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>, L<void* interface_basic_type|SPVM::Document::NativeAPI::BasicType>);>
 
-If a basic type C<basic_type> has an interface C<interface_basic_type>, returns 1. Otherwise returns 0.
-
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<basic_type> argument is a basic type object.
-
-The C<interface_basic_type> argument is a basic type object.
+If the basic type I<basic_type> has an interface I<interface_basic_type>, returns 1. Otherwise returns 0.
 
 =head2 is_super_class
 
-  int32_t (*is_super_class)(void* runtime, void* super_basic_type, void* child_basic_type);
+C<int32_t (*is_super_class)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* super_basic_type|SPVM::Document::NativeAPI::BasicType>, L<void* child_basic_type|SPVM::Document::NativeAPI::BasicType>);>
 
-If a basic type C<super_basic_type> is a super class of a basic type C<child_basic_type>, returns 1. Otherwise returns 0.
-
-The C<runtime> argument is a L<runtime|SPVM::Document::NativeAPI::Runtime> object.
-
-The C<super_basic_type> argument is a basic type object.
-
-The C<child_basic_type> argument is a basic type object.
+If I<super_basic_type> is a super class of I<child_basic_type>, returns 1. Otherwise returns 0.
 
 =head2 get_file
 
 C<const char* (*get_file)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* basic_type|SPVM::Document::NativeAPI::BasicType>);>
 
 Returns the file path of the basic type.
+
+This can be changed by a file directive.
+
+  #file /tempaltes/foo.html.ep
+  class Foo {
+  
+  }
 
 =head1 Native API IDs
 
