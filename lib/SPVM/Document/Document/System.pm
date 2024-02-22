387fb7478b59aeaec714711c128bbb7be9f36aa1
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-02-22 13:05:36 +0900

    Improve System doc.

--- lib/SPVM/Document/Document/System.pm
+++ lib/SPVM/Document/Document/System.pm
@@ -6,11 +6,13 @@ SPVM::Document::Language::System - System Setting in SPVM Language
 
 This document describes system setting in SPVM language.
 
-=head1 System
+=head1 Standard IO
 
-=head2 Standard IO
+SPVM has own C<stdin>, C<stdout>, and C<stderr>. These are set to the binary mode.
 
-SPVM has own C<stdin>, C<stdout>, and C<stderr> that is set to the binary mode.
+The say operator and the print operator output to this SPVM's C<stdout>.
+
+The warn operator outputs to this SPVM's C<stderr>.
 
 =head1 Copyright & License
 
