1d844a72c99f7028aae110642227df164d9e7403
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-02-26 13:11:53 +0900

    Improve SPVM::Document::NativeAPI::Compiler.

--- lib/SPVM/Document/NativeAPI/Compiler.pm
+++ lib/SPVM/Document/NativeAPI/Compiler.pm
@@ -4,137 +4,149 @@ SPVM::Document::NativeAPI::Compiler - Compiler Native APIs
 
 =head1 Usage
 
-  void* compiler = env->api->compiler->new_instance();
+  SPVM_API_COMPILER* api_compiler = env->api->compiler;
   
-  env->api->compiler->add_include_dir(compiler, "lib");
+  void* compiler = api_compiler->new_instance();
   
-  env->api->compiler->set_start_file(compiler, __FILE__);
-  env->api->compiler->get_start_line(compiler, __LINE__ + 1);
-  int32_t status = env->api->compiler->compile(compiler, "MyClass");
+  api_compiler->add_include_dir(compiler, "lib");
   
-  env->api->compiler->free_instance(compiler);
+  api_compiler->set_start_file(compiler, __FILE__);
+  api_compiler->get_start_line(compiler, __LINE__ + 1);
+  int32_t status = api_compiler->compile(compiler, "MyClass");
+  
+  api_compiler->free_instance(compiler);
 
 =head1 Description
 
-The compiler native APIs of L<SPVM> are the APIs to compile SPVM modules.
+The compiler native APIs in L<SPVM> are the APIs for SPVM compilers.
 
 =head1 Compiler Native APIs
 
 =head2 new_instance
-  
-  void* (*new_instance)(void);
 
-Creates a compiler object.
+C<void* (*new_instance)(void);>
+
+Creates a L<compiler|SPVM::Document::NativeAPI::Compiler> and returns it.
 
 =head2 free_instance
-  
-  void (*free_instance)(void* compiler);
+
+C<void (*free_instance)(L<void* compiler|SPVM::Document::NativeAPI::Compiler>);>
 
 Frees a compiler.
 
 =head2 get_start_line
-  
-  int32_t (*get_start_line)(void* compiler);
+
+C<int32_t (*get_start_line)(L<void* compiler|SPVM::Document::NativeAPI::Compiler>);>
 
 Gets the start line.
 
 =head2 set_start_line
-  
-  void (*set_start_line)(void* compiler, int32_t start_line);
 
-Sets a start line.
+C<void (*set_start_line)(L<void* compiler|SPVM::Document::NativeAPI::Compiler>, int32_t start_line);>
+
+Sets the start line I<start_line>.
 
 =head2 get_start_file
-  
-  const char* (*get_start_file)(void* compiler);
+
+C<const char* (*get_start_file)(L<void* compiler|SPVM::Document::NativeAPI::Compiler>);>
 
 Gets the start file.
 
 =head2 set_start_file
-  
-  void (*set_start_file)(void* compiler, const char* start_file);
 
-Set a start file.
+C<void (*set_start_file)(L<void* compiler|SPVM::Document::NativeAPI::Compiler>, const char* start_file);>
+
+Set the start file I<start_file>.
 
 =head2 get_include_dirs_length
-  
-  int32_t (*get_include_dirs_length)(void* compiler);
 
-Gets the length of the directories for searching modules.
+C<int32_t (*get_include_dirs_length)(L<void* compiler|SPVM::Document::NativeAPI::Compiler>);>
+
+Returns the length of the class searching directories.
 
 =head2 get_include_dir
 
-  const char* (*get_include_dir)(void* compiler, int32_t index);
+C<const char* (*get_include_dir)(L<void* compiler|SPVM::Document::NativeAPI::Compiler>, int32_t index);>
 
-Gets a directory for searching modules by an index.
+Gets a class searching directory given the index I<index>, and returns it.
 
 =head2 add_include_dir
   
-  void (*add_include_dir)(void* compiler, const char* include_dir);
+C<void (*add_include_dir)(L<void* compiler|SPVM::Document::NativeAPI::Compiler>, const char* include_dir);>
 
-Adds a directory for searching modules.
+Adds the class searching directory I<include_dir> at the end of class searching directories.
 
 =head2 prepend_include_dir
   
-  void (*prepend_include_dir)(void* compiler, const char* include_dir);
+C<void (*prepend_include_dir)(L<void* compiler|SPVM::Document::NativeAPI::Compiler>, const char* include_dir);>
 
-Prepends a directory for searching modules.
+Prepends the class searching directory I<include_dir>.
 
 =head2 clear_include_dirs
   
-  void (*clear_include_dirs)(SPVM_COMPILER* compiler);
+C<void (*clear_include_dirs)(SPVM_COMPILER* compiler);>
 
-Clears the directories for searching modules.
+Removes all class searching directories.
 
 =head2 add_class_file
 
-  void (*add_class_file)(void* compiler, const char* class_name);
+C<void (*add_class_file)(L<void* compiler|SPVM::Document::NativeAPI::Compiler>, const char* class_name);>
 
-Adds a L<class file|SPVM::Document::NativeAPI::ClassFile> object for a module.
+Creates the L<class file|SPVM::Document::NativeAPI::ClassFile> for the class given by I<class_name>, and sets it.
 
 If the class file already exists, nothing is performed.
 
 =head2 delete_class_file
 
-  void (*delete_class_file)(void* compiler, const char* class_name);
+C<void (*delete_class_file)(L<void* compiler|SPVM::Document::NativeAPI::Compiler>, const char* class_name);>
   
-Deletes a L<class file|SPVM::Document::NativeAPI::ClassFile> object for a module.
+Removes the L<class file|SPVM::Document::NativeAPI::ClassFile> for the class given by I<class_name>.
 
 =head2 get_class_file
 
-  void* (*get_class_file)(void* compiler, const char* class_name);
+C<void* (*get_class_file)(L<void* compiler|SPVM::Document::NativeAPI::Compiler>, const char* class_name);>
 
-Gets the L<class file|SPVM::Document::NativeAPI::ClassFile> object for a module.
+Gets the L<class file|SPVM::Document::NativeAPI::ClassFile> for the class given by I<class_name>.
 
 =head2 compile
   
-  int32_t (*compile)(void* compiler, const char* class_name);
+C<int32_t (*compile)(L<void* compiler|SPVM::Document::NativeAPI::Compiler>, const char* class_name);>
 
-Compiles a SPVM class.
+Compiles a SPVM class. Classes loaded in the class and loaded classes are also compiled. The L<runtime|/"get_runtime"> is updated.
+
+If the compilation is successful, returns 0, otherwise returns a non-zero value.
+
+This native API can be called repeatedly to compile other classes.
 
 =head2 get_error_messages_length
   
-  int32_t (*get_error_messages_length)(void* compiler);
+C<int32_t (*get_error_messages_length)(L<void* compiler|SPVM::Document::NativeAPI::Compiler>);>
 
 Gets the length of the compilation error messages.
 
 =head2 get_error_message
   
-  const char* (*get_error_message)(void* compiler, int32_t index);
+C<const char* (*get_error_message)(L<void* compiler|SPVM::Document::NativeAPI::Compiler>, int32_t index);>
 
-Gets a compiler error message by an index.
+Gets the compiler error message given the index I<index>.
 
 =head2 get_runtime
 
-  void* (*get_runtime)(void* compiler);
+C<void* (*get_runtime)(L<void* compiler|SPVM::Document::NativeAPI::Compiler>);>
 
-Gets the runtime.
+Gets the L<runtime|SPVM::Document::NativeAPI::Runtime>.
 
 =head2 compile_anon_class
   
-  int32_t (*compile_anon_class)(void* compiler, const char* source, const char** anon_basic_type_name_ptr);
+C<int32_t (*compile_anon_class)(L<void* compiler|SPVM::Document::NativeAPI::Compiler>, const char* source, const char** anon_basic_type_name_ptr);>
 
-Compiles a SPVM anon class with a source I<source>. The generated anon class name is set to I<anon_basic_type_name_ptr>.
+Compiles a SPVM anon class given the source code I<source>. Classes loaded in the class and loaded classes are also compiled. The L<runtime|/"get_runtime"> is updated.
+
+If the compilation is successful, returns 0, otherwise returns a non-zero value.
+
+The generated anon class name is set to I<anon_basic_type_name_ptr>.
+
+This native API can be called repeatedly to compile other classes.
 
 =head1 Native API IDs
 
@@ -156,6 +168,22 @@ Compiles a SPVM anon class with a source I<source>. The generated anon class nam
   15 get_error_messages_length
   16 get_runtime
   17 prepend_include_dir
+
+=head1 See Also
+
+=over 2
+
+=item * L<SPVM::Document::NativeAPI>
+
+=item * L<SPVM::Document::NativeAPI::ClassFile>
+
+=item * L<SPVM::Document::NativeAPI::Runtime>
+
+=item * L<SPVM::Document::NativeClass>
+
+=item * L<SPVM::Document>
+
+=back
 
 =head1 Copyright & License
 
