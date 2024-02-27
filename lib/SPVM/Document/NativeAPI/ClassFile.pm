cd5d29487c21d2fe5f474588b12786b4c1dc28e2
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-02-27 14:05:41 +0900

    Imrprove SPVM::Document::NativeAPI::ClassFile.

--- lib/SPVM/Document/NativeAPI/ClassFile.pm
+++ lib/SPVM/Document/NativeAPI/ClassFile.pm
@@ -20,67 +20,67 @@ The class file native APIs of L<SPVM> are the APIs to get and set class file inf
 
   const char* (*get_class_name)(void* compiler, void* class_file);
 
-Returns the class name.
+Returns the class name of the class file I<class_file>.
 
 =head2 get_file
 
   const char* (*get_file)(void* compiler, void* class_file);
 
-Returns the file path for display.
+Returns the file path for display. This is a field of the class file I<class_file>.
 
 =head2 set_file
 
   void (*set_file)(void* compiler, void* class_file, const char* file);
 
-Copies the file path I<file> for display and sets it.
+Copies I<file> and sets it to the file path for display. This is a field of the class file I<class_file>.
 
 =head2 get_dir
 
   const char* (*get_dir)(void* compiler, void* class_file);
 
-Returns the directory where the class is loaded.
+Returns the directory where the class is loaded. This is a field of the class file I<class_file>.
 
 =head2 set_dir
 
   void (*set_dir)(void* compiler, void* class_file, const char* dir);
 
-Copies the directory path I<dir> where the class is loaded and sets it.
+Copies the directory path I<dir>, and sets it to the directory where the class is loaded. This is a field of the class file I<class_file>.
 
 =head2 get_rel_file
 
   const char* (*get_rel_file)(void* compiler, void* class_file);
 
-Returns the relative file path of the class.
+Returns the relative file path of the class file I<class_file>.
 
 =head2 set_rel_file
 
   void (*set_rel_file)(void* compiler, void* class_file, const char* rel_file);
 
-Copies a relative file path I<rel_file> of the class and sets it.
+Copies I<rel_file> and sets it to the relative file path of the class file I<class_file>.
 
 =head2 get_content
 
   const char* (*get_content)(void* compiler, void* class_file);
 
-Returns the content.
+Returns the content of the class file I<class_file>.
 
 =head2 set_content
 
   void (*set_content)(void* compiler, void* class_file, const char* content);
 
-Copies a content I<content> and sets it.
+Copies the content I<content> and sets it to the content of the class file I<class_file>.
 
 =head2 get_content_length
 
   int32_t (*get_content_length)(void* compiler, void* class_file);
 
-Returns the length of the content.
+Returns the length of the content of the class file I<class_file>.
 
 =head2 set_content_length
 
   void (*set_content_length)(void* compiler, void* class_file, int32_t content_length);
 
-Sets the length of a content.
+Sets I<content_length> to the length of the content of the class file I<class_file>.
 
 =head1 Native API IDs
 
