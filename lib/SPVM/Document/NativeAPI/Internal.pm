87a0778833b0880be5f568091fea957827ec1786
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-02-27 14:18:27 +0900

    Improve SPVM::Document::NativeAPI::Internal.

--- lib/SPVM/Document/NativeAPI/Internal.pm
+++ lib/SPVM/Document/NativeAPI/Internal.pm
@@ -16,25 +16,25 @@ These APIs are used for the implementation of the SPVM language, so they should 
 
 =head2 get_ref_count
 
-C<int32_t (*get_ref_count)(SPVM_ENV* env, SPVM_VALUE* stack, void* object);>
+C<int32_t (*get_ref_count)(L<SPVM_ENV* env|SPVM::Document::NativeClass/"Runtime Environment">, L<SPVM_VALUE* stack|SPVM::Document::NativeClass/"Runtime Stack">, void* object);>
 
 Gets the reference count of the object I<object>.
 
 =head2 inc_ref_count
 
-C<void (*inc_ref_count)(SPVM_ENV* env, SPVM_VALUE* stack, void* object);>
+C<void (*inc_ref_count)(L<SPVM_ENV* env|SPVM::Document::NativeClass/"Runtime Environment">, L<SPVM_VALUE* stack|SPVM::Document::NativeClass/"Runtime Stack">, void* object);>
 
 Increments the reference count of the object I<object>.
 
 =head2 dec_ref_count
 
-C<void (*dec_ref_count)(SPVM_ENV* env, SPVM_VALUE* stack, void* object);>
+C<void (*dec_ref_count)(L<SPVM_ENV* env|SPVM::Document::NativeClass/"Runtime Environment">, L<SPVM_VALUE* stack|SPVM::Document::NativeClass/"Runtime Stack">, void* object);>
 
 Decrements the reference count of the object I<object>.
 
 =head2 leave_scope_local
 
-C<void (*leave_scope_local)(SPVM_ENV* env, SPVM_VALUE* stack, void** object_vars, int32_t* mortal_stack, int32_t* mortal_stack_top_ptr, int32_t original_mortal_stack_top);>
+C<void (*leave_scope_local)(L<SPVM_ENV* env|SPVM::Document::NativeClass/"Runtime Environment">, L<SPVM_VALUE* stack|SPVM::Document::NativeClass/"Runtime Stack">, void** object_vars, int32_t* mortal_stack, int32_t* mortal_stack_top_ptr, int32_t original_mortal_stack_top);>
 
 Performs a leaveing scope operation for local variables given object variables I<object_vars>, the mortal stack I<mortal_stack>, the address of the top of the mortal stack I<mortal_stack_top_ptr>, and the original top of the mortal stack I<original_mortal_stack_top>.
 
