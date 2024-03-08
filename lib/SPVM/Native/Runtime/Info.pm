1429bfb1efc83df29910e43008f25545a9bad2bb
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-03-09 07:58:15 +0900

    Add the Native::Runtime::Info class is added.
      [New Features]
        * Add the Native::Runtime::Info class is added.

---
+++ lib/SPVM/Native/Runtime/Info.pm
@@ -0,0 +1,61 @@
+package SPVM::SPVM::Native::Runtime::Info;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::SPVM::Native::Runtime::Info - Short Description
+
+=head1 Description
+
+The SPVM::Native::Runtime::Info class in L<SPVM> has methods to do someting.
+
+=head1 Usage
+
+  use SPVM::Native::Runtime::Info;
+
+=head1 Details
+
+
+
+=head1 Inheritance
+
+
+
+=head1 Interfaces
+
+
+
+=head1 Enumerations
+
+
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2024 Yuki Kimoto
+
+MIT License
+
