7a16bf02d522353e078d4de0b72cb6133baf13f4
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-03-09 09:37:03 +0900

    Add the doc of Native::Runtime::Info.

--- lib/SPVM/Native/Runtime/Info.pm
+++ lib/SPVM/Native/Runtime/Info.pm
@@ -6,52 +6,51 @@ package SPVM::Native::Runtime::Info;
 
 =head1 Name
 
-SPVM::Native::Runtime::Info - Short Description
+SPVM::Native::Runtime::Info - Information of A Runtime
 
 =head1 Description
 
-The Native::Runtime::Info class in L<SPVM> has methods to do someting.
+The Native::Runtime::Info class in L<SPVM> has methods to get information of a runtime.
 
 =head1 Usage
 
   use Native::Runtime::Info;
-
-=head1 Details
-
-
-
-=head1 Inheritance
-
-
-
-=head1 Interfaces
-
-
-
-=head1 Enumerations
-
-
+  
+  my $runtime_info = Native::Runtime::Info->new($runtime);
 
 =head1 Fields
 
+=head2 runtime
 
+C<has runtime : L<Native::Runtime|SPVM::Native::Runtime>;>
+
+The runtime.
 
 =head1 Class Methods
 
-
+C<static method new : Native::Runtime::Info ($runtime : L<Native::Runtime|SPVM::Native::Runtime>);>
 
 =head1 Instance Methods
 
+=head2 get_basic_type_names
 
+C<method get_basic_type_names : string[] ();>
 
+Gets all basic type names owned by the runtime, and return it.
 
-=head1 Repository
+=head2 get_class_names
 
+C<method get_class_names : string[] ();>
 
+Gets all basic type names by defined C<class> keyword owned by the runtime, and return it.
 
-=head1 Author
+These contains the names of interface types and multi-numeric types.
 
-Yuki Kimoto C<kimoto.yuki@gmail.com>
+=head2 get_method_names
+
+C<method get_method_names : string[] ($class_name : string);>
+
+Gets all method names owned by the class given the class name $class_name.
 
 =head1 Copyright & License
 
