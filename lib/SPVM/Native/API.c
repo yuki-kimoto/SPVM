e8cc13e905dc3bd36fa20acadef04e33797658af
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-03-19 08:15:44 +0900

    Add empty Native::API class.

---
+++ lib/SPVM/Native/API.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2024 Yuki Kimoto
+// MIT License
+
+#include "spvm_native.h"
+
+
+
+static const char* FILE_NAME = "Native/API.c";
+
+int32_t SPVM__Native__API__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+
---
+++ lib/SPVM/Native/API.pm
@@ -0,0 +1,61 @@
+package SPVM::Native::API;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Native::API - Short Description
+
+=head1 Description
+
+The Native::API class in L<SPVM> has methods to do someting.
+
+=head1 Usage
+
+  use Native::API;
+
+=head1 Details
+
+
+
+=head1 Inheritance
+
+
+
+=head1 Interfaces
+
+
+
+=head1 Enumerations
+
+
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2024 Yuki Kimoto
+
+MIT License
+
