66976df2d5b50a5e69025f4ca1ccc416626b49e9
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-03-19 12:38:43 +0900

    Add the SPVM::Builder::Native::Runtime::Info module.
      [Internal Changes]
        * Add the SPVM::Builder::Native::Runtime::Info module.

---
+++ lib/SPVM/Builder/Native/Runtime/Info.pm
@@ -0,0 +1,16 @@
+package SPVM::Builder::Native::Runtime::Info;
+
+use strict;
+use warnings;
+
+1;
+
+=head1 Name
+
+SPVM::Builder::Native::Runtime::Info - Runtime Information
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
