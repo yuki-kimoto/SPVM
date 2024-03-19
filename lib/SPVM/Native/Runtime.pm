242868aebc32f483d008a772fa9491cf3478670a
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-03-20 08:26:10 +0900

    Remove the Native::Runtime::Info class and its methods are moved to the Native::Runtime::Class.
      [Incompatible Changes]
        * Remove the Native::Runtime::Info class and its methods are moved to the Native::Runtime::Class.
        * Remove the info method in the Native class.

--- lib/SPVM/Native/Runtime.pm
+++ lib/SPVM/Native/Runtime.pm
@@ -84,6 +84,56 @@ C<static method new_env : L<Native::Env|SPVM::Native::Env> ();>
 
 Creates a new L<Native::Env|SPVM::Native::Env> ojbect, and returns it.
 
+=head2 get_basic_type_names
+
+C<method get_basic_type_names : string[] ();>
+
+Gets all basic type names owned by the runtime, and return it.
+
+=head2 get_class_names
+
+C<method get_class_names : string[] ();>
+
+Gets all basic type names by defined C<class> keyword owned by the runtime, and return it.
+
+These contains the names of interface types and multi-numeric types.
+
+=head2 get_method_names
+
+C<method get_method_names : string[] ($class_name : string);>
+
+Returns all method names owned by the class given the class name $class_name.
+
+=head2 get_field_names
+
+C<method get_field_names : string[] ($class_name : string);>
+
+Returnsall field names owned by the class given the class name $class_name.
+
+=head2 get_class_var_names
+
+C<method get_class_var_names : string[] ($class_name : string);>
+
+Returnsall class variable names owned by the class given the class name $class_name.
+
+=head2 get_method_by_name
+
+C<method get_method_by_name : L<Native::Method|SPVM::Native::Method> ($class_name : string, $method_name : string);>
+
+Gets a method given the class name $class_name and the method name $method_name, returns it.
+
+=head2 get_field_by_name
+
+C<method get_field_by_name : L<Native::Field|SPVM::Native::Field> ($class_name : string, $field_name : string);>
+
+Gets a field given the class name $class_name and the method name $method_name, returns it.
+
+=head2 get_class_var_by_name
+
+C<method get_class_var_by_name : L<Native::ClassVar|SPVM::Native::ClassVar> ($class_name : string, $class_var_name : string);>
+
+Gets a class variable given the class name $class_name and the method name $method_name, returns it.
+
 =head1 See Also
 
 =head2 Native::Compiler
