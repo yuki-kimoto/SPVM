b6063d7e2b78801dab48fbcde399a1e466d1f919
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-03-19 16:14:27 +0900

    Add _get_native_method_names _get_precompile_method_names _get_class_file.

--- lib/SPVM/Builder/Native/BasicType.pm
+++ lib/SPVM/Builder/Native/BasicType.pm
@@ -36,6 +36,59 @@ sub runtime {
   }
 }
 
+# Private Instance Methods - Only used in core.
+sub _get_native_method_names {
+  my ($self) = @_;
+  
+  my $methods_length = $self->get_methods_length;
+  
+  my $native_method_names = [];
+  
+  for (my $method_index = 0; $method_index < $methods_length; $method_index++) {
+    my $method = $self->get_method_by_index($method_index);
+    my $method_name = $method->get_name;
+    if ($method->is_native) {
+      push @$native_method_names, $method_name;
+    }
+  }
+  
+  return $native_method_names;
+}
+
+sub _get_precompile_method_names {
+  my ($self) = @_;
+  
+  my $methods_length = $self->get_methods_length;
+  
+  my $precompile_method_names = [];
+  
+  for (my $method_index = 0; $method_index < $methods_length; $method_index++) {
+    my $method = $self->get_method_by_index($method_index);
+    my $method_name = $method->get_name;
+    if ($method->is_precompile) {
+      push @$precompile_method_names, $method_name;
+    }
+  }
+  
+  return $precompile_method_names;
+}
+
+sub _get_class_file {
+  my ($self) = @_;
+  
+  my $class_dir = $self->get_class_dir;
+  
+  unless ($class_dir) {
+    die "The class_dir field must be defined.";
+  }
+  
+  my $class_rel_file = $self->get_class_rel_file;
+  
+  my $class_file = "$class_dir/$class_rel_file";
+  
+  return $class_file;
+}
+
 1;
 
 =head1 Name
