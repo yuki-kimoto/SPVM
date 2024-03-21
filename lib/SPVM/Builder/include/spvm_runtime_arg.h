93921d90d89ac0280316ab590ecf2ace600d6afd
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-03-21 09:10:51 +0900

    Add current_method field to struct spvm_runtime_arg.

--- lib/SPVM/Builder/include/spvm_runtime_arg.h
+++ lib/SPVM/Builder/include/spvm_runtime_arg.h
@@ -9,6 +9,7 @@
 struct spvm_runtime_arg {
   const char* name;
   SPVM_RUNTIME_BASIC_TYPE* basic_type;
+  SPVM_RUNTIME_METHOD* current_method;
   int32_t index;
   int32_t type_dimension;
   int32_t type_flag;
