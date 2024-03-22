93e7585e10c91c19726249d27a7b39fbe529d2ef
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-03-22 09:12:46 +0900

    Add the Native::Constant class.
      [Incompatible Changes]
        * Add the Native::Constant class.

---
+++ lib/SPVM/Native/Constant.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2024 Yuki Kimoto
+// MIT License
+
+#include "spvm_native.h"
+
+
+
+static const char* FILE_NAME = "Native/Constant.c";
+
+int32_t SPVM__Native__Constant__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+
