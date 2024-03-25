f4e210b6bc8a87e1ed12ef337d1578e9508f1e1f
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-03-25 10:46:04 +0900

    Fix the bug that the logical AND operator set the internal condition flag where it should not be changed.
      [Bug Fix]
        * Fix the bug that the logical AND operator set the internal condition flag where it should not be changed.

--- lib/SPVM/Builder/src/spvm_op.c
+++ lib/SPVM/Builder/src/spvm_op.c
@@ -2814,38 +2814,74 @@ SPVM_OP* SPVM_OP_build_logical_and(SPVM_COMPILER* compiler, SPVM_OP* op_logical_
   /*
     [Before]
     AND
-      first
-      last
+      left_operand
+      right_operand
   */
   
   /*
     [After]
-    BOOL              op_bool
-      IF              op_if1
+    SEQUENCE                    op_sequence
+      VAR                       op_var
+        VAR_DECL                op_var_decl
+      IF                        op_if_left
         CONDITION
-          first
-        IF            op_if2
+          left_operand
+        IF                      op_if_right
           CONDITION
-            last
-          DO_NOTHING  op_do_nothing2
-          DO_NOTHING  op_do_nothing3
-        DO_NOTHING    op_do_nothing1
+            right_operand
+          ASSIGN                op_assign_true_right
+            VAR_CONDITION_FLAG  op_var_condition_flag_true_right
+            VAR                 op_var_true_right
+          ASSIGN                op_assign_false_right
+            VAR_CONDITION_FLAG  op_var_condition_flag_false_right
+            VAR                 op_var_false_right
+        ASSIGN                  op_assign_true_left
+          VAR_CONDITION_FLAG    op_var_condition_flag_false_left
+          VAR                   op_var_true_left
+      VAR                       op_var_ret
   */
   
-  SPVM_OP* op_do_nothing1 = SPVM_OP_new_op(compiler, SPVM_OP_C_ID_DO_NOTHING, op_logical_and->file, op_logical_and->line);
-  SPVM_OP* op_do_nothing2 = SPVM_OP_new_op(compiler, SPVM_OP_C_ID_DO_NOTHING, op_logical_and->file, op_logical_and->line);
-  SPVM_OP* op_do_nothing3 = SPVM_OP_new_op(compiler, SPVM_OP_C_ID_DO_NOTHING, op_logical_and->file, op_logical_and->line);
+  SPVM_OP* op_sequence = SPVM_OP_new_op(compiler, SPVM_OP_C_ID_SEQUENCE, op_logical_and->file, op_logical_and->line);
   
-  SPVM_OP* op_if1 = SPVM_OP_new_op(compiler, SPVM_OP_C_ID_IF, op_logical_and->file, op_logical_and->line);
-  SPVM_OP* op_if2 = SPVM_OP_new_op(compiler, SPVM_OP_C_ID_IF, op_if1->file, op_if1->line);
+  SPVM_OP* op_name_var = SPVM_OP_new_op_name_tmp_var(compiler, op_logical_and->file, op_logical_and->line);
+  SPVM_OP* op_var = SPVM_OP_new_op_var(compiler, op_name_var);
+  SPVM_OP* op_type_var = SPVM_OP_new_op_int_type(compiler, op_logical_and->file, op_logical_and->line);
+  SPVM_OP* op_var_decl = SPVM_OP_new_op_var_decl(compiler, op_logical_and->file, op_logical_and->line);
+  SPVM_OP_build_var_decl(compiler, op_var_decl, op_var, op_type_var, NULL);
+  
+  SPVM_OP_insert_child(compiler, op_sequence, op_sequence->last, op_var);
+  
+  SPVM_OP* op_var_true_rigth = SPVM_OP_clone_op_var(compiler, op_var);
+  SPVM_OP* op_var_condition_flag_true_right = SPVM_OP_new_op_var_condition_flag(compiler, op_logical_and->file, op_logical_and->line);
+  SPVM_OP* op_assign_true_rigth = SPVM_OP_new_op(compiler, SPVM_OP_C_ID_ASSIGN, op_logical_and->file, op_logical_and->line);
+  SPVM_OP_build_assign(compiler, op_assign_true_rigth, op_var_true_rigth, op_var_condition_flag_true_right);
+  
+  SPVM_OP* op_var_false_right = SPVM_OP_clone_op_var(compiler, op_var);
+  SPVM_OP* op_var_condition_flag_false_right = SPVM_OP_new_op_var_condition_flag(compiler, op_logical_and->file, op_logical_and->line);
+  SPVM_OP* op_assign_false_right = SPVM_OP_new_op(compiler, SPVM_OP_C_ID_ASSIGN, op_logical_and->file, op_logical_and->line);
+  SPVM_OP_build_assign(compiler, op_assign_false_right, op_var_false_right, op_var_condition_flag_false_right);
+  
+  SPVM_OP* op_var_false_left = SPVM_OP_clone_op_var(compiler, op_var);
+  SPVM_OP* op_var_condition_flag_false_left = SPVM_OP_new_op_var_condition_flag(compiler, op_logical_and->file, op_logical_and->line);
+  SPVM_OP* op_assign_false_left = SPVM_OP_new_op(compiler, SPVM_OP_C_ID_ASSIGN, op_logical_and->file, op_logical_and->line);
+  SPVM_OP_build_assign(compiler, op_assign_false_left, op_var_false_left, op_var_condition_flag_false_left);
+  
+  SPVM_OP* op_if_left = SPVM_OP_new_op(compiler, SPVM_OP_C_ID_IF, op_logical_and->file, op_logical_and->line);
+  
+  SPVM_OP* op_if_right = SPVM_OP_new_op(compiler, SPVM_OP_C_ID_IF, op_if_left->file, op_if_left->line);
   
   int32_t no_scope = 1;
-  SPVM_OP_build_if_statement(compiler, op_if2, op_right_operand, op_do_nothing2, op_do_nothing3, no_scope);
-  SPVM_OP_build_if_statement(compiler, op_if1, op_left_operand, op_if2, op_do_nothing1, no_scope);
+  SPVM_OP_build_if_statement(compiler, op_if_right, op_right_operand, op_assign_true_rigth, op_assign_false_right, no_scope);
   
-  SPVM_OP* op_bool = SPVM_OP_new_op_bool(compiler, op_if1, op_logical_and->file, op_logical_and->line);
+  SPVM_OP_build_if_statement(compiler, op_if_left, op_left_operand, op_if_right, op_assign_false_left, no_scope);
   
-  return op_bool;
+  SPVM_OP_insert_child(compiler, op_sequence, op_sequence->last, op_if_left);
+  
+  SPVM_OP* op_var_ret = SPVM_OP_clone_op_var(compiler, op_var);
+  
+  SPVM_OP_insert_child(compiler, op_sequence, op_sequence->last, op_var_ret);
+  
+  return op_sequence;
 }
 
 SPVM_OP* SPVM_OP_build_logical_or(SPVM_COMPILER* compiler, SPVM_OP* op_logical_or, SPVM_OP* op_left_operand, SPVM_OP* op_right_operand) {
