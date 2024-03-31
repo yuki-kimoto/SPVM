a45955b73784fc71c7c0d49ee52f5880304562fc
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-04-01 08:42:47 +0900

    Improve English.

--- lib/SPVM/Document/Language/System.pm
+++ lib/SPVM/Document/Language/System.pm
@@ -10,15 +10,21 @@ This document describes system setting in the SPVM language.
 
 =head2 Standard IO
 
-SPVM has own C<stdin>, C<stdout>, and C<stderr>. These are set to the binary mode.
+SPVM opens own standard input, standard output, and standard error for a runtime just after a runtime is build.
 
-The say operator and the print operator output to this SPVM's C<stdout>.
+Their open modes are set to the L<binary mode|https://learn.microsoft.com/en-us/cpp/c-runtime-library/text-and-binary-mode-file-i-o?view=msvc-170> in Windows.
 
-The warn operator outputs to this SPVM's C<stderr>.
+The L<print operator|SPVM::Document::Language::Operators/"print Operator"> outputs to this SPVM's standard output.
+
+The L<say operator|SPVM::Document::Language::Operators/"say Operator"> outputs to this SPVM's standard output.
+
+The L<warn operator|SPVM::Document::Language::Operators/"warn Operator"> outputs to this SPVM's standard error.
 
 =head1 See Also
 
 =over 2
+
+=item * L<SPVM::Document::Language::Operators>
 
 =item * L<SPVM::Document::Language>
 
