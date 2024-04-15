3371fd31ebceaf3c4c06e9e613e4b995354fc780
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-04-15 14:37:13 +0900

    The class generated by a anon method is not an anon class.
      [Imcompatible Changes]
        * The class generated by a anon method is not an anon class.

--- lib/SPVM/Builder/src/spvm_op.c
+++ lib/SPVM/Builder/src/spvm_op.c
@@ -320,7 +320,6 @@ SPVM_OP* SPVM_OP_build_class(SPVM_COMPILER* compiler, SPVM_OP* op_class, SPVM_OP
   
   if (strstr(basic_type_name, "::anon_method::")) {
     type->basic_type->access_control_type = SPVM_ATTRIBUTE_C_ID_PUBLIC;
-    basic_type->is_anon = 1;
     basic_type->is_generated_by_anon_method = 1;
   }
   else if (strstr(basic_type_name, "::anon_class::")) {
@@ -819,7 +818,7 @@ SPVM_OP* SPVM_OP_build_class(SPVM_COMPILER* compiler, SPVM_OP* op_class, SPVM_OP
     // The default of the access controll of the field is private.
     if (field->access_control_type == SPVM_ATTRIBUTE_C_ID_UNKNOWN) {
       // If anon method, field is public
-      if (basic_type->is_anon) {
+      if (basic_type->is_generated_by_anon_method) {
         field->access_control_type = SPVM_ATTRIBUTE_C_ID_PUBLIC;
       }
       // If multi-numeric type, field is public
