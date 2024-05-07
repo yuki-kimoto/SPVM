bb9e8749bd62a2028f3165465c3a71dea1048467
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-05-07 12:47:55 +0900

    Fix get_default_value bug and add arguments tests.

--- lib/SPVM/Builder/src/spvm_api_arg.c
+++ lib/SPVM/Builder/src/spvm_api_arg.c
@@ -21,7 +21,7 @@ SPVM_API_ARG* SPVM_API_ARG_new_api() {
     SPVM_API_ARG_get_stack_index,
     SPVM_API_ARG_get_current_method,
     SPVM_API_ARG_is_optional,
-    SPVM_API_ARG_get_current_method,
+    SPVM_API_ARG_get_default_value,
   };
   
   SPVM_API_ARG* native_apis = calloc(1, sizeof(native_apis_init));
