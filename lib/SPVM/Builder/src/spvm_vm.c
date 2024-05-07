db5c209fa76d339102f46ce985851e00f19c83a4
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-05-07 10:50:05 +0900

    Remove the following operation codes.
      [Internal Changes]
        * Remove the following operation codes.
          SPVM_OPCODE_C_ID_GET_STACK_OPTIONAL_BYTE
          SPVM_OPCODE_C_ID_GET_STACK_OPTIONAL_SHORT
          SPVM_OPCODE_C_ID_GET_STACK_OPTIONAL_INT
          SPVM_OPCODE_C_ID_GET_STACK_OPTIONAL_LONG
          SPVM_OPCODE_C_ID_GET_STACK_OPTIONAL_FLOAT
          SPVM_OPCODE_C_ID_GET_STACK_OPTIONAL_DOUBLE
          SPVM_OPCODE_C_ID_GET_STACK_OPTIONAL_OBJECT

--- lib/SPVM/Builder/src/spvm_vm.c
+++ lib/SPVM/Builder/src/spvm_vm.c
@@ -2204,43 +2204,6 @@ int32_t SPVM_VM_call_method(SPVM_ENV* env, SPVM_VALUE* stack, SPVM_RUNTIME_METHO
         SPVM_IMPLEMENT_GET_STACK_MULNUM_DOUBLE(env, &double_vars[opcode->operand0], stack, opcode->operand3 & 0xFF, args_width);
         break;
       }
-      case SPVM_OPCODE_C_ID_GET_STACK_OPTIONAL_BYTE: {
-        int32_t stack_index = opcode->operand3 & 0xFF;
-        SPVM_IMPLEMENT_GET_STACK_OPTIONAL_BYTE(env, &byte_vars[opcode->operand0], stack, stack_index, (int8_t)(uint8_t)opcode->operand1);
-        break;
-      }
-      case SPVM_OPCODE_C_ID_GET_STACK_OPTIONAL_SHORT: {
-        int32_t stack_index = opcode->operand3 & 0xFF;
-        SPVM_IMPLEMENT_GET_STACK_OPTIONAL_SHORT(env, &short_vars[opcode->operand0], stack, stack_index, (int16_t)(uint16_t)opcode->operand1);
-        break;
-      }
-      case SPVM_OPCODE_C_ID_GET_STACK_OPTIONAL_INT: {
-        int32_t stack_index = opcode->operand3 & 0xFF;
-        SPVM_IMPLEMENT_GET_STACK_OPTIONAL_INT(env, &int_vars[opcode->operand0], stack, stack_index, (int32_t)opcode->operand1);
-        break;
-      }
-      case SPVM_OPCODE_C_ID_GET_STACK_OPTIONAL_LONG: {
-        int32_t stack_index = opcode->operand3 & 0xFF;
-        SPVM_IMPLEMENT_GET_STACK_OPTIONAL_LONG(env, &long_vars[opcode->operand0], stack, stack_index, *(int64_t*)&opcode->operand1);
-        break;
-      }
-      case SPVM_OPCODE_C_ID_GET_STACK_OPTIONAL_FLOAT: {
-        int32_t stack_index = opcode->operand3 & 0xFF;
-        SPVM_VALUE default_value;
-        default_value.ival = (int32_t)opcode->operand1;
-        SPVM_IMPLEMENT_GET_STACK_OPTIONAL_FLOAT(env, &float_vars[opcode->operand0], stack, stack_index, default_value.fval);
-        break;
-      }
-      case SPVM_OPCODE_C_ID_GET_STACK_OPTIONAL_DOUBLE: {
-        int32_t stack_index = opcode->operand3 & 0xFF;
-        SPVM_IMPLEMENT_GET_STACK_OPTIONAL_DOUBLE(env, &double_vars[opcode->operand0], stack, stack_index, *(double*)&opcode->operand1);
-        break;
-      }
-      case SPVM_OPCODE_C_ID_GET_STACK_OPTIONAL_OBJECT: {
-        int32_t stack_index = opcode->operand3 & 0xFF;
-        SPVM_IMPLEMENT_GET_STACK_OPTIONAL_OBJECT(env, &object_vars[opcode->operand0], stack, stack_index);
-        break;
-      }
       case SPVM_OPCODE_C_ID_END_ARGS: {
         // Do nothing
         break;
