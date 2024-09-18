ae6a46a3293348472e536c5320fe3ee99b093c9b
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-09-18 12:58:23 +0900

    Add Cloner#default_cloner method.
      [New Features]
        * Add Cloner#default_cloner method.

--- lib/SPVM/Cloner.pm
+++ lib/SPVM/Cloner.pm
@@ -26,6 +26,27 @@ SPVM::Cloner - Interface Type for the Callback to Clone a Object
 
 L<Cloner|SPVM::Cloner> is the interface type for the callback to clone an object.
 
+=head1 Class Methods
+
+C<static method default_cloner : L<Cloner|SPVM::Cloner> ();>
+
+Returns a default cloner.
+
+The implementation of the default cloner is
+
+  method : object ($object : Cloneable) {
+  
+    my $clone = (object)undef;
+    
+    if ($object) {
+      $clone = $object->clone;
+    }
+    
+    return $clone;
+  };
+
+If the object $object is undef, returns undef, ohterwise calls L<Cloneable#clone|SPVM::Cloneable/"clone"> method and its return value.
+
 =head1 Interface Methodss
 
 C<required method : object ($object : object);>
