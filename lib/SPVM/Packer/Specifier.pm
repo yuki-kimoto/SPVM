56c7467b87c97d43f444bafcc66975d1e32b8068
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-09-28 07:55:07 +0900

    Add non-implemented Packer class.

---
+++ lib/SPVM/Packer/Specifier.pm
@@ -0,0 +1,65 @@
+package SPVM::Packer::Specifier;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::Packer::Specifier - Short Description
+
+=head1 Description
+
+Packer::Specifier class in L<SPVM> has methods to do someting.
+
+=head1 Usage
+
+  use Packer::Specifier;
+
+=head1 Details
+
+
+
+=head1 Super Class
+
+
+
+=head1 Interfaces
+
+
+
+=head1 Enumerations
+
+
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 See Also
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2024 Yuki Kimoto
+
+MIT License
+
