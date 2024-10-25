87f4d32649f4bfb537b589cbd024dd8e74a5d622
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-10-25 09:27:43 +0900

    Improve the type check of the field in get_field_object_by_name native API. The field must be an object type.
      [Changes with Incompatible Change]
        * Improve the type check of the field in get_field_object_by_name native API. The field must be an object type.

--- lib/SPVM/Builder/src/spvm_api.c
+++ lib/SPVM/Builder/src/spvm_api.c
@@ -1618,7 +1618,24 @@ SPVM_OBJECT* SPVM_API_get_field_object_by_name(SPVM_ENV* env, SPVM_VALUE* stack,
     *error_id = SPVM_API_die(env, stack, "The \"%s\" field is not found in the \"%s\" class or its super class.", field_name, basic_type_name, func_name, file, line);
     return NULL;
   };
-  SPVM_OBJECT* value = SPVM_API_get_field_object(env, stack, object, field);
+  
+  int32_t is_object_type = SPVM_API_TYPE_is_object_type(runtime, field->basic_type, field->type_dimension, field->type_flag);
+  
+  int32_t is_invalid_type = 0;
+  
+  SPVM_OBJECT* value = NULL;
+  if (is_object_type) {
+    value = SPVM_API_get_field_object(env, stack, object, field);
+  }
+  else {
+    is_invalid_type = 1;
+  }
+  
+  if (is_invalid_type) {
+    *error_id = SPVM_API_die(env, stack, "The type of the field must be an object type.", func_name, file, line);
+    return 0;
+  }
+  
   return value;
 }
 
