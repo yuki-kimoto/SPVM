6b16aa6378eaef3c0593a83e0c4112c4b6d91024
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-10-25 09:20:16 +0900

    Improve the type check of the field in get_field_long_by_name native API. The field must be within long type.
      [Changes with Incompatible Change]
        * Improve the type check of the field in get_field_long_by_name native API. The field must be within long type.

--- lib/SPVM/Builder/src/spvm_api.c
+++ lib/SPVM/Builder/src/spvm_api.c
@@ -1399,7 +1399,43 @@ int64_t SPVM_API_get_field_long_by_name(SPVM_ENV* env, SPVM_VALUE* stack, SPVM_O
     *error_id = SPVM_API_die(env, stack, "The \"%s\" field is not found in the \"%s\" class or its super class.", field_name, basic_type_name, func_name, file, line);
     return 0;
   };
-  int64_t value = SPVM_API_get_field_long(env, stack, object, field);
+  int32_t is_numeric_type = SPVM_API_TYPE_is_numeric_type(runtime, field->basic_type, field->type_dimension, field->type_flag);
+  
+  int32_t is_invalid_type = 0;
+  
+  int64_t value = 0;
+  if (is_numeric_type) {
+    switch (field->basic_type->id) {
+      case SPVM_NATIVE_C_BASIC_TYPE_ID_BYTE : {
+        value = SPVM_API_get_field_byte(env, stack, object, field);
+        break;
+      }
+      case SPVM_NATIVE_C_BASIC_TYPE_ID_SHORT : {
+        value = SPVM_API_get_field_short(env, stack, object, field);
+        break;
+      }
+      case SPVM_NATIVE_C_BASIC_TYPE_ID_INT : {
+        value = SPVM_API_get_field_int(env, stack, object, field);
+        break;
+      }
+      case SPVM_NATIVE_C_BASIC_TYPE_ID_LONG : {
+        value = SPVM_API_get_field_long(env, stack, object, field);
+        break;
+      }
+      default : {
+        is_invalid_type = 1;
+      }
+    }
+  }
+  else {
+    is_invalid_type = 1;
+  }
+  
+  if (is_invalid_type) {
+    *error_id = SPVM_API_die(env, stack, "The type of the field must be within long type.", func_name, file, line);
+    return 0;
+  }
+  
   return value;
 }
 
