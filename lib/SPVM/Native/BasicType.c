16f53c75ed9863882119625742fc36ee1144f938
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-04 08:12:12 +0900

    Cleanup get_anon_basic_type_by_index.

--- lib/SPVM/Native/BasicType.c
+++ lib/SPVM/Native/BasicType.c
@@ -497,11 +497,8 @@ int32_t SPVM__Native__BasicType__get_anon_basic_type_by_index(SPVM_ENV* env, SPV
     return env->die(env, stack, "The basic type is not found.", __func__, FILE_NAME, __LINE__);
   }
   
-  void* obj_address_anon_basic_type = env->new_object_by_name(env, stack, "Address", &error_id, __func__, FILE_NAME, __LINE__);
+  void* obj_address_anon_basic_type = env->new_pointer_object_by_name(env, stack, "Address", anon_basic_type, &error_id, __func__, FILE_NAME, __LINE__);
   if (error_id) { return error_id; }
-  
-  env->set_pointer(env, stack, obj_address_anon_basic_type, anon_basic_type);
-  
   stack[0].oval = obj_address_anon_basic_type;
   env->call_class_method_by_name(env, stack, "Native::BasicType", "new_pointer", 1, &error_id, __func__, FILE_NAME, __LINE__);
   if (error_id) { return error_id; }
