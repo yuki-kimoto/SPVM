3087c7124e1fccb5f94982ad97848a2f673b55d6
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-30 08:48:30 +0900

    Fix Native::Compiler bugs.

--- lib/SPVM/Native/Method.c
+++ lib/SPVM/Native/Method.c
@@ -95,6 +95,9 @@ int32_t SPVM__Native__Method__get_return_basic_type(SPVM_ENV* env, SPVM_VALUE* s
   void* obj_basic_type = stack[0].oval;
   env->set_no_free(env, stack, obj_basic_type, 1);
   
+  env->set_field_object_by_name(env, stack, obj_basic_type, "runtime", obj_runtime, &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
+  
   env->set_field_object_by_name(env, stack, obj_basic_type, "ref_method", obj_self, &error_id, __func__, FILE_NAME, __LINE__);
   if (error_id) { return error_id; }
   
@@ -165,6 +168,9 @@ int32_t SPVM__Native__Method__get_current_basic_type(SPVM_ENV* env, SPVM_VALUE* 
   if (error_id) { return error_id; }
   void* obj_current_basic_type = stack[0].oval;
   env->set_no_free(env, stack, obj_current_basic_type, 1);
+  
+  env->set_field_object_by_name(env, stack, obj_current_basic_type, "runtime", obj_runtime, &error_id, __func__, FILE_NAME, __LINE__);
+  if (error_id) { return error_id; }
   
   env->set_field_object_by_name(env, stack, obj_current_basic_type, "ref_method", obj_self, &error_id, __func__, FILE_NAME, __LINE__);
   if (error_id) { return error_id; }
