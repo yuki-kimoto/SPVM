d06e364929ba83f522503a60caa04aec61f62803
Yuki Kimoto <kimoto.yuki@gmail.com>
2024-11-30 08:15:10 +0900

    Add Native::BasicType#ref_method field.

--- lib/SPVM/Native/Method.c
+++ lib/SPVM/Native/Method.c
@@ -95,7 +95,7 @@ int32_t SPVM__Native__Method__get_return_basic_type(SPVM_ENV* env, SPVM_VALUE* s
   void* obj_basic_type = stack[0].oval;
   env->set_no_free(env, stack, obj_basic_type, 1);
   
-  env->set_field_object_by_name(env, stack, obj_basic_type, "runtime", obj_self, &error_id, __func__, FILE_NAME, __LINE__);
+  env->set_field_object_by_name(env, stack, obj_basic_type, "ref_method", obj_self, &error_id, __func__, FILE_NAME, __LINE__);
   if (error_id) { return error_id; }
   
   stack[0].oval = obj_basic_type;
@@ -166,7 +166,7 @@ int32_t SPVM__Native__Method__get_current_basic_type(SPVM_ENV* env, SPVM_VALUE* 
   void* obj_current_basic_type = stack[0].oval;
   env->set_no_free(env, stack, obj_current_basic_type, 1);
   
-  env->set_field_object_by_name(env, stack, obj_current_basic_type, "runtime", obj_self, &error_id, __func__, FILE_NAME, __LINE__);
+  env->set_field_object_by_name(env, stack, obj_current_basic_type, "ref_method", obj_self, &error_id, __func__, FILE_NAME, __LINE__);
   if (error_id) { return error_id; }
   
   stack[0].oval = obj_current_basic_type;
