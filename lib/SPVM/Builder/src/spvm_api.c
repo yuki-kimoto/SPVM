f0bc28e7f5ace35af30ec5a0367d64bc48fbde04
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-01-02 07:24:04 +0900

    Cleanup the logic of optional arguments.

--- lib/SPVM/Builder/src/spvm_api.c
+++ lib/SPVM/Builder/src/spvm_api.c
@@ -4845,15 +4845,14 @@ int32_t SPVM_API_call_method_common(SPVM_ENV* env, SPVM_VALUE* stack, SPVM_RUNTI
       
       if (!no_need_call) {
         
-        // Set default values of optional arguments
-        for (int32_t arg_index = method->required_args_length; arg_index < method->args_length; arg_index++) {
+        for (int32_t arg_index = 0; arg_index < method->args_length; arg_index++) {
           SPVM_RUNTIME_ARG* arg = &method->args[arg_index];
           
+          // Set a default value of an optional argument
           if (arg->stack_index >= args_width) {
-            
-            assert(arg->is_optional);
-            
-            stack[arg->stack_index] = arg->default_value;
+            if (arg->is_optional) {
+              stack[arg->stack_index] = arg->default_value;
+            }
           }
         }
         
