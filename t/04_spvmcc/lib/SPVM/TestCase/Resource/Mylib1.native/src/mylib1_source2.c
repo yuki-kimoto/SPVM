b68f18681157a97b955128fd8632ae4826d18aa6
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-01-09 08:24:14 +0900

    Use resource in tests.

---
+++ t/04_spvmcc/lib/SPVM/TestCase/Resource/Mylib1.native/src/mylib1_source2.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+
+#include "mylib1_source2.h"
+
+const char* mylib1_source2_func1 () {
+  
+  return "Mylib1__mylib1_source2_func1";
+}
+
+const char* mylib1_source2_func2 () {
+  
+  return "Mylib1__mylib1_source2_func2";
+}
