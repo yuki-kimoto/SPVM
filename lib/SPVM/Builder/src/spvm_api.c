c096b727f82ed5884921750a14f0058dd4d49030
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-04-21 08:57:48 +0900

    Change an argument name in SPVM_API_leave_scope_local.

--- lib/SPVM/Builder/src/spvm_api.c
+++ lib/SPVM/Builder/src/spvm_api.c
@@ -4983,10 +4983,10 @@ void SPVM_API_leave_scope(SPVM_ENV* env, SPVM_VALUE* stack, int32_t original_mor
   *current_mortal_stack_top_ptr = original_mortal_stack_top;
 }
 
-void SPVM_API_leave_scope_local(SPVM_ENV* env, SPVM_VALUE* stack, SPVM_OBJECT** object_vars, int32_t* mortal_stack, int32_t* mortal_stack_top_ptr, int32_t original_mortal_stack_top) {
+void SPVM_API_leave_scope_local(SPVM_ENV* env, SPVM_VALUE* stack, SPVM_OBJECT** object_vars, int32_t* mortal_stack_typed_var_index, int32_t* mortal_stack_top_ptr, int32_t original_mortal_stack_top) {
   
   for (int32_t mortal_stack_index = original_mortal_stack_top; mortal_stack_index < *mortal_stack_top_ptr; mortal_stack_index++) {
-    int32_t var_index = mortal_stack[mortal_stack_index];
+    int32_t var_index = mortal_stack_typed_var_index[mortal_stack_index];
     SPVM_OBJECT** ref = (SPVM_OBJECT**)&object_vars[var_index];
     if (*ref != NULL) {
       SPVM_API_assign_object(env, stack, ref, NULL);
