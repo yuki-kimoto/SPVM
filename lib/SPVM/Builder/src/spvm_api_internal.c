24b41cc2f8b6a09f1f7767312eb846e608976852
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-04-21 10:05:07 +0900

    Fix an argument in SPVM_API_leave_scope_local_v2.

--- lib/SPVM/Builder/src/spvm_api_internal.c
+++ lib/SPVM/Builder/src/spvm_api_internal.c
@@ -17,6 +17,7 @@ SPVM_API_INTERNAL* SPVM_API_INTERNAL_new_api() {
     SPVM_API_INTERNAL_dec_ref_count,
     SPVM_API_INTERNAL_leave_scope_local,
     SPVM_API_INTERNAL_get_stack_tmp_buffer,
+    SPVM_API_INTERNAL_leave_scope_local_v2,
   };
   SPVM_API_INTERNAL* env_internal = calloc(1, sizeof(env_internal_init));
   memcpy(env_internal, env_internal_init, sizeof(env_internal_init));
@@ -48,9 +49,9 @@ void SPVM_API_INTERNAL_leave_scope_local(SPVM_ENV* env, SPVM_VALUE* stack, SPVM_
   SPVM_API_leave_scope_local(env, stack, object_vars, mortal_stack, mortal_stack_top_ptr, original_mortal_stack_top);
 }
 
-void SPVM_API_INTERNAL_leave_scope_local_v2(SPVM_ENV* env, SPVM_VALUE* stack, SPVM_OBJECT** mortal_stack, int32_t* mortal_stack_top_ptr, int32_t* mortal_stack_tops, int32_t* mortal_stack_tops_index_ptr) {
+void SPVM_API_INTERNAL_leave_scope_local_v2(SPVM_ENV* env, SPVM_VALUE* stack, SPVM_OBJECT** mortal_stack, int32_t* mortal_stack_top_ptr, int32_t* mortal_stack_tops, int32_t mortal_stack_tops_index) {
   
-  SPVM_API_leave_scope_local_v2(env, stack, mortal_stack, mortal_stack_top_ptr, mortal_stack_tops, mortal_stack_tops_index_ptr);
+  SPVM_API_leave_scope_local_v2(env, stack, mortal_stack, mortal_stack_top_ptr, mortal_stack_tops, mortal_stack_tops_index);
 }
 
 char* SPVM_API_INTERNAL_get_stack_tmp_buffer(SPVM_ENV* env, SPVM_VALUE* stack) {
