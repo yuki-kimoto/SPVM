dd7498bc200e44eabf776476da378f8ad293a4a0
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-05-01 10:26:50 +0900

    Remove unused dump_dependency_cpan.

--- lib/SPVM/Builder/Exe.pm
+++ lib/SPVM/Builder/Exe.pm
@@ -1030,48 +1030,6 @@ sub get_user_defined_basic_type_names {
   return $class_names;
 }
 
-sub dump_dependency_cpan {
-  my ($class, $script_name) = @_;
-  
-  my $info = SPVM::Builder::ScriptInfo->new(script_name => $script_name);
-  
-  my $runtime = $info->runtime;
-  
-  my $class_names = $info->get_class_names;
-  
-  my $dependency_infos = [];
-  
-  for my $class_name (sort @$class_names) {
-    
-    my $basic_type = $runtime->get_basic_type_by_name($class_name);
-    
-    my $version_string = $basic_type->get_version_string;
-    
-    my $dependency_info;
-    
-    if ($class_name eq "SPVM") {
-      $dependency_info = "SPVM";
-    }
-    else {
-      $dependency_info = "SPVM::$class_name";
-    }
-    
-    if (length $version_string) {
-      $dependency_info .= " $version_string";
-    }
-    
-    push @$dependency_infos, $dependency_info;
-  }
-  
-  my $spvm_version_string = $runtime->get_spvm_version_string;
-  
-  unshift @$dependency_infos, "SPVM $spvm_version_string";
-  
-  my $dependency_cpan = join("\x0A", @$dependency_infos) . "\x0A";
-  
-  return $dependency_cpan;
-}
-
 1;
 
 =head1 Name
