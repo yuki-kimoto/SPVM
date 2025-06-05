28a61590fc6876bbcd0702c72d89a87e684dac3c
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-06-06 07:49:32 +0900

    renamed:    t/04_spvmcc/lib/SPVM/TestCase/ExternalObject.spvm -> t/04_spvmcc/lib/SPVM/TestCase/ExternalFunction.spvm.

---
+++ t/04_spvmcc/lib/SPVM/TestCase/ExternalFunction.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2025 [--user-name]
+// MIT License
+
+#include "spvm_native.h"
+
+
+
+static const char* FILE_NAME = "TestCase/ExternalFunction.c";
+
+int32_t SPVM__TestCase__ExternalFunction__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  return 0;
+}
+
+
