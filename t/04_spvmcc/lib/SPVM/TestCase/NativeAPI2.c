dabbbc6733c9a3b44f9f4a1f07dfedb1ec544270
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-06-06 07:40:52 +0900

    Refactor t/04_spvmcc/lib/SPVM/TestCase/NativeAPI2.c.

--- t/04_spvmcc/lib/SPVM/TestCase/NativeAPI2.c
+++ t/04_spvmcc/lib/SPVM/TestCase/NativeAPI2.c
@@ -11,9 +11,7 @@
 #include "mylib2_source1.h"
 
 int32_t SPVM__TestCase__NativeAPI2__mul(SPVM_ENV* env, SPVM_VALUE* stack) {
-  (void)env;
-  (void)stack;
-
+  
   int32_t total = stack[0].ival * stack[1].ival;
   
   stack[0].ival = total;
@@ -32,8 +30,6 @@ int32_t SPVM__TestCase__NativeAPI2__mul(SPVM_ENV* env, SPVM_VALUE* stack) {
 }
 
 int32_t SPVM__TestCase__NativeAPI2__src_foo(SPVM_ENV* env, SPVM_VALUE* stack) {
-  (void)env;
-  (void)stack;
   
   if (foo() == 3) {
     stack[0].ival = 1;
@@ -46,9 +42,7 @@ int32_t SPVM__TestCase__NativeAPI2__src_foo(SPVM_ENV* env, SPVM_VALUE* stack) {
 }
 
 int32_t SPVM__TestCase__NativeAPI2__src_bar(SPVM_ENV* env, SPVM_VALUE* stack) {
-  (void)env;
-  (void)stack;
-
+  
   if (bar() == 4) {
     stack[0].ival = 1;
   }
@@ -60,9 +54,7 @@ int32_t SPVM__TestCase__NativeAPI2__src_bar(SPVM_ENV* env, SPVM_VALUE* stack) {
 }
 
 int32_t SPVM__TestCase__NativeAPI2__src_baz(SPVM_ENV* env, SPVM_VALUE* stack) {
-  (void)env;
-  (void)stack;
-
+  
   if (baz() == 5) {
     stack[0].ival = 1;
   }
