03abd0a74409410a01cdf1696897a326b4b22b29
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-06-06 07:57:21 +0900

    Add external_sum method.

--- t/04_spvmcc/lib/SPVM/TestCase/ExternalFunction.c
+++ t/04_spvmcc/lib/SPVM/TestCase/ExternalFunction.c
@@ -4,7 +4,7 @@ int32_t external_sum(int32_t x, int32_t y);
 
 static const char* FILE_NAME = "TestCase/ExternalFunction.c";
 
-int32_t SPVM__TestCase__ExternalFunction__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
+int32_t SPVM__TestCase__ExternalFunction__external_sum(SPVM_ENV* env, SPVM_VALUE* stack) {
   
   int32_t x = stack[0].ival;
   
