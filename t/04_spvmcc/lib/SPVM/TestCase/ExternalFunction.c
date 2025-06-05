067b25cbae1230526fbb29d1715c2997452ef374
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-06-06 07:55:56 +0900

    Call external_sum.

--- t/04_spvmcc/lib/SPVM/TestCase/ExternalFunction.c
+++ t/04_spvmcc/lib/SPVM/TestCase/ExternalFunction.c
@@ -1,15 +1,18 @@
-// Copyright (c) 2025 [--user-name]
-// MIT License
-
 #include "spvm_native.h"
 
-
+int32_t external_sum(int32_t x, int32_t y);
 
 static const char* FILE_NAME = "TestCase/ExternalFunction.c";
 
 int32_t SPVM__TestCase__ExternalFunction__foo(SPVM_ENV* env, SPVM_VALUE* stack) {
   
+  int32_t x = stack[0].ival;
+  
+  int32_t y = stack[1].ival;
+  
+  int32_t ret = external_sum(x, y);
+  
+  stack[0].ival = ret;
+  
   return 0;
 }
-
-
