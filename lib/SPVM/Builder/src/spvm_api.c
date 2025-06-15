92d9068e7818c70933dec1b5e2d285a35a503991
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-06-16 08:45:53 +0900

    SPVM_API_is_numeric_type do not use compiler functions.

--- lib/SPVM/Builder/src/spvm_api.c
+++ lib/SPVM/Builder/src/spvm_api.c
@@ -5721,11 +5721,12 @@ int32_t SPVM_API_call_instance_method(SPVM_ENV* env, SPVM_VALUE* stack, const ch
 
 int32_t SPVM_API_is_numeric_type(SPVM_RUNTIME* runtime, SPVM_RUNTIME_BASIC_TYPE* basic_type, int32_t type_dimension, int32_t type_flag) {
   
-  SPVM_COMPILER* compiler = runtime->compiler;
-  
-  int32_t is_numeric_type = SPVM_TYPE_is_numeric_type(compiler, basic_type->id, type_dimension, type_flag);
-  
-  return is_numeric_type;
+  if (basic_type->category == SPVM_NATIVE_C_BASIC_TYPE_CATEGORY_NUMERIC && type_dimension == 0 && !(type_flag & SPVM_NATIVE_C_TYPE_FLAG_REF)) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
 }
 
 int32_t SPVM_API_is_object_type(SPVM_RUNTIME* runtime, SPVM_RUNTIME_BASIC_TYPE* basic_type, int32_t type_dimension, int32_t type_flag) {
