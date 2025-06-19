26a1f85c30db3daea64e8858d022750afdcce2e8
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-06-19 11:09:39 +0900

    Fix struct spvm_runtime_local_vars_base.

--- lib/SPVM/Builder/include/spvm_runtime_local_vars_base.h
+++ lib/SPVM/Builder/include/spvm_runtime_local_vars_base.h
@@ -8,16 +8,16 @@
 
 struct spvm_runtime_local_vars_base {
   void* head;
-  int8_t** byte_vars_base;
-  int16_t** short_vars_base;
-  int32_t** int_vars_base;
-  int64_t** long_vars_base;
-  float** float_vars_base;
-  double** double_vars_base;
-  void*** object_vars_base;
-  void*** ref_vars_base;
-  int32_t** mortal_stack_base;
-  int32_t** mortal_stack_tops_base;
+  void* byte_vars_base;
+  void* short_vars_base;
+  void* int_vars_base;
+  void* long_vars_base;
+  void* float_vars_base;
+  void* double_vars_base;
+  void* object_vars_base;
+  void* ref_vars_base;
+  void* mortal_stack_base;
+  void* mortal_stack_tops_base;
 };
 
 #endif
