b3e9b194ca952ab25b5ee1b2c7a69424d24d9e85
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-09-25 14:39:34 +0900

    Fix module link names.

--- lib/SPVM/Document/Modules.pm
+++ lib/SPVM/Document/Modules.pm
@@ -6,165 +6,165 @@ SPVM::Document::Modules - SPVM Standard Modules
 
 This document lists standard modules in the SPVM language. Standard modules are the modules contained in the distribution of the SPVM language.
 
-=head1 Standard Modules
+=head1 SPVM Standard Modules
 
 =over 2
 
-=item * L<Address|SPVM::Address>
+=item * L<SPVM::Address|SPVM::Address>
 
-=item * L<Array|SPVM::Array>
+=item * L<SPVM::Array|SPVM::Array>
 
-=item * L<Bool|SPVM::Bool>
+=item * L<SPVM::Bool|SPVM::Bool>
 
-=item * L<Byte|SPVM::Byte>
+=item * L<SPVM::Byte|SPVM::Byte>
 
-=item * L<ByteList|SPVM::ByteList>
+=item * L<SPVM::ByteList|SPVM::ByteList>
 
-=item * L<Callback|SPVM::Callback>
+=item * L<SPVM::Callback|SPVM::Callback>
 
-=item * L<Callback::Grep|SPVM::Callback::Grep>
+=item * L<SPVM::Callback::Grep|SPVM::Callback::Grep>
 
-=item * L<Callback::Map|SPVM::Callback::Map>
+=item * L<SPVM::Callback::Map|SPVM::Callback::Map>
 
-=item * L<Callback::MapExpand|SPVM::Callback::MapExpand>
+=item * L<SPVM::Callback::MapExpand|SPVM::Callback::MapExpand>
 
-=item * L<Cloneable|SPVM::Cloneable>
+=item * L<SPVM::Cloneable|SPVM::Cloneable>
 
-=item * L<Cloner|SPVM::Cloner>
+=item * L<SPVM::Cloner|SPVM::Cloner>
 
-=item * L<CommandInfo|SPVM::CommandInfo>
+=item * L<SPVM::CommandInfo|SPVM::CommandInfo>
 
-=item * L<Comparable|SPVM::Comparable>
+=item * L<SPVM::Comparable|SPVM::Comparable>
 
-=item * L<Comparator|SPVM::Comparator>
+=item * L<SPVM::Comparator|SPVM::Comparator>
 
-=item * L<Comparator::Double|SPVM::Comparator::Double>
+=item * L<SPVM::Comparator::Double|SPVM::Comparator::Double>
 
-=item * L<Comparator::Float|SPVM::Comparator::Float>
+=item * L<SPVM::Comparator::Float|SPVM::Comparator::Float>
 
-=item * L<Comparator::Int|SPVM::Comparator::Int>
+=item * L<SPVM::Comparator::Int|SPVM::Comparator::Int>
 
-=item * L<Comparator::Long|SPVM::Comparator::Long>
+=item * L<SPVM::Comparator::Long|SPVM::Comparator::Long>
 
-=item * L<Comparator::String|SPVM::Comparator::String>
+=item * L<SPVM::Comparator::String|SPVM::Comparator::String>
 
-=item * L<Complex_2d|SPVM::Complex_2d>
+=item * L<SPVM::Complex_2d|SPVM::Complex_2d>
 
-=item * L<Complex_2f|SPVM::Complex_2f>
+=item * L<SPVM::Complex_2f|SPVM::Complex_2f>
 
-=item * L<Double|SPVM::Double>
+=item * L<SPVM::Double|SPVM::Double>
 
-=item * L<DoubleList|SPVM::DoubleList>
+=item * L<SPVM::DoubleList|SPVM::DoubleList>
 
-=item * L<EqualityChecker|SPVM::EqualityChecker>
+=item * L<SPVM::EqualityChecker|SPVM::EqualityChecker>
 
-=item * L<EqualityChecker::Address|SPVM::EqualityChecker::Address>
+=item * L<SPVM::EqualityChecker::Address|SPVM::EqualityChecker::Address>
 
-=item * L<EqualityCheckable|SPVM::EqualityCheckable>
+=item * L<SPVM::EqualityCheckable|SPVM::EqualityCheckable>
 
-=item * L<Error|SPVM::Error>
+=item * L<SPVM::Error|SPVM::Error>
 
-=item * L<Error::Compile|SPVM::Error::Compile>
+=item * L<SPVM::Error::Compile|SPVM::Error::Compile>
 
-=item * L<Error::FieldNotSpecified|SPVM::Error::FieldNotSpecified>
+=item * L<SPVM::Error::FieldNotSpecified|SPVM::Error::FieldNotSpecified>
 
-=item * L<Error::MethodCallNotPermitted|SPVM::Error::MethodCallNotPermitted>
+=item * L<SPVM::Error::MethodCallNotPermitted|SPVM::Error::MethodCallNotPermitted>
 
-=item * L<Error::NotSupported|SPVM::Error::NotSupported>
+=item * L<SPVM::Error::NotSupported|SPVM::Error::NotSupported>
 
-=item * L<Error::System|SPVM::Error::System>
+=item * L<SPVM::Error::System|SPVM::Error::System>
 
-=item * L<Error::Unicode::InvalidUTF8|SPVM::Error::Unicode::InvalidUTF8>
+=item * L<SPVM::Error::Unicode::InvalidUTF8|SPVM::Error::Unicode::InvalidUTF8>
 
-=item * L<Float|SPVM::Float>
+=item * L<SPVM::Float|SPVM::Float>
 
-=item * L<FloatList|SPVM::FloatList>
+=item * L<SPVM::FloatList|SPVM::FloatList>
 
-=item * L<Fn|SPVM::Fn>
+=item * L<SPVM::Fn|SPVM::Fn>
 
-=item * L<Format|SPVM::Format>
+=item * L<SPVM::Format|SPVM::Format>
 
-=item * L<Hash|SPVM::Hash>
+=item * L<SPVM::Hash|SPVM::Hash>
 
-=item * L<Hash::Entry|SPVM::Hash::Entry>
+=item * L<SPVM::Hash::Entry|SPVM::Hash::Entry>
 
-=item * L<Immutable::ByteList|SPVM::Immutable::ByteList>
+=item * L<SPVM::Immutable::ByteList|SPVM::Immutable::ByteList>
 
-=item * L<Immutable::DoubleList|SPVM::Immutable::DoubleList>
+=item * L<SPVM::Immutable::DoubleList|SPVM::Immutable::DoubleList>
 
-=item * L<Immutable::FloatList|SPVM::Immutable::FloatList>
+=item * L<SPVM::Immutable::FloatList|SPVM::Immutable::FloatList>
 
-=item * L<Immutable::IntList|SPVM::Immutable::IntList>
+=item * L<SPVM::Immutable::IntList|SPVM::Immutable::IntList>
 
-=item * L<Immutable::LongList|SPVM::Immutable::LongList>
+=item * L<SPVM::Immutable::LongList|SPVM::Immutable::LongList>
 
-=item * L<Immutable::ShortList|SPVM::Immutable::ShortList>
+=item * L<SPVM::Immutable::ShortList|SPVM::Immutable::ShortList>
 
-=item * L<Immutable::StringList|SPVM::Immutable::StringList>
+=item * L<SPVM::Immutable::StringList|SPVM::Immutable::StringList>
 
-=item * L<Int|SPVM::Int>
+=item * L<SPVM::Int|SPVM::Int>
 
-=item * L<IntList|SPVM::IntList>
+=item * L<SPVM::IntList|SPVM::IntList>
 
-=item * L<List|SPVM::List>
+=item * L<SPVM::List|SPVM::List>
 
-=item * L<Long|SPVM::Long>
+=item * L<SPVM::Long|SPVM::Long>
 
-=item * L<LongList|SPVM::LongList>
+=item * L<SPVM::LongList|SPVM::LongList>
 
-=item * L<Native|SPVM::Native>
+=item * L<SPVM::Native|SPVM::Native>
 
-=item * L<Native::Arg|SPVM::Native::Arg>
+=item * L<SPVM::Native::Arg|SPVM::Native::Arg>
 
-=item * L<Native::BasicType|SPVM::Native::BasicType>
+=item * L<SPVM::Native::BasicType|SPVM::Native::BasicType>
 
-=item * L<Native::ClassFile|SPVM::Native::ClassFile>
+=item * L<SPVM::Native::ClassFile|SPVM::Native::ClassFile>
 
-=item * L<Native::ClassVar|SPVM::Native::ClassVar>
+=item * L<SPVM::Native::ClassVar|SPVM::Native::ClassVar>
 
-=item * L<Native::Compiler|SPVM::Native::Compiler>
+=item * L<SPVM::Native::Compiler|SPVM::Native::Compiler>
 
-=item * L<Native::Env|SPVM::Native::Env>
+=item * L<SPVM::Native::Env|SPVM::Native::Env>
 
-=item * L<Native::Field|SPVM::Native::Field>
+=item * L<SPVM::Native::Field|SPVM::Native::Field>
 
-=item * L<Native::Method|SPVM::Native::Method>
+=item * L<SPVM::Native::Method|SPVM::Native::Method>
 
-=item * L<Native::MethodCall|SPVM::Native::MethodCall>
+=item * L<SPVM::Native::MethodCall|SPVM::Native::MethodCall>
 
-=item * L<Native::Runtime|SPVM::Native::Runtime>
+=item * L<SPVM::Native::Runtime|SPVM::Native::Runtime>
 
-=item * L<Native::Stack|SPVM::Native::Stack>
+=item * L<SPVM::Native::Stack|SPVM::Native::Stack>
 
-=item * L<Point|SPVM::Point>
+=item * L<SPVM::Point|SPVM::Point>
 
-=item * L<Point3D|SPVM::Point3D>
+=item * L<SPVM::Point3D|SPVM::Point3D>
 
-=item * L<Scope::Guard|SPVM::Scope::Guard>
+=item * L<SPVM::Scope::Guard|SPVM::Scope::Guard>
 
-=item * L<Short|SPVM::Short>
+=item * L<SPVM::Short|SPVM::Short>
 
-=item * L<ShortList|SPVM::ShortList>
+=item * L<SPVM::ShortList|SPVM::ShortList>
 
-=item * L<Sort|SPVM::Sort>
+=item * L<SPVM::Sort|SPVM::Sort>
 
-=item * L<SPVM|SPVM::SPVM>
+=item * L<SPVM::SPVM|SPVM::SPVM>
 
-=item * L<Stringable|SPVM::Stringable>
+=item * L<SPVM::Stringable|SPVM::Stringable>
 
-=item * L<StringBuffer|SPVM::StringBuffer>
+=item * L<SPVM::StringBuffer|SPVM::StringBuffer>
 
-=item * L<Stringer|SPVM::Stringer>
+=item * L<SPVM::Stringer|SPVM::Stringer>
 
-=item * L<StringList|SPVM::StringList>
+=item * L<SPVM::StringList|SPVM::StringList>
 
-=item * L<Sync::Mutex|SPVM::Sync::Mutex>
+=item * L<SPVM::Sync::Mutex|SPVM::Sync::Mutex>
 
 =back
 
-=head1 CPAN Modules
+=head1 SPVM CPAN Modules
 
-L<CPAN Modules - SPVM Wiki|https://github.com/yuki-kimoto/SPVM/wiki/CPAN-Modules>
+L<SPVM CPAN Modules - SPVM Wiki|https://github.com/yuki-kimoto/SPVM/wiki/CPAN-Modules>
 
 =head1 See Also
 
