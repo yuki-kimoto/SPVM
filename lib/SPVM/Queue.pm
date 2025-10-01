c5b367086a70230e6f480633a305b991a1251b94
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-10-01 11:59:38 +0900

    Add an empty Queue class.

---
+++ lib/SPVM/Queue.pm
@@ -0,0 +1,71 @@
+package SPVM::Queue;
+
+
+
+1;
+
+=encoding utf8
+
+=head1 Name
+
+SPVM::Queue - Short Description
+
+=head1 Description
+
+Queue class in L<SPVM> has methods to do someting.
+
+=head1 Usage
+
+  use Queue;
+
+=head1 Details
+
+
+
+=head1 Super Class
+
+
+
+=head1 Interfaces
+
+
+
+=head1 Enumerations
+
+
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Well Known Child Classes
+
+
+
+=head1 See Also
+
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+Yuki Kimoto C<kimoto.yuki@gmail.com>
+
+=head1 Copyright & License
+
+Copyright (c) 2025 Yuki Kimoto
+
+MIT License
+
