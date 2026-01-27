7b2727452c950ba0e68a180d0e74276a1e3d145a
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-01-27 14:45:03 +0900

    Remove the unused opcodes SPVM_OPCODE_C_ID_GET_DIE_ERROR_ID and SPVM_OPCODE_C_ID_SET_DIE_ERROR_ID from both the header and source files.   #836

--- lib/SPVM/Builder/src/spvm_opcode.c
+++ lib/SPVM/Builder/src/spvm_opcode.c
@@ -303,8 +303,6 @@ const char* const* SPVM_OPCODE_C_ID_NAMES(void) {
     "SAY",
     "WARN",
     "DIE",
-    "GET_DIE_ERROR_ID",
-    "SET_DIE_ERROR_ID",
     "CLEAR_EVAL_ERROR_ID",
     "GET_EVAL_ERROR_ID",
     "GET_CLASS_ID",
