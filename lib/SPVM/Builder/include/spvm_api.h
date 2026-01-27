e609cb815961833143a904f4a59f8a8dc9523ea1
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-01-27 09:49:01 +0900

    Add the SPVM_API_die_v2 function to spvm_api.c, which supports optimized exception handling by storing metadata (function name, file, line) directly onto the stack and allocating the exact memory needed for the exception message.#836

--- lib/SPVM/Builder/include/spvm_api.h
+++ lib/SPVM/Builder/include/spvm_api.h
@@ -305,6 +305,7 @@ SPVM_OBJECT* SPVM_API_get_exception(SPVM_ENV* env, SPVM_VALUE* stack);
 SPVM_OBJECT* SPVM_API_new_stack_trace_no_mortal(SPVM_ENV* env, SPVM_VALUE* stack, SPVM_OBJECT* exception, SPVM_RUNTIME_METHOD* method, int32_t line);
 SPVM_OBJECT* SPVM_API_new_stack_trace(SPVM_ENV* env, SPVM_VALUE* stack, SPVM_OBJECT* exception, SPVM_RUNTIME_METHOD* method, int32_t line);
 int32_t SPVM_API_die(SPVM_ENV* env, SPVM_VALUE* stack, const char* message, ...);
+int32_t SPVM_API_die_v2(SPVM_ENV* env, SPVM_VALUE* stack, const char* message, const char* func_name, const char* file, int32_t line, ...);
 
 // Reference count
 void SPVM_API_inc_ref_count(SPVM_ENV* env, SPVM_VALUE* stack, SPVM_OBJECT* object);
--- lib/SPVM/Builder/src/spvm_api.c
+++ lib/SPVM/Builder/src/spvm_api.c
@@ -7237,3 +7237,33 @@ int32_t SPVM_API_die(SPVM_ENV* env, SPVM_VALUE* stack, const char* message, ...)
   
   return SPVM_NATIVE_C_BASIC_TYPE_ID_ERROR_CLASS;
 }
+
+int32_t SPVM_API_die_v2(SPVM_ENV* env, SPVM_VALUE* stack, const char* message, const char* func_name, const char* file, int32_t line, ...) {
+  
+  va_list args;
+  
+  /* Calculate the required length for the exception message */
+  va_start(args, line);
+  int32_t length = vsnprintf(NULL, 0, message, args);
+  va_end(args);
+
+  /* Create the exception message with the exact length */
+  void* obj_exception = SPVM_API_new_string_no_mortal(env, stack, NULL, length);
+  char* exception = (char*)SPVM_API_get_chars(env, stack, obj_exception);
+
+  /* Write the formatted string to the allocated memory */
+  va_start(args, line);
+  vsnprintf(exception, length + 1, message, args);
+  va_end(args);
+
+  /* Set the exception object */
+  SPVM_API_set_exception(env, stack, obj_exception);
+
+  /* Set exception metadata to the stack indices after setting the exception object */
+  stack[SPVM_API_C_STACK_INDEX_EXCEPTION_METHOD_ABS_NAME].oval = (void*)func_name;
+  stack[SPVM_API_C_STACK_INDEX_EXCEPTION_FILE].oval = (void*)file;
+  stack[SPVM_API_C_STACK_INDEX_EXCEPTION_LINE].ival = line;
+  stack[SPVM_API_C_STACK_INDEX_EXCEPTION_CALL_DEPTH].ival = stack[SPVM_API_C_STACK_INDEX_CALL_DEPTH].ival;
+
+  return SPVM_NATIVE_C_BASIC_TYPE_ID_ERROR_CLASS;
+}
