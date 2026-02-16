d6365a62c69821c8ade38982a3c692a492946b2f
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-02-16 14:20:44 +0900

    Revert "Add -lbcrypt to perllibs on Windows in SPVM::Builder::CC to support BCryptGenRandom and fix a typo."
    This reverts commit 5233daafcbed83f2b77e452ec4ffa5f6838b257a.

--- lib/SPVM/Builder/CC.pm
+++ lib/SPVM/Builder/CC.pm
@@ -710,12 +710,6 @@ sub link {
   
   my $ld = $config->ld;
   
-  my $perllibs = '-lm';
-  if ($^O eq 'MSWin32') {
-    # On Windows, bcrypt is linked for the BCryptGenRandom function.
-    $perllibs .= ' -lbcrypt';
-  }
-  
   my $cbuilder_config = {
     ld => $ld,
     lddlflags => '',
@@ -723,11 +717,11 @@ sub link {
     libpth => '',
     libperl => '',
     
-    # "perllibs" should be empty string, but ExtUtils::CBuilder outputs "INPUT()" into 
+    # "perllibs" should be empty string, but ExtUtils::CBuiler outputs "INPUT()" into 
     # Linker Script File(.lds) when "perllibs" is empty string.
     # This is syntax error in Linker Script File(.lds)
     # For the reason, libm is linked which seems to have no effect.
-    perllibs => $perllibs,
+    perllibs => '-lm',
   };
   
   my $quiet = $self->detect_quiet($config);
