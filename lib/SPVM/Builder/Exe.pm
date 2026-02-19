bb567aa2b6685b96ae141a89c02d4765bc355b98
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-02-19 14:10:23 +0900

    Move copy_spvm_archive_files and its helper to SPVM::Builder::SPVMArchiveLoader.

--- lib/SPVM/Builder/Exe.pm
+++ lib/SPVM/Builder/Exe.pm
@@ -346,7 +346,7 @@ sub new {
     File::Copy::copy($json_file, "$spvm_archive_extract_dir/spvm-archive.json");
     
     # Copy classes and other resources using filtered logic
-    $self->copy_spvm_archive_files($spvm_archive_dir, $spvm_archive_extract_dir, $self->{spvm_archive_info}{classes_h});
+    SPVM::Builder::SPVMArchiveLoader->copy_spvm_archive_files($spvm_archive_dir, $spvm_archive_extract_dir, $self->{spvm_archive_info}{classes_h});
     
     # 5. Setup paths (Common)
     $compiler->add_include_dir("$spvm_archive_extract_dir/SPVM");
@@ -454,14 +454,14 @@ sub build_exe_file {
     }
     
     # Copy build files
-    $self->copy_spvm_archive_files($build_work_dir, $spvm_archive_out, $spvmcc_info->{classes_h});
+    SPVM::Builder::SPVMArchiveLoader->copy_spvm_archive_files($build_work_dir, $spvm_archive_out, $spvmcc_info->{classes_h});
     
     # Copy from existing archive
     my $spvm_archive_info;
     if (defined $spvm_archive) {
       my $spvm_archive_extract_dir = $self->{spvm_archive_extract_dir};
       $spvm_archive_info = $self->{spvm_archive_info};
-      $self->copy_spvm_archive_files($spvm_archive_extract_dir, $spvm_archive_out, $spvm_archive_info->{classes_h});
+      SPVM::Builder::SPVMArchiveLoader->copy_spvm_archive_files($spvm_archive_extract_dir, $spvm_archive_out, $spvm_archive_info->{classes_h});
     }
     
     # Write spvm-archive.json
@@ -1566,98 +1566,6 @@ sub exists_in_spvm_archive_info {
   }
   
   return $exists_in_spvm_archive_info;
-}
-
-sub create_class_name_from_object_path {
-  my ($tar_file) = @_;
-  
-  my $class_name = $tar_file;
-  $class_name =~ s|^object/||;
-  $class_name =~ s/\..+$//;
-  $class_name =~ s/\//::/g;
-  $class_name =~ s/^SPVM:://;
-  
-  return $class_name;
-}
-
-sub copy_spvm_archive_files {
-  my ($self, $src_dir, $dest_dir, $classes_h) = @_;
-  
-  # Normalize the base source directory to use forward slashes for robust matching
-  my $normalized_src_dir = $src_dir;
-  $normalized_src_dir =~ tr|\\|/|;
-  $normalized_src_dir =~ s|/+$||; # Remove trailing slash for consistent substitution
-
-  File::Find::find(
-    {
-      wanted => sub {
-        my $src_path = $File::Find::name;
-        
-        # 1. Normalize the current found path to forward slashes for logic
-        my $normalized_src_path = $src_path;
-        $normalized_src_path =~ tr|\\|/|;
-        
-        # 2. Extract the relative path correctly by using normalized strings
-        my $rel_path = $normalized_src_path;
-        unless ($rel_path =~ s/^\Q$normalized_src_dir\E\///) {
-          # Skip if the path normalization failed to match the base directory
-          return if $rel_path eq $normalized_src_path;
-        }
-
-        # Skip if not a file
-        return unless -f $src_path;
-
-        my $should_copy = 0;
-
-        # 3. Check for files based on specific rules for directories and extensions
-
-        # 3-1. SPVM source files (.spvm) from SPVM directory
-        if ($rel_path =~ m|^SPVM/| && $rel_path =~ /\.spvm$/) {
-          my $class_name = &create_class_name_from_object_path($rel_path);
-          if ($classes_h->{$class_name}) {
-            $should_copy = 1;
-          }
-        }
-        # 3-2. Object files (.o) from object directory or SPVM directory
-        elsif (($rel_path =~ m|^object/| || $rel_path =~ m|^SPVM/|) && $rel_path =~ /\.o$/) {
-          my $class_name = &create_class_name_from_object_path($rel_path);
-          if ($classes_h->{$class_name}) {
-            $should_copy = 1;
-          }
-        }
-        # 3-3. Library files (.a, .lib) from lib directory
-        elsif ($rel_path =~ m|^lib/| && $rel_path =~ /\.(a|lib)$/) {
-          $should_copy = 1;
-        }
-        # 3-4. Header files from include directory (excluding hidden files)
-        elsif ($rel_path =~ m|^include/|) {
-          my $basename = File::Basename::basename($rel_path);
-          unless ($basename =~ /^\./) {
-            $should_copy = 1;
-          }
-        }
-
-        return unless $should_copy;
-        
-        # 4. Build the destination path by joining the base and the relative part
-        # We use File::Spec for cross-platform safety
-        my $dest_path = File::Spec->catfile($dest_dir, split('/', $rel_path));
-        
-        # 5. Ensure the destination directory exists (File::Basename is safe with catfile result)
-        my $dest_parent = File::Basename::dirname($dest_path);
-        unless (-d $dest_parent) {
-          File::Path::mkpath($dest_parent) or Carp::confess "Cannot create directory '$dest_parent': $!";
-        }
-        
-        # 6. Copy and preserve mtime
-        File::Copy::copy($src_path, $dest_path) or Carp::confess "Copy failed from '$src_path' to '$dest_path': $!";
-        my $mtime = (stat $src_path)[9];
-        utime $mtime, $mtime, $dest_path;
-      },
-      no_chdir => 1,
-    },
-    $src_dir
-  );
 }
 
 sub merge_spvmcc_info {
