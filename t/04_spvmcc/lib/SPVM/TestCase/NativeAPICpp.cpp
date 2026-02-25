d393fa1d31786a43334fae625ae280f372eff335
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-02-25 14:54:29 +0900

    Update C++ test to pass value by reference to thread.

--- t/04_spvmcc/lib/SPVM/TestCase/NativeAPICpp.cpp
+++ t/04_spvmcc/lib/SPVM/TestCase/NativeAPICpp.cpp
@@ -6,22 +6,21 @@
 
 extern "C" {
 
-void ThreadFunc(int num) {
-  
+void ThreadFunc(int32_t& num) {
+  num += 1;
 }
 
 int32_t SPVM__TestCase__NativeAPICpp__call_cpp_func(SPVM_ENV* env, SPVM_VALUE* stack) {
-  
+
   int32_t value = stack[0].ival;
-  
+
   value *= 2;
-  
-  std::thread th1(ThreadFunc, 1);
+
+  std::thread th1(ThreadFunc, std::ref(value));
   th1.join();
-  
+
   stack[0].ival = value;
-  
+
   return 0;
 }
-
 }
