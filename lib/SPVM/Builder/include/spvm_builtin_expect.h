1d26d61feee7c1965c99656c496471b97fafc348
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-03-06 13:25:28 +0900

    Fix MSVC C4067 warning by nested evaluation of __has_builtin.

--- lib/SPVM/Builder/include/spvm_builtin_expect.h
+++ lib/SPVM/Builder/include/spvm_builtin_expect.h
@@ -1,6 +1,12 @@
 /*
  * Define __builtin_expect as a no-op if the compiler does not support it.
  */
-#if !defined(__has_builtin) || !__has_builtin(__builtin_expect)
-#  define __builtin_expect(x, expected_value) (x)
+#ifndef __builtin_expect
+#  ifdef __has_builtin
+#    if !__has_builtin(__builtin_expect)
+#      define __builtin_expect(x, expected_value) (x)
+#    endif
+#  else
+#    define __builtin_expect(x, expected_value) (x)
+#  endif
 #endif
