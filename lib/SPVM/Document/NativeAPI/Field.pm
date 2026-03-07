160bae90ea1619d84bcdb3d885c6ddbb44e6679d
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-03-07 09:52:08 +0900

    Update SPVM::Document::NativeAPI::Field

--- lib/SPVM/Document/NativeAPI/Field.pm
+++ lib/SPVM/Document/NativeAPI/Field.pm
@@ -10,9 +10,9 @@ The field native APIs in L<SPVM> are the APIs to get definition information for 
 
   SPVM_API_FIELD* api_field = env->api->field;
   
-  void* basic_type = env->get_basic_type(env, stack, "Foo");
+  SPVM_NATIVE_BASIC_TYPE* basic_type = env->get_basic_type(env, stack, "Foo");
   
-  void* field = env->api->basic_type->get_field_by_name(env->runtime, basic_type, "value");
+  SPVM_NATIVE_FIELD* field = env->api->basic_type->get_field_by_name(env->runtime, basic_type, "value");
   
   const char* field_name = api_field->get_name(env->runtime, field);
 
@@ -20,57 +20,57 @@ The field native APIs in L<SPVM> are the APIs to get definition information for 
 
 =head2 get_name
 
-C<const char* (*get_name)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* field|SPVM::Document::NativeAPI::Field>);>
+C<const char* (*get_name)(L<SPVM_NATIVE_RUNTIME* runtime|SPVM::Document::NativeAPI::Runtime>, L<SPVM_NATIVE_FIELD* field|SPVM::Document::NativeAPI::Field>);>
 
 Returns the name of the field I<field>.
 
 =head2 get_index
 
-C<int32_t (*get_index)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* field|SPVM::Document::NativeAPI::Field>);>
+C<int32_t (*get_index)(L<SPVM_NATIVE_RUNTIME* runtime|SPVM::Document::NativeAPI::Runtime>, L<SPVM_NATIVE_FIELD* field|SPVM::Document::NativeAPI::Field>);>
 
 Returns the index of the field I<field>.
 
 =head2 get_offset
 
-C<int32_t (*get_offset)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* field|SPVM::Document::NativeAPI::Field>);>
+C<int32_t (*get_offset)(L<SPVM_NATIVE_RUNTIME* runtime|SPVM::Document::NativeAPI::Runtime>, L<SPVM_NATIVE_FIELD* field|SPVM::Document::NativeAPI::Field>);>
 
 Returns the byte offset of the field I<field>. The byte offset is the position in bytes where the field is allocated in its belonging class.
 
 =head2 get_basic_type
 
-C<void* (*get_basic_type)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* field|SPVM::Document::NativeAPI::Field>);>
+C<SPVM_NATIVE_BASIC_TYPE* (*get_basic_type)(L<SPVM_NATIVE_RUNTIME* runtime|SPVM::Document::NativeAPI::Runtime>, L<SPVM_NATIVE_FIELD* field|SPVM::Document::NativeAPI::Field>);>
 
 Returns the L<basic type|SPVM::Document::NativeAPI::BasicType> of the field I<field>.
 
 =head2 get_type_dimension
 
-C<int32_t (*get_type_dimension)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* field|SPVM::Document::NativeAPI::Field>);>
+C<int32_t (*get_type_dimension)(L<SPVM_NATIVE_RUNTIME* runtime|SPVM::Document::NativeAPI::Runtime>, L<SPVM_NATIVE_FIELD* field|SPVM::Document::NativeAPI::Field>);>
 
 Returns the type dimension of the field I<field>.
 
 =head2 get_type_flag
 
-C<int32_t (*get_type_flag)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* field|SPVM::Document::NativeAPI::Field>);>
+C<int32_t (*get_type_flag)(L<SPVM_NATIVE_RUNTIME* runtime|SPVM::Document::NativeAPI::Runtime>, L<SPVM_NATIVE_FIELD* field|SPVM::Document::NativeAPI::Field>);>
 
 Returns the L<type flag ID|SPVM::Document::NativeAPI/"Type Flag IDs"> of the field I<field>.
 
 =head2 get_current_basic_type
 
-C<void* (*get_current_basic_type)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* field|SPVM::Document::NativeAPI::Field>);>
+C<SPVM_NATIVE_BASIC_TYPE* (*get_current_basic_type)(L<SPVM_NATIVE_RUNTIME* runtime|SPVM::Document::NativeAPI::Runtime>, L<SPVM_NATIVE_FIELD* field|SPVM::Document::NativeAPI::Field>);>
 
 Returns the L<basic type|SPVM::Document::NativeAPI::BasicType> that owns the field I<field>.
 
 =head2 get_exists_offset
 
-C<int32_t (*get_exists_offset)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* field|SPVM::Document::NativeAPI::Field>);>
+C<int32_t (*get_exists_offset)(L<SPVM_NATIVE_RUNTIME* runtime|SPVM::Document::NativeAPI::Runtime>, L<SPVM_NATIVE_FIELD* field|SPVM::Document::NativeAPI::Field>);>
 
 Returns the byte offset of the exists flag of the field I<field>.
 
 =head2 get_exists_bit
 
-C<int32_t (*get_exists_bit)(L<void* runtime|SPVM::Document::NativeAPI::Runtime>, L<void* field|SPVM::Document::NativeAPI::Field>);>
+C<int32_t (*get_exists_bit)(L<SPVM_NATIVE_RUNTIME* runtime|SPVM::Document::NativeAPI::Runtime>, L<SPVM_NATIVE_FIELD* field|SPVM::Document::NativeAPI::Field>);>
 
-Return the bit shift of the exsits flag of the field I<field>.
+Return the bit shift of the exists flag of the field I<field>.
 
 =head1 Native API IDs
 
