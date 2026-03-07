d951a8ba44aea24ac5bb0663c382ca6ec08b97b0
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-03-07 09:48:32 +0900

    Update SPVM::Document::NativeAPI::ClassFile

--- lib/SPVM/Document/NativeAPI/ClassFile.pm
+++ lib/SPVM/Document/NativeAPI/ClassFile.pm
@@ -10,7 +10,9 @@ The class file native APIs of L<SPVM> are the APIs to get and set class file inf
   
   SPVM_API_CLASS_FILE* api_class_file = env->api->class_file;
   
-  void* class_file = env->api->compiler->get_class_file(compiler, "MyClass");
+  SPVM_NATIVE_COMPILER* compiler = env->api->compiler->new_instance();
+  
+  SPVM_NATIVE_CLASS_FILE* class_file = env->api->compiler->get_class_file(compiler, "MyClass");
   
   const char* class_name = api_class_file->get_class_name(compiler, class_file);
 
@@ -18,67 +20,67 @@ The class file native APIs of L<SPVM> are the APIs to get and set class file inf
 
 =head2 get_class_name
 
-  const char* (*get_class_name)(void* compiler, void* class_file);
+C<const char* (*get_class_name)(L<SPVM_NATIVE_COMPILER* compiler|SPVM::Document::NativeAPI::Compiler>, L<SPVM_NATIVE_CLASS_FILE* class_file|SPVM::Document::NativeAPI::ClassFile>);>
 
 Returns the class name of the class file I<class_file>.
 
 =head2 get_file
 
-  const char* (*get_file)(void* compiler, void* class_file);
+C<const char* (*get_file)(L<SPVM_NATIVE_COMPILER* compiler|SPVM::Document::NativeAPI::Compiler>, L<SPVM_NATIVE_CLASS_FILE* class_file|SPVM::Document::NativeAPI::ClassFile>);>
 
 Returns the file path for display. This is a field of the class file I<class_file>.
 
 =head2 set_file
 
-  void (*set_file)(void* compiler, void* class_file, const char* file);
+C<void (*set_file)(L<SPVM_NATIVE_COMPILER* compiler|SPVM::Document::NativeAPI::Compiler>, L<SPVM_NATIVE_CLASS_FILE* class_file|SPVM::Document::NativeAPI::ClassFile>, const char* file);>
 
 Copies I<file> and sets it to the file path for display. This is a field of the class file I<class_file>.
 
 =head2 get_dir
 
-  const char* (*get_dir)(void* compiler, void* class_file);
+C<const char* (*get_dir)(L<SPVM_NATIVE_COMPILER* compiler|SPVM::Document::NativeAPI::Compiler>, L<SPVM_NATIVE_CLASS_FILE* class_file|SPVM::Document::NativeAPI::ClassFile>);>
 
 Returns the directory where the class is loaded. This is a field of the class file I<class_file>.
 
 =head2 set_dir
 
-  void (*set_dir)(void* compiler, void* class_file, const char* dir);
+C<void (*set_dir)(L<SPVM_NATIVE_COMPILER* compiler|SPVM::Document::NativeAPI::Compiler>, L<SPVM_NATIVE_CLASS_FILE* class_file|SPVM::Document::NativeAPI::ClassFile>, const char* dir);>
 
 Copies the directory path I<dir>, and sets it to the directory where the class is loaded. This is a field of the class file I<class_file>.
 
 =head2 get_rel_file
 
-  const char* (*get_rel_file)(void* compiler, void* class_file);
+C<const char* (*get_rel_file)(L<SPVM_NATIVE_COMPILER* compiler|SPVM::Document::NativeAPI::Compiler>, L<SPVM_NATIVE_CLASS_FILE* class_file|SPVM::Document::NativeAPI::ClassFile>);>
 
 Returns the relative file path of the class file I<class_file>.
 
 =head2 set_rel_file
 
-  void (*set_rel_file)(void* compiler, void* class_file, const char* rel_file);
+C<void (*set_rel_file)(L<SPVM_NATIVE_COMPILER* compiler|SPVM::Document::NativeAPI::Compiler>, L<SPVM_NATIVE_CLASS_FILE* class_file|SPVM::Document::NativeAPI::ClassFile>, const char* rel_file);>
 
 Copies I<rel_file> and sets it to the relative file path of the class file I<class_file>.
 
 =head2 get_content
 
-  const char* (*get_content)(void* compiler, void* class_file);
+C<const char* (*get_content)(L<SPVM_NATIVE_COMPILER* compiler|SPVM::Document::NativeAPI::Compiler>, L<SPVM_NATIVE_CLASS_FILE* class_file|SPVM::Document::NativeAPI::ClassFile>);>
 
 Returns the content of the class file I<class_file>.
 
 =head2 set_content
 
-  void (*set_content)(void* compiler, void* class_file, const char* content);
+C<void (*set_content)(L<SPVM_NATIVE_COMPILER* compiler|SPVM::Document::NativeAPI::Compiler>, L<SPVM_NATIVE_CLASS_FILE* class_file|SPVM::Document::NativeAPI::ClassFile>, const char* content);>
 
 Copies the content I<content> and sets it to the content of the class file I<class_file>.
 
 =head2 get_content_length
 
-  int32_t (*get_content_length)(void* compiler, void* class_file);
+C<int32_t (*get_content_length)(L<SPVM_NATIVE_COMPILER* compiler|SPVM::Document::NativeAPI::Compiler>, L<SPVM_NATIVE_CLASS_FILE* class_file|SPVM::Document::NativeAPI::ClassFile>);>
 
 Returns the length of the content of the class file I<class_file>.
 
 =head2 set_content_length
 
-  void (*set_content_length)(void* compiler, void* class_file, int32_t content_length);
+C<void (*set_content_length)(L<SPVM_NATIVE_COMPILER* compiler|SPVM::Document::NativeAPI::Compiler>, L<SPVM_NATIVE_CLASS_FILE* class_file|SPVM::Document::NativeAPI::ClassFile>, int32_t content_length);>
 
 Sets I<content_length> to the length of the content of the class file I<class_file>.
 
