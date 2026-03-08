55826df85c53436c2ba7df1f175b7a866da3de7a
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-03-09 07:50:04 +0900

    Use SPVM_OBJ for internal native APIs in spvm_native.h and SPVM::Document::NativeAPI::Internal.

--- lib/SPVM/Builder/include/spvm_native.h
+++ lib/SPVM/Builder/include/spvm_native.h
@@ -635,11 +635,11 @@ struct spvm_api_string_buffer {
 };
 
 struct spvm_api_internal {
-  int32_t (*get_ref_count)(SPVM_ENV* env, SPVM_VALUE* stack, void* object);
-  void (*inc_ref_count)(SPVM_ENV* env, SPVM_VALUE* stack, void* object);
-  void (*dec_ref_count)(SPVM_ENV* env, SPVM_VALUE* stack, void* object);
+  int32_t (*get_ref_count)(SPVM_ENV* env, SPVM_VALUE* stack, SPVM_OBJ* object);
+  void (*inc_ref_count)(SPVM_ENV* env, SPVM_VALUE* stack, SPVM_OBJ* object);
+  void (*dec_ref_count)(SPVM_ENV* env, SPVM_VALUE* stack, SPVM_OBJ* object);
   char* (*get_stack_tmp_buffer)(SPVM_ENV* env, SPVM_VALUE* stack);
-  void (*leave_scope_local)(SPVM_ENV* env, SPVM_VALUE* stack, void** object_vars, int32_t* mortal_stack_typed_var_index, int32_t* mortal_stack_top_ptr, int32_t* mortal_stack_tops, int32_t mortal_stack_tops_index);
+  void (*leave_scope_local)(SPVM_ENV* env, SPVM_VALUE* stack, SPVM_OBJ** object_vars, int32_t* mortal_stack_typed_var_index, int32_t* mortal_stack_top_ptr, int32_t* mortal_stack_tops, int32_t mortal_stack_tops_index);
 };
 
 struct spvm_api_type {
--- lib/SPVM/Document/NativeAPI/Internal.pm
+++ lib/SPVM/Document/NativeAPI/Internal.pm
@@ -16,19 +16,19 @@ These APIs are used for the implementation of the SPVM language, so they should 
 
 =head2 get_ref_count
 
-C<int32_t (*get_ref_count)(L<SPVM_ENV* env|SPVM::Document::NativeClass/"Runtime Environment">, L<SPVM_VALUE* stack|SPVM::Document::NativeClass/"Runtime Stack">, void* object);>
+C<int32_t (*get_ref_count)(L<SPVM_ENV* env|SPVM::Document::NativeClass/"Runtime Environment">, L<SPVM_VALUE* stack|SPVM::Document::NativeClass/"Runtime Stack">, L<SPVM_OBJ* object|SPVM::Document::NativeAPI/"SPVM_OBJ">);>
 
 Returns the reference count of the object I<object>.
 
 =head2 inc_ref_count
 
-C<void (*inc_ref_count)(L<SPVM_ENV* env|SPVM::Document::NativeClass/"Runtime Environment">, L<SPVM_VALUE* stack|SPVM::Document::NativeClass/"Runtime Stack">, void* object);>
+C<void (*inc_ref_count)(L<SPVM_ENV* env|SPVM::Document::NativeClass/"Runtime Environment">, L<SPVM_VALUE* stack|SPVM::Document::NativeClass/"Runtime Stack">, L<SPVM_OBJ* object|SPVM::Document::NativeAPI/"SPVM_OBJ">);>
 
 Increments the reference count of the object I<object>.
 
 =head2 dec_ref_count
 
-C<void (*dec_ref_count)(L<SPVM_ENV* env|SPVM::Document::NativeClass/"Runtime Environment">, L<SPVM_VALUE* stack|SPVM::Document::NativeClass/"Runtime Stack">, void* object);>
+C<void (*dec_ref_count)(L<SPVM_ENV* env|SPVM::Document::NativeClass/"Runtime Environment">, L<SPVM_VALUE* stack|SPVM::Document::NativeClass/"Runtime Stack">, L<SPVM_OBJ* object|SPVM::Document::NativeAPI/"SPVM_OBJ">);>
 
 Decrements the reference count of the object I<object>.
 
@@ -44,9 +44,9 @@ Currently, this internal native API is the same as L<get_stack_tmp_buffer|SPVM::
 
 =head2 leave_scope_local
 
-C<void (*leave_scope_local)(SPVM_ENV* env, SPVM_VALUE* stack, void** object_vars, int32_t* mortal_stack_typed_var_index, int32_t* mortal_stack_top_ptr, int32_t* mortal_stack_tops, int32_t mortal_stack_tops_index);>
+C<void (*leave_scope_local)(SPVM_ENV* env, SPVM_VALUE* stack, L<SPVM_OBJ** object_vars|SPVM::Document::NativeAPI/"SPVM_OBJ">, int32_t* mortal_stack_typed_var_index, int32_t* mortal_stack_top_ptr, int32_t* mortal_stack_tops, int32_t mortal_stack_tops_index);>
 
-Performs a leaveing scope operation for local variables given object variables I<object_vars>, the mortal stack I<mortal_stack>, the address of the top of the mortal stack I<mortal_stack_top_ptr>, and the mortal stack tops I<mortal_stack_tops>, and the index of the top of the mortal stack I<mortal_stack_tops_index>.
+Performs a leaving scope operation for local variables given object variables I<object_vars>, the mortal stack I<mortal_stack>, the address of the top of the mortal stack I<mortal_stack_top_ptr>, and the mortal stack tops I<mortal_stack_tops>, and the index of the top of the mortal stack I<mortal_stack_tops_index>.
 
 The value referenced by I<mortal_stack_top_ptr> is updated.
 
