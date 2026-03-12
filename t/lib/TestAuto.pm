296eac2f1a086e8e71ecc65bef20d6448057b7a3
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-03-12 12:53:28 +0900

    Simplify TestAuto.pm by removing explicit directory checks and using File::Temp as a default fallback.

--- t/lib/TestAuto.pm
+++ t/lib/TestAuto.pm
@@ -10,27 +10,17 @@ my $BUILD_DIR;
 sub import {
   my $test_dir;
 
-  my $test_dir_builder_base = '00_builder';
-  my $test_dir_syntax_base = '01_syntax';
   my $test_dir_vm_base = '02_vm';
   my $test_dir_precompile_base = '03_precompile';
   my $test_dir_spvmcc_base = '04_spvmcc';
   my $test_dir_spvmdist_base = '05_spvmdist';
 
-  my $test_dir_builder = "t/$test_dir_builder_base";
-  my $test_dir_syntax = "t/$test_dir_syntax_base";
   my $test_dir_vm = "t/$test_dir_vm_base";
   my $test_dir_precompile = "t/$test_dir_precompile_base";
   my $test_dir_spvmcc = "t/$test_dir_spvmcc_base";
   my $test_dir_spvmdist = "t/$test_dir_spvmdist_base";
 
-  if ($FindBin::Bin =~ /t\/$test_dir_builder_base\b/) {
-    $test_dir = $test_dir_builder;
-  }
-  elsif ($FindBin::Bin =~ /t\/$test_dir_syntax_base\b/) {
-    $test_dir = $test_dir_syntax;
-  }
-  elsif ($FindBin::Bin =~ /t\/$test_dir_vm_base\b/) {
+  if ($FindBin::Bin =~ /t\/$test_dir_vm_base\b/) {
     $test_dir = $test_dir_vm;
   }
   elsif ($FindBin::Bin =~ /t\/$test_dir_precompile_base\b/) {
@@ -44,9 +34,6 @@ sub import {
   }
   elsif ($FindBin::Bin =~ /t\/utils\b/) {
     $test_dir = 't/utils';
-  }
-  else {
-    die "\"$FindBin::Bin\" is an invalid test directory";
   }
   
   unless ($test_dir) {
