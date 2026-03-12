ce42e630e7266185b3442baa369362afd3437f33
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-03-12 12:58:42 +0900

    Simplify TestAuto.pm further by removing spvmcc and utils from explicit directory matching.

--- t/lib/TestAuto.pm
+++ t/lib/TestAuto.pm
@@ -12,23 +12,15 @@ sub import {
 
   my $test_dir_vm_base = '02_vm';
   my $test_dir_precompile_base = '03_precompile';
-  my $test_dir_spvmcc_base = '04_spvmcc';
 
   my $test_dir_vm = "t/$test_dir_vm_base";
   my $test_dir_precompile = "t/$test_dir_precompile_base";
-  my $test_dir_spvmcc = "t/$test_dir_spvmcc_base";
 
   if ($FindBin::Bin =~ /t\/$test_dir_vm_base\b/) {
     $test_dir = $test_dir_vm;
   }
   elsif ($FindBin::Bin =~ /t\/$test_dir_precompile_base\b/) {
     $test_dir = $test_dir_precompile;
-  }
-  elsif ($FindBin::Bin =~ /t\/$test_dir_spvmcc_base\b/) {
-    $test_dir = $test_dir_spvmcc;
-  }
-  elsif ($FindBin::Bin =~ /t\/utils\b/) {
-    $test_dir = 't/utils';
   }
   
   unless ($test_dir) {
