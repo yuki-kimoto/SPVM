6d40f6a0b17f8ac140b845586f04d65c308ea340
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-03-13 11:08:25 +0900

    Revert "Update TestUtil::MyLib to traverse up to 100 levels with root detection and throw an exception if Makefile.PL is not found."
    This reverts commit 82354512fde634ce11b29db55426a11b28a6b71d.

--- t/lib/TestUtil/MyLib.pm
+++ t/lib/TestUtil/MyLib.pm
@@ -4,44 +4,32 @@ use strict;
 use warnings;
 
 use FindBin;
-use Cwd 'abs_path';
 
 sub import {
-  my $current_dir = abs_path($FindBin::Bin);
-  my @test_lib_dirs;
-  my $found_project_root;
+  my $dir = $FindBin::Bin;
+  my $test_lib_dir;
 
-  # (Comment: Traverse up to 100 levels to find lib directories)
-  for (1..100) {
-    # (Comment: If lib directory exists at this level, add it to the list)
-    if (-d "$current_dir/lib") {
-      push @test_lib_dirs, "$current_dir/lib";
-    }
-    
-    # (Comment: Stop if Makefile.PL is found)
-    if (-f "$current_dir/Makefile.PL") {
-      $found_project_root = 1;
+  # (Comment: Traverse up to 10 levels to find the lib directory)
+  for (1..10) {
+    # (Comment: Stop if Makefile.PL is found at this level, as this marks the project root)
+    if (-f "$dir/Makefile.PL") {
       last;
     }
     
-    # (Comment: Get parent directory)
-    my $parent_dir = abs_path("$current_dir/..");
-    
-    # (Comment: Stop if the parent directory is the same as current directory, meaning root is reached)
-    if ($parent_dir eq $current_dir) {
+    # (Comment: Check for lib directory at the current level)
+    if (-d "$dir/lib") {
+      $test_lib_dir = "$dir/lib";
       last;
     }
     
-    $current_dir = $parent_dir;
+    $dir .= "/..";
   }
 
-  # (Comment: Throw an exception if the project root was not found)
-  unless ($found_project_root) {
-    die "The project root (Makefile.PL) could not be found. Reached the system root or 100 levels from $FindBin::Bin.";
+  if ($test_lib_dir) {
+    # (Comment: Add the found lib directory to @INC if it's not already present)
+    unshift @INC, $test_lib_dir unless grep { $_ eq $test_lib_dir } @INC;
   }
-
-  # (Comment: Just unshift everything found)
-  unshift @INC, @test_lib_dirs;
 }
 
 1;
+
