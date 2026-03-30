0020bf1b8dbd41b1e8a9e274c6f3d4b6153accf2
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-03-31 07:48:15 +0900

    Use qq() in the Perl script for spawn_compile to safely handle Windows path delimiters and improve quoting consistency.

--- lib/SPVM/Builder/CC.pm
+++ lib/SPVM/Builder/CC.pm
@@ -624,11 +624,11 @@ sub spawn_compile {
   my $perl_script_for_compile =
     q|my ($log_dir, $cc_cmd_string, @cc_cmd) = @ARGV; | .
     q|my $process_id = $$; | .
-    q|my $log_stdout = "$log_dir/$process_id.stdout"; | .
-    q|my $log_stderr = "$log_dir/$process_id.stderr"; | .
-    q|open(STDOUT, ">", $log_stdout) or warn "Cannot open file '$log_stdout':$!"; | .
-    q|open(STDERR, ">", $log_stderr) or warn "Cannot open file '$log_stderr':$!"; | .
-    q|print "$cc_cmd_string\n"; | . 
+    q|my $log_stdout = qq($log_dir/$process_id.stdout); | .
+    q|my $log_stderr = qq($log_dir/$process_id.stderr); | .
+    q|open(STDOUT, ">", $log_stdout) or warn qq(Cannot open file '$log_stdout':$!); | .
+    q|open(STDERR, ">", $log_stderr) or warn qq(Cannot open file '$log_stderr':$!); | .
+    q|print qq($cc_cmd_string\n); | . 
     q|system(@cc_cmd); | . 
     q|exit($? >> 8);|
   ;
