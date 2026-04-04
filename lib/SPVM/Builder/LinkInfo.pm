97f4aa0873c6497eda358633c2d1380e312b914d
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-04-04 16:32:52 +0900

    Support Windows whitespace in library directories by quoting each path in create_ldflags.

--- lib/SPVM/Builder/LinkInfo.pm
+++ lib/SPVM/Builder/LinkInfo.pm
@@ -114,7 +114,10 @@ sub create_ldflags {
   }
   
   my $lib_dirs = $config->lib_dirs;
-  push @merged_ldflags, map { $config->create_option($config->lib_dir_option_name, $_) } grep { length $_ } @$lib_dirs;
+  push @merged_ldflags, map {
+    $config->create_option($config->lib_dir_option_name, SPVM::Builder::Util::quote_literal_for_command($_))
+  }
+  grep { length $_ } @$lib_dirs;
   
   my $libs = $config->libs;
   my @lib_ldflags;
