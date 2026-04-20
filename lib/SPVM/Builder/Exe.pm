884353c6b77f765e7759b9594d0132ded20c9c58
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-04-20 13:16:17 +0900

    Add spvm_main as an alias for the main function to ensure symbol visibility and avoid collisions.

--- lib/SPVM/Builder/Exe.pm
+++ lib/SPVM/Builder/Exe.pm
@@ -484,6 +484,7 @@ sub create_bootstrap_main_func_source {
   
   $source .= <<"EOS";
 int main(int command_args_length, char **command_args);
+int spvm_main(int command_args_length, char **command_args);
 
 int main(int command_args_length, char **command_args) {
   
@@ -608,6 +609,11 @@ int main(int command_args_length, char **command_args) {
   
   return error_id;
 }
+
+int spvm_main(int command_args_length, char **command_args) {
+  return main(command_args_length, command_args);
+}
+
 EOS
 
   return $source;
