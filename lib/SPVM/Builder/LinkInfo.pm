c89d3cb916f74c412011691853e9dac0c1e6bbd7
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-04-22 09:59:42 +0900

    Remove dynamic_lib_ and exe_ logic from LinkInfo.

--- lib/SPVM/Builder/LinkInfo.pm
+++ lib/SPVM/Builder/LinkInfo.pm
@@ -89,28 +89,10 @@ sub create_ldflags {
   my $ldflags = $config->ldflags;
   push @merged_ldflags, grep { length $_ } @{$config->ldflags};
   
-  my $field_names = $config->get_ld_system_field_names;
+  my $ld_system_field_names = $config->get_ld_system_field_names;
   my $output_type = $config->output_type;
-  for my $field_name (@$field_names) {
-    
-    my $add;
-    if ($field_name =~ /^dynamic_lib_(?:.+_)?ldflags$/) {
-      if ($output_type eq 'dynamic_lib') {
-        $add = 1;
-      }
-    }
-    elsif ($field_name =~ /^exe_(?:.+_)?ldflags$/) {
-      if ($output_type eq 'exe') {
-        $add = 1;
-      }
-    }
-    else {
-      $add = 1;
-    }
-    
-    if ($add) {
-      push @merged_ldflags, grep { length $_ } @{$config->$field_name};
-    }
+  for my $ld_system_field_name (@$ld_system_field_names) {
+    push @merged_ldflags, grep { length $_ } @{$config->$ld_system_field_name};
   }
   
   my $lib_dirs = $config->lib_dirs;
