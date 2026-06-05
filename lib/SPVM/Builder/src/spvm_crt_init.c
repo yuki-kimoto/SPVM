7aa854e8032a75ff8e9c00e7901473f043eda203
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-06-05 13:53:23 +0900

    Add spvm_crt_init.c.

---
+++ lib/SPVM/Builder/src/spvm_crt_init.c
@@ -0,0 +1,10 @@
+/* Suppress MSVC GUI pop-ups on calling assert function by redirecting to stderr. */
+/* Uses CRT$XIU section hook to execute this setup before main/DllMain. */
+#if defined(_MSC_VER)
+  static void spvm_crt_init(void) {
+    _set_error_mode(_OUT_TO_STDERR);
+  }
+  
+  #pragma section(".CRT$XIU", read)
+  __declspec(allocate(".CRT$XIU")) void (*spvm_crt_init_ptr)(void) = spvm_crt_init;
+#endif
