4462f908e7285fd7968701992abf41798740a2c3
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-08-17 10:09:37 +0900

    Pass caller info in exchange api instance method.

--- lib/SPVM/BlessedObject/Class.pm
+++ lib/SPVM/BlessedObject/Class.pm
@@ -29,7 +29,11 @@ sub AUTOLOAD {
   # For an instance method call
   $method_name =~ s/^BlessedObject::Class:://;
   
-  my $ret = $self->__api->call_method($self, $method_name, @_);
+  my ($caller_class_name, $caller_file, $caller_line) = caller;
+  my $caller_method_abs_name = "$caller_class_name#(Unknown)";
+  my $caller_info = SPVM::Builder::CallerInfo->new(method_abs_name => $caller_method_abs_name, file => $caller_file, line => $caller_line);
+  
+  my $ret = $self->__api->call_method($self, $method_name, @_, $caller_info);
   
   return $ret;
 }
