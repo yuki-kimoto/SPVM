1baca7931f8f0e984eb76510e7e84684aa812b5d
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-08-20 15:00:29 +0900

    Revert d00ead2863a98409fd966e266c72a14f5667e6bc.

--- lib/SPVM/Builder/CC.pm
+++ lib/SPVM/Builder/CC.pm
@@ -391,7 +391,11 @@ sub prepare_compile_class_common {
 sub resolve_dl_func_list {
   my ($self, $link_info) = @_;
   
-  my $category = $link_info->config->category;
+  my $config = $link_info->config;
+  
+  my $class_name = $config->class_name;
+  
+  my $category = $config->category;
   
   if ($category eq 'native' || $category eq 'precompile') {
     my $runtime = $self->runtime;
@@ -399,38 +403,24 @@ sub resolve_dl_func_list {
       confess("'runtime' field must be defined.");
     }
     
-    my $dl_func_list = [];
-    my %seen_class_name;
+    my $basic_type = $runtime->get_basic_type_by_name($class_name);
     
-    my @class_names;
-    push @class_names, $link_info->config->class_name;
-    for my $compile_info (@{$link_info->compile_infos}) {
-      push @class_names, $compile_info->config->class_name;
+    # Get normal methods
+    my $method_names = $basic_type->get_method_names_by_category($category);
+    
+    # Create the dynamic link function list for the class
+    my $dl_func_list = SPVM::Builder::Util::create_dl_func_list($class_name, $method_names, {category => $category});
+    
+    # Get anon methods from anon basic types
+    my $anon_basic_type_names = $basic_type->get_anon_basic_type_names;
+    for my $anon_basic_type_name (@$anon_basic_type_names) {
+      my $anon_basic_type = $runtime->get_basic_type_by_name($anon_basic_type_name);
+      my $anon_method_names = $anon_basic_type->get_method_names_by_category($category);
+      
+      # Create the dynamic link function list for each anon class and merge it
+      my $anon_dl_func_list = SPVM::Builder::Util::create_dl_func_list($anon_basic_type_name, $anon_method_names, {category => $category});
+      push @$dl_func_list, @$anon_dl_func_list;
     }
-    
-    for my $class_name (@class_names) {
-      next if $seen_class_name{$class_name}++;
-      
-      my $basic_type = $runtime->get_basic_type_by_name($class_name);
-      unless ($basic_type) {
-        next;
-      }
-      
-      my $method_names = $basic_type->get_method_names_by_category($category);
-      
-      my $class_dl_func_list = SPVM::Builder::Util::create_dl_func_list($class_name, $method_names, {category => $category});
-      push @$dl_func_list, @$class_dl_func_list;
-      
-      my $anon_basic_type_names = $basic_type->get_anon_basic_type_names;
-      for my $anon_basic_type_name (@$anon_basic_type_names) {
-        my $anon_basic_type = $runtime->get_basic_type_by_name($anon_basic_type_name);
-        my $anon_method_names = $anon_basic_type->get_method_names_by_category($category);
-        
-        my $anon_dl_func_list = SPVM::Builder::Util::create_dl_func_list($anon_basic_type_name, $anon_method_names, {category => $category});
-        push @$dl_func_list, @$anon_dl_func_list;
-      }
-    }
-    
     $link_info->dl_func_list($dl_func_list);
   }
 }
