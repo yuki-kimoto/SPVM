e5506bff9e7c573a714dd02e65814bdb44919e08
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-08-31 13:45:36 +0900

    Fix die in doc.

--- lib/SPVM/Document/NativeClass.pm
+++ lib/SPVM/Document/NativeClass.pm
@@ -421,7 +421,7 @@ A message can be set to L<exception variable|SPVM::Document::Language::Exception
 
 L<die|SPVM::Document::NativeAPI/"die"> native API can be used to throw an exception easily.
 
-  return env->die_v2("The value must be %d.", 3, __func__, FILE_NAME, __LINE__);
+  return env->die("The value must be %d.", __func__, FILE_NAME, __LINE__, 3);
 
 =head2 Pointer Class
 
